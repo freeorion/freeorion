c16a7b7d9c537fa71f733a37e5442fb37d649fb4
Joseph Bissonnette <oceanmachine@dbd5520b-6a0a-0410-a553-f54834df5b05>
2003-05-29 05:25:10 +0000

    Consolidated SitRep files.
    svn path=/trunk/FreeOrion/; revision=56

--- Empire/SitRepEntry.cpp
+++ Empire/SitRepEntry.cpp
@@ -1,6 +1,15 @@
 #include "SitRepEntry.h"
 
 
+#define TECH_SIT_REP_IMAGE  1  // to be replaced by global define..
+#define BUILD_SIT_REP_IMAGE  2  // to be replaced by global define.
+#define COLONIZE_SIT_REP_IMAGE  3  // to be replaced by global define..
+#define COMBAT_SIT_REP_IMAGE  4  // to be replaced by global define..
+#define FLEET_SIT_REP_IMAGE  5  // to be replaced by global define..
+
+
+
+
 SitRepEntry::SitRepEntry()
 {
 }
@@ -9,3 +18,185 @@ SitRepEntry::~SitRepEntry()
 {
 }
 
+
+
+
+
+
+BuildSitRepEntry::BuildSitRepEntry(BuildSitRepEntryType buildType, int planetID, int fleetID)
+{
+  m_planet_id = planetID;
+  m_fleet_id = fleetID;
+  m_build_type = buildType;
+  printf("BuildSitRepEntry created!\n");
+}
+
+
+int BuildSitRepEntry::ImageID()
+{
+  return BUILD_SIT_REP_IMAGE;
+}
+
+const std::string& BuildSitRepEntry::SummaryText()
+{
+  return m_summary_text;
+}
+
+bool BuildSitRepEntry::ExecuteLink()
+{
+  return true;
+}
+
+
+
+
+
+ColonizeSitRepEntry::ColonizeSitRepEntry(int planetID, int fleetID, bool colo_success, int failEmpireID)
+{
+  m_planet_id = planetID;
+  m_fleet_id = fleetID;
+  m_success = colo_success;
+  m_fail_empire_id = failEmpireID;
+  printf("ColonizeSitRepEntry created!\n");
+}
+
+
+int ColonizeSitRepEntry::ImageID()
+{
+  return COLONIZE_SIT_REP_IMAGE;
+}
+
+int ColonizeSitRepEntry::PlanetID()
+{
+  return m_planet_id;
+}
+
+int ColonizeSitRepEntry::FleetID()
+{
+  return m_fleet_id;
+}
+
+bool ColonizeSitRepEntry::ColonizeSuccess()
+{
+  return m_success;
+}
+
+int ColonizeSitRepEntry::FailEmpireID()
+{
+  return m_fail_empire_id;
+}
+
+const std::string& ColonizeSitRepEntry::SummaryText()
+{
+  return m_summary_text;
+}
+
+bool ColonizeSitRepEntry::ExecuteLink()
+{
+  return true;
+}
+
+
+
+
+CombatSitRepEntry::CombatSitRepEntry(int systemID, int initialOwner, CombatFleetMap* fleetDataMap)
+{
+  m_system_id = systemID;
+  m_initial_owner = initialOwner;
+  printf("CombatSitRepEntry created!\n");
+}
+
+
+int CombatSitRepEntry::ImageID()
+{
+  return COMBAT_SIT_REP_IMAGE;
+}
+
+int CombatSitRepEntry::SystemID()
+{
+  return m_system_id;
+}
+
+int CombatSitRepEntry::InitialOwnerID()
+{
+  return m_initial_owner;
+}
+
+const std::string& CombatSitRepEntry::SummaryText()
+{
+  return m_summary_text;
+}
+
+const CombatFleetMap* CombatSitRepEntry::FleetDataMapPtr()
+{
+  return &m_combat_fleet_map;
+}
+
+bool CombatSitRepEntry::ExecuteLink()
+{
+  return true;
+}
+
+
+
+FleetArrivalSitRepEntry::FleetArrivalSitRepEntry(int systemID, int fleetID, bool newExplore)
+{
+  m_system_id = systemID;
+  m_fleet_id = fleetID;
+  m_new_explore = newExplore;
+  printf("FleetArrivalSitRepEntry created!\n");
+}
+
+
+int FleetArrivalSitRepEntry::ImageID()
+{
+  return FLEET_SIT_REP_IMAGE;
+}
+
+int FleetArrivalSitRepEntry::SystemID()
+{
+  return m_system_id;
+}
+
+int FleetArrivalSitRepEntry::FleetID()
+{
+  return m_fleet_id;
+}
+
+bool FleetArrivalSitRepEntry::NewExplore()
+{
+  return m_new_explore;
+}
+
+const std::string& FleetArrivalSitRepEntry::SummaryText()
+{
+  return m_summary_text;
+}
+
+bool FleetArrivalSitRepEntry::ExecuteLink()
+{
+  return true;
+}
+
+
+
+TechSitRepEntry::TechSitRepEntry()
+{
+  printf("TechSitRepEntry created!\n");
+}
+
+
+int TechSitRepEntry::ImageID()
+{
+  return TECH_SIT_REP_IMAGE;
+}
+
+const std::string& TechSitRepEntry::SummaryText()
+{
+  return m_summary_text;
+}
+
+bool TechSitRepEntry::ExecuteLink()
+{
+  return true;
+}
--- Empire/SitRepEntry.h
+++ Empire/SitRepEntry.h
@@ -3,6 +3,8 @@
 
 
 #include <string>
+#include <vector>
+#include <map>
 
 
 /** Base class for SitRepEntries. Serves as base for ColonizeSitRepEntry, FleetArrivalSitRepEntry, CombatSitRepEntry, NewTechSitRepEntry, BuildSitRepEntry.
@@ -27,5 +29,147 @@ class SitRepEntry
 
 	std::string    m_summary_text;
 };
+
+
+
+
+
+enum BuildSitRepEntryType {
+  MAX_INDUSTRY_HIT,
+  MAX_TECH_HIT,
+  SHIP_BUILT
+};
+
+/** class for SitRepEntries for build events */
+class BuildSitRepEntry : public SitRepEntry
+{
+ public:
+    /** \name Structors */ //@{
+    BuildSitRepEntry(BuildSitRepEntryType buildType, int planetID, int fleetID);
+	~BuildSitRepEntry() {};
+	//@}
+
+    const std::string&  SummaryText();  ///< returns the string to display in the SitRep
+    int                 ImageID();      ///< returns the ID of the image to display with this entry
+
+ protected:
+    bool    ExecuteLink();  ///< causes the entry to trigger the appropriate UI display for this event, returns true on success
+
+    BuildSitRepEntryType   m_build_type;
+	int                    m_planet_id;
+    int                    m_fleet_id;
+  
+};
+
+
+
+
+/** class for SitRepEntries for colonization attempts */
+class ColonizeSitRepEntry : public SitRepEntry
+{
+ public:
+    /** \name Structors */ //@{
+    ColonizeSitRepEntry(int planetID, int fleetID, bool colo_success, int failEmpireID);
+	~ColonizeSitRepEntry() {};
+	//@}
+
+
+    int                 ImageID();         ///< returns the ID of the image to display with this entry
+	int                 PlanetID();        ///< returns ID of planet where colonization was attempted.  Provides accessibility to AI.
+	int                 FleetID();         ///< returns ID of the fleet.  Provides accessibility to AI.
+	bool                ColonizeSuccess(); ///< returns whether or not the colonization was successful. Provides accessibility to AI.
+	int                 FailEmpireID();    ///< in the case that the attempt fails, returns the ID of the empire that was successful in colonizing the planet (currently the only possible cause for colonization failure is that a planet was already colonized).  Provides accessibility to AI.
+
+ protected:
+    const std::string&  SummaryText();     ///< returns the string to display in the SitRep
+    bool           ExecuteLink();  ///< causes the entry to trigger the appropriate UI display for this event, returns true on success.
+  
+	int            m_planet_id;
+	int            m_fleet_id;
+	bool           m_success;
+	int            m_fail_empire_id;
+};
+
+
+struct CombatShipSummary {
+  int     ShipTypeID;
+  int     InitialShipCount;
+  int     FinalShipCount;
+};
+
+typedef std::vector<CombatShipSummary> CombatShipList;
+typedef std::map<int, CombatShipList> CombatFleetMap;  ///< maps empire ID's to CombatShipLists
+
+
+
+/** class for SitRepEntries for battles */
+class CombatSitRepEntry : public SitRepEntry
+{
+ public:
+    /** \name Structors */ //@{
+    CombatSitRepEntry(int systemID, int initialOwner, CombatFleetMap* fleetDataMap);
+	virtual ~CombatSitRepEntry() {};
+	//@}
+
+    int                    ImageID();         ///< returns the ID of the image to display with this entry
+	int                    SystemID();        ///< returns ID of system where combat occurred.  Provides accessibility to AI.
+	int                    InitialOwnerID();  ///< returns the ID of the empire that controlled the system prior to combat.  Provides accessbility to AI.
+	const CombatFleetMap*  FleetDataMapPtr(); ///< returns a pointer to the combat results struct.  Provides accessbility to AI.
+  
+ protected:
+    const std::string&     SummaryText();     ///< returns the string to display in the SitRep
+    bool           ExecuteLink();  ///< causes the entry to trigger the appropriate UI display for this event, returns true on success.
+
+	int            m_system_id;
+	int            m_initial_owner;
+	CombatFleetMap m_combat_fleet_map;
+
+};
+
+
+
+/** class for SitRepEntries for fleet arrivals */
+class FleetArrivalSitRepEntry : public SitRepEntry
+{
+ public:
+    /** \name Structors */ //@{
+    FleetArrivalSitRepEntry(int systemID, int fleetID, bool newExplore);
+	~FleetArrivalSitRepEntry() {};
+	//@}
+
+
+    int     ImageID();         ///< returns the ID of the image to display with this entry
+	int     SystemID();        ///< returns ID of system arrived at.  Provides accessibility to AI.
+	int     FleetID();         ///< returns the ID of the fleet.  Provides accessibility to AI.
+	bool    NewExplore();      ///< returns whether or not the system was explored for the first time.  Provides accessibility to AI.
+    const std::string&  SummaryText();  ///< returns the string to display in the SitRep
+
+ protected:
+    bool    ExecuteLink();     ///< causes the entry to trigger the appropriate UI display for this  event, returns true on success.
+	int            m_system_id;
+	int            m_fleet_id;
+	bool           m_new_explore;
+
+};
+
+
+
+/** class for SitRepEntries for new tech advances */
+class TechSitRepEntry : public SitRepEntry
+{
+ public:
+    /** \name Structors */ //@{
+    TechSitRepEntry();
+	~TechSitRepEntry() {};
+	//@}
+
+    int                 ImageID();      ///< returns the ID of the image to display with this entry
+    const std::string&  SummaryText();  ///< returns the string to display in the SitRep
+
+ protected:
+    bool                ExecuteLink();     ///< causes the entry to trigger the appropriate UI display for this event, returns true on success.    
+  
+};
+
 
 #endif // _SitRepEntry_h_
