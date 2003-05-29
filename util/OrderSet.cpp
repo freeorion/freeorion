d168e294a8ff2ccdc26152d30dbf5e22d9aecdc7
Zach Laine <whatwasthataddress@gmail.com>
2003-05-29 03:35:55 +0000

    Fleshed out the OrderSet class, including writing the templated search member function.
    svn path=/trunk/FreeOrion/; revision=44

--- util/OrderSet.cpp
+++ util/OrderSet.cpp
@@ -1,41 +1,47 @@
 #include "OrderSet.h"
 
-OrderSet::OrderSet() : 
-   m_lowest_free_index(0)
+OrderSet::OrderSet()
 {
+}
+
+OrderSet::~OrderSet()
+{
+   Reset();
 }
 
 const Order* OrderSet::ExamineOrder(int order) const
 {
    const Order* retval = 0;
+   OrderMap::const_iterator it = m_orders.find(order);
+   if (it != m_orders.end()) {
+      retval = it->second;
+   }
    return retval;
 }
    
-template <class Pred> const Order* OrderSet::FindOrder(Pred p) const
+int OrderSet::IssueOrder(Order* order)
 {
-   const Order* retval = 0;
-   return retval;
-}
-
-template <class Pred> std::set<const Order*> OrderSet::FindOrders(Pred p) const
-{
-   std::set<const Order*> retval;
-   return retval;
-}
-
-int OrderSet::IssueOrder(const Order& order)
-{
-   int retval = 0;
+   int retval = ((m_orders.rbegin() != m_orders.rend()) ? m_orders.rbegin()->first + 1 : 0);
+   m_orders[retval] = order;
    return retval;
 }
 
 bool OrderSet::RecindOrder(int order)
 {
    bool retval = false;
+   OrderMap::iterator it = m_orders.find(order);
+   if (it != m_orders.end()) {
+      m_orders.erase(it);
+      retval = true;
+   }
    return retval;
 }
 
 void OrderSet::Reset()
 {
+   for (OrderMap::iterator it = m_orders.begin(); it != m_orders.end(); ++it) {
+      delete it->second;
+   }
+   m_orders.clear();
 }
 
--- util/OrderSet.h
+++ util/OrderSet.h
@@ -1,47 +1,61 @@
 #ifndef _OrderSet_h_
 #define _OrderSet_h_
 
-/*#ifndef	_Order_h_
+#ifndef	_Order_h_
 #include "Order.h"
-#endif*/
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-// ONLY TEMPORARY!!!!!
-class Order {};
-// ONLY TEMPORARY!!!!!
-////////////////////////////////////////////////////////////////////////////////////////////////////
+#endif
 
 #include <map>
-#include <set>
+#include <vector>
 
-/** a collection of orders that may be searched using arbitrary predicate functions */
-class OrderSet : private std::map<int, Order>
+/** a collection of orders that may be searched using arbitrary predicate functions and functors*/
+class OrderSet
 {
+private:
+   typedef std::map<int, Order*> OrderMap;
+
 public:
-   typedef std::map<int, Order>::const_iterator iterator; ///< defines a public const iterator
+   typedef OrderMap::const_iterator    const_iterator;   ///< defines a public const_iterator type for OrderSet
+   typedef std::vector<const Order*>   OrderVec;         ///< the return type of FindOrders()
 
    /** \name Structors */ //@{
    OrderSet();
+   ~OrderSet();
    //@}
    
    /** \name Accessors */ //@{
    const Order*   ExamineOrder(int order) const;   ///< returns a pointer to any order, so that it can be examined through its accessors; returns 0 if no such order exists
    
-   template <class Pred> const Order*            FindOrder(Pred p) const;  ///< returns the first order that matches predicate p, or 0 if none matches
-   template <class Pred> std::set<const Order*>  FindOrders(Pred p) const; ///< returns a set of the orders that match p
+   /** returns all the orders that match \a pred.  Predicates used with this function must take a single const Order* 
+      parameter and must return bool or a type for which there is a conversion to bool.*/
+   template <class Pred>
+   OrderVec FindOrders(Pred pred) const
+   {
+      OrderVec retval;
+      for (OrderMap::const_iterator it = m_orders.begin(); it != m_orders.end(); ++it) {
+         const Order* o = it->second;
+         if (pred(o))
+            retval.push_back(o);
+      }
+      return retval;
+   }
 
-   iterator begin() const {return std::map<int, Order>::begin();} ///< returns the begin (const) iterator for the OrderSet
-   iterator end() const {return std::map<int, Order>::end();}     ///< returns the end (const) iterator for the OrderSet
+   const_iterator begin() const {return m_orders.begin();} ///< returns the begin const_iterator for the OrderSet
+   const_iterator end() const {return m_orders.end();}     ///< returns the end const_iterator for the OrderSet
    //@}
 
-   /** \name Mutators */ //@{   
-   int            IssueOrder(const Order& order);  ///< stores an order in the OrderSet; returns an index that can be used to reference the order
-   bool           RecindOrder(int order);          ///< removes the order from the OrderSet; returns true on success, false if there was no such order or the order is non-recindable
+   /** \name Mutators */ //@{
+   /** stores an order in the OrderSet; returns an index that can be used to reference the order.  \warning The 
+      OrderSet assumes that the Order is allocated on the heap, and takes ownership of it.  Do not delete any 
+      Order* passed to OrderSet.*/
+   int            IssueOrder(Order* order);
+   
+   bool           RecindOrder(int order);    ///< removes the order from the OrderSet; returns true on success, false if there was no such order or the order is non-recindable
    void           Reset(); ///< clears all orders; should be called at the beginning of a new turn
    //@}
 
 private:
-   int   m_lowest_free_index; ///< the lowest unused nonnegative index number
+   OrderMap m_orders;
 };
 
 #endif // _OrderSet_h_
