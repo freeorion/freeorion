49a33e43a91f083baa71f8aa20e89d50746c9b7e
Zach Laine <whatwasthataddress@gmail.com>
2003-08-27 02:18:25 +0000

    Changed some includes to forward declarations, changed some controls to FO style controls, reformatted.
    svn path=/trunk/FreeOrion/; revision=303

--- UI/IntroScreen.h
+++ UI/IntroScreen.h
@@ -1,3 +1,4 @@
+// -*- C++ -*-
 //IntroScreen.h
 
 #ifndef _IntroScreen_h_
@@ -7,84 +8,53 @@
 #include "GGApp.h"
 #endif
 
-#ifndef _GGWnd_h_
-#include "GGWnd.h"
-#endif
-
-#ifndef _GGTexture_h_
-#include "GG::Texture.h"
-#endif
-
-#ifndef _GGButton_h_
-#include "GGButton.h"
-#endif
-
 #ifndef _CUI_Wnd_h_
 #include "CUI_Wnd.h"
 #endif
 
+class CUIButton;
+namespace GG {class Texture;}
+
 //! This is the first screen the user sees in FreeOrion.  It will always be the
 //! size of the Application main window.  It will display a splash screen with 
 //! a menu window on one side.
-
 class IntroScreen : public CUI_Wnd
 {
 public:
-//! \name Structors
-//!@{
-
+    /** \name Structors*/ //!@{
     IntroScreen();                              //!< default ctor
     IntroScreen(const GG::XMLElement &elem);    //!< construction from an XML element
     ~IntroScreen();                             //!< default dtor
+    //!@}
 
-//!@}
+    /** \name Accessors*/ //!@{
+    //!@}
 
-//! \name Accessors
-//!@{
-
-//!@}
-
-//! \name Mutators
-//!@{
-
-//    virtual int Render();    //!< rendering code
-
+    /** \name Mutators*/ //!@{
     void OnSinglePlayer();  //!< called when m_single_player is clicked
     void OnMultiPlayer();   //!< called when m_multi_player is clicked
     void OnOptions();    //!< called when m_options is clicked
     void OnAbout();    //!< called when m_about is clicked
     void OnExitGame();    //!< called when m_exit_game is clicked
     
-    inline virtual void Close() {OnExitGame();}    //!< override to exit the game 
+    virtual void Close() {OnExitGame();}    //!< override to exit the game 
+    //!@}
 
-//!@}
-
-//! \name Overrides
-//!@{
-    
     virtual GG::XMLElement XMLEncode() const;    //!< load from XML element
 
-//!@}
-
 private:
-//! \name Data Members
-//!@{
-
+    /** \name Data Members*/ //!@{
     GG::Texture* m_background;    //!< the background texture
+    //!@}
 
-//!@}
+    /** \name GG Controls*/ //!@{
+    CUIButton* m_single_player;    //!< opens up the single player game dialog
+    CUIButton* m_multi_player;    //!< opens up the multi player game dialog
+    CUIButton* m_options;	//!< opens the options dialog
+    CUIButton* m_about;	//!< opens a dialog to choose to see credits or license
+    CUIButton* m_exit_game;    //!< button that exits the program
+    //!@}
 
-//! \name GG Controls
-//!@{
-
-    GG::Button* m_single_player;    //!< opens up the single player game dialog
-    GG::Button* m_multi_player;    //!< opens up the multi player game dialog
-    GG::Button* m_options;	//!< opens the options dialog
-    GG::Button* m_about;	//!< opens a dialog to choose to see credits or license
-    GG::Button* m_exit_game;    //!< button that exits the program
-
-//!@}
-
-};//IntroScreen
+};
 
 #endif
