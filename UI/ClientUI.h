5e3be0f6c8376dadb2321a292e9c684e68768036
Zach Laine <whatwasthataddress@gmail.com>
2003-09-10 22:21:28 +0000

    Simplified the screen-switching code and removed some demo code; also changed as many #includes to forward declarations as possible.
    svn path=/trunk/FreeOrion/; revision=327

--- UI/ClientUI.h
+++ UI/ClientUI.h
@@ -3,36 +3,35 @@
 #ifndef _ClientUI_h_
 #define _ClientUI_h_
 
-// include relevant controls
-#ifndef _ToolContainer_h_
-#include "ToolContainer.h"
+#ifndef _SitRepEntry_h_
+#include "../util/SitRepEntry.h"
 #endif
 
 #ifndef _StringTable_h_
 #include "StringTable.h"
 #endif
 
-#ifndef _GalaxyMapScreen_h_
-#include "GalaxyMapScreen.h"
-#endif
-
-// include log4cpp stuff
-#include <log4cpp/Appender.hh>
-#include <log4cpp/Category.hh>
-#include <log4cpp/PatternLayout.hh>
-#include <log4cpp/FileAppender.hh>
-
-// FreeOrion includes
-#ifndef _SitRepEntry_h_
-#include "../util/SitRepEntry.h"
+#ifndef _GGWnd_h_
+#include "GGWnd.h"
 #endif
 
 class ClientNetworkCore;
 class ClientUniverse;
 class ClientEmpire;
-class Tech;
 class Combat;
-class GalaxyMapScreen;
+class MapWnd;
+class IntroScreen;
+class Tech;
+class ToolContainer;
+class ToolWnd;
+namespace GG {
+class Clr;
+class SubTexture;
+class XMLElement;
+}
+namespace log4cpp {
+class Category;
+}
 
 //! \brief ClientUI Main Module
 //!This is the main class of the ClientUI module.
@@ -44,7 +43,7 @@ class ClientUI
 public:
     //!Internal States
     //!These determine what screen is currently being displayed
-    enum        
+    enum State
     {
         STATE_STARTUP     = 0,
         STATE_INTRO       = 1,
@@ -86,18 +85,20 @@ public:
     
     //! @param parent A pointer to the Wnd that should contain the tooltip
     //! @param tool A pointer to a ToolWnd to be associated with that window
-    bool AttachToolWnd(GG::Wnd* parent, ToolWnd* tool) {if(m_tooltips) return m_tooltips->AttachToolWnd(parent, tool);};    //!< Adds a ToolWnd to the given window
+    bool AttachToolWnd(GG::Wnd* parent, ToolWnd* tool);    //!< Adds a ToolWnd to the given window
 
     // GameCore Interface functions
     // calling these changes internal state to display the proper screen
     // and initializes and displays the screen
     // see Interface Doc for details
     //!@{
+    void InitTurn();      //!< resets all active controls to use the latest data when it has been changed at the beginning of a new turn
+    
     void ScreenIntro();                        //!< Intro Screen
     void ScreenSettings(const ClientNetworkCore& net);    //!< Game/Network Options Screen
     void ScreenEmpireSelect();                    //!< Empire Selection Screen
     void ScreenTurnStart();                    //!< Turn Start Splash Screen
-    
+
     void ScreenMap();     //!< Universe Map Screen
 
     //! @param events vector containing all the events to be listed
@@ -141,7 +142,7 @@ public:
     bool ZoomToEncyclopediaEntry(const std::string& str);    //!< Opens the encyclodedia screen and presents the entry for the given item
     //!@}
     
-    static ClientUI*    GetClientUI() {return the_UI;}      //!< returns a pointer to the singleton ClientUI class
+    static ClientUI*    GetClientUI() {return s_the_UI;}   //!< returns a pointer to the singleton ClientUI class
 
     //! @param message The message to display
     static void MessageBox(const std::string& message);    //!< shows a message dialog box with the given message     
@@ -186,24 +187,31 @@ public:
 
     static GG::Clr     MULTIEDIT_INT_COLOR;
 
+    static GG::Clr     STAT_INCR_COLOR; //!< used to color increasing stats text (eg "+2")
+    static GG::Clr     STAT_DECR_COLOR; //!< used to color decreasing stats text (eg "-3")
+
     // game UI windows
     static GG::Clr     SIDE_PANEL_COLOR;
+    static GG::Clr     SIDE_PANEL_BUILD_PROGRESSBAR_COLOR;
+    static int         SIDE_PANEL_PLANET_NAME_PTS;
+    static int         SIDE_PANEL_PTS;
+
     //!@}
 
 private:
-    void UnregisterCurrent(bool delete_it = false);    //!< removes the current window from the Zlist and deletes it if delete_it is true
+    void HideAllWindows();              //!< hides all the UI windows from view
 
-    const Uint32 TOOLTIP_DELAY;    //!<number of milliseconds to initialize tooltips to
-    ToolContainer* m_tooltips;        //!< the single toolcontainer object
-    int m_state;                    //!< represents the screen currently being displayed
-    GG::Wnd* m_current_window;    //!< a pointer to the window (screen) currently being displays
-    
-    StringTable* m_string_table;    //!< a string table to lookup international strings
-    
-    GalaxyMapScreen* m_galaxy_map;    //!< the galaxy map screen
+    const Uint32 TOOLTIP_DELAY;         //!< number of milliseconds to initialize tooltips to
+    ToolContainer* m_tooltips;          //!< the single toolcontainer object
+    State m_state;                      //!< represents the screen currently being displayed
 
-    static log4cpp::Category& s_logger;        //!< log4cpp logging category
-    static ClientUI* the_UI;                   //!<pointer to the one and only ClientUI object
+    StringTable* m_string_table;        //!< a string table to lookup international strings
+
+    IntroScreen* m_intro_screen;        //!< the intro (and main menu) screen first showed when the game starts up
+    MapWnd*      m_map_wnd;             //!< the galaxy map
+
+    static log4cpp::Category& s_logger; //!< log4cpp logging category
+    static ClientUI* s_the_UI;          //!<pointer to the one and only ClientUI object
 };//ClientUI
 
 
