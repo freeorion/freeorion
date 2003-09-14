25cd79ac756124819028983b630f9853a33da8f1
Zach Laine <whatwasthataddress@gmail.com>
2003-09-14 00:36:33 +0000

    Added nebulae, zooming with the mouse wheel, and functions to turn system names on or off.
    svn path=/trunk/FreeOrion/; revision=344

--- UI/MapWnd.cpp
+++ UI/MapWnd.cpp
@@ -13,23 +13,36 @@
 #include "SystemIcon.h"
 #include "../universe/UniverseObject.h"
 
+#include <boost/random/uniform_smallint.hpp>
+#include <boost/random/mersenne_twister.hpp>
+
 #include <vector>
 
 namespace {
 const int SIDE_PANEL_WIDTH = 300;
+const int MAP_MARGIN_WIDTH = 50;    // the number of pixels of system-less space around all four sides of the starfield
+const double ZOOM_STEP_SIZE = 1.25;
+const int NUM_NEBULA_TEXTURES = 5;
 }
 
-const int MapWnd::MAX_SCALE_FACTOR = 8;
+// static(s)
+const double MapWnd::MIN_SCALE_FACTOR = 0.5;
+const double MapWnd::MAX_SCALE_FACTOR = 8.0;
+const int    MapWnd::NUM_BACKGROUNDS = 3;
 
 MapWnd::MapWnd() :
-    GG::Wnd(0, 0, 5, 5, GG::Wnd::CLICKABLE | GG::Wnd::DRAGABLE),
+    GG::Wnd(-GG::App::GetApp()->AppWidth(), -GG::App::GetApp()->AppHeight(), 
+            static_cast<int>(ClientUniverse::UNIVERSE_WIDTH * MAX_SCALE_FACTOR) + GG::App::GetApp()->AppWidth() + MAP_MARGIN_WIDTH, 
+            static_cast<int>(ClientUniverse::UNIVERSE_WIDTH * MAX_SCALE_FACTOR) + GG::App::GetApp()->AppHeight() + MAP_MARGIN_WIDTH, 
+            GG::Wnd::CLICKABLE | GG::Wnd::DRAGABLE),
+    m_backgrounds(NUM_BACKGROUNDS),
+    m_bg_scroll_rate(NUM_BACKGROUNDS),
+    m_bg_position_X(NUM_BACKGROUNDS),
+    m_bg_position_Y(NUM_BACKGROUNDS),
+    m_zoom_factor(1.0),
     m_drag_offset(-1, -1),
     m_dragged(false)
 {
-    //resize to the exact size of the universe....this will eventually change
-    SizeMove(0, 0, static_cast<int>(ClientUniverse::UNIVERSE_WIDTH * MAX_SCALE_FACTOR), 
-             static_cast<int>(ClientUniverse::UNIVERSE_WIDTH * MAX_SCALE_FACTOR));
-
     SetText("MapWnd");
 
     m_side_panel = new SidePanel(GG::App::GetApp()->AppWidth() - SIDE_PANEL_WIDTH, 0, SIDE_PANEL_WIDTH, GG::App::GetApp()->AppHeight());
@@ -37,76 +50,68 @@ MapWnd::MapWnd() :
     Connect(SelectedSystemSignal(), &SidePanel::SetSystem, m_side_panel);
 
     //set up background images
-    m_background[0] = boost::shared_ptr<GG::Texture>(new GG::Texture());
-    m_background[0]->Load(ClientUI::ART_DIR + "starfield1.png");
+    m_backgrounds[0].reset(new GG::Texture());
+    m_backgrounds[0]->Load(ClientUI::ART_DIR + "starfield1.png");
     m_bg_position_X[0] = 10.0;
     m_bg_position_Y[0] = 10.0;
-    m_bg_scroll_rate[0] = 0.5;
+    m_bg_scroll_rate[0] = 0.125;
     
-    m_background[1] = boost::shared_ptr<GG::Texture>(new GG::Texture());
-    m_background[1]->Load(ClientUI::ART_DIR + "starfield2.png");
+    m_backgrounds[1].reset(new GG::Texture());
+    m_backgrounds[1]->Load(ClientUI::ART_DIR + "starfield2.png");
     m_bg_position_X[1] = 10.0;
     m_bg_position_Y[1] = 10.0;
     m_bg_scroll_rate[1] = 0.25;
     
-    m_background[2] = boost::shared_ptr<GG::Texture>(new GG::Texture());
-    m_background[2]->Load(ClientUI::ART_DIR + "starfield3.png");
+    m_backgrounds[2].reset(new GG::Texture());
+    m_backgrounds[2]->Load(ClientUI::ART_DIR + "starfield3.png");
     m_bg_position_X[2] = 10.0;
     m_bg_position_Y[2] = 10.0;
-    m_bg_scroll_rate[2] = 0.12;
+    m_bg_scroll_rate[2] = 0.5;
+
+    // set up nebulae
+    boost::mt19937 gen;
+    int num_nebulae = boost::uniform_smallint<int>(1, 4)(gen);
+    m_nebulae.resize(num_nebulae);
+    m_nebula_centers.resize(num_nebulae);
+    boost::uniform_smallint<int> universe_placement(0, ClientUniverse::UNIVERSE_WIDTH);
+    boost::uniform_smallint<int> nebula_type(1, NUM_NEBULA_TEXTURES);
+    for (int i = 0; i < num_nebulae; ++i) {
+        std::string nebula_filename = "nebula" + boost::lexical_cast<std::string>(nebula_type(gen)) + ".png";
+        m_nebulae[i].reset(new GG::Texture());
+        m_nebulae[i]->Load(ClientUI::ART_DIR + nebula_filename);
+        m_nebula_centers[i] = GG::Pt(universe_placement(gen), universe_placement(gen));
+    }
 }
 
 MapWnd::~MapWnd()
 {
 }
 
-void MapWnd::InitTurn()
+GG::Pt MapWnd::ClientUpperLeft() const
 {
-    ClientUniverse::ObjectIDVec system_IDs = ClientApp::Universe().FindObjectIDs(IsSystem);
-    for (unsigned int i = 0; i < system_IDs.size(); ++i) {
-        SystemIcon* icon = new SystemIcon(system_IDs[i]);
-        AttachChild(icon);
-        GG::Connect(icon->LeftClickedSignal(), &MapWnd::SelectSystem, this);
-    }
-    MoveChildUp(m_side_panel);
-    m_side_panel->Hide();
+    return UpperLeft() + GG::Pt(GG::App::GetApp()->AppWidth(), GG::App::GetApp()->AppHeight());
 }
-
 
 int MapWnd::Render()
 {
     RenderBackgrounds();
-    glDisable(GL_TEXTURE_2D);
-    GG::FlatRectangle(UpperLeft().x, UpperLeft().y, LowerRight().x, LowerRight().y, GG::CLR_ZERO, GG::CLR_WHITE, 2);
-    glEnable(GL_TEXTURE_2D);
     return 1;
 }
 
 int MapWnd::LButtonDown (const GG::Pt &pt, Uint32 keys)
 {
-    m_drag_offset = pt - UpperLeft();
+    m_drag_offset = pt - ClientUpperLeft();
     return 1;
 }
 
 int MapWnd::LDrag (const GG::Pt &pt, const GG::Pt &move, Uint32 keys)
 {
     GG::Pt move_to_pt = pt - m_drag_offset;
-    GG::Pt wnd_sz = WindowDimensions();
-
-    if (0 < move_to_pt.x)
-        move_to_pt.x = 0;
-    if (0 < move_to_pt.y)
-        move_to_pt.y = 0;
-
-    if (move_to_pt.x < GG::App::GetApp()->AppWidth() - wnd_sz.x)
-        move_to_pt.x = GG::App::GetApp()->AppWidth() - wnd_sz.x;
-    if (move_to_pt.y < GG::App::GetApp()->AppHeight() - wnd_sz.y)
-        move_to_pt.y = GG::App::GetApp()->AppHeight() - wnd_sz.y;
-
-    GG::Pt final_move = move_to_pt - UpperLeft();
+    CorrectMapPosition(move_to_pt);
+    GG::Pt final_move = move_to_pt - ClientUpperLeft();
     m_side_panel->OffsetMove(-final_move);
     MoveBackgrounds(final_move);
-    MoveTo(move_to_pt);
+    MoveTo(move_to_pt - GG::Pt(GG::App::GetApp()->AppWidth(), GG::App::GetApp()->AppHeight()));
 
     m_dragged = true;
 
@@ -129,28 +134,119 @@ int MapWnd::LClick (const GG::Pt &pt, Uint32 keys)
     return 1;
 }
 
+int MapWnd::MouseWheel(const GG::Pt& pt, int move, Uint32 keys)
+{
+    GG::Pt ul = ClientUpperLeft();
+    GG::Pt center = GG::Pt( GG::App::GetApp()->AppWidth() / 2,  GG::App::GetApp()->AppHeight() / 2);
+    GG::Pt ul_offset = ul - center;
+    if (0 < move) {
+        if (m_zoom_factor < MAX_SCALE_FACTOR) {
+            ul_offset.x = static_cast<int>(ul_offset.x * ZOOM_STEP_SIZE);
+            ul_offset.y = static_cast<int>(ul_offset.y * ZOOM_STEP_SIZE);
+            m_zoom_factor *= ZOOM_STEP_SIZE;
+        } else {
+            ul_offset.x = static_cast<int>(ul_offset.x * MAX_SCALE_FACTOR / m_zoom_factor);
+            ul_offset.y = static_cast<int>(ul_offset.y * MAX_SCALE_FACTOR / m_zoom_factor);
+            m_zoom_factor = MAX_SCALE_FACTOR;
+        }
+    } else {
+        if (MIN_SCALE_FACTOR < m_zoom_factor) {
+            ul_offset.x = static_cast<int>(ul_offset.x / ZOOM_STEP_SIZE);
+            ul_offset.y = static_cast<int>(ul_offset.y / ZOOM_STEP_SIZE);
+            m_zoom_factor /= ZOOM_STEP_SIZE;
+        } else {
+            ul_offset.x = static_cast<int>(ul_offset.x * MIN_SCALE_FACTOR / m_zoom_factor);
+            ul_offset.y = static_cast<int>(ul_offset.y * MIN_SCALE_FACTOR / m_zoom_factor);
+            m_zoom_factor = MIN_SCALE_FACTOR;
+        }
+    }
+
+    for (unsigned int i = 0; i < m_stars.size(); ++i) {
+        const System* system = 
+            dynamic_cast<const System*>(HumanClientApp::Universe().Object(m_stars[i]->SystemID()));
+        GG::Pt icon_ul(static_cast<int>((system->X() - SystemIcon::ICON_WIDTH / 2) * m_zoom_factor), 
+                       static_cast<int>((system->Y() - SystemIcon::ICON_HEIGHT / 2) * m_zoom_factor));
+        m_stars[i]->SizeMove(icon_ul.x, icon_ul.y, 
+                             static_cast<int>(icon_ul.x + SystemIcon::ICON_WIDTH * m_zoom_factor + 0.5), 
+                             static_cast<int>(icon_ul.y + SystemIcon::ICON_HEIGHT * m_zoom_factor + 0.5));
+    }
+    GG::Pt map_move = ul_offset + center - ul;
+    OffsetMove(map_move);
+    m_side_panel->OffsetMove(-map_move);
+
+    // this correction ensures that zooming in doesn't leave too large a margin to the side
+    GG::Pt move_to_pt = ul = ClientUpperLeft();
+    CorrectMapPosition(move_to_pt);
+    GG::Pt final_move = move_to_pt - ul;
+    m_side_panel->OffsetMove(-final_move);
+    MoveBackgrounds(final_move);
+    MoveTo(move_to_pt - GG::Pt(GG::App::GetApp()->AppWidth(), GG::App::GetApp()->AppHeight()));
+
+    return 1;
+}
+
+void MapWnd::InitTurn()
+{
+    m_stars.clear();
+    ClientUniverse::ObjectIDVec system_IDs = ClientApp::Universe().FindObjectIDs(IsSystem);
+    for (unsigned int i = 0; i < system_IDs.size(); ++i) {
+        SystemIcon* icon = new SystemIcon(system_IDs[i], m_zoom_factor);
+        m_stars.push_back(icon);
+        AttachChild(icon);
+        GG::Connect(icon->LeftClickedSignal(), &MapWnd::SelectSystem, this);
+    }
+    MoveChildUp(m_side_panel);
+    m_side_panel->Hide();
+}
+
+void MapWnd::ShowSystemNames()
+{
+    for (unsigned int i = 0; i < m_stars.size(); ++i)
+        m_stars[i]->ShowName();
+}
+
+void MapWnd::HideSystemNames()
+{
+    for (unsigned int i = 0; i < m_stars.size(); ++i)
+        m_stars[i]->HideName();
+}
+
 void MapWnd::RenderBackgrounds()
 {
-    float x, y;
-    for (int i = 0; i < GMAP_NUM_BACKGROUNDS; ++i) {
-        glColor3f(1.0, 1.0, 1.0);
-        x = std::fmod(m_bg_position_X[i], m_background[i]->Width());
-        while (x < GG::App::GetApp()->AppWidth()) {
-            y = std::fmod(m_bg_position_Y[i], m_background[i]->Height());
-            while (y < GG::App::GetApp()->AppHeight()) {
-                m_background[i]->OrthoBlit(static_cast<int>(x), static_cast<int>(y));
-                y += m_background[i]->Height();
+    double x, y;
+    glColor3d(1.0, 1.0, 1.0);
+    for (int i = 0; i < NUM_BACKGROUNDS; ++i) {
+        int bg_width = m_backgrounds[i]->Width();
+        int bg_height = m_backgrounds[i]->Height();
+        int app_width = GG::App::GetApp()->AppWidth();
+        int app_height = GG::App::GetApp()->AppHeight();
+        x = std::fmod(m_bg_position_X[i], bg_width);
+        while (x < app_width + bg_width) {
+            y = std::fmod(m_bg_position_Y[i], bg_height);
+            while (y < app_height + bg_height) {
+                m_backgrounds[i]->OrthoBlit(static_cast<int>(x - bg_width), static_cast<int>(y - bg_height), false);
+                y += m_backgrounds[i]->Height();
             }
-            x += m_background[i]->Width();
+            x += m_backgrounds[i]->Width();
         }
+    }
+
+    for (unsigned int i = 0; i < m_nebulae.size(); ++i) {
+        GG::Pt ul = ClientUpperLeft() + 
+            GG::Pt(static_cast<int>((m_nebula_centers[i].x - m_nebulae[i]->Width() / 2.0) * m_zoom_factor),
+                   static_cast<int>((m_nebula_centers[i].y - m_nebulae[i]->Height() / 2.0) * m_zoom_factor));
+        m_nebulae[i]->OrthoBlit(ul, 
+                                ul + GG::Pt(static_cast<int>(m_nebulae[i]->Width() * m_zoom_factor), 
+                                            static_cast<int>(m_nebulae[i]->Width() * m_zoom_factor)), 
+                                false);
     }
 }
 
 void MapWnd::MoveBackgrounds(const GG::Pt& move)
 {   
-    float move_x, move_y;
+    double move_x, move_y;
     
-    for (int i = 0; i < GMAP_NUM_BACKGROUNDS; ++i) {
+    for (int i = 0; i < NUM_BACKGROUNDS; ++i) {
         move_x = move.x * m_bg_scroll_rate[i];
         move_y = move.y * m_bg_scroll_rate[i];
         m_bg_position_X[i] += move_x;
@@ -161,4 +257,34 @@ void MapWnd::MoveBackgrounds(const GG::Pt& move)
 void MapWnd::SelectSystem(int systemID)
 {
     m_selected_system_signal(systemID);
+}
+
+void MapWnd::CorrectMapPosition(GG::Pt &move_to_pt)
+{
+    int contents_width = static_cast<int>(m_zoom_factor * ClientUniverse::UNIVERSE_WIDTH);
+    int app_width =  GG::App::GetApp()->AppWidth();
+    int app_height =  GG::App::GetApp()->AppHeight();
+    if (app_width < contents_width) {
+        if (MAP_MARGIN_WIDTH < move_to_pt.x)
+            move_to_pt.x = MAP_MARGIN_WIDTH;
+        if (move_to_pt.x < app_width - contents_width - 2 * MAP_MARGIN_WIDTH)
+            move_to_pt.x = app_width - contents_width - 2 * MAP_MARGIN_WIDTH;
+    } else {
+        if (move_to_pt.x < 0)
+            move_to_pt.x = 0;
+        if (app_width - contents_width < move_to_pt.x)
+            move_to_pt.x = app_width - contents_width;
+    }
+
+    if (app_height < contents_width) {
+        if (MAP_MARGIN_WIDTH < move_to_pt.y)
+            move_to_pt.y = MAP_MARGIN_WIDTH;
+        if (move_to_pt.y < app_height - contents_width - 2 * MAP_MARGIN_WIDTH)
+            move_to_pt.y = app_height - contents_width - 2 * MAP_MARGIN_WIDTH;
+    } else {
+        if (move_to_pt.y < 0)
+            move_to_pt.y = 0;
+        if (app_height - contents_width < move_to_pt.y)
+            move_to_pt.y = app_height - contents_width;
+    }
 }
--- UI/MapWnd.h
+++ UI/MapWnd.h
@@ -9,8 +9,6 @@
 #include "GGWnd.h"
 #endif
 
-#define GMAP_NUM_BACKGROUNDS 3
-
 class SidePanel;
 class SystemIcon;
 class Icon;
@@ -24,11 +22,11 @@ class Texture;
 class MapWnd : public GG::Wnd
 {
 public:
-    static const int MAX_SCALE_FACTOR;
-    
-    typedef std::vector<SystemIcon*> StarControlVec;
-    typedef std::vector<Icon*> IconVec;
-    
+    static const double MIN_SCALE_FACTOR;
+    static const double MAX_SCALE_FACTOR;
+
+    typedef std::vector<SystemIcon*> SysIconVec;
+
     enum     //!< enumeration for indices to the icon vector
     {
         FLEET_ICON          = 0,
@@ -38,68 +36,65 @@ public:
         INDUSTRY_ICON       = 4,
         POPULATION_ICON     = 5
     };
-    
+
     //! \name Signal Types //!@{
     typedef boost::signal<void (int)> SelectedSystemSignalType; //!< emitted when the user selects a star system
     //!@}
+
     //! \name Slot Types //!@{
     typedef SelectedSystemSignalType::slot_type SelectedSystemSlotType; //!< type of functor invoked when the user selects a star system
     //!@}
-    
-public:
 
     //! \name Structors //!@{
     MapWnd();    //!< default ctor
     ~MapWnd();   //!< default dtor
-    
     //!@}
-    
+
     //! \name Accessors //!@{
+    virtual GG::Pt ClientUpperLeft() const;
 
+    double ZoomFactor() const {return m_zoom_factor;}
     //!@}
-    
-    //! \name Mutators //!@{
-    void           InitTurn();        //!< called at the start of each turn by
 
+    //! \name Mutators //!@{
     virtual int    Render();
     virtual int    LButtonDown(const GG::Pt& pt, Uint32 keys);
     virtual int    LDrag(const GG::Pt& pt, const GG::Pt& move, Uint32 keys);
     virtual int    LButtonUp(const GG::Pt& pt, Uint32 keys);
     virtual int    LClick(const GG::Pt& pt, Uint32 keys);
-    
+    virtual int    MouseWheel(const GG::Pt& pt, int move, Uint32 keys);
+
+    void           InitTurn();        //!< called at the start of each turn by
+    void           ShowSystemNames(); //!< enables the system name text
+    void           HideSystemNames(); //!< disables the system name text
+
     SelectedSystemSignalType& SelectedSystemSignal() {return m_selected_system_signal;}
 
     void SelectSystem(int systemID); //!< catches emitted signals from the system icons
     //!@}
 
 private:
-    //! \name Private methods //!@{
-    void RenderBackgrounds();    //!< this renders the backgrounds onto the screen
-    void MoveBackgrounds(const GG::Pt& move);        //!< this scrolls the backgrounds at their respective rates
-    
-    //!@}
-    
-private:
-    //! \name Background Data Storage //!@{
-    // TODO: Convert this to a "Background" class
-    boost::shared_ptr<GG::Texture>    m_background[GMAP_NUM_BACKGROUNDS];  //!< array, 3 layers of backgrounds
-    float           m_bg_scroll_rate[GMAP_NUM_BACKGROUNDS]; //!< array, the rates at which each background scrolls
-    float           m_bg_position_X[GMAP_NUM_BACKGROUNDS]; //!< array, the X position of the first full background image
-    float           m_bg_position_Y[GMAP_NUM_BACKGROUNDS]; //!< array, the Y positions of the backgrounds
-    
-    //!@}
+    void RenderBackgrounds();    //!< renders the backgrounds onto the screen
+    void MoveBackgrounds(const GG::Pt& move); //!< scrolls the backgrounds at their respective rates
+    void CorrectMapPosition(GG::Pt &move_to_pt); //!< ensures that the map data are positioned sensibly
 
-    SidePanel*      m_side_panel;     //! the planet view panel on the side of the main map
-    StarControlVec  m_stars;          //! a vector of StarControls
-    IconVec         m_icons;          //! a list of icons
+    std::vector<boost::shared_ptr<GG::Texture> > m_backgrounds; //!< starfield backgrounds
+    std::vector<boost::shared_ptr<GG::Texture> > m_nebulae;     //!< decorative nebula textures
+    std::vector<GG::Pt>   m_nebula_centers; //!< the centerpoints of each of the nebula textures
+    std::vector<double>   m_bg_scroll_rate; //!< array, the rates at which each background scrolls
+    std::vector<double>   m_bg_position_X;  //!< array, the X position of the first full background image
+    std::vector<double>   m_bg_position_Y;  //!< array, the Y positions of the backgrounds
 
-    GG::Pt          m_drag_offset;    //! the distance the cursor is from the upper-left corner of the window during a drag ((-1, -1) if no drag is occurring)
-    bool            m_dragged;        //! tracks whether or not a drag occurs during a left button down sequence of events
-    
-    //! \name Signals //!@{
+    double          m_zoom_factor;  //! the current zoom level; clamped to [MIN_SCALE_FACTOR, MAX_SCALE_FACTOR]
+    SidePanel*      m_side_panel;   //! the planet view panel on the side of the main map
+    SysIconVec      m_stars;        //! a vector of StarControls
+
+    GG::Pt          m_drag_offset;  //! the distance the cursor is from the upper-left corner of the window during a drag ((-1, -1) if no drag is occurring)
+    bool            m_dragged;      //! tracks whether or not a drag occurs during a left button down sequence of events
+
     SelectedSystemSignalType m_selected_system_signal;
-    
-    //!@}
+
+    static const int NUM_BACKGROUNDS;
 
     friend class GalaxyMapScreen;    //this is basically a part of that screen anyway
 };
