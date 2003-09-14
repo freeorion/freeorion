e492c8553b7c9a0240b28a8f26c796f58bf0c6c5
Zach Laine <whatwasthataddress@gmail.com>
2003-09-14 00:43:58 +0000

    Made several changes to accomdate changes in MapWnd.
    svn path=/trunk/FreeOrion/; revision=347

--- UI/SystemIcon.cpp
+++ UI/SystemIcon.cpp
@@ -2,37 +2,37 @@
 
 #include "SystemIcon.h"
 
+#include "ClientUI.h"
+#include "GGDrawUtil.h"
+#include "GGStaticGraphic.h"
+#include "GGTextControl.h"
 #include "../client/human/HumanClientApp.h"
 #include "../universe/System.h"
-#include "GGDrawUtil.h"
-#include "GGApp.h"
-#include "ClientUI.h"
-#include "GGTextControl.h"
-#include "MapWnd.h"
 
-const int SystemIcon::ICON_WIDTH = 64;
-const int SystemIcon::ICON_HEIGHT = 64;
+// static(s)
+const int SystemIcon::ICON_WIDTH = 32;
+const int SystemIcon::ICON_HEIGHT = 32;
 
-SystemIcon::SystemIcon(int id) :
-    GG::Control(0,0,1,1, GG::Wnd::CLICKABLE)
+SystemIcon::SystemIcon(int id, double zoom) :
+    GG::Control(0, 0, 1, 1, GG::Wnd::CLICKABLE),
+    m_systemID(id),
+    m_static_graphic(0),
+    m_name(0)
 {
-    m_systemID = id;
-
     const System* sys = dynamic_cast<const System*>(ClientApp::Universe().Object(id));
 
     SetText(sys->Name());
 
     //resize to the proper size
-    //TEMP: this will be read from the universe
-    SizeMove((int)(sys->X() * MapWnd::MAX_SCALE_FACTOR),
-             (int)(sys->Y() * MapWnd::MAX_SCALE_FACTOR),
-             (int)(sys->X() * MapWnd::MAX_SCALE_FACTOR + ICON_WIDTH),
-             (int)(sys->Y() * MapWnd::MAX_SCALE_FACTOR + ICON_HEIGHT)); //to position of this universe object
+    SizeMove((int)(sys->X() * zoom - ICON_WIDTH / 2),
+             (int)(sys->Y() * zoom - ICON_HEIGHT / 2),
+             (int)(sys->X() * zoom - ICON_WIDTH / 2 + ICON_WIDTH),
+             (int)(sys->Y() * zoom - ICON_HEIGHT / 2 + ICON_HEIGHT)); //to position of this universe object
 
     //load the proper graphic for the color of the star
     boost::shared_ptr<GG::Texture> graphic;
     switch (sys->Star()) {
-    case System::YELLOW:       
+    case System::YELLOW:
         graphic = HumanClientApp::GetApp()->GetTexture(ClientUI::ART_DIR + "/stars/yellow1.png");
         break;
     case System::RED_GIANT:
@@ -48,11 +48,12 @@ SystemIcon::SystemIcon(int id) :
     }//end switch
 
     //setup static graphic
-    m_static_graphic = new GG::StaticGraphic(0, 0, ICON_WIDTH, ICON_HEIGHT, graphic);
+    m_static_graphic = new GG::StaticGraphic(0, 0, ICON_WIDTH, ICON_HEIGHT, graphic, GG::SG_FITGRAPHIC);
     AttachChild(m_static_graphic);
 
     //set up the name text control
-    m_name = new GG::TextControl(0, Height(), Width(), 12, sys->Name(), ClientUI::FONT, ClientUI::PTS, GG::TF_CENTER, ClientUI::TEXT_COLOR);
+    m_name = new GG::TextControl(0, 0, sys->Name(), ClientUI::FONT, ClientUI::PTS, ClientUI::TEXT_COLOR);
+    PositionSystemName();
     AttachChild(m_name);
 
     Show();
@@ -61,5 +62,28 @@ SystemIcon::SystemIcon(int id) :
 
 SystemIcon::~SystemIcon()
 {
-    // TODO: destruction code
+}
+
+void SystemIcon::SizeMove(int x1, int y1, int x2, int y2)
+{
+    Wnd::SizeMove(x1, y1, x2, y2);
+    if (m_static_graphic)
+        m_static_graphic->SizeMove(0, 0, x2 - x1, y2 - y1);
+    PositionSystemName();
+}
+
+void SystemIcon::ShowName()
+{
+    m_name->Show();
+}
+
+void SystemIcon::HideName()
+{
+    m_name->Hide();
+}
+
+void SystemIcon::PositionSystemName()
+{
+    if (m_name)
+        m_name->MoveTo(Width() / 2 - m_name->Width() / 2, Height());
 }
--- UI/SystemIcon.h
+++ UI/SystemIcon.h
@@ -3,29 +3,16 @@
 #ifndef _SystemIcon_h_
 #define _SystemIcon_h_
 
-
-#ifndef _GGTexture_h_
-#include "GGTexture.h"
-#endif
-
-#ifndef _GGTextControl_h_
-#include "GGTextControl.h"
-#endif
-
 #ifndef _GGControl_h_
 #include "GGControl.h"
 #endif
 
-#ifndef _GGStaticGraphic_h_
-#include "GGStaticGraphic.h"
-#endif
+namespace GG {
+class StaticGraphic;
+class TextControl;
+}
 
-#ifndef _ClientUI_h_
-#include "ClientUI.h"
-#endif
-
-//! This class is a GUI control that allows interaction with a star system
-/**
+/** a GUI control that allows interaction with a star system.
     This class allows user interaction with star systems
     on the galaxy map.  It contains the graphic to display the 
     system, along with the object ID of the UniverseObject associated
@@ -35,38 +22,44 @@
 class SystemIcon : public GG::Control
 {
 public:
+    using Wnd::SizeMove;
+
     //! \name Static Constants //!@{
     static const int ICON_WIDTH;
     static const int ICON_HEIGHT;
     //!@}
-    
-public:
+
     //! \name Signal Types //!@{
     typedef boost::signal<void (int)> LeftClickedSignalType; //!< emitted when the user left clicks the icon, returns the objectID
     typedef boost::signal<void (int)> RightClickedSignalType; //!< emitted when the user right clicks the icon, returns the objectID
     typedef boost::signal<void (int)> LeftDoubleClickedSignalType; //!< emitted when the user left double-clicks the icon, returns the object id
-    
     //!@}
+
     //! \name Slot Types //!@{
     typedef LeftClickedSignalType::slot_type LeftClickedSlotType; //!< type of functor invoked when the user left clicks
     typedef RightClickedSignalType::slot_type RightClickedSlotType; //!< type of functor invoked when the user right clicks
     typedef LeftDoubleClickedSignalType::slot_type LeftDoubleClickedSlotType; //!< type of functor invoked when the user left double-clicks
-    
     //!@}
+
     //! \name Structors //!@{
-    SystemIcon(int id); //!< standard construction from a universe ID
+    SystemIcon(int id, double zoom); //!< construct from a universe ID, to be placed in a MapWnd at zoom level \a zoom
     ~SystemIcon();      //!< dtor
-    
     //!@}
+
     //! \name Accessors //!@{
-    
+    int SystemID() const {return m_systemID;}
     //!@}
+
     //! \name Mutators //!@{
-    virtual int Render() {return 1;}
-    virtual int LClick(const GG::Pt& pt, Uint32 keys) {if(!Disabled()) m_left_click_signal(m_systemID); return 1;}
-    virtual int RClick(const GG::Pt& pt, Uint32 keys) {if(!Disabled()) m_right_click_signal(m_systemID); return 1;}
-    virtual int LDoubleClick(const GG::Pt& pt, Uint32 keys) {if(!Disabled()) m_left_double_click_signal(m_systemID); return 1;}
-    
+    virtual void   SizeMove(int x1, int y1, int x2, int y2);
+    virtual int    Render() {return 1;}
+    virtual int    LClick(const GG::Pt& pt, Uint32 keys) {if(!Disabled()) m_left_click_signal(m_systemID); return 1;}
+    virtual int    RClick(const GG::Pt& pt, Uint32 keys) {if(!Disabled()) m_right_click_signal(m_systemID); return 1;}
+    virtual int    LDoubleClick(const GG::Pt& pt, Uint32 keys) {if(!Disabled()) m_left_double_click_signal(m_systemID); return 1;}
+
+    void           ShowName(); //!< enables the system name text
+    void           HideName(); //!< disables the system name text
+
     LeftClickedSignalType&       LeftClickedSignal() {return m_left_click_signal;}
     RightClickedSignalType&      RightClickedSignal() {return m_right_click_signal;}
     LeftDoubleClickedSignalType& LeftDoubleClickedSignal() {return m_left_double_click_signal;}
@@ -76,8 +69,10 @@ protected:
     int                 m_systemID;    //!< the ID of the System object associated with this SystemIcon
     GG::StaticGraphic*  m_static_graphic; //!< the control used to render the displayed texture
     GG::TextControl*    m_name;        //!< the control that holds the name of the system
-    
+
 private:
+    void PositionSystemName();
+
     //! \name Signals //!@{
     LeftClickedSignalType       m_left_click_signal;
     RightClickedSignalType      m_right_click_signal;
