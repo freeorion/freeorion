b4fa3e8927896fa937e7074263cafa3a7a69bd65
Joshua Barczak <jbarcz1@dbd5520b-6a0a-0410-a553-f54834df5b05>
2003-09-17 01:14:46 +0000

    Added catch handlers for common exceptions.  This way we're less likely to miss them when they occur
    svn path=/trunk/FreeOrion/; revision=357

--- client/human/chmain.cpp
+++ client/human/chmain.cpp
@@ -1,6 +1,6 @@
 #include "HumanClientApp.h"
 #include "XMLDoc.h"
-
+#include <boost/lexical_cast.hpp>
 
 #ifdef __cplusplus
 extern "C" // use C-linkage, as required by SDL
@@ -11,12 +11,25 @@ int main(int argc, char* argv[])
    GG::XMLDoc xml_doc;
    xml_doc.ReadDoc(xml_file);
    xml_file.close();
-
    HumanClientApp g_app(xml_doc.root_node.Child("HumanClientApp"));
-   g_app.SetXMLDoc(xml_doc);
-
-   g_app(); // run app (intialization and main process loop)
-
+   
+   try
+   {
+        g_app.SetXMLDoc(xml_doc);
+        g_app(); // run app (intialization and main process loop)
+    }
+    catch(std::invalid_argument& e)
+    {
+        g_app.Logger().debugStream() <<"main() caught exception(invalid arg): " << e.what();
+    }
+    catch(std::runtime_error& e)
+    {
+        g_app.Logger().debugStream() << "main() caught exception(runtime err): " << e.what();
+    }
+    catch(boost::bad_lexical_cast e)
+    {
+        g_app.Logger().debugStream() <<"caught exception: boost bad lexical cast: "<<e.what();
+    }
    return 0;
 
 }
