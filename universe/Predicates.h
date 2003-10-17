928dd0b348ec5045729ff3c004d6fff6ef26384e
Zach Laine <whatwasthataddress@gmail.com>
2003-10-17 17:24:53 +0000

    Moved inline functions to new implementation file, and added new functors to locate fleets of a certain empire that are stationary, moving, and ordered to move but still at a system.
    svn path=/trunk/FreeOrion/; revision=448

--- universe/Predicates.h
+++ universe/Predicates.h
@@ -2,30 +2,47 @@
 #ifndef _Predicates_h_
 #define _Predicates_h_
 
-#ifndef _Universe_h_
-#include "Universe.h"
-#endif
-
-#ifndef _System_h_
-#include "System.h"
-#endif
-
-#ifndef _Planet_h_
-#include "Planet.h"
-#endif
-
-#ifndef _Fleet_h_
-#include "Fleet.h"
-#endif
-
+class UniverseObject;
 
 /** returns true iff \a obj is a System object. */
-inline bool IsSystem(const UniverseObject* obj) {return dynamic_cast<const System*>(obj);}
+bool IsSystem(const UniverseObject* obj);
 
 /** returns true iff \a obj is a Planet object. */
-inline bool IsPlanet(const UniverseObject* obj) {return dynamic_cast<const Planet*>(obj);}
+bool IsPlanet(const UniverseObject* obj);
 
 /** returns true iff \a obj is a Fleet object. */
-inline bool IsFleet(const UniverseObject* obj) {return dynamic_cast<const Fleet*>(obj);}
+bool IsFleet(const UniverseObject* obj);
+
+/** returns true iff \a obj is a Ship object. */
+bool IsShip(const UniverseObject* obj);
+
+/** returns true iff \a obj is a Fleet belonging to the given empire object that is under orders to move, but is not yet moving. 
+    If the given empire is -1, all stationary fleets will be returned.  Note that it is preferable to use this functor on System
+    searches, rather than Universe ones. */
+struct IsStationaryFleetFunctor
+{
+    IsStationaryFleetFunctor(int empire) : empire_id(empire) {}
+    bool operator()(const UniverseObject* obj) const;
+    const int empire_id;
+};
+
+/** returns true iff \a obj is a Fleet belonging to the given empire object that is parked at a System, not under orders to move.  
+    If the given empire is -1, all orderd moving fleets will be returned.  Note that it is preferable to use this functor on System
+    searches, rather than Universe ones. */
+struct IsOrderedMovingFleetFunctor
+{
+    IsOrderedMovingFleetFunctor(int empire) : empire_id(empire) {}
+    bool operator()(const UniverseObject* obj) const;
+    const int empire_id;
+};
+
+/** returns true iff \a obj is a moving Fleet belonging to the given empire object that is moving between systems.  
+    If the given empire is -1, all moving fleets will be returned. */
+struct IsMovingFleetFunctor
+{
+    IsMovingFleetFunctor(int empire) : empire_id(empire) {}
+    bool operator()(const UniverseObject* obj) const;
+    const int empire_id;
+};
 
 #endif // _Predicates_h_
