4839553c7b990e1071c15b8e7e6038e5bf2167d3
Zach Laine <whatwasthataddress@gmail.com>
2003-11-14 05:04:38 +0000

    Renamed TechLevel Tech, as discussed online, and removed superfluous comments.
    svn path=/trunk/FreeOrion/; revision=528

--- Empire/TechManager.cpp
+++ Empire/TechManager.cpp
@@ -1,4 +1,3 @@
-
 #include "TechManager.h"
 
 TechManager& TechManager::instance()
@@ -27,11 +26,11 @@ TechManager::const_iterator TechManager::end() const
 
 /** Technology Lookup By ID */ 
  /**
-   method to get at a TechLevel object given its ID.
+   method to get at a Tech object given its ID.
    Returns a pointer to the tech with the given ID, or NULL
    if no such tech exists in the manager.  This is the const version.
 */
-const TechLevel* TechManager::Lookup(int ID) const
+const Tech* TechManager::Lookup(int ID) const
 {
     const_iterator itr = m_tech_map.find(ID);
     
@@ -45,11 +44,11 @@ const TechLevel* TechManager::Lookup(int ID) const
     }
 }
 /**
-   method to get at a TechLevel object given its ID.
+   method to get at a Tech object given its ID.
    Returns a pointer to the tech with the given ID, or NULL
    if no such tech exists in the manager
 */
-TechLevel* TechManager::Lookup(int ID)
+Tech* TechManager::Lookup(int ID)
 {
     iterator itr = m_tech_map.find(ID);
     
@@ -80,31 +79,31 @@ TechManager::iterator TechManager::end()
 /**
    reads a file containing technology items and populates itself
    with the technologies in the file.  For V0.1, this will simply
-   create the hardcoded technology levels described in TechLevel.h
+   create the hardcoded technology levels described in Tech.h
 */
 bool TechManager::LoadTechTree(const std::string& TechFileName)
 {
     // make hardcoded techlevels as per v0.1 requirements document
-    TechLevel* mark2 = new TechLevel(((int)TechLevel::TECH_MARK2), std::string("Mark II"), 250);
-    TechLevel* dbase = new TechLevel(((int)TechLevel::TECH_BASE),  std::string("Defense Base"), 350);
-    TechLevel* mark3 = new TechLevel(((int)TechLevel::TECH_MARK3), std::string("Mark III"), 750);
-    TechLevel* mark4 = new TechLevel(((int)TechLevel::TECH_MARK4), std::string("Mark IV"), 1650);
+    Tech* mark2 = new Tech(((int)Tech::TECH_MARK2), std::string("Mark II"), 250);
+    Tech* dbase = new Tech(((int)Tech::TECH_BASE),  std::string("Defense Base"), 350);
+    Tech* mark3 = new Tech(((int)Tech::TECH_MARK3), std::string("Mark III"), 750);
+    Tech* mark4 = new Tech(((int)Tech::TECH_MARK4), std::string("Mark IV"), 1650);
     
-    std::pair<int, TechLevel*> the_pair;
+    std::pair<int, Tech*> the_pair;
     
-    the_pair.first = (int)TechLevel::TECH_MARK2;
+    the_pair.first = (int)Tech::TECH_MARK2;
     the_pair.second = mark2;
     m_tech_map.insert(the_pair);
     
-    the_pair.first = (int)TechLevel::TECH_BASE;
+    the_pair.first = (int)Tech::TECH_BASE;
     the_pair.second = dbase;
     m_tech_map.insert(the_pair);
     
-    the_pair.first = (int)TechLevel::TECH_MARK3;
+    the_pair.first = (int)Tech::TECH_MARK3;
     the_pair.second = mark3;
     m_tech_map.insert(the_pair);
     
-    the_pair.first = (int)TechLevel::TECH_MARK4;
+    the_pair.first = (int)Tech::TECH_MARK4;
     the_pair.second = mark4;
     m_tech_map.insert(the_pair);
     
@@ -113,7 +112,7 @@ bool TechManager::LoadTechTree(const std::string& TechFileName)
 
 /**
    Removes all technologies in the TechManager and deallocates them
-   Any pointers, iterators, or references to TechLevel objects in 
+   Any pointers, iterators, or references to Tech objects in 
    this manager will be invalidated
 */
 void TechManager::ClearAll()
--- Empire/TechManager.h
+++ Empire/TechManager.h
@@ -1,10 +1,10 @@
-
+// -*- C++ -*-
 #ifndef _TechManager_h_
 #define _TechManager_h_
 
 #include <map>
 
-#ifndef _TechLevel_h_
+#ifndef _Tech_h_
 #include "TechLevel.h"
 #endif
 
@@ -21,102 +21,64 @@
 *       - constructing a technology database
 *       - iterating over technologies and looking them up by ID
 *    
-*   Only the TechManager should create TechLevel objects.
+*   Only the TechManager should create Tech objects.
 */
 class TechManager
 {
 public:
     /** \name Iterator Types */ //@{
-    typedef std::map<int, TechLevel*>::iterator iterator;
-    typedef std::map<int, TechLevel*>::const_iterator const_iterator;
-
-   //@}
+    typedef std::map<int, Tech*>::iterator iterator;
+    typedef std::map<int, Tech*>::const_iterator const_iterator;
+    //@}
     
     /** \name Accessors */ //@{
-    /// Provides access to a single instance of the TechManager class.
+    /** Provides access to a single instance of the TechManager class. */ 
     static TechManager& instance();
-   
-    /// Returns a const iterator to the first TechLevel
-    /**
-       begin and end allow 
-       iteration over the techlevels in this manager
-    */
-    const_iterator begin() const;
-    /// Returns a const iterator past the last TechLevel
-    /**
-       begin and end allow 
-       iteration over the techlevels in this manager
-    */
-    const_iterator end() const;
-    
-    
-   
-     /// Returns a pointer to the TechLevel with the given ID.
-     /**
-       method to get at a TechLevel object given its ID.
-       Returns a pointer to the tech with the given ID, or NULL
-       if no such tech exists in the manager.  This is the const version.
-    */
-    const TechLevel* Lookup(int ID) const;
-    /// Returns a pointer to the TechLevel with the given ID.
-    /**
-       method to get at a TechLevel object given its ID.
-       Returns a pointer to the tech with the given ID, or NULL
-       if no such tech exists in the manager
-    */
-    TechLevel* Lookup(int ID);
-  
-    /**
-       begin and end allow 
-       iteration over the techlevels in this manager
-    */
-    iterator begin();
-    /**
-       begin and end allow 
-       iteration over the techlevels in this manager
-    */
-    iterator end();
 
+    /** Returns a const iterator to the first Tech */
+    const_iterator begin() const;
+    
+    /** Returns a const iterator past the last Tech */
+    const_iterator end() const;
+
+    /** Returns a pointer to the tech with the given ID, or NULL
+        if no such tech exists in the manager.*/
+    const Tech* Lookup(int ID) const;
+
+    /** Returns a pointer to the tech with the given ID, or NULL
+        if no such tech exists in the manager */
+    Tech* Lookup(int ID);
+  
+    /** Returns an iterator to the first Tech */
+    iterator begin();
+
+    /** Returns an iterator past the last Tech */
+    iterator end();
     //@}
 
 
     /** \name Mutators */ //@{
-    
-    /// For version 0.1, creates hardcoded techlevels.
-    /**
-       reads a file containing technology items and populates itself
-       with the technologies in the file.  For V0.1, this will simply
-       create the hardcoded technology levels described in TechLevel.h
-    */
+    // For version 0.1, creates hardcoded techlevels.
+    /** reads a file containing technology items and populates itself
+        with the technologies in the file.  For V0.1, this will simply
+        create the hardcoded technology levels described in Tech.h */
     bool LoadTechTree(const std::string& TechFileName);
 
-    /// Empties the manager and deallocates all managed objects.
-    /**
-       Removes all technologies in the TechManager and deallocates them
-       Any pointers, iterators, or references to TechLevel objects in 
-       this manager will be invalidated
-    */
+    /** Removes all technologies in the TechManager and deallocates them.
+        Any pointers, iterators, or references to Tech objects in 
+        this manager will be invalidated */
     void ClearAll();
-    
     //@}
 
 protected:
-    
     /** \name Constructors */ //@{
-    /// Constructs an empty manager.
-    /**
-    * Constructor.  Constructs an empty manager
-    */
+    /** Constructs an empty manager. */
     TechManager();
     //@}
-    
 
 private:
-
-    std::map<int, TechLevel*> m_tech_map;
+    std::map<int, Tech*> m_tech_map;
     int m_next_id;
-
 };
-
 
 #endif
