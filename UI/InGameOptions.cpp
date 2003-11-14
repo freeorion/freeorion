940a79c0b74884479d1220ec3b5d22690d64937b
Zach Laine <whatwasthataddress@gmail.com>
2003-11-14 05:14:23 +0000

    Implemented saving and loading (multiplayer loading is not available from this window).
    svn path=/trunk/FreeOrion/; revision=532

--- UI/InGameOptions.cpp
+++ UI/InGameOptions.cpp
@@ -1,39 +1,60 @@
-//InGameOptions.cpp
-
 #include "InGameOptions.h"
 
 #include "ClientUI.h"
 #include "CUIControls.h"
-#include "GGApp.h"
+#include "GGButton.h"
 #include "GGClr.h"
 #include "GGDrawUtil.h"
 #include "dialogs/GGFileDlg.h"
+#include "../client/human/HumanClientApp.h"
+#include "../util/OptionsDB.h"
+
+#include <boost/filesystem/operations.hpp>
 
 #include <fstream>
 
-////////////////////////////////////////////
-//   CONSTANTS
-////////////////////////////////////////////
+namespace {
+void Options(OptionsDB& db)
+{
+    db.Add('S',
+           "save-directory", 
+           "The directory in which saved games are saved and from which they are loaded.  Directory names are taken to be relative to the location of the executable.",
+           std::string("save"));
+}
 
+bool foo_bool = RegisterOptions(&Options);
+}
 
-////////////////////////////////////////////
-//   CONSTRUCTION/DESTRUCTION
-////////////////////////////////////////////
 
 InGameOptions::InGameOptions():
-    CUI_Wnd(ClientUI::String("INGAMEOPTIONS_WINDOW_TITLE"),(GG::App::GetApp()->AppWidth()/2) - 55,
-    (GG::App::GetApp()->AppHeight()/2) - 120,135,240,
-    GG::Wnd::CLICKABLE | GG::Wnd::DRAGABLE | GG::Wnd::MODAL),
-    m_end_with_done(false)
+    CUI_Wnd(ClientUI::String("INGAMEOPTIONS_WINDOW_TITLE"), (GG::App::GetApp()->AppWidth() / 2) - 55,
+            (GG::App::GetApp()->AppHeight() / 2) - 120, 135, 240, GG::Wnd::CLICKABLE | GG::Wnd::DRAGABLE | GG::Wnd::MODAL)
 {
-
     m_save_btn = new CUIButton(30,40,75,ClientUI::String("INGAMEOPTIONS_SAVE"));
     m_load_btn = new CUIButton(30,80,75,ClientUI::String("INGAMEOPTIONS_LOAD"));
     m_quit_btn = new CUIButton(30,120,75,ClientUI::String("INGAMEOPTIONS_QUIT"));
     m_done_btn = new CUIButton(30,180,75,ClientUI::String("DONE"));
 
-    Init();    //attaches children and connects signals to slots
-}//InGameOptions()
+    Init(); //attaches children and connects signals to slots
+}
+
+InGameOptions::~InGameOptions()
+{
+}
+
+int InGameOptions::Render()
+{
+    CUI_Wnd::Render();
+    return 1;
+}
+
+int InGameOptions::Keypress (GG::Key key, Uint32 key_mods)
+{
+    if ((key == GG::GGK_RETURN) || (key == GG::GGK_ESCAPE)) {
+        Done(); // Same behaviour as if "done" was pressed
+    }
+    return 1;
+}
 
 void InGameOptions::Init()
 {
@@ -44,69 +65,54 @@ void InGameOptions::Init()
     AttachChild(m_done_btn);
 
     //attach signals
-    GG::Connect(m_save_btn->ClickedSignal(), &InGameOptions::OnSave, this);
-    GG::Connect(m_load_btn->ClickedSignal(), &InGameOptions::OnLoad, this);
-    GG::Connect(m_quit_btn->ClickedSignal(), &InGameOptions::OnQuit, this);
-    GG::Connect(m_done_btn->ClickedSignal(), &InGameOptions::OnDone, this);
+    GG::Connect(m_save_btn->ClickedSignal(), &InGameOptions::Save, this);
+    GG::Connect(m_load_btn->ClickedSignal(), &InGameOptions::Load, this);
+    GG::Connect(m_quit_btn->ClickedSignal(), &InGameOptions::Quit, this);
+    GG::Connect(m_done_btn->ClickedSignal(), &InGameOptions::Done, this);
 
-    m_quit = false;
-
-}//Init()
-
-InGameOptions::~InGameOptions()
-{
-
-}//~InGameOptions
-
-///////////////////////////////////////////////
-//   MUTATORS
-///////////////////////////////////////////////
-
-int InGameOptions::Render()
-{
-    CUI_Wnd::Render();
-
-    return true;
-}//Render()
-
-int InGameOptions::Keypress (GG::Key key, Uint32 key_mods)
-{
-    if ((key == GG::GGK_RETURN) || (key == GG::GGK_ESCAPE)) // Same behaviour as if "done" was pressed
-    {
-      OnDone();
+    if (!HumanClientApp::GetApp()->SinglePlayerGame()) {
+        if (HumanClientApp::GetApp()->PlayerID() != NetworkCore::HOST_PLAYER_ID)
+            m_save_btn->Disable();
+        m_load_btn->Disable();
     }
-    return 1;
-}//Keypress()
+}
 
-///////////////////////////////////////////////
-//   ACCESSORS
-///////////////////////////////////////////////
-
-///////////////////////////////////////////////
-//   EVENT HANDLERS
-///////////////////////////////////////////////
-
-void InGameOptions::OnSave()
+void InGameOptions::Save()
 {
+    std::vector<std::pair<std::string, std::string> > save_file_types;
+    save_file_types.push_back(std::pair<std::string, std::string>(ClientUI::String("INGAMEOPTIONS_SAVE_FILES"), "*.sav"));
 
-    // TODO: implement saving of game
+    GG::FileDlg dlg(GetOptionsDB().Get<std::string>("save-directory"), "", true, false, save_file_types, 
+                    ClientUI::FONT, ClientUI::PTS, ClientUI::WND_COLOR, ClientUI::WND_OUTER_BORDER_COLOR, ClientUI::TEXT_COLOR);
+    dlg.Run();
+    std::string filename;
+    if (!dlg.Result().empty()) {
+        filename = *dlg.Result().begin();
+        if (filename.find(".sav") != filename.size() - 4)
+            filename += ".sav";
 
-}//OnSave()
+        Message response;
+        bool save_failed = HumanClientApp::GetApp()->NetworkCore().SendSynchronousMessage(HostSaveGameMessage(HumanClientApp::GetApp()->PlayerID(), filename), response);
+        if (save_failed) {
+            ClientUI::MessageBox("Could not save game as \"" + filename + "\".");
+        } else {
+            CloseClicked();
+        }
+    }
+}
 
-void InGameOptions::OnLoad()
+void InGameOptions::Load()
 {
+    if (HumanClientApp::GetApp()->LoadSinglePlayerGame())
+        CloseClicked();
+}
 
-    // TODO: implement loading of game
-
-}//OnLoad()
-
-void InGameOptions::OnQuit()
+void InGameOptions::Quit()
 {
     GG::App::GetApp()->Exit(0);
-}//OnQuit()
+}
 
-void InGameOptions::OnDone()
+void InGameOptions::Done()
 {
     m_done = true;
-}//OnDone()
-
+}
--- UI/InGameOptions.h
+++ UI/InGameOptions.h
@@ -1,90 +1,38 @@
+// -*- C++ -*-
 //InGameOptions.h
-
 #ifndef _InGameOptions_h_
 #define _InGameOptions_h_
-
-#ifndef _GGWnd_h_
-#include "GGWnd.h"
-#endif
-
-#ifndef _GGEdit_h_
-#include "GGEdit.h"
-#endif
-
-#ifndef _GGButton_h_
-#include "GGButton.h"
-#endif
-
-#ifndef __GGSpin_h_
-#include "GGSpin.h"
-#endif
-
-#ifndef __GGMultiEdit_h_
-#include "GGMultiEdit.h"
-#endif
-
-#ifndef _GGStaticGraphic_h_
-#include "GGStaticGraphic.h"
-#endif
 
 #ifndef _CUI_Wnd_h_
 #include "CUI_Wnd.h"
 #endif
 
+class CUIButton;
+
 class InGameOptions : public CUI_Wnd
 {
-private:
-    
 public:
-//! \name Structors
-//!@{
-    InGameOptions();   //!< default ctor
-    ~InGameOptions(); //!< default dtor
+    /** \name Structors */ //@{
+    InGameOptions();  //!< default ctor
+    ~InGameOptions(); //!< dtor
+    //@}
 
-//!@}
-
-//! \name Mutators
-//!@{
-    virtual int Render();    //!< drawing code
+    /** \name Mutators */ //@{
+    virtual int Render();
     virtual int Keypress (GG::Key key, Uint32 key_mods);
-    
-//!@}
-
-public:
-//! \name Accessors
-//!@{
-
-//!@}
-
-public:
-//! \name Event Handlers
-//!@{
-    void OnSave();        //!< when m_save_btn button is pressed
-    void OnLoad();        //!< when m_load_btn button is pressed
-    void OnQuit();        //!< when m_quit_btn button is pressed
-    void OnDone();        //!< when m_done_btn is pressed
-
-//!@}
-
-public:
-//! \name Data Members
-//!@{
-       bool m_end_with_done;    //!< determines whether or not we ended the dialog with OK or not
-       bool m_quit;		//!< If true, then quit the game
-//!@}
-private:
-//! \name Controls
-//!@{
-
-    GG::Button*           m_save_btn;     //!< Save game button
-    GG::Button*           m_load_btn;     //!< Load game button
-    GG::Button*           m_done_btn;     //!< Done button
-    GG::Button*           m_quit_btn;	  //!< Quit game button
-
-//!@}
+    //@}
 
 private:
-    void Init();    //!< Attaches children and connects signals
+    void Init();          //!< Attaches children and connects signals
+    void Save();        //!< when m_save_btn button is pressed
+    void Load();        //!< when m_load_btn button is pressed
+    void Quit();        //!< when m_quit_btn button is pressed
+    void Done();        //!< when m_done_btn is pressed
 
-};//InGameOptions
+    CUIButton*  m_save_btn;   //!< Save game button
+    CUIButton*  m_load_btn;   //!< Load game button
+    CUIButton*  m_done_btn;   //!< Done button
+    CUIButton*  m_quit_btn;	  //!< Quit game button
+};
+
 #endif
