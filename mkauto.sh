121a1790d1b376cb693050a8fdd42a7854ae4a6f
Dennis Brakhane <dennis@dbd5520b-6a0a-0410-a553-f54834df5b05>
2004-01-29 17:15:01 +0000

    mkauto will now automatically upload the new file if SF_USERNAME is set
    svn path=/trunk/FreeOrion/; revision=570

--- mkauto.sh
+++ mkauto.sh
@@ -17,7 +17,7 @@ tar cvhf FO-autotools.tar			\
 tar f FO-autotools.tar --delete m4/CVS
 rm -f FO-autotools.tar.bz2
 bzip2 -9 FO-autotools.tar
-if [ x$SF_USERNAME != x -a x$1 = xU ]; then
+if [ x$SF_USERNAME != x -a x$1 != xNO_UPLOAD ]; then
   echo Uploading new version to Sourceforge
   scp FO-autotools.tar.bz2 $SF_USERNAME@freeorion.sourceforge.net:/home/groups/f/fr/freeorion/htdocs
 fi
