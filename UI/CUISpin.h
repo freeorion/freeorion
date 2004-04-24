6302c4a5757ab9dd83fab9f843da697c81f98f05
Zach Laine <whatwasthataddress@gmail.com>
2004-04-24 17:53:01 +0000

    Completed implementing this class.
    svn path=/trunk/FreeOrion/; revision=843

--- UI/CUISpin.h
+++ UI/CUISpin.h
@@ -16,16 +16,16 @@ template <class T> class CUISpin : public GG::Spin<T>
 public:
     /** \name Structors */ //@{
     CUISpin(int x, int y, int w, T value, T step, T min, T max, bool edits) :
-        Spin(x, y, w, value, step, min, max, edits, ClientUI::FONT, ClientUI::PTS, ClientUI::CTRL_BORDER_COLOR, 
-             ClientUI::TEXT_COLOR, GG::CLR_ZERO, new CUIArrowButton(0, 0, 1, 1, ClientUI::CTRL_BORDER_COLOR), 
-             new CUIArrowButton(0, 0, 1, 1, ClientUI::CTRL_BORDER_COLOR))
+        GG::Spin<T>(x, y, w, value, step, min, max, edits, ClientUI::FONT, ClientUI::PTS, ClientUI::CTRL_BORDER_COLOR, 
+                    ClientUI::TEXT_COLOR, GG::CLR_ZERO, new CUIArrowButton(0, 0, 1, 1, SHAPE_UP, ClientUI::DROP_DOWN_LIST_ARROW_COLOR), 
+                    new CUIArrowButton(0, 0, 1, 1, SHAPE_DOWN, ClientUI::DROP_DOWN_LIST_ARROW_COLOR))
     {
     }
 
     /** ctor that constructs an CUISpin object from an XMLElement. \throw std::invalid_argument May throw 
         std::invalid_argument if \a elem does not encode a CUISpin object*/
-    CUISpin(const XMLElement& elem) : 
-        Spin(elem.Child(Spin<T>::XMLTypeName()))
+    CUISpin(const GG::XMLElement& elem) : 
+        GG::Spin<T>(elem.Child(Spin<T>::XMLTypeName()))
     {
         if (elem.Tag() != XMLTypeName())
             throw std::invalid_argument("Attempted to construct a " + XMLTypeName() + " from an XMLElement that had a tag other than \"" + XMLTypeName() + "\"");
@@ -37,7 +37,7 @@ public:
     virtual GG::XMLElement XMLEncode() const
     {
         GG::XMLElement retval(XMLTypeName());
-        retval.AppendChild(Spin::XMLEncode());
+        retval.AppendChild(GG::Spin<T>::XMLEncode());
         return retval;
     }
     //@}
@@ -45,22 +45,37 @@ public:
     /** \name Mutators */ //@{
     virtual bool Render()
     {
-        Clr color_to_use = Disabled() ? DisabledColor(Color()) : Color();
-        Clr int_color_to_use = Disabled() ? DisabledColor(InteriorColor()) : InteriorColor();
-        Pt ul = UpperLeft(), lr = LowerRight();
+        GG::Clr color_to_use = Disabled() ? DisabledColor(Color()) : Color();
+        GG::Clr int_color_to_use = Disabled() ? DisabledColor(InteriorColor()) : InteriorColor();
+        GG::Pt ul = UpperLeft(), lr = LowerRight();
         FlatRectangle(ul.x, ul.y, lr.x, lr.y, int_color_to_use, color_to_use, 1);
-        if (!m_editable) {
-            m_edit->OffsetMove(UpperLeft());
-            m_edit->Render();
-            m_edit->OffsetMove(-UpperLeft());
+        if (!EditsAllowed()) {
+            GetEdit()->OffsetMove(UpperLeft());
+            GetEdit()->Render();
+            GetEdit()->OffsetMove(-UpperLeft());
         }
-        m_up_bn->OffsetMove(UpperLeft());
-        m_dn_bn->OffsetMove(UpperLeft());
-        m_up_bn->Render();
-        m_dn_bn->Render();
-        m_up_bn->OffsetMove(-UpperLeft());
-        m_dn_bn->OffsetMove(-UpperLeft());
+        UpButton()->OffsetMove(UpperLeft());
+        DownButton()->OffsetMove(UpperLeft());
+        UpButton()->Render();
+        DownButton()->Render();
+        UpButton()->OffsetMove(-UpperLeft());
+        DownButton()->OffsetMove(-UpperLeft());
         return true;
+    }
+
+    virtual void SizeMove(int x1, int y1, int x2, int y2)
+    {
+        Wnd::SizeMove(x1, y1, x2, y2);
+        const int BORDER_THICK = 1;
+        const int BUTTON_MARGIN = 3;
+        const int BN_HEIGHT = (Height() - 3 * BUTTON_MARGIN) / 2;
+        const int BN_WIDTH = BN_HEIGHT;
+        const int BN_X_POS = Width() - BN_WIDTH - BORDER_THICK;
+        GetEdit()->SizeMove(0, 0, BN_X_POS, Height());
+        UpButton()->SizeMove(BN_X_POS, BORDER_THICK,
+                             BN_X_POS + BN_WIDTH, BUTTON_MARGIN + BN_HEIGHT);
+        DownButton()->SizeMove(BN_X_POS, BUTTON_MARGIN + BN_HEIGHT + BUTTON_MARGIN,
+                               BN_X_POS + BN_WIDTH, BUTTON_MARGIN + BN_HEIGHT + BUTTON_MARGIN + BN_HEIGHT);
     }
     //@}
 
