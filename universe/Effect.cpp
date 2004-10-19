02a4ec24a4c8dc5d66d4413db75da095b4c2ca62
Zach Laine <whatwasthataddress@gmail.com>
2004-10-19 05:04:37 +0000

    Initial add.
    svn path=/trunk/FreeOrion/; revision=1186

---
+++ universe/Effect.cpp
@@ -0,0 +1,424 @@
+#include "Effect.h"
+
+#include "../util/Parse.h"
+
+using namespace Effect;
+
+namespace {
+    Effect::EffectBase* NewSetMeter(const GG::XMLElement& elem)             {return new Effect::SetMeter(elem);}
+    //Effect::EffectBase* NewSetEmpireStockpile(const GG::XMLElement& elem) {return new Effect::SetEmpireStockpile(elem);}
+    Effect::EffectBase* NewSetPlanetType(const GG::XMLElement& elem)        {return new Effect::SetPlanetType(elem);}
+    Effect::EffectBase* NewSetPlanetSize(const GG::XMLElement& elem)        {return new Effect::SetPlanetSize(elem);}
+    Effect::EffectBase* NewAddOwner(const GG::XMLElement& elem)             {return new Effect::AddOwner(elem);}
+    Effect::EffectBase* NewRemoveOwner(const GG::XMLElement& elem)          {return new Effect::RemoveOwner(elem);}
+    //Effect::EffectBase* NewCreate(const GG::XMLElement& elem)             {return new Effect::Create(elem);}
+    Effect::EffectBase* NewDestroy(const GG::XMLElement& elem)              {return new Effect::Destroy(elem);}
+    Effect::EffectBase* NewAddSpecial(const GG::XMLElement& elem)           {return new Effect::AddSpecial(elem);}
+    Effect::EffectBase* NewRemoveSpecial(const GG::XMLElement& elem)        {return new Effect::RemoveSpecial(elem);}
+    Effect::EffectBase* NewSetStarType(const GG::XMLElement& elem)          {return new Effect::SetStarType(elem);}
+    Effect::EffectBase* NewSetAvailability(const GG::XMLElement& elem)      {return new Effect::SetAvailability(elem);}
+    Effect::EffectBase* NewSetEffectTarget(const GG::XMLElement& elem)      {return new Effect::SetEffectTarget(elem);}
+}
+
+GG::XMLObjectFactory<Effect::EffectBase> Effect::EffectFactory()
+{
+    static GG::XMLObjectFactory<Effect::EffectBase> factory;
+    static bool init = false;
+    if (!init) {
+        factory.AddGenerator("Effect::SetMeter", &NewSetMeter);
+        //factory.AddGenerator("Effect::SetEmpireStockpile", &NewSetEmpireStockpile);
+        factory.AddGenerator("Effect::SetPlanetType", &NewSetPlanetType);
+        factory.AddGenerator("Effect::SetPlanetSize", &NewSetPlanetSize);
+        factory.AddGenerator("Effect::AddOwner", &NewAddOwner);
+        factory.AddGenerator("Effect::RemoveOwner", &NewRemoveOwner);
+        //factory.AddGenerator("Effect::Create", &NewCreate);
+        factory.AddGenerator("Effect::Destroy", &NewDestroy);
+        factory.AddGenerator("Effect::AddSpecial", &NewAddSpecial);
+        factory.AddGenerator("Effect::RemoveSpecial", &NewRemoveSpecial);
+        factory.AddGenerator("Effect::SetAvailability", &NewSetAvailability);
+        factory.AddGenerator("Effect::SetEffectTarget", &NewSetEffectTarget);
+        init = true;
+    }
+    return factory;
+}
+
+///////////////////////////////////////////////////////////
+// EffectsGroup                                          //
+///////////////////////////////////////////////////////////
+EffectsGroup::EffectsGroup(const Condition::ConditionBase* scope, const Condition::ConditionBase* activation, const std::vector<EffectBase*>& effects) :
+    m_scope(scope),
+    m_activation(activation),
+    m_effects(effects)
+{
+}
+
+EffectsGroup::EffectsGroup(const GG::XMLElement& elem)
+{
+    m_scope = Condition::ConditionFactory().GenerateObject(elem.Child("m_scope"));
+    m_activation = Condition::ConditionFactory().GenerateObject(elem.Child("m_activation"));
+    for (GG::XMLElement::const_child_iterator it = elem.Child("effects").child_begin(); it != elem.Child("effects").child_end(); ++it) {
+        m_effects.push_back(EffectFactory().GenerateObject(*it));
+    }
+}
+
+EffectsGroup::~EffectsGroup()
+{
+    delete m_scope;
+    delete m_activation;
+    for (unsigned int i = 0; i < m_effects.size(); ++i) {
+        delete m_effects[i];
+    }
+}
+
+void EffectsGroup::Execute(int source_id) const
+{
+    Universe& universe = GetUniverse();
+    UniverseObject* source = universe.Object(source_id);
+
+    // evaluate the activation condition only on the source object
+    Condition::ConditionBase::ObjectSet condition_targets;
+    Condition::ConditionBase::ObjectSet condition_non_targets;
+    condition_non_targets.insert(source);
+    m_activation->Eval(source, condition_targets, condition_non_targets);
+
+    // if the activation condition did not evaluate to true for the source object, do nothing
+    if (condition_targets.empty())
+        return;
+
+    // evaluate the scope condition
+    condition_targets.clear();
+    condition_non_targets.clear();
+    for (Universe::const_iterator it = universe.begin(); it != universe.end(); ++it) {
+        condition_non_targets.insert(it->second);
+    }
+    m_scope->Eval(source, condition_targets, condition_non_targets);
+
+    // execute effects on targets in scope
+    for (Condition::ConditionBase::ObjectSet::const_iterator it = condition_targets.begin(); it != condition_targets.end(); ++it) {
+        for (unsigned int i = 0; i < m_effects.size(); ++i) {
+            m_effects[i]->Execute(source, *it);
+        }
+    }
+}
+
+
+///////////////////////////////////////////////////////////
+// EffectBase                                            //
+///////////////////////////////////////////////////////////
+EffectBase::~EffectBase()
+{
+}
+
+
+///////////////////////////////////////////////////////////
+// SetMeter                                              //
+///////////////////////////////////////////////////////////
+SetMeter::SetMeter(MeterType meter, const ValueRef::ValueRefBase<double>* value, bool max) :
+    m_meter(meter),
+    m_value(value),
+    m_max(max)
+{
+}
+
+SetMeter::SetMeter(const GG::XMLElement& elem)
+{
+    if (elem.Tag() != "Effect::SetMeter")
+        throw std::invalid_argument("Attempted to construct a Effect::SetMeter from an XMLElement that had a tag other than \"Effect::SetMeter\"");
+
+    m_meter = boost::lexical_cast<MeterType>(elem.Child("meter").Text());
+    m_value = ParseArithmeticExpression<double>(elem.Child("value").Text());
+    m_max = boost::lexical_cast<bool>(elem.Child("max").Text());
+}
+
+SetMeter::~SetMeter()
+{
+    delete m_value;
+}
+
+void SetMeter::Execute(const UniverseObject* source, UniverseObject* target) const
+{
+    if (Meter* m = target->GetMeter(m_meter)) {
+        double val = m_value->Eval(source, target);
+        m_max ? m->SetMax(val) : m->SetCurrent(val);
+    }
+}
+
+
+///////////////////////////////////////////////////////////
+// SetPlanetType                                         //
+///////////////////////////////////////////////////////////
+SetPlanetType::SetPlanetType(const ValueRef::ValueRefBase<PlanetType>* type) :
+    m_type(type)
+{
+}
+
+SetPlanetType::SetPlanetType(const GG::XMLElement& elem)
+{
+    if (elem.Tag() != "Effect::SetPlanetType")
+        throw std::invalid_argument("Attempted to construct a Effect::SetPlanetType from an XMLElement that had a tag other than \"Effect::SetPlanetType\"");
+
+    m_type = ParseArithmeticExpression<PlanetType>(elem.Text());
+}
+
+SetPlanetType::~SetPlanetType()
+{
+    delete m_type;
+}
+
+void SetPlanetType::Execute(const UniverseObject* source, UniverseObject* target) const
+{
+    if (Planet* p = dynamic_cast<Planet*>(target)) {
+	p->SetType(m_type->Eval(source, target));
+    }
+}
+
+
+///////////////////////////////////////////////////////////
+// SetPlanetSize                                         //
+///////////////////////////////////////////////////////////
+SetPlanetSize::SetPlanetSize(const ValueRef::ValueRefBase<PlanetSize>* size) :
+    m_size(size)
+{
+}
+
+SetPlanetSize::SetPlanetSize(const GG::XMLElement& elem)
+{
+    if (elem.Tag() != "Effect::SetPlanetSize")
+        throw std::invalid_argument("Attempted to construct a Effect::SetPlanetSize from an XMLElement that had a tag other than \"Effect::SetPlanetSize\"");
+
+    m_size = ParseArithmeticExpression<PlanetSize>(elem.Text());
+}
+
+SetPlanetSize::~SetPlanetSize()
+{
+    delete m_size;
+}
+
+void SetPlanetSize::Execute(const UniverseObject* source, UniverseObject* target) const
+{
+    if (Planet* p = dynamic_cast<Planet*>(target)) {
+	p->SetSize(m_size->Eval(source, target));
+    }
+}
+
+
+///////////////////////////////////////////////////////////
+// AddOwner                                              //
+///////////////////////////////////////////////////////////
+AddOwner::AddOwner(const ValueRef::ValueRefBase<int>* empire_id) :
+    m_empire_id(empire_id)
+{
+}
+
+AddOwner::AddOwner(const GG::XMLElement& elem)
+{
+    if (elem.Tag() != "Effect::AddOwner")
+        throw std::invalid_argument("Attempted to construct a Effect::AddOwner from an XMLElement that had a tag other than \"Effect::AddOwner\"");
+
+    m_empire_id = ParseArithmeticExpression<int>(elem.Text());
+}
+
+AddOwner::~AddOwner()
+{
+    delete m_empire_id;
+}
+
+void AddOwner::Execute(const UniverseObject* source, UniverseObject* target) const
+{
+    // TODO : verify that such an empire exists
+    target->AddOwner(m_empire_id->Eval(source, target));
+}
+
+
+///////////////////////////////////////////////////////////
+// RemoveOwner                                           //
+///////////////////////////////////////////////////////////
+RemoveOwner::RemoveOwner(const ValueRef::ValueRefBase<int>* empire_id) :
+    m_empire_id(empire_id)
+{
+}
+
+RemoveOwner::RemoveOwner(const GG::XMLElement& elem)
+{
+    if (elem.Tag() != "Effect::RemoveOwner")
+        throw std::invalid_argument("Attempted to construct a Effect::RemoveOwner from an XMLElement that had a tag other than \"Effect::RemoveOwner\"");
+
+    m_empire_id = ParseArithmeticExpression<int>(elem.Text());
+}
+
+RemoveOwner::~RemoveOwner()
+{
+    delete m_empire_id;
+}
+
+void RemoveOwner::Execute(const UniverseObject* source, UniverseObject* target) const
+{
+    // TODO : verify that such an empire exists
+    target->RemoveOwner(m_empire_id->Eval(source, target));
+}
+
+
+///////////////////////////////////////////////////////////
+// Create                                                //
+///////////////////////////////////////////////////////////
+// TODO: create multiple Create*s for different kinds of objects
+/*class Effect::Create : public Effect::EffectBase
+{
+public:
+    Create();
+    Create(const GG::XMLElement& elem);
+
+    virtual void Execute(const UniverseObject* source, UniverseObject* target) const;
+};*/
+
+
+///////////////////////////////////////////////////////////
+// Destroy                                               //
+///////////////////////////////////////////////////////////
+Destroy::Destroy()
+{
+}
+
+Destroy::Destroy(const GG::XMLElement& elem)
+{
+    if (elem.Tag() != "Effect::Destroy")
+        throw std::invalid_argument("Attempted to construct a Effect::Destroy from an XMLElement that had a tag other than \"Effect::Destroy\"");
+}
+
+void Destroy::Execute(const UniverseObject* source, UniverseObject* target) const
+{
+    GetUniverse().Delete(target->ID());
+}
+
+
+///////////////////////////////////////////////////////////
+// AddSpecial                                            //
+///////////////////////////////////////////////////////////
+AddSpecial::AddSpecial(const std::string& name) :
+    m_name(name)
+{
+}
+
+AddSpecial::AddSpecial(const GG::XMLElement& elem)
+{
+    if (elem.Tag() != "Effect::AddSpecial")
+        throw std::invalid_argument("Attempted to construct a Effect::AddSpecial from an XMLElement that had a tag other than \"Effect::AddSpecial\"");
+
+    m_name = elem.Text();
+}
+
+void AddSpecial::Execute(const UniverseObject* source, UniverseObject* target) const
+{
+    // TODO : implement after Specials are implemented
+}
+
+
+///////////////////////////////////////////////////////////
+// RemoveSpecial                                         //
+///////////////////////////////////////////////////////////
+RemoveSpecial::RemoveSpecial(const std::string& name) :
+    m_name(name)
+{
+}
+
+RemoveSpecial::RemoveSpecial(const GG::XMLElement& elem)
+{
+    if (elem.Tag() != "Effect::RemoveSpecial")
+        throw std::invalid_argument("Attempted to construct a Effect::RemoveSpecial from an XMLElement that had a tag other than \"Effect::RemoveSpecial\"");
+
+    m_name = elem.Text();
+}
+
+void RemoveSpecial::Execute(const UniverseObject* source, UniverseObject* target) const
+{
+    // TODO : implement after Specials are implemented
+}
+
+
+///////////////////////////////////////////////////////////
+// SetStarType                                           //
+///////////////////////////////////////////////////////////
+SetStarType::SetStarType(const ValueRef::ValueRefBase<StarType>* type) :
+    m_type(type)
+{
+}
+
+SetStarType::SetStarType(const GG::XMLElement& elem)
+{
+    if (elem.Tag() != "Effect::SetStarType")
+        throw std::invalid_argument("Attempted to construct a Effect::SetStarType from an XMLElement that had a tag other than \"Effect::SetStarType\"");
+
+    m_type = ParseArithmeticExpression<StarType>(elem.Text());
+}
+
+SetStarType::~SetStarType()
+{
+    delete m_type;
+}
+
+void SetStarType::Execute(const UniverseObject* source, UniverseObject* target) const
+{
+    if (System* s = dynamic_cast<System*>(target)) {
+	s->SetStarType(m_type->Eval(source, target));
+    }
+}
+
+
+///////////////////////////////////////////////////////////
+// SetAvailability                                           //
+///////////////////////////////////////////////////////////
+SetAvailability::SetAvailability(const std::string& tech_name, const ValueRef::ValueRefBase<int>* empire_id, bool available, bool grant_tech) :
+    m_tech_name(tech_name),
+    m_empire_id(empire_id),
+    m_available(available),
+    m_grant_tech(grant_tech)
+{
+}
+
+SetAvailability::SetAvailability(const GG::XMLElement& elem)
+{
+    if (elem.Tag() != "Effect::SetAvailability")
+        throw std::invalid_argument("Attempted to construct a Effect::SetAvailability from an XMLElement that had a tag other than \"Effect::SetAvailability\"");
+
+    m_tech_name = elem.Child("tech_name").Text();
+    m_empire_id = ParseArithmeticExpression<int>(elem.Child("empire_id").Text());
+    m_available = boost::lexical_cast<bool>(elem.Child("available").Text());
+    m_grant_tech = boost::lexical_cast<bool>(elem.Child("grant_tech").Text());
+}
+
+SetAvailability::~SetAvailability()
+{
+    delete m_empire_id;
+}
+
+void SetAvailability::Execute(const UniverseObject* source, UniverseObject* target) const
+{
+    // TODO: implement after availability is implemented
+}
+
+
+///////////////////////////////////////////////////////////
+// SetEffectTarget                                       //
+///////////////////////////////////////////////////////////
+SetEffectTarget::SetEffectTarget(const ValueRef::ValueRefBase<int>* effect_target_id) :
+    m_effect_target_id(effect_target_id)
+{
+}
+
+SetEffectTarget::SetEffectTarget(const GG::XMLElement& elem)
+{
+    if (elem.Tag() != "Effect::SetEffectTarget")
+        throw std::invalid_argument("Attempted to construct a Effect::SetEffectTarget from an XMLElement that had a tag other than \"Effect::SetEffectTarget\"");
+
+    m_effect_target_id = ParseArithmeticExpression<int>(elem.Child("effect_target_id").Text());
+}
+
+SetEffectTarget::~SetEffectTarget()
+{
+    delete m_effect_target_id;
+}
+
+void SetEffectTarget::Execute(const UniverseObject* source, UniverseObject* target) const
+{
+    // TODO: implement after Effect targets are implemented
+}
---
+++ universe/Effect.h
@@ -0,0 +1,230 @@
+// -*- C++ -*-
+#ifndef _Effect_h_
+#define _Effect_h_
+
+#include "Condition.h"
+
+namespace Effect {
+    class EffectsGroup;
+    class EffectBase;
+    class SetMeter;
+    class SetEmpireStockpile;
+    class SetEmpireStockpile;
+    class SetPlanetType;
+    class SetPlanetSize;
+    class AddOwner;
+    class RemoveOwner;
+    //class Create;
+    class Destroy;
+    class AddSpecial;
+    class RemoveSpecial;
+    class SetStarType;
+    class SetAvailability;
+    class SetEffectTarget;
+    GG::XMLObjectFactory<EffectBase> EffectFactory(); ///< an XML factory that creates the right subclass of EffectBase from a given XML element
+}
+
+class Effect::EffectsGroup
+{
+public:
+    EffectsGroup(const Condition::ConditionBase* scope, const Condition::ConditionBase* activation, const std::vector<EffectBase*>& effects);
+    EffectsGroup(const GG::XMLElement& elem);
+    virtual ~EffectsGroup();
+
+    virtual void Execute(int source_id) const;
+
+protected:
+    const Condition::ConditionBase* m_scope;
+    const Condition::ConditionBase* m_activation;
+    std::vector<EffectBase*>        m_effects;
+};
+
+class Effect::EffectBase
+{
+public:
+    virtual ~EffectBase();
+
+    virtual void Execute(const UniverseObject* source, UniverseObject* target) const = 0;
+};
+
+/** Sets the meter of the given kind to \a value.  The max value of the meter is set if \a max == true; otherwise the current value of the
+    meter is set.  If the target of the Effect has no meters, nothing is done. */
+class Effect::SetMeter : public Effect::EffectBase
+{
+public:
+    SetMeter(MeterType meter, const ValueRef::ValueRefBase<double>* value, bool max);
+    SetMeter(const GG::XMLElement& elem);
+    virtual ~SetMeter();
+
+    virtual void Execute(const UniverseObject* source, UniverseObject* target) const;
+
+private:
+    MeterType                             m_meter;
+    const ValueRef::ValueRefBase<double>* m_value;
+    bool                                  m_max;
+};
+
+/*class Effect::SetEmpireStockpile : public Effect::EffectBase
+{
+public:
+    SetEmpireStockpile(StockpileType stockpile, const ValueRef::ValueRefBase<int>* empire_id, const ValueRef::ValueRefBase<double>* amount);
+    SetEmpireStockpile(const GG::XMLElement& elem);
+    virtual ~SetEmpireStockpile();
+
+    virtual void Execute(const UniverseObject* source, UniverseObject* target) const;
+
+private:
+StockpileType                         m_stockpile;
+    const ValueRef::ValueRefBase<int>*    m_empire_id;
+    const ValueRef::ValueRefBase<double>* m_amount;
+};*/
+
+/** Sets the planet type of the target to \a type.  This has no effect on non-Planet targets. */
+class Effect::SetPlanetType : public Effect::EffectBase
+{
+public:
+    SetPlanetType(const ValueRef::ValueRefBase<PlanetType>* type);
+    SetPlanetType(const GG::XMLElement& elem);
+    virtual ~SetPlanetType();
+
+    virtual void Execute(const UniverseObject* source, UniverseObject* target) const;
+
+private:
+    const ValueRef::ValueRefBase<PlanetType>* m_type;
+};
+
+/** Sets the planet size of the target to \a size.  This has no effect on non-Planet targets. */
+class Effect::SetPlanetSize : public Effect::EffectBase
+{
+public:
+    SetPlanetSize(const ValueRef::ValueRefBase<PlanetSize>* size);
+    SetPlanetSize(const GG::XMLElement& elem);
+    virtual ~SetPlanetSize();
+
+    virtual void Execute(const UniverseObject* source, UniverseObject* target) const;
+
+private:
+    const ValueRef::ValueRefBase<PlanetSize>* m_size;
+};
+
+/** Adds empire \a empire_id as an owner of the target.  This has no effect if \a empire_id was already an owner of the target object. */
+class Effect::AddOwner : public Effect::EffectBase
+{
+public:
+    AddOwner(const ValueRef::ValueRefBase<int>* empire_id);
+    AddOwner(const GG::XMLElement& elem);
+    virtual ~AddOwner();
+
+    virtual void Execute(const UniverseObject* source, UniverseObject* target) const;
+
+private:
+    const ValueRef::ValueRefBase<int>* m_empire_id;
+};
+
+/** Removes empire \a empire_id as an owner of the target.  This has no effect if \a empire_id was not already an owner of the target object. */
+class Effect::RemoveOwner : public Effect::EffectBase
+{
+public:
+    RemoveOwner(const ValueRef::ValueRefBase<int>* empire_id);
+    RemoveOwner(const GG::XMLElement& elem);
+    virtual ~RemoveOwner();
+
+    virtual void Execute(const UniverseObject* source, UniverseObject* target) const;
+
+private:
+    const ValueRef::ValueRefBase<int>* m_empire_id;
+};
+
+// TODO: create multiple Create*s for different kinds of objects
+/*class Effect::Create : public Effect::EffectBase
+{
+public:
+    Create();
+    Create(const GG::XMLElement& elem);
+
+    virtual void Execute(const UniverseObject* source, UniverseObject* target) const;
+};*/
+
+/** Destroys the target object. */
+class Effect::Destroy : public Effect::EffectBase
+{
+public:
+    Destroy();
+    Destroy(const GG::XMLElement& elem);
+
+    virtual void Execute(const UniverseObject* source, UniverseObject* target) const;
+};
+
+/** Adds the Special with the name \a name to the target object. */
+class Effect::AddSpecial : public Effect::EffectBase
+{
+public:
+    AddSpecial(const std::string& name);
+    AddSpecial(const GG::XMLElement& elem);
+
+    virtual void Execute(const UniverseObject* source, UniverseObject* target) const;
+
+private:
+    std::string m_name;
+};
+
+/** Removes the Special with the name \a name to the target object.  This has no effect if no such Special was already attached to the target object. */
+class Effect::RemoveSpecial : public Effect::EffectBase
+{
+public:
+    RemoveSpecial(const std::string& name);
+    RemoveSpecial(const GG::XMLElement& elem);
+
+    virtual void Execute(const UniverseObject* source, UniverseObject* target) const;
+
+private:
+    std::string m_name;
+};
+
+/** Sets the star type of the target to \a type.  This has no effect on non-System targets. */
+class Effect::SetStarType : public Effect::EffectBase
+{
+public:
+    SetStarType(const ValueRef::ValueRefBase<StarType>* type);
+    SetStarType(const GG::XMLElement& elem);
+    virtual ~SetStarType();
+
+    virtual void Execute(const UniverseObject* source, UniverseObject* target) const;
+
+private:
+    const ValueRef::ValueRefBase<StarType>* m_type;
+};
+
+/** Sets the availability of tech \a tech_name to empire \a empire_id.  If \a grant_tech is true, the tech is fully available, just as if it were
+    researched normally; otherwise, only the items that the tech grants are made available.  Note that this means this Effect is intended also to
+    be used to unlock buildings, ships, etc.  The tech and/or its items are made available if \a available is true, or unavailable otherwise. */
+class Effect::SetAvailability : public Effect::EffectBase
+{
+public:
+    SetAvailability(const std::string& tech_name, const ValueRef::ValueRefBase<int>* empire_id, bool available, bool grant_tech);
+    SetAvailability(const GG::XMLElement& elem);
+    virtual ~SetAvailability();
+
+    virtual void Execute(const UniverseObject* source, UniverseObject* target) const;
+
+private:
+    std::string                        m_tech_name;
+    const ValueRef::ValueRefBase<int>* m_empire_id;
+    bool                               m_available;
+    bool                               m_grant_tech;
+};
+
+class Effect::SetEffectTarget : public Effect::EffectBase
+{
+public:
+    SetEffectTarget(const ValueRef::ValueRefBase<int>* effect_target_id);
+    SetEffectTarget(const GG::XMLElement& elem);
+    virtual ~SetEffectTarget();
+
+    virtual void Execute(const UniverseObject* source, UniverseObject* target) const;
+
+private:
+    const ValueRef::ValueRefBase<int>* m_effect_target_id;
+};
+
+#endif // _Effect_h_
