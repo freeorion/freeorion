07364d67e345700776ceda9351b02e3d584e6c75
Zach Laine <whatwasthataddress@gmail.com>
2004-10-31 16:23:36 +0000

    Initial add.
    svn path=/trunk/FreeOrion/; revision=1234

---
+++ universe/ShipDesign.cpp
@@ -0,0 +1,69 @@
+#include "ShipDesign.h"
+
+#include "../util/AppInterface.h"
+#include "XMLDoc.h"
+
+#include <boost/lexical_cast.hpp>
+using boost::lexical_cast;
+
+////////////////////////////////////////////////
+// ShipDesign
+////////////////////////////////////////////////
+ShipDesign::ShipDesign() : 
+    empire(-1),
+    name(""),
+    description(""),
+    cost(10000000.0),
+    speed(1.0),
+    colonize(false),
+    attack(0),
+    defense(0)
+{
+}
+
+ShipDesign::ShipDesign(const GG::XMLElement& elem)
+{
+    if (elem.Tag() != "ShipDesign" )
+        throw std::invalid_argument("Attempted to construct a ShipDesign from an XMLElement that had a tag other than \"ShipDesign\"");
+
+    empire = lexical_cast<int>(elem.Child("empire").Text());
+    name = elem.Child("name").Text();
+    description = elem.Child("description").Text();
+    cost = lexical_cast<double>(elem.Child("cost").Text());
+    speed = lexical_cast<double>(elem.Child("speed").Text());
+    colonize = lexical_cast<bool>(elem.Child("colonize").Text());
+
+    /////////////////////////////////////////////////////////////////////////////
+    // V0.3 ONLY!!!!
+    attack = lexical_cast<int>(elem.Child("attack").Text());
+    defense = lexical_cast<int>(elem.Child("defense").Text());
+    // V0.3 ONLY!!!!
+    /////////////////////////////////////////////////////////////////////////////
+}
+
+GG::XMLElement ShipDesign::XMLEncode() const
+{
+    using GG::XMLElement;
+    using boost::lexical_cast;
+
+    XMLElement retval("ShipDesign");
+    retval.AppendChild(XMLElement("empire", lexical_cast<std::string>(empire)));
+    retval.AppendChild(XMLElement("name", name));
+    retval.AppendChild(XMLElement("description", description));
+    retval.AppendChild(XMLElement("cost", lexical_cast<std::string>(cost)));
+    retval.AppendChild(XMLElement("speed", lexical_cast<std::string>(speed)));
+    retval.AppendChild(XMLElement("colonize", lexical_cast<std::string>(colonize)));
+    /////////////////////////////////////////////////////////////////////////////
+    // V0.3 ONLY!!!!
+    retval.AppendChild(XMLElement("attack", lexical_cast<std::string>(attack)));
+    retval.AppendChild(XMLElement("defense", lexical_cast<std::string>(defense)));
+    // V0.3 ONLY!!!!
+    /////////////////////////////////////////////////////////////////////////////
+    return retval;
+}
+
+const ShipDesign* GetShipDesign(int empire_id, const std::string& name)
+{
+    Empire* empire = Empires().Lookup(empire_id);
+    return empire->GetShipDesign(name);
+}
---
+++ universe/ShipDesign.h
@@ -0,0 +1,40 @@
+// -*- C++ -*-
+#ifndef _ShipDesign_h_
+#define _ShipDesign_h_
+
+#include "XMLDoc.h"
+
+#include <string>
+
+/** a class representing a ship design */
+struct ShipDesign
+{
+    /** \name Structors */ //@{
+    ShipDesign(); ///< default ctor
+    ShipDesign(const GG::XMLElement& elem); ///< ctor that constructs a ShipDesign object from an XMLElement. \throw std::invalid_argument May throw std::invalid_argument if \a elem does not encode a ShipDesign object
+    //@}
+
+    int         empire;      ///< the empire that designed this ship
+    std::string name;        ///< the name of the design
+    std::string description; ///< short description of the design, will maybe last after v.2
+    double      cost;        ///< the cost of the design, in PP
+    double      speed;       ///< the number of map-units the ship can move in one turn
+    bool        colonize;    ///< whether or not the ship is capable of creating a new colony
+
+    /////////////////////////////////////////////////////////////////////////////
+    // V0.3 ONLY!!!!
+    int         attack;      ///< the attack value of the design
+    int         defense;     ///< the defense value of the design
+    // V0.3 ONLY!!!!
+    /////////////////////////////////////////////////////////////////////////////
+
+    /** \name Accessors */ //@{
+  	GG::XMLElement XMLEncode() const; ///< constructs an XMLElement from a ShipDesign object
+    //@}
+};
+
+const ShipDesign* GetShipDesign(int empire_id, const std::string& name);
+
+#endif // _ShipDesign_h_
+
+
