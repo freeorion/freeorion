1d61ae2f9a34ce8eae8982763b4fb49945029192
Zach Laine <whatwasthataddress@gmail.com>
2005-01-21 21:08:04 +0000

    Moved the CircleArc() function here from TechWnd.cpp for mor general use.
    svn path=/trunk/FreeOrion/; revision=1358

--- UI/CUIDrawUtil.cpp
+++ UI/CUIDrawUtil.cpp
@@ -310,3 +310,59 @@ bool InFleetMarker(const GG::Pt& pt, int x1, int y1, int x2, int y2, ShapeOrient
     FindIsoscelesTriangleVertices(x1, y1, x2, y2, orientation, x1_, y1_, x2_, y2_, x3_, y3_);
     return InTriangle(pt, x1_, y1_, x2_, y2_, x3_, y3_);
 }
+
+void CircleArc(int x1, int y1, int x2, int y2, double theta1, double theta2, bool filled_shape)
+{
+    int wd = x2 - x1, ht = y2 - y1;
+    double center_x = x1 + wd / 2.0;
+    double center_y = y1 + ht / 2.0;
+    double r = std::min(wd / 2.0, ht / 2.0);
+    const double PI = 3.141594;
+
+    // correct theta* values to range [0, 2pi)
+    if (theta1 < 0)
+        theta1 += (int(-theta1 / (2 * PI)) + 1) * 2 * PI;
+    else if (theta1 >= 2 * PI)
+        theta1 -= int(theta1 / (2 * PI)) * 2 * PI;
+    if (theta2 < 0)
+        theta2 += (int(-theta2 / (2 * PI)) + 1) * 2 * PI;
+    else if (theta2 >= 2 * PI)
+        theta2 -= int(theta2 / (2 * PI)) * 2 * PI;
+
+    const int      SLICES = std::min(3 + std::max(wd, ht), 50);  // this is a good guess at how much to tesselate the circle coordinates (50 segments max)
+    const double   HORZ_THETA = (2 * PI) / SLICES;
+
+    static std::map<int, std::vector<double> > unit_circle_coords;
+    std::vector<double>& unit_vertices = unit_circle_coords[SLICES];
+    bool calc_vertices = unit_vertices.size() == 0;
+    if (calc_vertices) {
+        unit_vertices.resize(2 * (SLICES + 1), 0.0);
+        double theta = 0.0f;
+        for (int j = 0; j <= SLICES; theta += HORZ_THETA, ++j) { // calculate x,y values for each point on a unit circle divided into SLICES arcs
+            unit_vertices[j*2] = std::cos(-theta);
+            unit_vertices[j*2+1] = std::sin(-theta);
+        }
+    }
+    int first_slice_idx = int(theta1 / HORZ_THETA + 1);
+    int last_slice_idx = int(theta2 / HORZ_THETA - 1);
+    if (theta1 >= theta2)
+        last_slice_idx += SLICES;
+
+    if (filled_shape) {
+        glBegin(GL_TRIANGLE_FAN);
+        glVertex2f(center_x, center_y);
+    }
+    // point on circle at angle theta1
+    double theta1_x = std::cos(-theta1), theta1_y = std::sin(-theta1);
+    glVertex2f(center_x + theta1_x * r, center_y + theta1_y * r);
+    // angles in between theta1 and theta2, if any
+    for (int i = first_slice_idx; i <= last_slice_idx; ++i) {
+        int X = (i > SLICES ? (i - SLICES) : i) * 2, Y = X + 1;
+        glVertex2f(center_x + unit_vertices[X] * r, center_y + unit_vertices[Y] * r);
+    }
+    // theta2
+    double theta2_x = std::cos(-theta2), theta2_y = std::sin(-theta2);
+    glVertex2f(center_x + theta2_x * r, center_y + theta2_y * r);
+    if (filled_shape)
+        glEnd();
+}
