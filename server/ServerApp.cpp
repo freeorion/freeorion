79effc05384f53670fd6a6a64aad3f4c3fde878d
Dennis Brakhane <dennis@dbd5520b-6a0a-0410-a553-f54834df5b05>
2006-01-04 11:49:40 +0000

    Added a note for those confused by the "SDL dummy video driver" warnings.
    svn path=/trunk/FreeOrion/; revision=1694

--- server/ServerApp.cpp
+++ server/ServerApp.cpp
@@ -1333,12 +1333,17 @@ void ServerApp::Run()
     }
 }
 
+#ifdef FREEORION_LINUX
+#  include <iostream> // for informal dummy videodriver message
+#endif
+
 void ServerApp::SDLInit()
 {
 #ifdef FREEORION_LINUX
     // Dirty hack to active the dummy video handler of SDL; if the user has already set SDL_VIDEODRIVER, we'll trust him
     if (getenv("SDL_VIDEODRIVER") == NULL) {
         putenv("SDL_VIDEODRIVER=dummy");
+	std::cerr << "NOTE: All warnings about using \"the SDL dummy video driver\" can safely be ignored." << std::endl;
     }
 #endif
 
