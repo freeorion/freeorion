d2bbd0337260dee8808327b77482a8d6dd4c9094
Ronald Nölte <noelte@dbd5520b-6a0a-0410-a553-f54834df5b05>
2006-03-11 17:52:27 +0000

    Project folder for Gg
    svn path=/trunk/FreeOrion/; revision=1814

---
+++ msvc/FreeOrion/GG/Config.h
@@ -0,0 +1,14 @@
+#define error_t int
+#define HAVE_STDIO_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_CTYPE_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_ERRNO_H 1
+#define __WIN32__
+#define HAVE_MEMCPY 1
+#define HAVE_MEMMOVE 1
+#define LTDL_OBJDIR ".libs"
+#define LTDL_DLOPEN_DEPLIBS 1
+#define LTDL_SHLIBPATH_VAR "PATH"
+#define LTDL_SHLIB_EXT ".dll"
