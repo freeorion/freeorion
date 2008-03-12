e99a06c3115e88a7475edda48edd1186b56e7e19
Zach Laine <whatwasthataddress@gmail.com>
2008-03-12 02:38:53 +0000

    Changed StringFile's parsing code to accept multiline strings.  Multiline strings must be preceeded and followed by three single-quotes ('''), and newlines between such pairs of triple-quotes are treated as newlines in the string.  Escaped newlines (\n) are still treated as before.  Link-tags can be specified within double square brackets ([[ and ]]).  For instance, "[[tech PRO_INDUSTRY_CENTRE_III]]" is expanded, by substituting the second argument's stringtable value, like this: "<tech PRO_INDUSTRY_CENTRE_III>Supreme Industrial Centre</tech>".
    svn path=/trunk/FreeOrion/; revision=2391

--- UI/StringTable.cpp
+++ UI/StringTable.cpp
@@ -12,44 +12,114 @@
 #  endif
 #endif
 #include <boost/filesystem/fstream.hpp>
+#include <boost/xpressive/xpressive.hpp>
+#include <boost/algorithm/string/replace.hpp>
 
-using namespace std;
-
-namespace {
-    void HandleNewlines(std::string& str)
-    {
-        std::string::size_type pos = 0;
-        while ((pos = str.find("\\n", pos)) != std::string::npos) {
-            str.replace(pos, 2, "\n");
-        }
-    }
-}
 
 // static(s)
-const string StringTable::S_DEFAULT_FILENAME = "eng_stringtable.txt";
-const string StringTable::S_ERROR_STRING = "ERROR: ";
+const std::string StringTable::S_DEFAULT_FILENAME = "eng_stringtable.txt";
+const std::string StringTable::S_ERROR_STRING = "ERROR: ";
 
 // StringTable
 StringTable::StringTable():
     m_filename(S_DEFAULT_FILENAME)
 { Load(); }
 
-StringTable::StringTable(const string& filename):
+StringTable::StringTable(const std::string& filename):
     m_filename(filename)
 { Load(); }
 
 StringTable::~StringTable()
 {}
 
-const string& StringTable::operator[] (std::string index) const
+const std::string& StringTable::operator[] (std::string index) const
 {
     static std::string error_retval;
-    map<string, string>::const_iterator it = m_strings.find(index);
+    std::map<std::string, std::string>::const_iterator it = m_strings.find(index);
     return it == m_strings.end() ? error_retval = S_ERROR_STRING + index : it->second;
 }
 
 void StringTable::Load()
 {
+    std::ifstream ifs(m_filename.c_str());
+    std::string file_contents;
+    if (ifs) {
+        int c;
+        while ((c = ifs.get()) != std::ifstream::traits_type::eof()) {
+            file_contents += c;
+        }
+    } else {
+        Logger().errorStream() << "Could not open or read StringTable file \"" << m_filename << "\"";
+        return;
+    }
+
+    using namespace boost::xpressive;
+
+    const sregex IDENTIFIER = +_w;
+    const sregex COMMENT = '#' >> *(~_n) >> _n;
+    const sregex KEY = IDENTIFIER;
+    const sregex SINGLE_LINE_VALUE = *(~_n);
+    const sregex MULTI_LINE_VALUE = after("'''") >> *_ >> before("'''");
+    const sregex FILE_ =
+        IDENTIFIER >>
+        +(*(space | COMMENT) >>
+          KEY >> _n >>
+          (("'''" >> MULTI_LINE_VALUE >> "'''" >> _n) | SINGLE_LINE_VALUE >> _n));
+    const sregex REFERENCE =
+        "[[" >> *space >> (s1 = IDENTIFIER) >> +space >> (s2 = IDENTIFIER) >> *space >> "]]";
+
+    smatch matches;
+    if (regex_search(file_contents, matches, FILE_)) {
+        smatch::nested_results_type::const_iterator it = matches.nested_results().begin();
+        smatch::nested_results_type::const_iterator end = matches.nested_results().end();
+        std::size_t match_index = 0;
+        std::string key;
+        for (; it != end; ++it) {
+            if (!match_index) {
+                m_language = it->str();
+                ++match_index;
+            } else if (it->regex_id() == KEY.regex_id() ||
+                       it->regex_id() == SINGLE_LINE_VALUE.regex_id() ||
+                       it->regex_id() == MULTI_LINE_VALUE.regex_id()) {
+                if (match_index % 2) {
+                    key = it->str();
+                } else {
+                    if (m_strings.find(key) == m_strings.end()) {
+                        m_strings[key] = it->str();
+                        boost::algorithm::replace_all(m_strings[key], "\\n", "\n");
+                    } else {
+                        Logger().errorStream() << "Duplicate string ID found: '" << key << "' in file: '" << m_filename << "'.  Ignoring duplicate.";
+                    }
+                }
+                ++match_index;
+            }
+        }
+        for (std::map<std::string, std::string>::iterator map_it = m_strings.begin(); map_it != m_strings.end(); ++map_it)
+        {
+            sregex_iterator it(map_it->second.begin(), map_it->second.end(), REFERENCE);
+            sregex_iterator end;
+            std::size_t offset = 0;
+            bool b = false;
+            for (; it != end; ) {
+                b = true;
+                const smatch& match = *it;
+                std::map<std::string, std::string>::iterator map_lookup_it = m_strings.find(match[2]);
+                if (map_lookup_it != m_strings.end()) {
+                    std::string substitution =
+                        '<' + match[1].str() + ' ' + match[2].str() + '>' + map_lookup_it->second + "</" + match[1].str() + '>';
+                    map_it->second.replace(match.position() + offset, match.length(), substitution);
+                    offset = match.position() + substitution.length();
+                    it = sregex_iterator(map_it->second.begin() + offset,
+                                         map_it->second.end(),
+                                         REFERENCE);
+                }
+            }
+        }
+    } else {
+        Logger().errorStream() << "StringTable file \"" << m_filename << "\" is malformed";
+    }
+
+#if 0
     string temp;
     string temp2;
     std::ifstream ifs;
@@ -87,5 +157,6 @@ void StringTable::Load()
             Logger().debugStream() << "Inserted(" << (string)temp << ", " << string(temp2) << ")";
         }*/
     }
+#endif
 }
 
