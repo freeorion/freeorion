ce002b12c4f8dcf9ab0e8ec23c91cd140084d70c
Geoff <g_topping@hotmail.com>
2009-05-15 12:34:03 +0000

    Added missing python file from recent AI commit by OndrejR
    svn path=/trunk/FreeOrion/; revision=3043

---
+++ default/AI/PlanetUtilAI.py
@@ -0,0 +1,30 @@
+import freeOrionAIInterface as fo
+
+def getPlanetsInSystemsIDs(systemIDs):
+    "creates a list with all planets known to the empire"
+
+    universe = fo.getUniverse()
+
+    planetIDs = []
+    
+    for systemID in systemIDs:
+
+        system = universe.getSystem(systemID)
+        if (system == None): continue
+
+        planetIDs.extend(system.planetIDs)
+
+    return planetIDs
+
+def getOwnedPlanetsByEmpire(planetIDs, empireID):
+    "return list of planets ownned by empireID"
+    
+    result = []
+    
+    universe = fo.getUniverse()
+    for planetID in planetIDs:
+        planet = universe.getPlanet(planetID)
+        if (not planet.unowned) and planet.ownedBy(empireID):
+            result.append(planetID)
+    
+    return result
