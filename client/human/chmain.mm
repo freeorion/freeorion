2f1070842fa958efaf7f718dddbe415a1df21ff5
Geoff <g_topping@hotmail.com>
2009-10-08 04:16:32 +0000

    Fix for exiting when help text is requested with -h for OSX, similar to revision 3132
    svn path=/trunk/FreeOrion/; revision=3199

--- client/human/chmain.mm
+++ client/human/chmain.mm
@@ -2,6 +2,8 @@
 
 #import <Cocoa/Cocoa.h>
 
+#include "../../util/OptionsDB.h"
+
 #include "chmain.h"
 #include <iostream>
 
@@ -11,6 +13,12 @@ int main(int argc, char *argv[])
         std::cerr << "main() failed config." << std::endl;
         return 1;
     }
-        
+
+    // did the player request help output?
+    if (GetOptionsDB().Get<bool>("help")) {
+        GetOptionsDB().GetUsage(std::cerr);
+        return 0;   // quit without actually starting game
+    }
+
     return NSApplicationMain(argc,  (const char **) argv);
 }
