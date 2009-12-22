42a1bb9da4cab182e2b21baf9f45e422a08e0a29
Ondrej Riha <ondrejr@dbd5520b-6a0a-0410-a553-f54834df5b05>
2009-12-22 13:09:37 +0000

    fixed bug 2795714 by wrwrwr
    svn path=/trunk/FreeOrion/; revision=3287

--- UI/TechTreeWnd.cpp
+++ UI/TechTreeWnd.cpp
@@ -868,6 +868,7 @@ public:
     virtual void LClick(const GG::Pt& pt, GG::Flags<GG::ModKey> mod_keys);
     virtual void LDoubleClick(const GG::Pt& pt, GG::Flags<GG::ModKey> mod_keys);
     virtual void MouseHere(const GG::Pt& pt, GG::Flags<GG::ModKey> mod_keys);
+    virtual void MouseWheel(const GG::Pt& pt, int move, GG::Flags<GG::ModKey> mod_keys) {ForwardEventToParent();}
 
     void Update();
 
