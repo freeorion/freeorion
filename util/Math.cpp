1bc33d0816ef69e136382a76c093899348c23013
Zach Laine <whatwasthataddress@gmail.com>
2010-04-07 04:38:03 +0000

    Added new util/Math.h function, PointInPartialEllipse(), and fixed an indexing error in PointInEllipse().
    svn path=/trunk/FreeOrion/; revision=3438

--- util/Math.cpp
+++ util/Math.cpp
@@ -3,34 +3,58 @@
 #include <boost/numeric/ublas/matrix.hpp>
 
 
+namespace {
+    bool PointInEllipseImpl(double x, double y,
+                            double ellipse_x, double ellipse_y,
+                            double major, double minor,
+                            double theta_major,
+                            double theta_begin,
+                            double theta_end)
+    {
+        typedef boost::numeric::ublas::matrix<double, boost::numeric::ublas::row_major> Matrix;
+        Matrix v(2, 1);
+        v(0, 0) = x - ellipse_x;
+        v(1, 0) = y - ellipse_y;
+
+        const double SIN = std::sin(-theta_major);
+        const double COS = std::cos(-theta_major);
+        Matrix rotate(2, 2);
+        rotate(0, 0) = COS;
+        rotate(0, 1) = -SIN;
+        rotate(1, 0) = SIN;
+        rotate(1, 1) = COS;
+
+        v = prod(rotate, v);
+
+        if (theta_begin != theta_end) {
+            double theta = std::atan2(y, x);
+            if (theta < theta_begin || theta_end < theta)
+                return false;
+        }
+
+        Matrix scale(2, 2);
+        scale(0, 0) = 1.0 / major;
+        scale(0, 1) = 0.0;
+        scale(1, 0) = 0.0;
+        scale(1, 1) = 1.0 / minor;
+
+        v = prod(scale, v);
+
+        return v(0, 0) * v(0, 0) + v(1, 0) * v(1, 0) < 1.0;
+    }
+
+}
+
 bool PointInEllipse(double x, double y,
                     double ellipse_x, double ellipse_y,
                     double major, double minor,
                     double theta_major)
-{
-    typedef boost::numeric::ublas::matrix<double, boost::numeric::ublas::row_major> Matrix;
-    Matrix v(2, 1);
-    v(0, 0) = x - ellipse_x;
-    v(0, 1) = y - ellipse_y;
+{ return PointInEllipseImpl(x, y, ellipse_x, ellipse_y, major, minor, theta_major, 0, 0); }
 
-    const double SIN = std::sin(-theta_major);
-    const double COS = std::cos(-theta_major);
-    Matrix rotate(2, 2);
-    rotate(0, 0) = COS;
-    rotate(0, 1) = -SIN;
-    rotate(1, 0) = SIN;
-    rotate(1, 1) = COS;
-
-    v = prod(rotate, v);
-
-    Matrix scale(2, 2);
-    scale(0, 0) = 1.0 / major;
-    scale(0, 1) = 0.0;
-    scale(1, 0) = 0.0;
-    scale(1, 1) = 1.0 / minor;
-
-    v = prod(scale, v);
-
-    return v(0, 0) * v(0, 0) + v(0, 1) * v(0, 1) < 1.0;
-}
-
+bool PointInPartialEllipse(double x, double y,
+                           double ellipse_x, double ellipse_y,
+                           double major, double minor,
+                           double theta_major,
+                           double theta_begin,
+                           double theta_end)
+{ return PointInEllipseImpl(x, y, ellipse_x, ellipse_y, major, minor, theta_major, theta_begin, theta_end); }
--- util/Math.h
+++ util/Math.h
@@ -10,4 +10,16 @@ bool PointInEllipse(double x, double y,
                     double major, double minor,
                     double theta_major);
 
+/** Returns true iff <i>(x, y)</i> falls inside the angular portion
+    <i>(theta_begin, theta_end)</i> ellipse centered at <i>(ellipse_x,
+    ellipse_y)</i>, with major and minor axes \a major and \a minor, with the
+    major axis at angle \a theta_major.  The angular region <i>(theta_begin,
+    theta_end)</i> is taken to be relative to \a theta_major. */
+bool PointInPartialEllipse(double x, double y,
+                           double ellipse_x, double ellipse_y,
+                           double major, double minor,
+                           double theta_major,
+                           double theta_begin,
+                           double theta_end);
+
 #endif // _Math_h_
