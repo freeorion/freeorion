f147dfbca6707ff299d9f94b53f284d1032843a7
Zach Laine <whatwasthataddress@gmail.com>
2010-04-21 22:46:51 +0000

    Corrected OpenSteer::SimpleVehicle::measurePathCurvature().  It no longer produces NaNs for _curvature and _smoothedCurvature.
    svn path=/trunk/FreeOrion/; revision=3517

--- combat/OpenSteer/SimpleVehicle.cpp
+++ combat/OpenSteer/SimpleVehicle.cpp
@@ -75,7 +75,18 @@ int OpenSteer::SimpleVehicle::serialNumberCounter = 0;
 // constructor
 
 
-OpenSteer::SimpleVehicle::SimpleVehicle (void)
+OpenSteer::SimpleVehicle::SimpleVehicle (void) :
+    _mass(),
+    _radius(),
+    _speed(),
+    _maxForce(),
+    _maxSpeed(),
+    _curvature(),
+    _lastForward(),
+    _lastPosition(),
+    _smoothedPosition(),
+    _smoothedCurvature(),
+    _smoothedAcceleration()
 {
     // set inital state
     reset ();
@@ -272,13 +283,15 @@ OpenSteer::SimpleVehicle::measurePathCurvature (const float elapsedTime)
     if (elapsedTime > 0)
     {
         const Vec3 dP = _lastPosition - position ();
-        const Vec3 dF = (_lastForward - forward ()) / dP.length ();
-        const Vec3 lateral = dF.perpendicularComponent (forward ());
-        const float sign = (lateral.dot (side ()) < 0) ? 1.0f : -1.0f;
-        _curvature = lateral.length() * sign;
-        blendIntoAccumulator (elapsedTime * 4.0f,
-                              _curvature,
-                              _smoothedCurvature);
+        if (dP != Vec3()) {
+            const Vec3 dF = (_lastForward - forward ()) / dP.length ();
+            const Vec3 lateral = dF.perpendicularComponent (forward ());
+            const float sign = (lateral.dot (side ()) < 0) ? 1.0f : -1.0f;
+            _curvature = lateral.length() * sign;
+            blendIntoAccumulator (elapsedTime * 4.0f,
+                                  _curvature,
+                                  _smoothedCurvature);
+        }
         _lastForward = forward ();
         _lastPosition = position ();
     }
