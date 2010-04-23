eb5f8f8b56a1d90def5f82cdb58b12369d154c65
Geoff <g_topping@hotmail.com>
2010-04-23 03:41:03 +0000

    Copied human client icon to main freeorion directory under name FreeOrion.ico
    svn path=/trunk/FreeOrion/; revision=3523

--- msvc2008/src/Version.cpp
+++ msvc2008/src/Version.cpp
@@ -1,7 +1,7 @@
 #include "../../util/Version.h"
 
 namespace {
-    static const std::string retval = "v0.3.14 [Rev 3480ish]";
+    static const std::string retval = "v0.3.14 [Rev 3520ish]";
 }
 
 const std::string& FreeOrionVersionString()
