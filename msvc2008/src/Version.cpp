cab2190702d1d8d323198980637a97f6258255ec
Geoff <g_topping@hotmail.com>
2010-04-24 22:15:53 +0000

    Adding alternate, apparently complete, German stringtable by Lathanda
    svn path=/trunk/FreeOrion/; revision=3549

--- msvc2008/src/Version.cpp
+++ msvc2008/src/Version.cpp
@@ -1,7 +1,7 @@
 #include "../../util/Version.h"
 
 namespace {
-    static const std::string retval = "v0.3.14 [Rev 3545]";
+    static const std::string retval = "v0.3.14 [Rev 3549]";
 }
 
 const std::string& FreeOrionVersionString()
