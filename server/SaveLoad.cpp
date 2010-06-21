1e39d8d28d08e2a4659eb8e2c7a71fa5d5024cd8
Geoff <g_topping@hotmail.com>
2010-06-21 08:37:15 +0000

    -Added LoadGamePlayerSetupData function that loads some data from a save game, but doesn't deserialize the universe or rest of the game state.  This is useful when dealing with save file and player connections when info about these issues is needed from previous saves, but the gamestate isn't. -Layout tweaks in SaveLoad.cpp
    svn path=/trunk/FreeOrion/; revision=3632

--- server/SaveLoad.cpp
+++ server/SaveLoad.cpp
@@ -21,22 +21,35 @@
 #include <fstream>
 
 
-void SaveGame(const std::string& filename, const ServerSaveGameData& server_save_game_data, const std::vector<PlayerSaveGameData>& player_save_game_data, const Universe& universe)
+void SaveGame(const std::string& filename, const ServerSaveGameData& server_save_game_data,
+              const std::vector<PlayerSaveGameData>& player_save_game_data, const Universe& universe)
 {
     std::ofstream ofs(filename.c_str(), std::ios_base::binary);
     FREEORION_OARCHIVE_TYPE oa(ofs);
     oa << BOOST_SERIALIZATION_NVP(server_save_game_data);
-    Universe::s_encoding_empire = ALL_EMPIRES;
     oa << BOOST_SERIALIZATION_NVP(player_save_game_data);
+    Universe::s_encoding_empire = ALL_EMPIRES;
     Serialize(oa, universe);
 }
 
-void LoadGame(const std::string& filename, ServerSaveGameData& server_save_game_data, std::vector<PlayerSaveGameData>& player_save_game_data, Universe& universe)
+void LoadGame(const std::string& filename, ServerSaveGameData& server_save_game_data,
+              std::vector<PlayerSaveGameData>& player_save_game_data, Universe& universe)
 {
     std::ifstream ifs(filename.c_str(), std::ios_base::binary);
     FREEORION_IARCHIVE_TYPE ia(ifs);
     ia >> BOOST_SERIALIZATION_NVP(server_save_game_data);
-    Universe::s_encoding_empire = ALL_EMPIRES;
     ia >> BOOST_SERIALIZATION_NVP(player_save_game_data);
+    Universe::s_encoding_empire = ALL_EMPIRES;
     Deserialize(ia, universe);
 }
+
+void LoadGamePlayerSetupData(const std::string& filename, ServerSaveGameData& server_save_game_data,
+                             std::vector<PlayerSaveGameData>& player_save_game_data)
+{
+    std::ifstream ifs(filename.c_str(), std::ios_base::binary);
+    FREEORION_IARCHIVE_TYPE ia(ifs);
+    ia >> BOOST_SERIALIZATION_NVP(server_save_game_data);
+    ia >> BOOST_SERIALIZATION_NVP(player_save_game_data);
+    // skipping Universe deserialization which is not needed for this function
+}
+
--- server/SaveLoad.h
+++ server/SaveLoad.h
@@ -17,4 +17,9 @@ void SaveGame(const std::string& filename, const ServerSaveGameData& server_save
 void LoadGame(const std::string& filename, ServerSaveGameData& server_save_game_data,
               std::vector<PlayerSaveGameData>& player_save_game_data, Universe& universe);
 
+/** Loads player setup information and server state from a savefile \a filename
+  * but does not load full gamestate. */
+void LoadGamePlayerSetupData(const std::string& filename, ServerSaveGameData& server_save_game_data,
+                             std::vector<PlayerSaveGameData>& player_save_game_data);
+
 #endif
