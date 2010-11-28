419e090b227ffedabb1552faf61ce76096822c3e
Geoff <g_topping@hotmail.com>
2010-11-28 09:39:52 +0000

    Added GiGiOgre project and tweaked existing GiGi project for MSVC 2010.
    svn path=/trunk/FreeOrion/; revision=3848

---
+++ msvc2010/src/Version.cpp
@@ -0,0 +1,10 @@
+#include "../../util/Version.h"
+
+namespace {
+    static const std::string retval = "post-v0.3.15 [SVN 3801]";
+}
+
+const std::string& FreeOrionVersionString()
+{
+    return retval;
+}
