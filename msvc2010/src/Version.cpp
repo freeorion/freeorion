94f346c495655a905100d56e35a318276c5ec3f2
Geoff <g_topping@hotmail.com>
2010-12-04 08:14:36 +0000

    -Removed unnecessary python file from GiGi MSVC 2010 project file. -Updated MSVC2010 Version.cpp
    svn path=/trunk/FreeOrion/; revision=3863

--- msvc2010/src/Version.cpp
+++ msvc2010/src/Version.cpp
@@ -1,7 +1,7 @@
 #include "../../util/Version.h"
 
 namespace {
-    static const std::string retval = "post-v0.3.15 [SVN 3801]";
+    static const std::string retval = "post-v0.3.15 [SVN 3863]";
 }
 
 const std::string& FreeOrionVersionString()
