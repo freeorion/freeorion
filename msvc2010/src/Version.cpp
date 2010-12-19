73531ab49d775f040cff51e45e4d9bad014a2668
Geoff <g_topping@hotmail.com>
2010-12-19 22:09:33 +0000

    Modified various techs, buildings and ship parts to reduce stealth and detection values, and to give homeworlds a few starlane jumps of fleet supply at the start of the game.  This should make exploration more possible and useful.
    svn path=/trunk/FreeOrion/; revision=3896

--- msvc2010/src/Version.cpp
+++ msvc2010/src/Version.cpp
@@ -1,7 +1,7 @@
 #include "../../util/Version.h"
 
 namespace {
-    static const std::string retval = "post-v0.3.15 [SVN 3893]";
+    static const std::string retval = "post-v0.3.15 [SVN 3896]";
 }
 
 const std::string& FreeOrionVersionString()
