8f84a18f63d7fcbf93b5459d633811bd3c8c98d2
Geoff <g_topping@hotmail.com>
2011-01-04 10:10:10 +0000

    Updated MSVC 2008 GiGi project file.
    svn path=/trunk/FreeOrion/; revision=3948

--- msvc2008/src/Version.cpp
+++ msvc2008/src/Version.cpp
@@ -1,7 +1,7 @@
 #include "../../util/Version.h"
 
 namespace {
-    static const std::string retval = "post-v0.3.15 [SVN 3890]";
+    static const std::string retval = "post-v0.3.15 [SVN 3948]";
 }
 
 const std::string& FreeOrionVersionString()
