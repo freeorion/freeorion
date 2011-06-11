56e79192a852d04ba8a49e98daafc53fe80f16e8
Geoff <g_topping@hotmail.com>
2011-06-11 21:13:05 +0000

    Added another missed pair of files to MSVC 2008 project files, and updated Version.cpp.
    svn path=/trunk/FreeOrion/; revision=3993

--- msvc2008/src/Version.cpp
+++ msvc2008/src/Version.cpp
@@ -1,7 +1,7 @@
 #include "../../util/Version.h"
 
 namespace {
-    static const std::string retval = "post-v0.3.15 [SVN 3992] (MSVC 2008)";
+    static const std::string retval = "post-v0.3.15 [SVN 3993] (MSVC 2008)";
 }
 
 const std::string& FreeOrionVersionString()
