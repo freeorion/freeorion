95a0e6e28890e6948567b58a6b393a734babbcbd
Geoff <g_topping@hotmail.com>
2011-06-24 03:56:46 +0000

    Updated MSVC 2010 project files to use Boost 1.46.1 and FreeType 2.4.4, and Python 2.7.
    svn path=/trunk/FreeOrion/; revision=4005

--- msvc2010/src/Version.cpp
+++ msvc2010/src/Version.cpp
@@ -1,7 +1,7 @@
 #include "../../util/Version.h"
 
 namespace {
-    static const std::string retval = "post-v0.3.15 [SVN 3995] MSVC 2010";
+    static const std::string retval = "post-v0.3.15 [SVN 4005] MSVC 2010";
 }
 
 const std::string& FreeOrionVersionString()
