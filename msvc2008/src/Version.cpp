63b433b2322a173685efa1ae7e1833494ab79da0
Geoff <g_topping@hotmail.com>
2011-07-10 03:40:17 +0000

    -Updating Version.cpp. For previous commit 4029 (that had no message): -Fixed issue where selected species wasn't being shown properly in lobby window. -Made AIs added to MP games be given random species and empire names (from list). -Made players joining an MP game be given human species, if "SP_HUMAN" is defined, or the first species if there are no humans. -Added a RandomSpeciesName function to SpeciesManager for convenience. -Made default names given to AIs or nameless human players be defined using stringtable entries instead of being fully hard-coded.
    svn path=/trunk/FreeOrion/; revision=4030

--- msvc2008/src/Version.cpp
+++ msvc2008/src/Version.cpp
@@ -1,7 +1,7 @@
 #include "../../util/Version.h"
 
 namespace {
-    static const std::string retval = "post-v0.3.15 [SVN 4027] (MSVC 2008)";
+    static const std::string retval = "post-v0.3.15 [SVN 4030] (MSVC 2008)";
 }
 
 const std::string& FreeOrionVersionString()
