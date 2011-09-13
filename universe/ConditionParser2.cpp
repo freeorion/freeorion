b363c64e65dd1f5d14f9986c8c2300086e5ac9a6
Geoff <g_topping@hotmail.com>
2011-09-13 07:41:45 +0000

    -Added Parsing for InSystem and Object conditions, both of which take a referenced object ID to match with. -Added parsing for HasSpecial with addedsince and addedbefore parameters to restrict the range of turns when the matched special needs to have been added. -Replaced DesignHasPartClosure used in parsing with StringIntRefIntRefClosure which is also used for the updated HasSpecial.
    svn path=/trunk/FreeOrion/; revision=4256

--- universe/ConditionParser2.cpp
+++ universe/ConditionParser2.cpp
@@ -13,6 +13,10 @@ extern ParamLabel high_label;
 extern ParamLabel class_label;
 extern ParamLabel empire_label;
 extern ParamLabel design_label;
+ParamLabel system_label("system");
+ParamLabel id_label("id");
+ParamLabel added_since_label("addedsince");
+ParamLabel added_before_label("addedbefore");
 
 rule<Scanner, ConditionClosure::context_t> condition2_p;
 
@@ -25,21 +29,11 @@ namespace {
         ConditionParser2Definition();
 
     private:
-        struct StringClosure : boost::spirit::classic::closure<StringClosure, Condition::ConditionBase*, std::string>
+        struct StringClosure : boost::spirit::classic::closure<StringClosure, Condition::ConditionBase*,
+                                                               std::string>
         {
             member1 this_;
             member2 name;
-        };
-
-        struct DesignHasPartClosure : boost::spirit::classic::closure<DesignHasPartClosure, Condition::ConditionBase*,
-                                                                      ValueRef::ValueRefBase<int>*,
-                                                                      ValueRef::ValueRefBase<int>*,
-                                                                      std::string>
-        {
-            member1 this_;
-            member2 int_ref_1;
-            member3 int_ref_2;
-            member4 name;
         };
 
         struct DesignHasPartClassClosure : boost::spirit::classic::closure<DesignHasPartClassClosure,
@@ -61,15 +55,27 @@ namespace {
             member2 int_ref;
         };
 
+        struct StringIntRefIntRefClosure : boost::spirit::classic::closure<StringIntRefIntRefClosure,
+                                                                           Condition::ConditionBase*,
+                                                                           std::string,
+                                                                           ValueRef::ValueRefBase<int>*,
+                                                                           ValueRef::ValueRefBase<int>*>
+        {
+            member1 this_;
+            member2 name;
+            member3 int_ref_1;
+            member4 int_ref_2;
+        };
+
         typedef rule<Scanner, StringClosure::context_t>                 StringRule;
-        typedef rule<Scanner, DesignHasPartClosure::context_t>          DesignHasPartRule;
         typedef rule<Scanner, DesignHasPartClassClosure::context_t>     DesignHasPartClassRule;
         typedef rule<Scanner, IntRefClosure::context_t>                 IntRefRule;
+        typedef rule<Scanner, StringIntRefIntRefClosure::context_t>     StringIntRefIntRefRule;
 
+        StringIntRefIntRefRule      has_special;
         StringRule                  owner_has_tech;
-        StringRule                  has_special;
         StringRule                  design_has_hull;
-        DesignHasPartRule           design_has_part;
+        StringIntRefIntRefRule      design_has_part;
         DesignHasPartClassRule      design_has_part_class;
         StringRule                  predefined_design;
         IntRefRule                  design_number;
@@ -77,6 +83,8 @@ namespace {
         IntRefRule                  visible_to_empire;
         IntRefRule                  explored_by_empire;
         IntRefRule                  fleet_supplyable;
+        IntRefRule                  in_system;
+        IntRefRule                  object_id;
     };
 
     ConditionParser2Definition::ConditionParser2Definition()
@@ -87,9 +95,14 @@ namespace {
             [owner_has_tech.this_ = new_<Condition::OwnerHasTech>(owner_has_tech.name)];
 
         has_special =
-            (str_p("hasspecial")
-             >> name_label >> name_p[has_special.name = arg1])
-            [has_special.this_ = new_<Condition::HasSpecial>(has_special.name)];
+            ((str_p("hasspecial")
+              >> name_label >>          name_p[     has_special.name = arg1]
+              >> added_since_label >>   int_expr_p[ has_special.int_ref_1 = arg1]
+              >> added_before_label >>  int_expr_p[ has_special.int_ref_2 = arg1]
+              [has_special.this_ = new_<Condition::HasSpecial>(has_special.name)])
+            | (str_p("hasspecial")
+               >> name_label >> name_p[has_special.name = arg1])
+               [has_special.this_ = new_<Condition::HasSpecial>(has_special.name)]);
 
         design_has_hull =
             (str_p("designhashull")
@@ -98,11 +111,11 @@ namespace {
 
         design_has_part =
             (str_p("designhaspart")
-             >> low_label >>    int_expr_p[ design_has_part.int_ref_2 = arg1]
-             >> high_label >>   int_expr_p[ design_has_part.int_ref_1 = arg1]
+             >> low_label >>    int_expr_p[ design_has_part.int_ref_1 = arg1]
+             >> high_label >>   int_expr_p[ design_has_part.int_ref_2 = arg1]
              >> name_label >>   name_p[     design_has_part.name = arg1])
-            [design_has_part.this_ = new_<Condition::DesignHasPart>(design_has_part.int_ref_2,
-                                                                    design_has_part.int_ref_1,
+            [design_has_part.this_ = new_<Condition::DesignHasPart>(design_has_part.int_ref_1,
+                                                                    design_has_part.int_ref_2,
                                                                     design_has_part.name)];
 
         design_has_part_class =
@@ -144,6 +157,16 @@ namespace {
              >> empire_label >> int_expr_p[fleet_supplyable.int_ref = arg1])
             [fleet_supplyable.this_ = new_<Condition::FleetSupplyableByEmpire>(fleet_supplyable.int_ref)];
 
+        in_system =
+            (str_p("insystem")
+             >> system_label >> int_expr_p[in_system.int_ref = arg1])
+            [in_system.this_ = new_<Condition::InSystem>(in_system.int_ref)];
+
+        object_id =
+            (str_p("object")
+             >> id_label >> int_expr_p[object_id.int_ref = arg1])
+            [object_id.this_ = new_<Condition::ObjectID>(object_id.int_ref)];
+
         condition2_p =
             owner_has_tech[condition2_p.this_ = arg1]
             | has_special[condition2_p.this_ = arg1]
@@ -155,7 +178,9 @@ namespace {
             | produced_by_empire[condition2_p.this_ = arg1]
             | explored_by_empire[condition2_p.this_ = arg1]
             | visible_to_empire[condition2_p.this_ = arg1]
-            | fleet_supplyable[condition2_p.this_ = arg1];
+            | fleet_supplyable[condition2_p.this_ = arg1]
+            | in_system[condition2_p.this_ = arg1]
+            | object_id[condition2_p.this_ = arg1];
     }
     ConditionParser2Definition condition2_def;
 }
