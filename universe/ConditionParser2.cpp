fdd007a0c4ac11b58705c2d39ebe387adf87b216
Geoff <g_topping@hotmail.com>
2011-10-01 19:36:35 +0000

    -Fixed (implemented?) HasSpecialSinceTurn condition parsing, which was ignoring low and high labels. -Grooming
    svn path=/trunk/FreeOrion/; revision=4314

--- universe/ConditionParser2.cpp
+++ universe/ConditionParser2.cpp
@@ -15,6 +15,8 @@ extern ParamLabel empire_label;
 extern ParamLabel design_label;
 ParamLabel id_label("id");
 
+ValueRef::ValueRefBase<int>* const NULL_INT_REF = 0;
+
 rule<Scanner, ConditionClosure::context_t> condition2_p;
 
 namespace {
@@ -87,81 +89,85 @@ namespace {
     ConditionParser2Definition::ConditionParser2Definition()
     {
         owner_has_tech =
-            (str_p("ownerhastech")
-             >> name_label >> name_p[owner_has_tech.name = arg1])
+            str_p("ownerhastech")
+             >> name_label >> name_p[owner_has_tech.name = arg1]
             [owner_has_tech.this_ = new_<Condition::OwnerHasTech>(owner_has_tech.name)];
 
         has_special =
-            ((str_p("hasspecialsinceturn")
-              >> name_label >>          name_p[     has_special.name = arg1]
-              >> low_label >>           int_expr_p[ has_special.int_ref_1 = arg1]
-              >> high_label >>          int_expr_p[ has_special.int_ref_2 = arg1]
-              [has_special.this_ = new_<Condition::HasSpecial>(has_special.name)])
+            (str_p("hasspecialsinceturn")
+             >> name_label >>      name_p[     has_special.name = arg1]
+             >> (low_label >>      int_expr_p[ has_special.int_ref_1 = arg1]
+                 |                 eps_p[      has_special.int_ref_1 = val(NULL_INT_REF)])
+             >> (high_label >>     int_expr_p[ has_special.int_ref_2 = arg1]
+                 |                 eps_p[      has_special.int_ref_2 = val(NULL_INT_REF)])
+             [has_special.this_ = new_<Condition::HasSpecial>(has_special.name,
+                                                              has_special.int_ref_1,
+                                                              has_special.int_ref_2)])
             | (str_p("hasspecial")
-               >> name_label >> name_p[has_special.name = arg1])
+               >> name_label >> name_p[has_special.name = arg1]
                [has_special.this_ = new_<Condition::HasSpecial>(has_special.name)]);
 
         design_has_hull =
-            (str_p("designhashull")
-             >> name_label >> name_p[design_has_hull.name = arg1])
+            str_p("designhashull")
+            >> name_label >> name_p[design_has_hull.name = arg1]
             [design_has_hull.this_ = new_<Condition::DesignHasHull>(design_has_hull.name)];
 
         design_has_part =
-            (str_p("designhaspart")
-             >> low_label >>    int_expr_p[ design_has_part.int_ref_1 = arg1]
-             >> high_label >>   int_expr_p[ design_has_part.int_ref_2 = arg1]
-             >> name_label >>   name_p[     design_has_part.name = arg1])
+            str_p("designhaspart")
+            >> low_label >>    int_expr_p[ design_has_part.int_ref_1 = arg1]
+            >> high_label >>   int_expr_p[ design_has_part.int_ref_2 = arg1]
+            >> name_label >>   name_p[     design_has_part.name = arg1]
             [design_has_part.this_ = new_<Condition::DesignHasPart>(design_has_part.int_ref_1,
                                                                     design_has_part.int_ref_2,
                                                                     design_has_part.name)];
 
         design_has_part_class =
-            (str_p("designhaspartclass")
-             >> low_label >>    int_expr_p[     design_has_part_class.int_ref_1 = arg1]
-             >> high_label >>   int_expr_p[     design_has_part_class.int_ref_2 = arg1]
-             >> class_label >>  part_class_p[   design_has_part_class.part_class = arg1])
+            str_p("designhaspartclass")
+            >> low_label >>    int_expr_p[     design_has_part_class.int_ref_1 = arg1]
+            >> high_label >>   int_expr_p[     design_has_part_class.int_ref_2 = arg1]
+            >> class_label >>  part_class_p[   design_has_part_class.part_class = arg1]
             [design_has_part_class.this_ = new_<Condition::DesignHasPartClass>(design_has_part_class.int_ref_1,
                                                                                design_has_part_class.int_ref_2,
                                                                                design_has_part_class.part_class)];
 
         predefined_design =
-            (str_p("design")
-             >> name_label >> name_p[predefined_design.name = arg1])
-             [predefined_design.this_ = new_<Condition::PredefinedShipDesign>(predefined_design.name)];
+            str_p("design")
+            >> name_label >> name_p[predefined_design.name = arg1]
+            [predefined_design.this_ = new_<Condition::PredefinedShipDesign>(predefined_design.name)];
 
         design_number =
-            (str_p("design")
-             >> design_label >> int_expr_p[design_number.int_ref = arg1])
+            str_p("design")
+            >> design_label >> int_expr_p[design_number.int_ref = arg1]
             [design_number.this_ = new_<Condition::NumberedShipDesign>(design_number.int_ref)];
 
         produced_by_empire =
-            (str_p("producedbyempire")
-             >> empire_label >> int_expr_p[produced_by_empire.int_ref = arg1])
+            str_p("producedbyempire")
+            >> empire_label >> int_expr_p[produced_by_empire.int_ref = arg1]
             [produced_by_empire.this_ = new_<Condition::ProducedByEmpire>(produced_by_empire.int_ref)];
 
         visible_to_empire =
-            (str_p("visibletoempire")
-             >> empire_label >> int_expr_p[visible_to_empire.int_ref = arg1])
+            str_p("visibletoempire")
+            >> empire_label >> int_expr_p[visible_to_empire.int_ref = arg1]
             [visible_to_empire.this_ = new_<Condition::VisibleToEmpire>(visible_to_empire.int_ref)];
 
         explored_by_empire =
-            (str_p("exploredbyempire")
-             >> empire_label >> int_expr_p[explored_by_empire.int_ref = arg1])
+            str_p("exploredbyempire")
+            >> empire_label >> int_expr_p[explored_by_empire.int_ref = arg1]
             [explored_by_empire.this_ = new_<Condition::ExploredByEmpire>(explored_by_empire.int_ref)];
 
         fleet_supplyable =
-            (str_p("fleetsupplyablebyempire")
-             >> empire_label >> int_expr_p[fleet_supplyable.int_ref = arg1])
+            str_p("fleetsupplyablebyempire")
+            >> empire_label >> int_expr_p[fleet_supplyable.int_ref = arg1]
             [fleet_supplyable.this_ = new_<Condition::FleetSupplyableByEmpire>(fleet_supplyable.int_ref)];
 
         in_system =
-            (str_p("insystem")
-             >> id_label >> int_expr_p[in_system.int_ref = arg1])
+            str_p("insystem")
+            >> id_label >> int_expr_p[in_system.int_ref = arg1]
             [in_system.this_ = new_<Condition::InSystem>(in_system.int_ref)];
 
         object_id =
-            (str_p("object")
-             >> id_label >> int_expr_p[object_id.int_ref = arg1])
+            str_p("object")
+            >> id_label >> int_expr_p[object_id.int_ref = arg1]
             [object_id.this_ = new_<Condition::ObjectID>(object_id.int_ref)];
 
         condition2_p =
