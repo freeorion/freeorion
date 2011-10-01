64fcb649ec339d337658b986459ef732cb517a6e
Geoff <g_topping@hotmail.com>
2011-10-01 19:07:17 +0000

    -Removed more unnecessary high = 999 (or 9999 or 99999) and low = 0 from conditions. -Code grooming in Meter.cpp
    svn path=/trunk/FreeOrion/; revision=4313

--- universe/Meter.cpp
+++ universe/Meter.cpp
@@ -22,26 +22,25 @@ Meter::Meter(double current_value, double initial_value) :
     m_initial_value(initial_value)
 {}
 
-double Meter::Current() const { return m_current_value; }
-double Meter::Initial() const { return m_initial_value; }
+double Meter::Current() const
+{ return m_current_value; }
 
-std::string Meter::Dump() const {
-    using boost::lexical_cast;
-    return "Cur: " + lexical_cast<std::string>(m_current_value) +
-           " Initial: " + lexical_cast<std::string>(m_initial_value);
-}
+double Meter::Initial() const
+{ return m_initial_value; }
 
-void Meter::SetCurrent(double current_value) { m_current_value = current_value; }
+std::string Meter::Dump() const
+{ return "Cur: " + boost::lexical_cast<std::string>(m_current_value) + " Initial: " + boost::lexical_cast<std::string>(m_initial_value); }
+
+void Meter::SetCurrent(double current_value)
+{ m_current_value = current_value; }
 
 void Meter::Set(double current_value, double initial_value) {
     m_current_value = current_value;
     m_initial_value = initial_value;
 }
 
-void Meter::ResetCurrent() {
-    m_current_value = DEFAULT_VALUE;
-    // initial unchanged
-}
+void Meter::ResetCurrent()
+{ m_current_value = DEFAULT_VALUE; } // initial unchanged
 
 void Meter::Reset() {
     m_current_value = DEFAULT_VALUE;
@@ -51,10 +50,8 @@ void Meter::Reset() {
 void Meter::AddToCurrent(double adjustment)
 { m_current_value += adjustment; }
 
-void Meter::ClampCurrentToRange(double min/* = DEFAULT_VALUE*/, double max/* = LARGE_VALUE*/) {
-    m_current_value = std::max(std::min(m_current_value, max), min);
-}
+void Meter::ClampCurrentToRange(double min/* = DEFAULT_VALUE*/, double max/* = LARGE_VALUE*/)
+{ m_current_value = std::max(std::min(m_current_value, max), min); }
 
-void Meter::BackPropegate() {
-    m_initial_value = m_current_value;
-}
+void Meter::BackPropegate()
+{ m_initial_value = m_current_value; }
