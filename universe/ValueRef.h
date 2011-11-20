0982ae77facb821b4c6b60925ee52f3c22b469b2
Geoff <g_topping@hotmail.com>
2011-11-20 08:24:51 +0000

    Constructor implementations into header.
    svn path=/trunk/FreeOrion/; revision=4502

--- universe/ValueRef.h
+++ universe/ValueRef.h
@@ -28,34 +28,75 @@ namespace boost {
 struct ScriptingContext {
     /** Empty context.  Useful for evaluating ValueRef::Constant that don't
       * depend on their context. */
-    ScriptingContext();
+    ScriptingContext() :
+        source(0),
+        effect_target(0),
+        condition_root_candidate(0),
+        condition_local_candidate(0)
+    {}
 
     /** Context with only a source object.  Useful for evaluating effectsgroup
       * scope and activation conditions that have no external candidates or
       * effect target to propegate. */
-    explicit ScriptingContext(const UniverseObject* source_);
+    explicit ScriptingContext(const UniverseObject* source_) :
+        source(source_),
+        effect_target(0),
+        condition_root_candidate(0),
+        condition_local_candidate(0)
+    {}
 
-    ScriptingContext(const UniverseObject* source_, UniverseObject* target_);
+    ScriptingContext(const UniverseObject* source_, UniverseObject* target_) :
+        source(source_),
+        effect_target(target_),
+        condition_root_candidate(0),
+        condition_local_candidate(0)
+    {}
 
     ScriptingContext(const UniverseObject* source_, UniverseObject* target_,
-                     const boost::any& current_value_);
+                     const boost::any& current_value_) :
+        source(source_),
+        effect_target(target_),
+        condition_root_candidate(0),
+        condition_local_candidate(0),
+        current_value(current_value_)
+    {}
 
     /** For evaluating ValueRef in an Effect::Execute function.  Keeps input
       * context, but specifies the current value. */
-    ScriptingContext(const ScriptingContext& context,
-                     const boost::any& current_value_);
+    ScriptingContext(const ScriptingContext& parent_context,
+                     const boost::any& current_value_) :
+        source(parent_context.source),
+        effect_target(parent_context.effect_target),
+        condition_root_candidate(parent_context.condition_root_candidate),
+        condition_local_candidate(parent_context.condition_local_candidate),
+        current_value(current_value_)
+    {}
 
     /** For recusrive evaluation of Conditions.  Keeps source and effect_target
       * from input context, but sets local candidate with input object, and if
       * there is no root candidate in the parent context, then the input object
       * becomes the root candidate. */
     ScriptingContext(const ScriptingContext& parent_context,
-                     const UniverseObject* condition_local_candidate);
+                     const UniverseObject* condition_local_candidate) :
+        source(                     parent_context.source),
+        effect_target(              parent_context.effect_target),
+        condition_root_candidate(   parent_context.condition_root_candidate ?
+                                        parent_context.condition_root_candidate :
+                                        condition_local_candidate),                 // if parent context doesn't already have a root candidate, the new local candidate is the root
+        condition_local_candidate(  condition_local_candidate),                     // new local candidate
+        current_value(              parent_context.current_value)
+    {}
 
     ScriptingContext(const UniverseObject* source_, UniverseObject* target_,
                      const boost::any& current_value_,
                      const UniverseObject* condition_root_candidate_,
-                     const UniverseObject* condition_local_candidate_);
+                     const UniverseObject* condition_local_candidate_) :
+        source(source_),
+        effect_target(0),
+        condition_root_candidate(condition_root_candidate_),
+        condition_local_candidate(condition_local_candidate_),
+        current_value(current_value_)
+    {}
 
     const UniverseObject*   source;
     UniverseObject*         effect_target;
