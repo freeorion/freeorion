5b3da91196177d25a119d1a38127606d51194245
Geoff <g_topping@hotmail.com>
2012-01-01 23:32:10 +0000

    XCode files update from night.
    svn path=/trunk/FreeOrion/; revision=4552

--- Xcode/Version.cpp
+++ Xcode/Version.cpp
@@ -1,7 +1,7 @@
 #include "../util/Version.h"
 
 namespace {
-    static const std::string retval = "v0.3.16 [SVN 4047] XCode";
+    static const std::string retval = "post-v0.3.17 [GitMirror / SVN r4549] Xcode";
 }
 
 const std::string& FreeOrionVersionString()
