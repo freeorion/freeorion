8578fd989a582968ba9143053dbab6109f717779
Geoff <g_topping@hotmail.com>
2012-01-26 20:39:51 +0000

    Fixed commenting out from revision 4619 which had a misplaced end-of-comment tag, which was breaking the parsing, causing zero-stealth krill fleets to be visible from far off systems.
    svn path=/trunk/FreeOrion/; revision=4623

--- msvc2010/src/Version.cpp
+++ msvc2010/src/Version.cpp
@@ -1,7 +1,7 @@
 #include "../../util/Version.h"
 
 namespace {
-    static const std::string retval = "v0.4 [SVN 4599] MSVC 2010";
+    static const std::string retval = "v0.4 [SVN 4623] MSVC 2010";
 }
 
 const std::string& FreeOrionVersionString()
