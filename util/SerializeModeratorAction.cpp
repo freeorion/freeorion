6780aa14318d2c232bb59ed40931228a91ff3d44
Vezzra <vezzra@gmx.net>
2013-03-04 13:57:21 +0000

    -Updated Xcode project file
    -Added the "template void Moderator::...::serialize<FREEORION_OARCHIVE_TYPE>..." and "template void Moderator::...::serialize<FREEORION_IARCHIVE_TYPE>..." lines to SerializeModeratorAction.cpp for each ModeratorAction class to fix build error on OSX
    
    svn path=/trunk/FreeOrion/; revision=5832

--- util/SerializeModeratorAction.cpp
+++ util/SerializeModeratorAction.cpp
@@ -14,12 +14,18 @@ template <class Archive>
 void Moderator::ModeratorAction::serialize(Archive& ar, const unsigned int version)
 {}
 
+template void Moderator::ModeratorAction::serialize<FREEORION_OARCHIVE_TYPE>(FREEORION_OARCHIVE_TYPE&, const unsigned int);
+template void Moderator::ModeratorAction::serialize<FREEORION_IARCHIVE_TYPE>(FREEORION_IARCHIVE_TYPE&, const unsigned int);
+
 template <class Archive>
 void Moderator::DestroyUniverseObject::serialize(Archive& ar, const unsigned int version)
 {
     ar  & BOOST_SERIALIZATION_BASE_OBJECT_NVP(ModeratorAction)
         & BOOST_SERIALIZATION_NVP(m_object_id);
 }
+
+template void Moderator::DestroyUniverseObject::serialize<FREEORION_OARCHIVE_TYPE>(FREEORION_OARCHIVE_TYPE&, const unsigned int);
+template void Moderator::DestroyUniverseObject::serialize<FREEORION_IARCHIVE_TYPE>(FREEORION_IARCHIVE_TYPE&, const unsigned int);
 
 template <class Archive>
 void Moderator::SetOwner::serialize(Archive& ar, const unsigned int version)
@@ -28,3 +34,6 @@ void Moderator::SetOwner::serialize(Archive& ar, const unsigned int version)
         & BOOST_SERIALIZATION_NVP(m_object_id)
         & BOOST_SERIALIZATION_NVP(m_new_owner_empire_id);
 }
+
+template void Moderator::SetOwner::serialize<FREEORION_OARCHIVE_TYPE>(FREEORION_OARCHIVE_TYPE&, const unsigned int);
+template void Moderator::SetOwner::serialize<FREEORION_IARCHIVE_TYPE>(FREEORION_IARCHIVE_TYPE&, const unsigned int);
