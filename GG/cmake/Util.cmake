8b35e3f9c4aa448b12d7ab2a11e20970e5e5224a
Geoff <g_topping@hotmail.com>
2013-04-10 22:07:27 +0000

    CMake simplification patch by adrian_broher.
    svn path=/trunk/FreeOrion/; revision=5974

--- GG/cmake/Util.cmake
+++ GG/cmake/Util.cmake
@@ -25,53 +25,12 @@ macro (list_contains var value)
   endforeach (value2)
 endmacro ()
 
-# This macro is an internal utility macro that updates compilation and
-# linking flags based on interactions among the features in a variant.
-#
-#   feature_interactions(prefix
-#                        feature1 feature2 ...)
-#
-# where "prefix" is the prefix of the compilation and linking flags
-# that will be updated (e.g., ${prefix}_COMPILE_FLAGS). feature1,
-# feature2, etc. are the names of the features used in this particular
-# variant. If the features in this variant conflict, set
-# ${prefix}_OKAY to FALSE.
-macro (feature_interactions PREFIX)
-  # Don't build or link against a shared library and a static run-time
-  list_contains(IS_SHARED SHARED ${ARGN})
-  list_contains(IS_STATIC STATIC ${ARGN})
-  if (IS_SHARED AND IS_STATIC)
-    set(${PREFIX}_OKAY FALSE)
-  endif (IS_SHARED AND IS_STATIC)
-
-  # Visual C++-specific runtime library flags; with Visual C++, the dynamic
-  # runtime is multi-threaded only
-  if (MSVC)
-    list_contains(IS_DEBUG DEBUG ${ARGN})
-    if (IS_DEBUG)
-      if (IS_STATIC)
-        list(APPEND ${PREFIX}_COMPILE_FLAGS "/MTd")
-      else (IS_STATIC)
-        list(APPEND ${PREFIX}_COMPILE_FLAGS "/MDd")
-      endif (IS_STATIC)       
-    else (IS_DEBUG)
-      if (IS_STATIC)
-        list(APPEND ${PREFIX}_COMPILE_FLAGS "/MT")
-      else (IS_STATIC)
-        list(APPEND ${PREFIX}_COMPILE_FLAGS "/MD")
-      endif (IS_STATIC)       
-    endif (IS_DEBUG)
-  endif (MSVC)  
-endmacro ()
-
 # This macro is an internal utility macro that builds a particular variant of
 # a library.
 #
-#   library_variant(libname 
-#                   feature1 feature2 ...)
+#   library_all_variants(libname)
 #
-# where libname is the name of the library (e.g., "GiGiSDL") and feature1,
-# feature2, ... are the features that will be used in this variant.
+# where libname is the name of the library (e.g., "GiGiSDL")
 #
 # This macro will define a new library target based on libname and the
 # specific variant name, which depends on the utility target libname. The
@@ -81,129 +40,56 @@ endmacro ()
 #
 # If any of the features listed conflict with this library, no new targets
 # will be built.
-macro (library_variant LIBNAME)
-  set(THIS_VARIANT_COMPILE_FLAGS "${THIS_LIB_COMPILE_FLAGS}")
-  set(THIS_VARIANT_LINK_FLAGS "${THIS_LIB_LINK_FLAGS}")
-  set(THIS_VARIANT_LINK_LIBS ${THIS_LIB_LINK_LIBS})
-
-  # Determine if it is okay to build this variant
-  set(THIS_VARIANT_OKAY TRUE)
-  foreach (ARG ${ARGN})
-    # If the user specified that we should not build any variants of
-    # this kind, don't. For example, if the BUILD_SHARED option is
-    # off, don't build shared libraries.
-    if (NOT BUILD_${ARG})
-      set(THIS_VARIANT_OKAY FALSE)
-    endif (NOT BUILD_${ARG})
-
-    # Accumulate compile and link flags
-    list(APPEND THIS_VARIANT_COMPILE_FLAGS ${THIS_LIB_${ARG}_COMPILE_FLAGS} ${${ARG}_COMPILE_FLAGS})
-    list(APPEND THIS_VARIANT_LINK_FLAGS ${THIS_LIB_${ARG}_LINK_FLAGS} ${${ARG}_LINK_FLAGS})
-    list(APPEND THIS_VARIANT_LINK_LIBS ${THIS_LIB_${ARG}_LINK_LIBS} ${${ARG}_LINK_LIBS})
-  endforeach (ARG ${ARGN})
-
-  # Handle feature interactions
-  feature_interactions(THIS_VARIANT ${ARGN})
-
-  if (THIS_VARIANT_OKAY)
-    if (IS_STATIC)
-      set(VARIANT_LIBNAME ${LIBNAME}_static)
-    else ()
-      set(VARIANT_LIBNAME ${LIBNAME})
-    endif ()
-
-    # We handle static vs. dynamic libraries differently
-    list_contains(THIS_LIB_IS_STATIC STATIC ${ARGN})
-    if (THIS_LIB_IS_STATIC)
-      # On Windows, we need static and shared libraries to have
-      # different names, so we prepend "lib" to the name.
-      if (WIN32 AND NOT CYGWIN)
-        set(LIBPREFIX "lib")
-      else (WIN32 AND NOT CYGWIN)
-        set(LIBPREFIX "")
-      endif (WIN32 AND NOT CYGWIN)
-
-      # Add the library itself
-      add_library(${VARIANT_LIBNAME} STATIC ${THIS_LIB_SOURCES})
-
-      # Set properties on this library
-      set_target_properties(${VARIANT_LIBNAME}
-        PROPERTIES
-        OUTPUT_NAME "${LIBPREFIX}${LIBNAME}"
-        ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}
-        LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}
-        CLEAN_DIRECT_OUTPUT 1
-        COMPILE_FLAGS "${THIS_VARIANT_COMPILE_FLAGS}"
-        LINK_FLAGS "${THIS_VARIANT_LINK_FLAGS}"
-        LINK_SEARCH_END_STATIC true
-        LABELS "${PROJECT_NAME}"
-        )
-    elseif (THIS_LIB_MODULE)
-      # Add a module
-      add_library(${VARIANT_LIBNAME} MODULE ${THIS_LIB_SOURCES})
-
-      # Set properties on this library
-      set_target_properties(${VARIANT_LIBNAME}
-        PROPERTIES
-        OUTPUT_NAME ${LIBNAME}
-        ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}
-        LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}
-        CLEAN_DIRECT_OUTPUT 1
-        COMPILE_FLAGS "${THIS_VARIANT_COMPILE_FLAGS}"
-        LINK_FLAGS "${THIS_VARIANT_LINK_FLAGS}"
-        LABELS "${PROJECT_NAME}"
-        PREFIX ""
-       # SOVERSION "${BOOST_VERSION}"
-        )
-    else (THIS_LIB_IS_STATIC)
-      #TODO: Check the SOVERSION behavior on Linux and Windows
-      # Add a module
-      add_library(${VARIANT_LIBNAME} SHARED ${THIS_LIB_SOURCES})
-      # Set properties on this library
-      set_target_properties(${VARIANT_LIBNAME}
-        PROPERTIES
-        OUTPUT_NAME ${LIBNAME}
-        ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}
-        LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}
-        CLEAN_DIRECT_OUTPUT 1
-        COMPILE_FLAGS "${THIS_VARIANT_COMPILE_FLAGS}"
-        LINK_FLAGS "${THIS_VARIANT_LINK_FLAGS}"
-        LABELS "${PROJECT_NAME}"
-        # SOVERSION "${BOOST_VERSION}"
-        )
-    endif (THIS_LIB_IS_STATIC)
-
-    # Link against whatever libraries this library depends on
-    target_link_libraries(${VARIANT_LIBNAME} ${THIS_VARIANT_LINK_LIBS})
-    foreach (dependency ${THIS_LIB_DEPENDS})
-      target_link_libraries(${VARIANT_LIBNAME} "${dependency}")
-    endforeach (dependency)
-
-    # Setup installation properties
-    string(TOUPPER COMPONENT_${PROJECT_NAME} LIB_COMPONENT)
-
-    # Installation of this library variant
-    if ((THIS_LIB_IS_STATIC AND NOT RUNTIME_ONLY_PACKAGE OR
-         NOT THIS_LIB_IS_STATIC AND NOT DEVEL_ONLY_PACKAGE) AND
-        COMMAND package_compatible)
-        package_compatible(ok_to_install ${LIBNAME})
-        if (ok_to_install)
-            install(
-                TARGETS ${VARIANT_LIBNAME}
-                RUNTIME DESTINATION bin COMPONENT ${LIB_COMPONENT}
-                LIBRARY DESTINATION lib${LIB_SUFFIX} COMPONENT ${LIB_COMPONENT}
-                ARCHIVE DESTINATION lib${LIB_SUFFIX} COMPONENT ${LIB_COMPONENT}_DEVEL
-            )
-        endif ()
-    endif ()
-  endif ()
-endmacro ()
-
 macro (library_all_variants LIBNAME)
-    library_variant(${LIBNAME} STATIC DEBUG MULTI_THREADED)
-    library_variant(${LIBNAME} STATIC RELEASE MULTI_THREADED)
-    library_variant(${LIBNAME} SHARED DEBUG MULTI_THREADED)
-    library_variant(${LIBNAME} SHARED RELEASE MULTI_THREADED)
+  # On Windows, we need static and shared libraries to have
+  # different names, so we prepend "lib" to the name.
+  if (BUILD_STATIC AND WIN32 AND NOT CYGWIN)
+    set (LIBPREFIX "lib")
+  else ()
+    set(LIBPREFIX "")
+  endif ()
+
+  if (BUILD_SHARED)
+    add_library(${LIBNAME} SHARED ${THIS_LIB_SOURCES})
+  else ()
+    add_library(${LIBNAME} STATIC ${THIS_LIB_SOURCES})
+    set_target_properties(${LIBNAME}
+      PROPERTIES
+      LINK_SEARCH_END_STATIC true
+    )
+  endif ()
+
+  # Set properties on this library
+  set_target_properties(${LIBNAME}
+    PROPERTIES
+    OUTPUT_NAME "${LIBPREFIX}${LIBNAME}"
+    ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}
+    LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}
+    CLEAN_DIRECT_OUTPUT 1
+    COMPILE_FLAGS "${THIS_LIB_COMPILE_FLAGS}"
+    LINK_FLAGS "${THIS_LIB_LINK_FLAGS}"
+    LABELS "${PROJECT_NAME}"
+  )
+
+  # Link against whatever libraries this library depends on
+  target_link_libraries(${LIBNAME} ${THIS_VARIANT_LINK_LIBS} ${THIS_LIB_LINK_LIBS})
+
+  # Setup installation properties
+  string(TOUPPER COMPONENT_${PROJECT_NAME} LIB_COMPONENT)
+
+  # Installation of this library variant
+  if ((BUILD_STATIC AND NOT RUNTIME_ONLY_PACKAGE AND COMMAND package_compatible) OR
+      (BUILD_SHARED AND NOT DEVEL_ONLY_PACKAGE AND COMMAND package_compatible))
+      package_compatible(ok_to_install ${LIBNAME})
+      if (ok_to_install)
+          install(
+              TARGETS ${LIBNAME}
+              RUNTIME DESTINATION bin COMPONENT ${LIB_COMPONENT}
+              LIBRARY DESTINATION lib${LIB_SUFFIX} COMPONENT ${LIB_COMPONENT}
+              ARCHIVE DESTINATION lib${LIB_SUFFIX} COMPONENT ${LIB_COMPONENT}_DEVEL
+          )
+      endif ()
+  endif ()
 endmacro ()
 
 macro (get_pkg_config_libs name)
