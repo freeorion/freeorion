9721dfdaaf407976ffe13f65f49a51067ab5a487
Isaac Supeene <isupeene@ualberta.ca>
2014-02-10 03:14:42 +0000

    - Added some comments to Enum.h.
    svn path=/trunk/FreeOrion/; revision=6877

--- GG/GG/Enum.h
+++ GG/GG/Enum.h
@@ -42,6 +42,9 @@ namespace GG {
 
     #define GG_ENUM_NAME_BUFFER_SIZE 80
 
+    /** This class is not meant for public consumption.
+      * Access this class through the functions generated
+      * in the GG_ENUM or GG_CLASS_ENUM macro invocation. */
     template <class EnumType>
     class EnumMap {
     public:
@@ -57,33 +60,18 @@ namespace GG {
         std::map<EnumType, std::string> m_value_to_name_map;
     };
 
-    template <class EnumType>
-    const std::string& EnumMap<EnumType>::operator[](EnumType value) const {
-        std::map<EnumType, std::string>::const_iterator it = m_value_to_name_map.find(value);
-        if (it != m_value_to_name_map.end()) {
-            return it->second;
-        } else {
-            static std::string none("None");
-            return none;
-        }
-    }
-
-    template <class EnumType>
-    EnumType EnumMap<EnumType>::operator[](const std::string& name) const {
-        std::map<std::string, EnumType>::const_iterator it = m_name_to_value_map.find(name);
-        if (it != m_name_to_value_map.end()) {
-            return it->second;
-        } else {
-            return BAD_VALUE;
-        }
-    }
-
+    /** Do not call this function directly.
+      * Instead, rely on the functions generated
+      * by the GG_ENUM or GG_CLASS_ENUM macro invocations. */
     template <class EnumType>
     EnumMap<EnumType>& GetEnumMap() {
         static EnumMap<EnumType> map;
         return map;
     }
-
+    
+    /** Do not call this function directly.
+      * Instead, rely on the functions generated
+      * by the GG_ENUM or GG_CLASS_ENUM macro invocations. */
     template <class EnumType>
     void BuildEnumMap(EnumMap<EnumType>& map, const std::string& enum_name, const char* comma_separated_names) {
         std::stringstream name_stream(comma_separated_names);
@@ -95,34 +83,10 @@ namespace GG {
         }
     }
 
-    template <class EnumType>
-    void EnumMap<EnumType>::Insert(int& default_value, const std::string& entry) {
-        std::stringstream name_and_value(entry);
-
-        std::string name;
-        std::getline(name_and_value, name, '=');
-
-        std::string value_str;
-        EnumType value;
-        if (std::getline(name_and_value, value_str)) {
-            value = (EnumType)strtol(value_str.c_str(), 0, 0);
-        }
-        else {
-            value = (EnumType)default_value;
-        }
-
-        boost::trim(name);
-
-        m_name_to_value_map[name] = value;
-        m_value_to_name_map[value] = name;
-        default_value = value + 1;
-    }
-
-    template <class EnumType>
-    size_t EnumMap<EnumType>::size() const {
-        return m_name_to_value_map.size();
-    }
-
+/** An enum macro for use inside classes.
+  * Enables <<, >>, and EnumToString for your enum,
+  * all of which will exist in whatever namespace this
+  * macro is used. */
 #define GG_CLASS_ENUM(EnumName, ...)                                                    \
     enum EnumName {                                                                     \
         __VA_ARGS__                                                                     \
@@ -156,6 +120,10 @@ namespace GG {
         return map[value];                                                              \
     }
 
+/** An enum macro for use outside of classes.
+  * Enables <<, >>, and EnumToString for your enum,
+  * all of which will exist in whatever namespace this
+  * macro is used. */
 #define GG_ENUM(EnumName, ...)                                                          \
     enum EnumName {                                                                     \
         __VA_ARGS__                                                                     \
@@ -187,6 +155,58 @@ namespace GG {
             ::GG::BuildEnumMap(map, #EnumName, #__VA_ARGS__);                           \
                                                                                         \
         return map[value];                                                              \
+    }
+
+      /////////////
+     // EnumMap //
+    /////////////
+    template <class EnumType>
+    const std::string& EnumMap<EnumType>::operator[](EnumType value) const {
+        std::map<EnumType, std::string>::const_iterator it = m_value_to_name_map.find(value);
+        if (it != m_value_to_name_map.end()) {
+            return it->second;
+        } else {
+            static std::string none("None");
+            return none;
+        }
+    }
+
+    template <class EnumType>
+    EnumType EnumMap<EnumType>::operator[](const std::string& name) const {
+        std::map<std::string, EnumType>::const_iterator it = m_name_to_value_map.find(name);
+        if (it != m_name_to_value_map.end()) {
+            return it->second;
+        } else {
+            return BAD_VALUE;
+        }
+    }
+
+    template <class EnumType>
+    size_t EnumMap<EnumType>::size() const {
+        return m_name_to_value_map.size();
+    }
+
+    template <class EnumType>
+    void EnumMap<EnumType>::Insert(int& default_value, const std::string& entry) {
+        std::stringstream name_and_value(entry);
+
+        std::string name;
+        std::getline(name_and_value, name, '=');
+
+        std::string value_str;
+        EnumType value;
+        if (std::getline(name_and_value, value_str)) {
+            value = (EnumType)strtol(value_str.c_str(), 0, 0);
+        }
+        else {
+            value = (EnumType)default_value;
+        }
+
+        boost::trim(name);
+
+        m_name_to_value_map[name] = value;
+        m_value_to_name_map[value] = name;
+        default_value = value + 1;
     }
 
 } // namespace GG
