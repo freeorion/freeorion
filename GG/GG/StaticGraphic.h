143daa946303fb240a4d349a2868ca34b8dd0a75
Geoff <g_topping@hotmail.com>
2014-03-22 21:07:08 +0000

    minor grooming
    svn path=/trunk/FreeOrion/; revision=6987

--- GG/GG/StaticGraphic.h
+++ GG/GG/StaticGraphic.h
@@ -58,8 +58,12 @@ class GG_API StaticGraphic : public Control
 {
 public:
     /** \name Structors */ ///@{
-    StaticGraphic(X x, Y y, X w, Y h, const boost::shared_ptr<Texture>& texture, Flags<GraphicStyle> style = GRAPHIC_NONE, Flags<WndFlag> flags = Flags<WndFlag>()); ///< creates a StaticGraphic from a pre-existing Texture.
-    StaticGraphic(X x, Y y, X w, Y h, const SubTexture& subtexture, Flags<GraphicStyle> style = GRAPHIC_NONE, Flags<WndFlag> flags = Flags<WndFlag>()); ///< creates a StaticGraphic from a pre-existing SubTexture.
+     ///< creates a StaticGraphic from a pre-existing Texture.
+    StaticGraphic(X x, Y y, X w, Y h, const boost::shared_ptr<Texture>& texture,
+                  Flags<GraphicStyle> style = GRAPHIC_NONE, Flags<WndFlag> flags = Flags<WndFlag>());
+    ///< creates a StaticGraphic from a pre-existing SubTexture.
+    StaticGraphic(X x, Y y, X w, Y h, const SubTexture& subtexture,
+                  Flags<GraphicStyle> style = GRAPHIC_NONE, Flags<WndFlag> flags = Flags<WndFlag>());
     //@}
 
     /** \name Accessors */ ///@{
