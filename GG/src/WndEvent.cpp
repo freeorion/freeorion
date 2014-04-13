f34dd5d1b555a5c08ff8307178dbae93cb605b2e
Geoff <g_topping@hotmail.com>
2014-04-13 11:54:58 +0000

    grooming
    svn path=/trunk/FreeOrion/; revision=7059

--- GG/src/WndEvent.cpp
+++ GG/src/WndEvent.cpp
@@ -47,18 +47,18 @@ namespace {
     bool RegisterModKeys()
     {
         FlagSpec<ModKey>& spec = FlagSpec<ModKey>::instance();
-        spec.insert(MOD_KEY_NONE, "MOD_KEY_NONE", true);
-        spec.insert(MOD_KEY_LSHIFT, "MOD_KEY_LSHIFT", true);
-        spec.insert(MOD_KEY_RSHIFT, "MOD_KEY_RSHIFT", true);
-        spec.insert(MOD_KEY_LCTRL, "MOD_KEY_LCTRL", true);
-        spec.insert(MOD_KEY_RCTRL, "MOD_KEY_RCTRL", true);
-        spec.insert(MOD_KEY_LALT, "MOD_KEY_LALT", true);
-        spec.insert(MOD_KEY_RALT, "MOD_KEY_RALT", true);
-        spec.insert(MOD_KEY_LMETA, "MOD_KEY_LMETA", true);
-        spec.insert(MOD_KEY_RMETA, "MOD_KEY_RMETA", true);
-        spec.insert(MOD_KEY_NUM, "MOD_KEY_NUM", true);
-        spec.insert(MOD_KEY_CAPS, "MOD_KEY_CAPS", true);
-        spec.insert(MOD_KEY_MODE, "MOD_KEY_MODE", true);
+        spec.insert(MOD_KEY_NONE,   "MOD_KEY_NONE",     true);
+        spec.insert(MOD_KEY_LSHIFT, "MOD_KEY_LSHIFT",   true);
+        spec.insert(MOD_KEY_RSHIFT, "MOD_KEY_RSHIFT",   true);
+        spec.insert(MOD_KEY_LCTRL,  "MOD_KEY_LCTRL",    true);
+        spec.insert(MOD_KEY_RCTRL,  "MOD_KEY_RCTRL",    true);
+        spec.insert(MOD_KEY_LALT,   "MOD_KEY_LALT",     true);
+        spec.insert(MOD_KEY_RALT,   "MOD_KEY_RALT",     true);
+        spec.insert(MOD_KEY_LMETA,  "MOD_KEY_LMETA",    true);
+        spec.insert(MOD_KEY_RMETA,  "MOD_KEY_RMETA",    true);
+        spec.insert(MOD_KEY_NUM,    "MOD_KEY_NUM",      true);
+        spec.insert(MOD_KEY_CAPS,   "MOD_KEY_CAPS",     true);
+        spec.insert(MOD_KEY_MODE,   "MOD_KEY_MODE",     true);
         return true;
     }
     bool dummy = RegisterModKeys();
