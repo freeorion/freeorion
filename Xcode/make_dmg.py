ffaa15b800fb27895c63a429ab8ba6c573b67c6c
Vezzra <vezzra@gmx.net>
2014-08-08 08:48:24 +0000

    Changed make_dmg.py script to reflect changes to make_versioncpp.py from commit#7373 (use last changed revision of the checked out directory, not the revision of the entire repository)
    svn path=/trunk/FreeOrion/; revision=7409

--- Xcode/make_dmg.py
+++ Xcode/make_dmg.py
@@ -4,23 +4,24 @@ import os
 import subprocess as sp
 from string import Template
 
-if len(sys.argv) != 2:
+if len(sys.argv) != 3:
     print "ERROR: invalid parameters."
-    print "make_dmg.py <built products dir>"
+    print "make_dmg.py <project rootdir> <built products dir>"
     quit()
 
+os.chdir(sys.argv[1])
 wc_rev = "XXXX"
 try:
     svn_proc = sp.Popen(["svn", "info"], stdout=sp.PIPE)
     svn_info = svn_proc.communicate()[0]
 
     for line in svn_info.splitlines():
-        if line.startswith("Revision:"):
-            wc_rev = line.partition(" ")[2]
+        if line.startswith("Last Changed Rev:"):
+            wc_rev = line.rpartition(" ")[2]
 except:
     print "WARNING: No properly installed SVN client found, can't determine SVN working copy revision"
 
-built_product_dir = sys.argv[1]
+built_product_dir = sys.argv[2]
 app = os.path.join(built_product_dir, "FreeOrion.app")
 dmg_file = "FreeOrion-%s-MacOSX-10.6.dmg" % wc_rev
 out_path = os.path.join(built_product_dir, dmg_file)
