a409d8ace032ac0fd9678b7880426973d87890b9
Geoff <g_topping@hotmail.com>
2014-09-10 04:48:04 +0000

    Restoring chmain.h
    svn path=/branches/SDL_Migration_And_Dependencies_Updates/; revision=7711

---
+++ client/human/chmain.h
@@ -0,0 +1,11 @@
+// -*- C++ -*-
+#ifndef _CHMAIN_H
+#define _CHMAIN_H
+
+#include <string>
+#include <vector>
+
+int mainConfigOptionsSetup(const std::vector<std::string>& args);
+int mainSetupAndRun();
+
+#endif
