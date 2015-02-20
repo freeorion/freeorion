1d943e18a3b4337a3539c3e9095885c6dfbdb993
Vezzra <vezzra@gmx.net>
2015-02-20 15:01:41 +0000

    Merged from trunk (up to r7970)
    svn path=/branches/SDL_Migration_And_Dependencies_Updates/; revision=7971

--- UI/DesignWnd.cpp
+++ UI/DesignWnd.cpp
@@ -2574,7 +2574,6 @@ void DesignWnd::MainPanel::ReregisterDesigns() {
     }
 }
 
-
 void DesignWnd::MainPanel::Sanitize() {
     SetHull(0);
     m_design_name->SetText(UserString("DESIGN_NAME_DEFAULT"));
