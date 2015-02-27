93b849065876e35577615f8c63d6d26695f77e09
Vezzra <vezzra@gmx.net>
2015-02-27 15:13:37 +0000

    Removed some more obsolete code from UniverseGenerator.h
    svn path=/trunk/FreeOrion/; revision=7984

--- universe/UniverseGenerator.h
+++ universe/UniverseGenerator.h
@@ -12,9 +12,6 @@
 // Minimum distance between systems in universe units [0.0, m_universe_width]
 const double    MIN_SYSTEM_SEPARATION       = 35.0;
 
-// Minimum distance between home systems in universe units [0.0, m_universe_width]
-const double    MIN_HOME_SYSTEM_SEPARATION  = 200.0;
-
 struct PlayerSetupData;
 
 // Class representing a position on the galaxy map, used
