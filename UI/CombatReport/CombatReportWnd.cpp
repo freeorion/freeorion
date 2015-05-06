f6b2ad5c36838d8643ac308f885784ca8c9b277d
geoffthemedio <geoffthemedio@gmail.com>
2015-05-06 14:14:13 +0200

    Connected signals so combat log links are clickable.

--- UI/CombatReport/CombatReportWnd.cpp
+++ UI/CombatReport/CombatReportWnd.cpp
@@ -3,6 +3,7 @@
 #include "../CUIControls.h"
 #include "../../util/i18n.h"
 #include "../../util/Logger.h"
+#include "../universe/ShipDesign.h"
 
 #include "CombatReportData.h"
 #include "GraphicalSummary.h"
@@ -20,7 +21,7 @@ namespace {
 // The implementation class for CombatReportWnd
 class CombatReportWnd::CombatReportPrivate {
 public:
-    CombatReportPrivate (CombatReportWnd& wnd):
+    CombatReportPrivate(CombatReportWnd& wnd):
         m_wnd(wnd),
         m_tabs(new GG::TabWnd(GG::X0, GG::Y0, GG::X1, GG::Y1, ClientUI::GetFont(), ClientUI::CtrlColor(), ClientUI::TextColor())),
         m_graphical(new GraphicalSummaryWnd()),
@@ -30,6 +31,10 @@ public:
         m_tabs->AddWnd(m_log, UserString("COMBAT_LOG"));
         m_wnd.AttachChild(m_tabs);
         m_wnd.GridLayout();
+
+        GG::Connect(m_log->LinkClickedSignal,       &CombatReportPrivate::HandleLinkClick,          this);
+        GG::Connect(m_log->LinkDoubleClickedSignal, &CombatReportPrivate::HandleLinkDoubleClick,    this);
+        GG::Connect(m_log->LinkRightClickedSignal,  &CombatReportPrivate::HandleLinkDoubleClick,    this);
     }
 
     void SetLog(int log_id) {
@@ -39,6 +44,64 @@ public:
 
     void DoLayout()
     { m_graphical->DoLayout(); }
+
+    void HandleLinkClick(const std::string& link_type, const std::string& data) {
+        using boost::lexical_cast;
+        try {
+            if (link_type == VarText::PLANET_ID_TAG) {
+                ClientUI::GetClientUI()->ZoomToPlanet(lexical_cast<int>(data));
+
+            } else if (link_type == VarText::SYSTEM_ID_TAG) {
+                ClientUI::GetClientUI()->ZoomToSystem(lexical_cast<int>(data));
+            } else if (link_type == VarText::FLEET_ID_TAG) {
+                ClientUI::GetClientUI()->ZoomToFleet(lexical_cast<int>(data));
+            } else if (link_type == VarText::SHIP_ID_TAG) {
+                ClientUI::GetClientUI()->ZoomToShip(lexical_cast<int>(data));
+            } else if (link_type == VarText::BUILDING_ID_TAG) {
+                ClientUI::GetClientUI()->ZoomToBuilding(lexical_cast<int>(data));
+            } else if (link_type == VarText::FIELD_ID_TAG) {
+                ClientUI::GetClientUI()->ZoomToField(lexical_cast<int>(data));
+
+            } else if (link_type == VarText::COMBAT_ID_TAG) {
+                ClientUI::GetClientUI()->ZoomToCombatLog(lexical_cast<int>(data));
+
+            } else if (link_type == VarText::EMPIRE_ID_TAG) {
+                ClientUI::GetClientUI()->ZoomToEmpire(lexical_cast<int>(data));
+            } else if (link_type == VarText::DESIGN_ID_TAG) {
+                ClientUI::GetClientUI()->ZoomToShipDesign(lexical_cast<int>(data));
+            } else if (link_type == VarText::PREDEFINED_DESIGN_TAG) {
+                if (const ShipDesign* design = GetPredefinedShipDesign(data))
+                    ClientUI::GetClientUI()->ZoomToShipDesign(design->ID());
+
+            } else if (link_type == VarText::TECH_TAG) {
+                ClientUI::GetClientUI()->ZoomToTech(data);
+            } else if (link_type == VarText::BUILDING_TYPE_TAG) {
+                ClientUI::GetClientUI()->ZoomToBuildingType(data);
+            } else if (link_type == VarText::FIELD_TYPE_TAG) {
+                ClientUI::GetClientUI()->ZoomToFieldType(data);
+            } else if (link_type == VarText::SPECIAL_TAG) {
+                ClientUI::GetClientUI()->ZoomToSpecial(data);
+            } else if (link_type == VarText::SHIP_HULL_TAG) {
+                ClientUI::GetClientUI()->ZoomToShipHull(data);
+            } else if (link_type == VarText::SHIP_PART_TAG) {
+                ClientUI::GetClientUI()->ZoomToShipPart(data);
+            } else if (link_type == VarText::SPECIES_TAG) {
+                ClientUI::GetClientUI()->ZoomToSpecies(data);
+            } else if (link_type == TextLinker::ENCYCLOPEDIA_TAG) {
+                ClientUI::GetClientUI()->ZoomToEncyclopediaEntry(data);
+            } else if (link_type == TextLinker::GRAPH_TAG) {
+                // todo: this maybe?
+            }
+
+        } catch (const boost::bad_lexical_cast&) {
+            ErrorLogger() << "CombatReportPrivate::HandleLinkClick caught lexical cast exception for link type: " << link_type << " and data: " << data;
+        }
+
+    }
+
+    void HandleLinkDoubleClick(const std::string& link_type, const std::string& data) {
+        HandleLinkClick(link_type, data);
+    }
 
 private:
     CombatReportWnd&        m_wnd;
@@ -55,10 +118,10 @@ private:
     }
 };
 
-CombatReportWnd::CombatReportWnd () :
+CombatReportWnd::CombatReportWnd() :
     CUIWnd(UserString("COMBAT_REPORT_TITLE"), GG::X(150), GG::Y(50), COMBAT_LOG_WIDTH, COMBAT_LOG_HEIGHT,
            GG::INTERACTIVE | GG::RESIZABLE | GG::DRAGABLE | GG::ONTOP | CLOSABLE),
-    m_impl(new CombatReportPrivate(*this) )
+    m_impl(new CombatReportPrivate(*this))
 { }
 
 CombatReportWnd::~CombatReportWnd()
