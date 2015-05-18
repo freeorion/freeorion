f9649ae910e6b5930c78604e80da5c1d30d4fb51
Vezzra <vezzra@gmx.net>
2015-05-18 21:17:18 +0200

    Enhanced placement of home systems
    Home system placement during universe generation now makes sure that home systems have a certain minimum number of planets within their near vicinity. The process tries to pick only systems has home systems that meet this criteria, if that fails, an according number of planets is added to the nearby systems of those home systems that don't meet the criteria.

--- default/python/universe_generation/options.py
+++ default/python/universe_generation/options.py
@@ -1,3 +1,7 @@
+import freeorion as fo
+from planets import planet_types_real, planet_sizes_real
+
+
 ###############################
 ## STAR GROUP NAMING OPTIONS ##
 ###############################
@@ -16,3 +20,27 @@ POSTFIX_STARGROUP_MODIFIERS = True
 TARGET_INDIV_RATIO_SMALL = 0.6  # in small galaxies, 60% of the star systems get individual names
 TARGET_INDIV_RATIO_LARGE = 0.3  # in large galaxies, 30% of the star systems get individual names
 NAMING_LARGE_GALAXY_SIZE = 200  # a galaxy with 200+ star systems is considered as large, below that value as small
+
+
+###################################
+## HOME SYSTEM SELECTION OPTIONS ##
+###################################
+
+# These options are needed in the home system selection/placement process. They determines the minimum number of
+# planets that a home system must have in its near vicinity, and define the extend of this "near vicinity".
+
+# The following two options are used to determine the minimum number of planets. This limit is
+# HS_MIN_PLANETS_IN_VICINITY_PER_SYSTEM planets per system within the near vicinity of a home system, capped at
+# HS_MIN_PLANETS_IN_VICINITY_TOTAL
+HS_MIN_PLANETS_IN_VICINITY_TOTAL = 10
+HS_MIN_PLANETS_IN_VICINITY_PER_SYSTEM = 1
+
+# This option defines the extend of what is considered the "near vicinity" of a home system. This are all systems that
+# are within the number of jumps specified by HS_VICINITY_RANGE.
+HS_VICINITY_RANGE = 3
+
+# These two options define which types of planets are counted when determining the number of planets in the near
+# vicinity of a home system. HS_ACCEPTABLE_PLANET_SIZES is actually only needed for the process of adding planets
+# to the near vicinity of a home system in case that's needed to meet the limit.
+HS_ACCEPTABLE_PLANET_TYPES = planet_types_real + (fo.planetType.asteroids,)
+HS_ACCEPTABLE_PLANET_SIZES = planet_sizes_real + (fo.planetSize.asteroids,)
