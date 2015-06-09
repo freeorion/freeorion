freeorion/freeorion

commit 7b94c925a15586dbabc2750149e0c79538b3c1f4

Move {INVALID,TEMPORARY}_OBJECT_ID into UniverseObject header
Both values are part of the UniverseObject concept.

universe/UniverseObject.h
@@ -23,6 +23,13 @@ class System;
 class SitRepEntry;
 struct UniverseObjectVisitor;
 
+// The ID number assigned to temporary universe objects
+FO_COMMON_API extern const int INVALID_OBJECT_ID;
+
+// The ID number assigned to a UniverseObject upon construction;
+// It is assigned an ID later when it is placed in the universe
+FO_COMMON_API extern const int TEMPORARY_OBJECT_ID;
+
 /** The abstract base class for all objects in the universe
   * The UniverseObject class itself has an ID number, a name, a position, an ID
   * of the system in which it is, a list of zero or more owners, and other

universe/ObjectMap.h
@@ -27,8 +27,6 @@ class Building;
 class Field;
 
 FO_COMMON_API extern const int ALL_EMPIRES;
-FO_COMMON_API extern const int TEMPORARY_OBJECT_ID;
-FO_COMMON_API extern const int INVALID_OBJECT_ID;
 
 /** Contains a set of objects that make up a (known or complete) Universe. */
 class FO_COMMON_API ObjectMap {

universe/Universe.cpp
@@ -333,8 +333,6 @@ namespace SystemPathing {
 using namespace SystemPathing;  // to keep GCC 4.2 on OSX happy
 
 extern const int ALL_EMPIRES            = -1;
-extern const int INVALID_OBJECT_ID      = -1;   //the ID number assigned to a UniverseObject upon construction; it is assigned an ID later when it is placed in the universe
-extern const int TEMPORARY_OBJECT_ID    = -2; // the ID number assigned to temporary objects
 // TODO: implement a robust, thread-safe solution for creating multiple client-local temporary objects with unique IDs that will never conflict with each other or the server.
 extern const int MAX_ID                 = 2000000000;
 

universe/Universe.h
@@ -5,6 +5,7 @@
 #include "Enums.h"
 #include "ObjectMap.h"
 #include "TemporaryPtr.h"
+#include "UniverseObject.h"
 
 #include <boost/signals2/signal.hpp>
 #include <boost/unordered_map.hpp>
@@ -30,7 +31,6 @@ class Empire;
 struct UniverseObjectVisitor;
 class XMLElement;
 class ShipDesign;
-class UniverseObject;
 class System;
 namespace Condition {
     struct ConditionBase;

universe/UniverseObject.cpp
@@ -14,6 +14,9 @@
 
 
 // static(s)
+const int INVALID_OBJECT_ID      = -1;
+const int TEMPORARY_OBJECT_ID    = -2;
+
 const double    UniverseObject::INVALID_POSITION  = -100000.0;
 const int       UniverseObject::INVALID_OBJECT_AGE = -(1 << 30) - 1;  // using big negative number to allow for potential negative object ages, which might be useful in the event of time travel.
 const int       UniverseObject::SINCE_BEFORE_TIME_AGE = (1 << 30) + 1;

commit 12f178ecc941aaafd08296f65e2f3e4ed15a9492

Fix documentation bug introduced by 7b94c92

universe/UniverseObject.h
@@ -23,11 +23,11 @@ class System;
 class SitRepEntry;
 struct UniverseObjectVisitor;
 
-// The ID number assigned to temporary universe objects
-FO_COMMON_API extern const int INVALID_OBJECT_ID;
-
 // The ID number assigned to a UniverseObject upon construction;
 // It is assigned an ID later when it is placed in the universe
+FO_COMMON_API extern const int INVALID_OBJECT_ID;
+
+// The ID number assigned to temporary universe objects
 FO_COMMON_API extern const int TEMPORARY_OBJECT_ID;
 
 /** The abstract base class for all objects in the universe