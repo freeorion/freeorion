6b3c524962f08d85eb994d4d1102356b76c2930b
Mitten-O <Mitten.O@gmail.com>
2015-07-29 14:44:25 +0300

    Create infrastructure for experimental applications.
    This creates a folder, helper application and
    CMake rules for making small binaries that
    build quickly and cna be used to experiment with specific
    pieces of the code base with minimal cycles.
    
    You can create an application simply by creating and
    running an instance of TestApp. There is also a TestDialog
    that will help in testing how windows work inside resizable dialogs.
    
    Included is an example test that tests the GG text control.

---
+++ experiments/GG/TestDialog.cpp
@@ -0,0 +1,28 @@
+#include "TestDialog.h"
+
+#include <GG/ClrConstants.h>
+#include <GG/DrawUtil.h>
+#include <GG/Layout.h>
+#include <GG/Font.h>
+
+TestDialog::TestDialog(GG::Wnd* child, const boost::shared_ptr<GG::Font>& font):
+    GG::Wnd(GG::X0, GG::Y0, GG::X(300), GG::Y(300), GG::DRAGABLE | GG::RESIZABLE | GG::MODAL | GG::INTERACTIVE) {
+    SetLayout(new GG::Layout(GG::X0, GG::Y0, GG::X1, GG::Y1, 1, 1, 2, 2));
+    GetLayout()->SetColumnStretch(0, 1.0);
+    //GetLayout()->SetRowStretch(0, 1.0);
+    GetLayout()->Add(child, 0 , 0);
+}
+
+void TestDialog::Render() {
+    GG::Wnd::Render();
+    GG::FlatRectangle ( UpperLeft(), LowerRight(),
+                        GG::CLR_GRAY, GG::CLR_SHADOW, 1u );
+}
+
+GG::Pt TestDialog::ClientLowerRight() const {
+    return LowerRight() - GG::Pt(GG::X(10), GG::Y(10) );
+}
+
+void TestDialog::SizeMove( const GG::Pt& ul, const GG::Pt& lr) {
+    GG::Wnd::SizeMove( ul, lr );
+}
