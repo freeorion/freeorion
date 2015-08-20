d83d2e2227ef0f6cc3add40daf44503969891c09
Dilvish-fo <dilvish.fo@gmail.com>
2015-08-20 00:47:21 -0700

    reverted commit 36215a63c49df03d4f00e9541722a8d4587d11d8 re left-click dependence from the ProductionQueue quantity and block size dropdowns; it appeared to create some crashing instabilities which we hadn't been able to track down yet.

--- UI/ProductionWnd.cpp
+++ UI/ProductionWnd.cpp
@@ -168,9 +168,6 @@ namespace {
                     quantity = quant;
                 }
             }
-
-            if ((quantity != prevQuant) || (blocksize != prevBlocksize))
-                QuantChangedSignal(quantity, blocksize);
         }
 
     private:
@@ -182,6 +179,25 @@ namespace {
         bool    amBlockType;
         bool    amOn;
         GG::Y   h;
+
+        void LosingFocus() {
+            amOn = false;
+            DropDownList::LosingFocus();
+        }
+
+        void LButtonDown(const GG::Pt&  pt, GG::Flags<GG::ModKey> mod_keys) {
+            amOn = !amOn;
+            DropDownList::LButtonDown(pt, mod_keys);
+        }
+
+        void LClick(const GG::Pt& pt, GG::Flags<GG::ModKey> mod_keys) {
+            if (this->Disabled())
+                return;
+
+            DropDownList::LClick(pt, mod_keys);
+            if ( (quantity != prevQuant) || (blocksize != prevBlocksize) )
+                QuantChangedSignal(quantity, blocksize);
+        }
     };
 
     //////////////////////////////////////////////////
