8f18ba796fe92f4498211fa68e10579da69f2d12
Szunti <Szunti@users.noreply.github.com>
2015-10-19 15:44:54 +0200

    Fix Multiedit alignment flags validating.

--- GG/src/MultiEdit.cpp
+++ GG/src/MultiEdit.cpp
@@ -1013,7 +1013,7 @@ void MultiEdit::ValidateStyle()
     if (m_style & MULTI_RIGHT) ++dup_ct;
     if (m_style & MULTI_CENTER) ++dup_ct;
     if (dup_ct != 1) {   // exactly one must be picked; when none or multiples are picked, use MULTI_LEFT by default
-        m_style &= ~(MULTI_RIGHT | MULTI_LEFT);
+        m_style &= ~(MULTI_RIGHT | MULTI_CENTER);
         m_style |= MULTI_LEFT;
     }
 
