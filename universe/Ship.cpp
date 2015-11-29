freeorion/freeorion

commit a72d77ff322388e1afa08709b3096943805f3e19

-Added max capacity meters to weapon part types -Modified weapon stats to alter the max capacity part meters -Made weapon part capacity meters be treated like persistent meters, with a cap given by the associated max capacity. -Added Ship-specific BackPropegateMeters function, to handle ship part meters -Renamed PC_SHORT_RANGE enum to PC_DIRECT_WEAPON -Removed missile and point-defense part type enums -Incomplete handling of flighter class weapons discintly from direct fire weapons. Doesn't affect combat yet. -grooming

universe/Ship.cpp
@@ -75,13 +75,15 @@ Ship::Ship(int empire_id, int design_id, const std::string& species_name,
             }
 
             switch (part->Class()) {
-            case PC_SHORT_RANGE:
-            case PC_MISSILES:
-            case PC_FIGHTERS:
-            case PC_POINT_DEFENSE:
             case PC_COLONY:
             case PC_TROOPS: {
                 m_part_meters[std::make_pair(METER_CAPACITY, part->Name())];
+                break;
+            }
+            case PC_DIRECT_WEAPON:
+            case PC_FIGHTERS: {
+                m_part_meters[std::make_pair(METER_CAPACITY,    part->Name())];
+                m_part_meters[std::make_pair(METER_MAX_CAPACITY,part->Name())];
                 break;
             }
             default:
@@ -438,6 +440,10 @@ std::vector<float> Ship::AllWeaponsDamage(float shield_DR /* = 0.0 */) const {
         return retval;
     const std::vector<std::string>& parts = design->Parts();
 
+    // TODO: get empire fighter damage
+    float empire_fighter_damage = 4.0f - shield_DR;
+
+
     // for each weapon part, get its damage meter value
     for (std::vector<std::string>::const_iterator part_it = parts.begin();
          part_it != parts.end(); ++part_it)
@@ -449,16 +455,15 @@ std::vector<float> Ship::AllWeaponsDamage(float shield_DR /* = 0.0 */) const {
         ShipPartClass part_class = part->Class();
 
         // get the attack power for each weapon part
-        float part_attack = 0.0;
-
-        if (part_class == PC_SHORT_RANGE    || part_class == PC_POINT_DEFENSE ||
-            part_class == PC_MISSILES       || part_class == PC_FIGHTERS)
-        {
-            part_attack = this->CurrentPartMeterValue(METER_CAPACITY, part_name);
+        if (part_class == PC_DIRECT_WEAPON) {
+            float part_attack = this->CurrentPartMeterValue(METER_CAPACITY, part_name);
+            if (part_attack > shield_DR)
+                retval.push_back(part_attack - shield_DR);
+        } else if (part_class == PC_FIGHTERS && empire_fighter_damage > 0.0f) {
+            // each fighter (number determined by capacity) shoots with the empire fighter strength stat
+            float all_fighters_combined_attack = empire_fighter_damage * this->CurrentPartMeterValue(METER_CAPACITY, part_name);
+            retval.push_back(all_fighters_combined_attack);
         }
-
-        if (part_attack > shield_DR)
-            retval.push_back(part_attack-shield_DR);
     }
     return retval;
 }
@@ -470,6 +475,14 @@ void Ship::SetFleetID(int fleet_id) {
     }
 }
 
+void Ship::BackPropegateMeters() {
+    UniverseObject::BackPropegateMeters();
+
+    // ship part meter back propegation, since base class function doesn't do this...
+    for (PartMeterMap::iterator it = m_part_meters.begin(); it != m_part_meters.end(); ++it)
+        it->second.BackPropegate();
+}
+
 void Ship::Resupply() {
     Meter* fuel_meter = UniverseObject::GetMeter(METER_FUEL);
     const Meter* max_fuel_meter = UniverseObject::GetMeter(METER_MAX_FUEL);
@@ -479,6 +492,22 @@ void Ship::Resupply() {
     }
 
     fuel_meter->SetCurrent(max_fuel_meter->Current());
+
+    // set all part capacities equal to any associated max capacity
+    // this "upgrades" any direct-fire weapon parts to their latest-allowed
+    // strengths, and replaces any lost fighters
+
+    for (PartMeterMap::iterator it = m_part_meters.begin(); it != m_part_meters.end(); ++it) {
+        if (it->first.first != METER_CAPACITY)
+            continue;
+        PartMeterMap::iterator max_it = m_part_meters.find(std::make_pair(METER_MAX_CAPACITY, it->first.second));
+        if (max_it == m_part_meters.end())
+            continue;
+
+        const Meter& max_meter = max_it->second;
+
+        it->second.SetCurrent(max_meter.Current());
+    }
 }
 
 void Ship::SetSpecies(const std::string& species_name) {
@@ -534,8 +563,15 @@ void Ship::ResetTargetMaxUnpairedMeters() {
     UniverseObject::GetMeter(METER_SPEED)->ResetCurrent();
     UniverseObject::GetMeter(METER_SPEED)->ResetCurrent();
 
-    for (PartMeterMap::iterator it = m_part_meters.begin(); it != m_part_meters.end(); ++it)
-    { it->second.ResetCurrent(); }
+    for (PartMeterMap::iterator it = m_part_meters.begin(); it != m_part_meters.end(); ++it) {
+       if (it->first.first == METER_CAPACITY) {
+            // special case for capacity... if it has an associated max capacity, don't reset it
+            PartMeterMap::iterator max_it = m_part_meters.find(std::make_pair(METER_MAX_CAPACITY, it->first.second));
+            if (max_it != m_part_meters.end())
+                continue;
+        }
+        it->second.ResetCurrent();
+    }
 }
 
 void Ship::SetShipMetersToMax() {
@@ -545,6 +581,17 @@ void Ship::SetShipMetersToMax() {
     UniverseObject::GetMeter(METER_FUEL)->SetCurrent(Meter::LARGE_VALUE);
     UniverseObject::GetMeter(METER_SHIELD)->SetCurrent(Meter::LARGE_VALUE);
     UniverseObject::GetMeter(METER_STRUCTURE)->SetCurrent(Meter::LARGE_VALUE);
+
+    // some part capacity meters may have an associated max capacity...
+    for (PartMeterMap::iterator it = m_part_meters.begin(); it != m_part_meters.end(); ++it) {
+        if (it->first.first != METER_CAPACITY)
+            continue;
+        PartMeterMap::iterator max_it = m_part_meters.find(std::make_pair(METER_MAX_CAPACITY, it->first.second));
+        if (max_it == m_part_meters.end())
+            continue;
+
+        it->second.SetCurrent(Meter::LARGE_VALUE);
+    }
 }
 
 void Ship::PopGrowthProductionResearchPhase() {
@@ -554,6 +601,8 @@ void Ship::PopGrowthProductionResearchPhase() {
     UniverseObject::GetMeter(METER_INDUSTRY)->SetCurrent(Ship::NextTurnCurrentMeterValue(METER_INDUSTRY));
     UniverseObject::GetMeter(METER_RESEARCH)->SetCurrent(Ship::NextTurnCurrentMeterValue(METER_RESEARCH));
     UniverseObject::GetMeter(METER_TRADE)->SetCurrent(Ship::NextTurnCurrentMeterValue(METER_TRADE));
+
+    // part capacity meters set to max only by being in supply
 
     StateChangedSignal();
 }
@@ -578,8 +627,26 @@ void Ship::ClampMeters() {
     UniverseObject::GetMeter(METER_SPEED)->ClampCurrentToRange();
     UniverseObject::GetMeter(METER_SPEED)->ClampCurrentToRange();
 
-    for (PartMeterMap::iterator it = m_part_meters.begin(); it != m_part_meters.end(); ++it)
+    // clamp most part meters to basic range limits
+    for (PartMeterMap::iterator it = m_part_meters.begin(); it != m_part_meters.end(); ++it) {
+        if (it->first.first == METER_CAPACITY)
+            continue;
         it->second.ClampCurrentToRange();
+    }
+
+    // special case extra clamping for paired CAPACTY meters dependent on their associated MAX_CAPACITY meter...
+    for (PartMeterMap::iterator it = m_part_meters.begin(); it != m_part_meters.end(); ++it) {
+        if (it->first.first != METER_CAPACITY)
+            continue;
+        PartMeterMap::iterator max_it = m_part_meters.find(std::make_pair(METER_MAX_CAPACITY, it->first.second));
+        if (max_it == m_part_meters.end()) {
+            // no found max capacity meter, revert to normal clamping for this capacity meter
+            it->second.ClampCurrentToRange();
+            continue;
+        }
+        const Meter& max_meter = max_it->second;
+        it->second.ClampCurrentToRange(Meter::DEFAULT_VALUE, max_meter.Current());
+    }
 }
 
 ////////////////////

Empire/Empire.cpp
@@ -3047,10 +3047,8 @@ void Empire::CheckProductionProgress() {
                     // later clamped, they will equal the max meter after effects
                     // have been applied, letting new ships start with maxed
                     // everything that is traced with an associated max meter.
-                    ship->UniverseObject::GetMeter(METER_FUEL)->SetCurrent(Meter::LARGE_VALUE);
-                    ship->UniverseObject::GetMeter(METER_SHIELD)->SetCurrent(Meter::LARGE_VALUE);
-                    ship->UniverseObject::GetMeter(METER_STRUCTURE)->SetCurrent(Meter::LARGE_VALUE);
-                    ship->BackPropegateMeters();
+                    ship->SetShipMetersToMax();
+
 
                     ship->Rename(NewShipName());
 

UI/DesignWnd.cpp
@@ -87,10 +87,7 @@ namespace {
         if (!part_type)
             return 0.0f;
         switch (part_type->Class()) {
-            case PC_SHORT_RANGE:
-            case PC_POINT_DEFENSE:
-            case PC_MISSILES:
-            case PC_FIGHTERS:
+            case PC_DIRECT_WEAPON:
             case PC_SHIELD:
             case PC_DETECTION:
             case PC_STEALTH:
@@ -102,6 +99,9 @@ namespace {
             case PC_RESEARCH:
             case PC_INDUSTRY:
             case PC_TRADE:
+                return part_type->Capacity();
+                break;
+            case PC_FIGHTERS:   // fighters may have additional stats, so might need to do something special for their "main" stat that just capacity...
                 return part_type->Capacity();
                 break;
             case PC_GENERAL:

UI/MeterBrowseWnd.cpp
@@ -478,7 +478,7 @@ void ShipDamageBrowseWnd::UpdateEffectLabelsAndValues(GG::Y& top) {
 
     std::string     name = ship->Name();
     const std::string& label_template = UserString("TT_SHIP_PART");
-    
+
     // for each weapon part, get its damage meter value
     for (std::vector<std::string>::const_iterator part_it = parts.begin();
          part_it != parts.end(); ++part_it)
@@ -488,7 +488,7 @@ void ShipDamageBrowseWnd::UpdateEffectLabelsAndValues(GG::Y& top) {
         if (!part)
             continue;
         ShipPartClass part_class = part->Class();
-        if (!(part_class == PC_SHORT_RANGE || part_class == PC_POINT_DEFENSE || part_class == PC_MISSILES || part_class == PC_FIGHTERS))
+        if (!(part_class == PC_DIRECT_WEAPON || part_class == PC_FIGHTERS))
             continue;
 
         // get the attack power for each weapon part
@@ -496,7 +496,7 @@ void ShipDamageBrowseWnd::UpdateEffectLabelsAndValues(GG::Y& top) {
         std::string text = boost::io::str(FlexibleFormat(label_template)
                                 % name
                                 % UserString(part_name));
-        
+
         GG::Label* label = new CUILabel(text, GG::FORMAT_RIGHT);
         label->MoveTo(GG::Pt(GG::X0, top));
         label->Resize(GG::Pt(METER_BROWSE_LABEL_WIDTH, m_row_height));

combat/CombatSystem.cpp
@@ -526,11 +526,8 @@ namespace {
             // get the attack power for each weapon part
             float part_attack = 0.0f;
 
-            if (part_class == PC_SHORT_RANGE    || part_class == PC_POINT_DEFENSE ||
-                part_class == PC_MISSILES       || part_class == PC_FIGHTERS)
-            {
+            if (part_class == PC_DIRECT_WEAPON || part_class == PC_FIGHTERS)
                 part_attack = ship->CurrentPartMeterValue(METER_CAPACITY, part_name);
-            }
 
             if (part_attack > 0.0f)
                 retval.push_back(PartAttackInfo(part_class, part_name, part_attack));
@@ -932,7 +929,7 @@ namespace {
                 }
             }
         } else if (attack_planet) { // treat planet defenses as short range
-            weapons.push_back(PartAttackInfo(PC_SHORT_RANGE, "", attack_planet->CurrentMeterValue(METER_DEFENSE)));
+            weapons.push_back(PartAttackInfo(PC_DIRECT_WEAPON, "", attack_planet->CurrentMeterValue(METER_DEFENSE)));
         }
         return weapons;
     }

parse/EnumParser.cpp
@@ -225,10 +225,8 @@ namespace parse {
     {
         const parse::lexer& tok = parse::lexer::instance();
         static enum_parser_rule<ShipPartClass>::type retval
-            =   tok.ShortRange_         [ _val = PC_SHORT_RANGE ]
-            |   tok.Missiles_           [ _val = PC_MISSILES ]
+            =   tok.ShortRange_         [ _val = PC_DIRECT_WEAPON ]
             |   tok.Fighters_           [ _val = PC_FIGHTERS ]
-            |   tok.PointDefense_       [ _val = PC_POINT_DEFENSE ]
             |   tok.Shield_             [ _val = PC_SHIELD ]
             |   tok.Armour_             [ _val = PC_ARMOUR ]
             |   tok.Troops_             [ _val = PC_TROOPS ]

parse/IntComplexValueRefParser.cpp
@@ -90,8 +90,8 @@ namespace parse {
                         //    [ _d = new_<ValueRef::Constant<std::string> >(_1) ]
                         //  )
                         > ( parse::label(Class_token) >
-                            ( tok.ShortRange_       | tok.Missiles_     | tok.Fighters_
-                            | tok.PointDefense_     | tok.Shield_       | tok.Armour_
+                            ( tok.ShortRange_       | tok.Fighters_     | tok.Shield_
+                            | tok.Armour_
                             | tok.Troops_           | tok.Detection_    | tok.Stealth_
                             | tok.Fuel_             | tok.Colony_       | tok.Speed_
                             | tok.General_          | tok.Bombard_      | tok.Research_

parse/ShipPartsParser.cpp
@@ -72,16 +72,16 @@ namespace {
             using phoenix::push_back;
 
             part_type_prefix
-                =    tok.Part_
-                >    parse::label(Name_token)        > tok.string [ _r1 = _1 ]
-                >    parse::label(Description_token) > tok.string [ _r2 = _1 ]
-                >    parse::label(Class_token)       > parse::enum_parser<ShipPartClass>() [ _r3 = _1 ]
+                =   tok.Part_
+                >   parse::label(Name_token)        > tok.string [ _r1 = _1 ]
+                >   parse::label(Description_token) > tok.string [ _r2 = _1 ]
+                >   parse::label(Class_token)       > parse::enum_parser<ShipPartClass>() [ _r3 = _1 ]
                 ;
 
             producible
-                =    tok.Unproducible_ [ _val = false ]
-                |    tok.Producible_ [ _val = true ]
-                |    eps [ _val = true ]
+                =   tok.Unproducible_ [ _val = false ]
+                |   tok.Producible_ [ _val = true ]
+                |   eps [ _val = true ]
                 ;
 
             slots
@@ -95,8 +95,8 @@ namespace {
                 ;
 
             location
-                =    parse::label(Location_token) > parse::detail::condition_parser [ _r1 = _1 ]
-                |    eps [ _r1 = new_<Condition::All>() ]
+                =   parse::label(Location_token) > parse::detail::condition_parser [ _r1 = _1 ]
+                |   eps [ _r1 = new_<Condition::All>() ]
                 ;
 
             common_params
@@ -105,21 +105,19 @@ namespace {
                 >   producible                                           [ _c = _1 ]
                 >   parse::detail::tags_parser()(_d)
                 >   location(_e)
-                >   -(
-                        parse::label(EffectsGroups_token) > parse::detail::effects_group_parser() [ _f = _1 ]
-                     )
-                >    parse::label(Icon_token)        > tok.string
+                > -(parse::label(EffectsGroups_token) > parse::detail::effects_group_parser() [ _f = _1 ])
+                >   parse::label(Icon_token)        > tok.string
                     [ _val = construct<PartHullCommonParams>(_a, _b, _c, _d, _e, _f, _1) ]
             ;
 
             part_type
-                =    part_type_prefix(_a, _b, _c)
-                >    (  ( parse::label(Capacity_token)  >> parse::double_ [ _d = _1 ])
-                      | ( parse::label(Damage_token)    >> parse::double_ [ _d = _1 ])
-                      |   eps [ _d = 0.0 ]
-                     )
-                >    slots(_f)
-                >    common_params [ _e = _1 ]
+                =   part_type_prefix(_a, _b, _c)
+                > (  (parse::label(Capacity_token)  >> parse::double_ [ _d = _1 ])
+                   | (parse::label(Damage_token)    >> parse::double_ [ _d = _1 ])
+                   |  eps [ _d = 0.0 ]
+                  )
+                >   slots(_f)
+                >   common_params [ _e = _1 ]
                     [ insert(_r1, new_<PartType>(_a, _b, _c, _d, _e, _f)) ]
                 ;
 

commit c9dfb641be4d47ccc2b1d6032bf25c0ade419bdc

Fixed issues with weapon ship part meters introduced by a72d77f
Changing the capacity ship part meter for weapons to a paired meter introduced several issues: space monsters that are created by the CreateShip effect had all their weapon strength set to 0, weapon species picks didn't get calculated correctly anymore, and the weapon strength of ships an empire has only partial visibility of were displayed incorrectly. This is an attempt to fix these issues.

There are other parts in the content scripts that affect weapon strength (experimenter outpost, head on a spike special) that might still be broken.

universe/Ship.cpp
@@ -134,7 +134,7 @@ void Ship::Copy(TemporaryPtr<const UniverseObject> copied_object, int empire_id)
             this->m_design_id =             copied_ship->m_design_id;
             for (PartMeterMap::const_iterator it = copied_ship->m_part_meters.begin();
                  it != copied_ship->m_part_meters.end(); ++it)
-            { this->m_part_meters[it->first]; }
+            { this->m_part_meters = copied_ship->m_part_meters; }
             this->m_species_name =          copied_ship->m_species_name;
 
             if (vis >= VIS_FULL_VISIBILITY) {
@@ -143,7 +143,6 @@ void Ship::Copy(TemporaryPtr<const UniverseObject> copied_object, int empire_id)
                 this->m_ordered_invade_planet_id  = copied_ship->m_ordered_invade_planet_id;
                 this->m_ordered_bombard_planet_id = copied_ship->m_ordered_bombard_planet_id;
                 this->m_last_turn_active_in_combat= copied_ship->m_last_turn_active_in_combat;
-                this->m_part_meters =               copied_ship->m_part_meters;
                 this->m_produced_by_empire_id =     copied_ship->m_produced_by_empire_id;
             }
         }
@@ -590,6 +589,7 @@ void Ship::SetShipMetersToMax() {
         if (max_it == m_part_meters.end())
             continue;
 
+        max_it->second.SetCurrent(Meter::LARGE_VALUE);
         it->second.SetCurrent(Meter::LARGE_VALUE);
     }
 }