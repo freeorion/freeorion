bcef7807319a9d44c873b679906af971e266c901
geoffthemedio <geoffthemedio@gmail.com>
2016-01-01 19:12:17 -0500

    Fixed multiple hangar parts not stacking.

--- combat/CombatSystem.cpp
+++ combat/CombatSystem.cpp
@@ -734,7 +734,7 @@ namespace {
                 }
 
             } else if (part_class == PC_FIGHTER_BAY) {
-                part_fighter_launch_capacities[part_name] = ship->CurrentPartMeterValue(METER_CAPACITY, part_name);
+                part_fighter_launch_capacities[part_name] += ship->CurrentPartMeterValue(METER_CAPACITY, part_name);
 
             } else if (part_class == PC_FIGHTER_WEAPON) {
                 fighter_attack = std::max(fighter_attack, ship->CurrentPartMeterValue(METER_CAPACITY, part_name));
