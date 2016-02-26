bce98797e652fbf8f5c61e66a01cbd109a55a34e
Vezzra <vezzra@gmx.net>
2016-02-26 16:18:30 +0100

    Universe generation: fixed non-reproducible distribution of space monsters even for the same seed
    In the generate_monsters function monster fleet plans have been stored in and randomly selected from a dict, which resulted in different space monster distribution even for the same RNG seed. Fixed that by using a list to store the monster fleet plans in addition to the dict which is still required to keep track of how often a monster fleet has already been spawned.

--- default/python/universe_generation/monsters.py
+++ default/python/universe_generation/monsters.py
@@ -22,16 +22,22 @@ def generate_monsters(monster_freq, systems):
     actual_tally = 0
 
     # get all monster fleets that have a spawn rate and limit both > 0 and at least one monster ship design in it
-    # (a monster fleet with no monsters in it is pointless) and store them with a spawn counter in a dict
+    # (a monster fleet with no monsters in it is pointless) and store them in a list
+    fleet_plans = fo.load_monster_fleet_plan_list("space_monster_spawn_fleets.txt")
+
+    # create a map where we store a spawn counter for each monster fleet
     # this counter will be set to the spawn limit initially and decreased every time the monster fleet is spawned
-    fleet_plans = {fp: fp.spawn_limit() for fp in fo.load_monster_fleet_plan_list("space_monster_spawn_fleets.txt")
-                   if fp.spawn_rate() > 0.0 and fp.spawn_limit() > 0 and fp.ship_designs()}
+    # this map (dict) needs to be separate from the list holding the fleet plans because the order in which items
+    # are stored in a dict is undefined (can be different each time), which would result in different distribution
+    # even when using the same seed for the RNG
+    spawn_limits = {fp: fp.spawn_limit() for fp in fleet_plans
+                    if fp.spawn_rate() > 0.0 and fp.spawn_limit() > 0 and fp.ship_designs()}
 
     # map nests to monsters for ease of reporting
     nest_name_map = dict(zip(["KRAKEN_NEST_SPECIAL", "SNOWFLAKE_NEST_SPECIAL", "JUGGERNAUT_NEST_SPECIAL"], ["SM_KRAKEN_1", "SM_SNOWFLAKE_1", "SM_JUGGERNAUT_1"]))
     tracked_plan_tries = {name: 0 for name in nest_name_map.values()}
     tracked_plan_counts = {name: 0 for name in nest_name_map.values()}
-    tracked_plan_valid_locations = {fp: 0 for fp, limit in fleet_plans.iteritems() if fp.name() in tracked_plan_counts}
+    tracked_plan_valid_locations = {fp: 0 for fp in fleet_plans if fp.name() in tracked_plan_counts}
     tracked_nest_valid_locations = {nest: 0 for nest in nest_name_map}
 
     if not fleet_plans:
@@ -67,7 +73,7 @@ def generate_monsters(monster_freq, systems):
                 tracked_plan_valid_locations[fp] += 1
 
         # filter out all monster fleets whose location condition allows this system and whose counter hasn't reached 0
-        suitable_fleet_plans = [fp for fp, counter in fleet_plans.iteritems() if counter and fp.location(system)]
+        suitable_fleet_plans = [fp for fp in fleet_plans if spawn_limits[fp] and fp.location(system)]
         # if there are no suitable monster fleets for this system, continue with the next
         if not suitable_fleet_plans:
             continue
@@ -89,7 +95,7 @@ def generate_monsters(monster_freq, systems):
         # all prerequisites and the test have been met, now spawn this monster fleet in this system
         print "Spawn", fleet_plan.name(), "at", fo.get_name(system)
         # decrement counter for this monster fleet
-        fleet_plans[fleet_plan] -= 1
+        spawn_limits[fleet_plan] -= 1
         # create monster fleet
         monster_fleet = fo.create_monster_fleet(system)
         # if fleet creation fails, report an error and try to continue with next system
@@ -104,7 +110,7 @@ def generate_monsters(monster_freq, systems):
 
     print "Actual # monster fleets placed: %d; Total Placement Expectation: %.1f" % (actual_tally, expectation_tally)
     # finally, compile some statistics to be dumped to the log later
-    statistics.monsters_summary = [(fp.name(), fp.spawn_limit() - counter) for fp, counter in fleet_plans.iteritems()]
+    statistics.monsters_summary = [(fp.name(), fp.spawn_limit() - counter) for fp, counter in spawn_limits.iteritems()]
     statistics.tracked_monsters_tries.update(tracked_plan_tries)
     statistics.tracked_monsters_summary.update(tracked_plan_counts)
     statistics.tracked_monsters_location_summary.update([(fp.name(), count) for fp, count in tracked_plan_valid_locations.iteritems()])
