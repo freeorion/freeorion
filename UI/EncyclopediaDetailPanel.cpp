14acaf56ffe65c25ffe45e88b4627bc301a58168
geoffthemedio <geoffthemedio@gmail.com>
2016-03-31 13:08:21 +0200

    Added exclusions to hull pedia entries.

--- UI/EncyclopediaDetailPanel.cpp
+++ UI/EncyclopediaDetailPanel.cpp
@@ -1132,6 +1132,20 @@ namespace {
             % hull->Speed()
             % hull->Structure());
 
+        const std::set<std::string>& exclusions = hull->Exclusions();
+        if (!exclusions.empty()) {
+            detailed_description += "\n\n" + UserString("ENC_SHIP_EXCLUSIONS");
+            for (std::set<std::string>::const_iterator ex_it = exclusions.begin(); ex_it != exclusions.end(); ++ex_it) {
+                if (const PartType* ex_part = GetPartType(*ex_it)) {
+                    detailed_description += LinkTaggedText(VarText::SHIP_PART_TAG, *ex_it) + "  ";
+                } else if (const HullType* ex_hull = GetHullType(*ex_it)) {
+                    detailed_description += LinkTaggedText(VarText::SHIP_HULL_TAG, *ex_it) + "  ";
+                } else {
+                    // unknown exclusion...?
+                }
+            }
+        }
+
         std::vector<std::string> unlocked_by_techs = TechsThatUnlockItem(ItemSpec(UIT_SHIP_HULL, item_name));
         if (!unlocked_by_techs.empty()) {
             detailed_description += "\n\n" + UserString("ENC_UNLOCKED_BY");
