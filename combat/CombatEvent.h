0bbd91ee5c479d75bcffa2d52a8203b28c1a8f31
LGM-Doyle <LGM-Doyle@users.noreply.github.com>
2016-04-25 12:02:13 -0400

    Add CombatEvent::CombatLogDetails and CombatEvent::SubEvents
    CombatLogDetails provides a more expansive description of a CombatEvent
    SubEvents provide a list of smaller events whose collective results make
    up the CombatEvent

--- combat/CombatEvent.h
+++ combat/CombatEvent.h
@@ -4,8 +4,14 @@
 #include <boost/shared_ptr.hpp>
 #include <boost/serialization/access.hpp>
 #include <boost/serialization/export.hpp>
+#include <vector>
 
 #include "../util/Export.h"
+
+// Should be unique_ptr, but we don't have c++11
+struct CombatEvent;
+typedef boost::shared_ptr<CombatEvent> CombatEventPtr;
+typedef boost::shared_ptr<const CombatEvent> ConstCombatEventPtr;
 
 /// An abstract base class for combat events
 struct FO_COMMON_API CombatEvent {
@@ -18,6 +24,16 @@ struct FO_COMMON_API CombatEvent {
         Describe the result of a combat event (i.e. what happened). */
     virtual std::string CombatLogDescription(int viewing_empire_id) const = 0;
 
+    /** Generate the combat log details.
+        Describe how it happened in enough detail to avoid a trip to the Pedia. */
+    virtual std::string CombatLogDetails(int empire_id) const
+    { return std::string(""); }
+
+    /** If the combat event is composed of smaller events then return a vector of the sub events,
+        otherwise returns an empty vector.
+    */
+    virtual std::vector<ConstCombatEventPtr> SubEvents(int viewing_empire_id) const
+    { return std::vector<ConstCombatEventPtr>(); }
 
 private:
     friend class boost::serialization::access;
@@ -26,8 +42,5 @@ private:
 };
 
 BOOST_CLASS_EXPORT_KEY(CombatEvent)
-
-// Should be unique_ptr, but we don't have c++11
-typedef boost::shared_ptr<CombatEvent> CombatEventPtr;
 
 #endif // COMBATEVENT_H
