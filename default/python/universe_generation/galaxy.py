5a2dfbc7dc46feb36c5ff684566027729b4352b6
LGM-Doyle <LGM-Doyle@users.noreply.github.com>
2016-05-04 07:44:20 -0400

    Move get_systems_within_jumps from galaxy.py to empires.py
    get_systems_within_jumps is only called from empires.py and galaxy.py
    deals with placing stars before starlanes are created.

--- default/python/universe_generation/galaxy.py
+++ default/python/universe_generation/galaxy.py
@@ -45,27 +45,6 @@ def calc_universe_width(shape, size):
     return width
 
 
-def get_systems_within_jumps(origin_system, jumps):
-    """
-    Returns all systems within jumps jumps of system origin_system (including origin_system).
-    If jumps is 0, return list that only contains system origin_system.
-    If jumps is negative, return empty list.
-    """
-    if jumps < 0:
-        return []
-    matching_systems = [origin_system]
-    next_origin_systems = [origin_system]
-    while jumps > 0:
-        origin_systems = list(next_origin_systems)
-        next_origin_systems = []
-        for system in origin_systems:
-            neighbor_systems = [s for s in fo.sys_get_starlanes(system) if s not in matching_systems]
-            next_origin_systems.extend(neighbor_systems)
-            matching_systems.extend(neighbor_systems)
-        jumps -= 1
-    return matching_systems
-
-
 def spiral_galaxy_calc_positions(positions, arms, size, width):
     """
     Calculate positions for the spiral galaxy shapes.
