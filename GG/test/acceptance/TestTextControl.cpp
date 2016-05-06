7222ee0c8f23f1edf7011434067289c845718af7
LGM-Doyle <LGM-Doyle@users.noreply.github.com>
2016-05-06 14:35:13 -0400

    C++ sampler: Declare variables with auto in TestTextControl
    auto uses the same type inference from templates to determine the type
    of variables.  It ensures that the type remains correct when upstream
    code changes without breaking downstream code.

--- GG/test/acceptance/TestTextControl.cpp
+++ GG/test/acceptance/TestTextControl.cpp
@@ -11,14 +11,14 @@ int main(int argc, char* argv[])
 {
     Application app(argc, argv);
 
-    const std::string message = "Are <i>we</i> Готово yet?"; // That Russian word means "Done", ha.
-    const std::set<GG::UnicodeCharset> charsets_ = GG::UnicodeCharsetsToRender(message);
+    const auto message = "Are <i>we</i> Готово yet?"; // That Russian word means "Done", ha.
+    const auto charsets_ = GG::UnicodeCharsetsToRender(message);
     const std::vector<GG::UnicodeCharset> charsets(charsets_.begin(), charsets_.end());
 
-    const std::shared_ptr<GG::Font> font =
+    const auto font =
     GG::GUI::GetGUI()->GetStyleFactory()->DefaultFont(12, &charsets[0], &charsets[0] + charsets.size());
 
-    GG::Wnd* quit_dlg =
+    auto* quit_dlg =
     new GG::ThreeButtonDlg(GG::X(10), GG::Y(10), message, font, GG::CLR_SHADOW,
                            GG::CLR_SHADOW, GG::CLR_SHADOW, GG::CLR_WHITE, 1);
 
