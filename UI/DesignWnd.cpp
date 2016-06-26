2bea7bff09884fb08f2d5da3d1a0697c1e859037
LGM-Doyle <LGM-Doyle@users.noreply.github.com>
2016-06-26 08:24:40 -0400

    In DesignWnd.cpp remove redundant SizeMove statement.

--- UI/DesignWnd.cpp
+++ UI/DesignWnd.cpp
@@ -1033,13 +1033,6 @@ void DesignWnd::PartPalette::DoLayout() {
         ++col;
     }
 
-    // place parts list.  note: assuming at least as many rows of class buttons
-    //                          as availability buttons, as should be the case
-    //                          given how num_non_class_buttons_per_row is determined
-    m_parts_list->SizeMove(GG::Pt(GG::X0, BUTTON_EDGE_PAD + ROW_OFFSET*(row + 1)),
-                           ClientSize() - GG::Pt(GG::X(2*BUTTON_SEPARATION), GG::Y(2*BUTTON_SEPARATION)));
-
-
     // place parts list.  note: assuming at least as many rows of class buttons as availability buttons, as should
     //                          be the case given how num_non_class_buttons_per_row is determined
     m_parts_list->SizeMove(GG::Pt(GG::X0, BUTTON_EDGE_PAD + ROW_OFFSET*(row + 1)), ClientSize() - GG::Pt(GG::X(BUTTON_SEPARATION), GG::Y(BUTTON_SEPARATION)));
