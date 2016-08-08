56444b2d4d5837f064756f75773af950a2defaf8
Cjkjvfnby <Cjkjvfnby@gmail.com>
2016-08-08 23:59:43 +0300

    - revert some extra cleanup

--- default/python/universe_generation/planets.py
+++ default/python/universe_generation/planets.py
@@ -4,6 +4,7 @@ import freeorion as fo
 import util
 import universe_tables as tables
 
+
 # tuple of all valid planet sizes (with "no world")
 planet_sizes_all = (fo.planetSize.tiny, fo.planetSize.small, fo.planetSize.medium, fo.planetSize.large,
                     fo.planetSize.huge, fo.planetSize.asteroids, fo.planetSize.gasGiant, fo.planetSize.noWorld)
