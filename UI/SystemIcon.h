7ab376feb7f9bee59a707b57144c03241a13d088
D Benage <dbenage.cx@gmail.com>
2016-09-04 00:37:11 -0500

    Use definition tag in place of FUNCTIONAL_SHIPYARD_BUILDING_LIST

--- UI/SystemIcon.h
+++ UI/SystemIcon.h
@@ -4,6 +4,14 @@
 #include <GG/GGFwd.h>
 #include <GG/Control.h>
 
+/** @content_tag{CTRL_SHIPYARD} Building is to be treated as a shipyard when formatting containing objects
+ * 
+ * For objects containing a building with this tag:
+ * * Planets have an underlined name in the sidepanel
+ * * Systems have their name underlined on the map and sidepanel
+ */
+const std::string TAG_SHIPYARD = "CTRL_SHIPYARD";
+
 class FleetButton;
 class RotatingGraphic;
 
