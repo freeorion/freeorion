605ce2c7216a37be9af33c8d87700ceca426c9c6
LGM-Doyle <LGM-Doyle@users.noreply.github.com>
2016-09-21 17:34:38 -0400

    Create IconTextBrowseWnd::PreRender.
    IconTextBrowseWnd was laying out all sub windows in its constructor
    taking several ms.  Repeated 100 times for a 100 fleet stack is longer
        than the frame interval.

--- UI/IconTextBrowseWnd.cpp
+++ UI/IconTextBrowseWnd.cpp
@@ -19,35 +19,44 @@ namespace {
     const GG::Y     ICON_BROWSE_ICON_HEIGHT(64);
 }
 
-IconTextBrowseWnd::IconTextBrowseWnd(const boost::shared_ptr<GG::Texture> texture, const std::string& title_text, const std::string& main_text) :
-    GG::BrowseInfoWnd(GG::X0, GG::Y0, ICON_BROWSE_TEXT_WIDTH + ICON_BROWSE_ICON_WIDTH, GG::Y1)
-{
-    m_icon = new GG::StaticGraphic(texture, GG::GRAPHIC_FITGRAPHIC | GG::GRAPHIC_PROPSCALE, GG::INTERACTIVE);
+IconTextBrowseWnd::IconTextBrowseWnd(const boost::shared_ptr<GG::Texture> texture,
+                                     const std::string& title_text,
+                                     const std::string& main_text) :
+    GG::BrowseInfoWnd(GG::X0, GG::Y0, ICON_BROWSE_TEXT_WIDTH + ICON_BROWSE_ICON_WIDTH, GG::Y1),
+    m_icon(0), m_title_text_label(0), m_main_text_label(0),
+    m_texture(texture), m_title_text(title_text), m_main_text(main_text)
+{ RequirePreRender(); }
+
+bool IconTextBrowseWnd::WndHasBrowseInfo(const Wnd* wnd, std::size_t mode) const {
+    assert(mode <= wnd->BrowseModes().size());
+    return true;
+}
+
+void IconTextBrowseWnd::PreRender() {
+    GG::Wnd::PreRender();
+
+    m_icon = new GG::StaticGraphic(m_texture, GG::GRAPHIC_FITGRAPHIC | GG::GRAPHIC_PROPSCALE, GG::INTERACTIVE);
     m_icon->Resize(GG::Pt(ICON_BROWSE_ICON_WIDTH, ICON_BROWSE_ICON_HEIGHT));
     AttachChild(m_icon);
 
     const GG::Y ROW_HEIGHT(IconTextBrowseWndRowHeight());
 
-    m_title_text = new CUILabel(title_text, GG::FORMAT_LEFT);
-    m_title_text->MoveTo(GG::Pt(m_icon->Width() + GG::X(EDGE_PAD), GG::Y0));
-    m_title_text->Resize(GG::Pt(ICON_BROWSE_TEXT_WIDTH, ROW_HEIGHT));
-    m_title_text->SetFont(ClientUI::GetBoldFont());
+    m_title_text_label = new CUILabel(m_title_text, GG::FORMAT_LEFT);
+    m_title_text_label->MoveTo(GG::Pt(m_icon->Width() + GG::X(EDGE_PAD), GG::Y0));
+    m_title_text_label->Resize(GG::Pt(ICON_BROWSE_TEXT_WIDTH, ROW_HEIGHT));
+    m_title_text_label->SetFont(ClientUI::GetBoldFont());
 
-    m_main_text = new CUILabel(main_text, GG::FORMAT_LEFT | GG::FORMAT_TOP | GG::FORMAT_WORDBREAK);
-    m_main_text->MoveTo(GG::Pt(m_icon->Width() + GG::X(EDGE_PAD), ROW_HEIGHT));
-    m_main_text->Resize(GG::Pt(ICON_BROWSE_TEXT_WIDTH, ICON_BROWSE_ICON_HEIGHT));
-    m_main_text->SetResetMinSize(true);
-    m_main_text->Resize(m_main_text->MinSize());
+    m_main_text_label = new CUILabel(m_main_text, GG::FORMAT_LEFT | GG::FORMAT_TOP | GG::FORMAT_WORDBREAK);
+    m_main_text_label->MoveTo(GG::Pt(m_icon->Width() + GG::X(EDGE_PAD), ROW_HEIGHT));
+    m_main_text_label->Resize(GG::Pt(ICON_BROWSE_TEXT_WIDTH, ICON_BROWSE_ICON_HEIGHT));
+    m_main_text_label->SetResetMinSize(true);
+    m_main_text_label->Resize(m_main_text_label->MinSize());
 
-    AttachChild(m_title_text);
-    AttachChild(m_main_text);
+    AttachChild(m_title_text_label);
+    AttachChild(m_main_text_label);
 
-    Resize(GG::Pt(ICON_BROWSE_TEXT_WIDTH + ICON_BROWSE_ICON_WIDTH, std::max(m_icon->Height(), ROW_HEIGHT + m_main_text->Height())));
-}
-
-bool IconTextBrowseWnd::WndHasBrowseInfo(const Wnd* wnd, std::size_t mode) const {
-    assert(mode <= wnd->BrowseModes().size());
-    return true;
+    Resize(GG::Pt(ICON_BROWSE_TEXT_WIDTH + ICON_BROWSE_ICON_WIDTH,
+                  std::max(m_icon->Height(), ROW_HEIGHT + m_main_text_label->Height())));
 }
 
 void IconTextBrowseWnd::Render() {
@@ -55,5 +64,6 @@ void IconTextBrowseWnd::Render() {
     GG::Pt      lr = LowerRight();
     const GG::Y ROW_HEIGHT(IconTextBrowseWndRowHeight());
     GG::FlatRectangle(ul, lr, ClientUI::WndColor(), ClientUI::WndOuterBorderColor(), 1);    // main background
-    GG::FlatRectangle(GG::Pt(ul.x + ICON_BROWSE_ICON_WIDTH, ul.y), GG::Pt(lr.x, ul.y + ROW_HEIGHT), ClientUI::WndOuterBorderColor(), ClientUI::WndOuterBorderColor(), 0);    // top title filled background
+    GG::FlatRectangle(GG::Pt(ul.x + ICON_BROWSE_ICON_WIDTH, ul.y), GG::Pt(lr.x, ul.y + ROW_HEIGHT),
+                      ClientUI::WndOuterBorderColor(), ClientUI::WndOuterBorderColor(), 0);    // top title filled background
 }
--- UI/IconTextBrowseWnd.h
+++ UI/IconTextBrowseWnd.h
@@ -11,12 +11,17 @@ public:
     IconTextBrowseWnd(const boost::shared_ptr<GG::Texture> texture, const std::string& title_text,
                       const std::string& main_text);
     virtual bool WndHasBrowseInfo(const Wnd* wnd, std::size_t mode) const;
+
+    virtual void PreRender();
     virtual void Render();
 
 private:
-    GG::StaticGraphic*  m_icon;
-    GG::Label*          m_title_text;
-    GG::Label*          m_main_text;
+    GG::StaticGraphic*                   m_icon;
+    GG::Label*                           m_title_text_label;
+    GG::Label*                           m_main_text_label;
+    const boost::shared_ptr<GG::Texture> m_texture;
+    const std::string                    m_title_text;
+    const std::string                    m_main_text;
 };
 
 #endif
