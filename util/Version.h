6fabde3aa09f02c423c557ccd0041ebb2943d305
Marcel Metz <mmetz@adrian-broher.net>
2016-10-10 21:38:30 +0200

    Apply #1047 review remarks
    * Documented version string structure.
    * Changed phrasing.

--- util/Version.h
+++ util/Version.h
@@ -2,7 +2,7 @@
 #define _Version_h_
 
 /** @file
- * @brief  Declares free functions to access the application and dependencies
+ * @brief  Declares free functions to access the application and dependency
  *      versions.
  */
 
@@ -12,6 +12,17 @@
 #include "Export.h"
 
 /** @brief  Returns the version string of FreeOrion.
+ *
+ * The version strings consists of:
+ *
+ * * the VERSION number composed from @e MAJOR.MINOR.PATCH numbers. An
+ *      optional '+' suffix indicates an in between test releases (think
+ *      0.4.6 + some more).  Release candidates are suffixed with rcNUMBER.
+ * * the BRANCH, which represents the Git branch used to create this build.
+ * * the DATE as ISO 8601 date format.
+ * * the SHORT_COMMIT_HASH, which is the shortes Git commit hash, which
+ *   identifies the commit this build was created from.
+ * * the BUILD_SYSTEM, which can be one of "CMake", "MSVC" or "XCode".
  *
  * @return  The version string with the format
  *      <em>VERSION BRANCH_NAME [build DATE.SHORT_COMMIT_HASH] BUILD_SYSTEM</em>
