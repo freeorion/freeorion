d8546ce1873faa9100b61a8a4bc8c1ef50e57790
LGM-Doyle <LGM-Doyle@users.noreply.github.com>
2016-10-12 15:42:07 -0400

    In EncyclopediaDetailPanel stop using 1 << 15 as widest possible text.
    Thread ClientWidth() through free functions when computing text widths.

--- UI/EncyclopediaDetailPanel.cpp
+++ UI/EncyclopediaDetailPanel.cpp
@@ -2341,7 +2341,7 @@ namespace {
                                             boost::shared_ptr<GG::Texture>& other_texture, int& turns,
                                             float& cost, std::string& cost_units, std::string& general_type,
                                             std::string& specific_type, std::string& detailed_description,
-                                            GG::Clr& color)
+                                            GG::Clr& color, GG::X width)
     {
         general_type = UserString("SP_PLANET_SUITABILITY");
 
@@ -2443,7 +2443,7 @@ namespace {
             GG::Flags<GG::TextFormat> format = GG::FORMAT_NONE;
             std::vector<boost::shared_ptr<GG::Font::TextElement> > text_elements =
                 font->ExpensiveParseFromTextToTextElements(species_name_column1, format);
-            GG::Pt extent = font->DetermineLines(species_name_column1, format, GG::X(1 << 15), text_elements, lines);
+            GG::Pt extent = font->DetermineLines(species_name_column1, format, width, text_elements, lines);
             max_species_name_column1_width = std::max(extent.x, max_species_name_column1_width);
 
             // Setting the planet's species allows all of it meters to reflect
@@ -2487,11 +2487,11 @@ namespace {
             GG::Flags<GG::TextFormat> format = GG::FORMAT_NONE;
             std::vector<boost::shared_ptr<GG::Font::TextElement> > text_elements =
                 font->ExpensiveParseFromTextToTextElements(species_name_column1, format);
-            GG::Pt extent = font->DetermineLines(species_name_column1, format, GG::X(1 << 15), text_elements, lines);
+            GG::Pt extent = font->DetermineLines(species_name_column1, format, width, text_elements, lines);
             while (extent.x < max_species_name_column1_width) {
                 species_name_column1 += "\t";
                 text_elements = font->ExpensiveParseFromTextToTextElements(species_name_column1, format);
-                extent = font->DetermineLines(species_name_column1, format, GG::X(1 << 15), text_elements, lines);
+                extent = font->DetermineLines(species_name_column1, format, width, text_elements, lines);
             }
 
             if (it->first > 0) {
@@ -2551,7 +2551,8 @@ namespace {
                                             boost::shared_ptr<GG::Texture>& other_texture, int& turns,
                                             float& cost, std::string& cost_units, std::string& general_type,
                                             std::string& specific_type, std::string& detailed_description,
-                                            GG::Clr& color, boost::weak_ptr<const ShipDesign>& incomplete_design)
+                                            GG::Clr& color, boost::weak_ptr<const ShipDesign>& incomplete_design,
+                                            GG::X width)
     {
         if (item_type == TextLinker::ENCYCLOPEDIA_TAG) {
             RefreshDetailPanelPediaTag(         item_type, item_name,
@@ -2609,7 +2610,7 @@ namespace {
         } else if (item_type == PLANET_SUITABILITY_REPORT) {
             RefreshDetailPanelSuitabilityTag(   item_type, item_name,
                                                 name, texture, other_texture, turns, cost, cost_units,
-                                                general_type, specific_type, detailed_description, color);
+                                                general_type, specific_type, detailed_description, color, width);
         } else if (item_type == TEXT_SEARCH_RESULTS) {
             RefreshDetailPanelSearchResultsTag( item_type, item_name,
                                                 name, texture, other_texture, turns, cost, cost_units,
@@ -2651,7 +2652,7 @@ void EncyclopediaDetailPanel::Refresh() {
     GetRefreshDetailPanelInfo(m_items_it->first, m_items_it->second,
                               name, texture, other_texture, turns, cost, cost_units,
                               general_type, specific_type, detailed_description, color,
-                              m_incomplete_design);
+                              m_incomplete_design, ClientWidth());
 
     if (m_items_it->first == TextLinker::GRAPH_TAG) {
         const std::string& graph_id = m_items_it->second;
