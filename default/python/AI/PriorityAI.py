14b31af1f84d1aed0a141cc61e318e5571b025a4
Dilvish-fo <dilvish.fo@gmail.com>
2016-11-15 18:37:12 -0800

    In sparse galaxies, moderate the reduction in AI colonization activity that occurs after spotting enemies.

--- default/python/AI/PriorityAI.py
+++ default/python/AI/PriorityAI.py
@@ -234,7 +234,7 @@ def _calculate_colonisation_priority():
     colony_cost = AIDependencies.COLONY_POD_COST * (1 + AIDependencies.COLONY_POD_UPKEEP * num_colonies)
     turns_to_build = 8  # TODO: check for susp anim pods, build time 10
     mil_prio = foAI.foAIstate.get_priority(PriorityType.PRODUCTION_MILITARY)
-    allotted_portion = ([[[0.6, 0.8], [0.3, 0.4]], [[0.8, 0.9], [0.3, 0.4]]][galaxy_is_sparse]
+    allotted_portion = ([[[0.6, 0.8], [0.3, 0.4]], [[0.8, 0.9], [0.4, 0.6]]][galaxy_is_sparse]
                        [any(enemies_sighted)][fo.empireID() % 2])
     # if ( foAI.foAIstate.get_priority(AIPriorityType.PRIORITY_PRODUCTION_COLONISATION)
     # > 2 * foAI.foAIstate.get_priority(AIPriorityType.PRIORITY_PRODUCTION_MILITARY)):
