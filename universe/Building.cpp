af3158b841bf213aee995bbf17499a7f49731c79
LGM-Doyle <LGM-Doyle@users.noreply.github.com>
2017-02-01 17:30:37 -0500

    Remove error message for missing building location in script context.
    The offline Pedia will always be missing the location.
    
    Issue raised in review #1262.

--- universe/Building.cpp
+++ universe/Building.cpp
@@ -242,10 +242,8 @@ float BuildingType::ProductionCost(int empire_id, int location_id) const {
         const auto arbitrary_large_number = 999999.9f;
 
         std::shared_ptr<UniverseObject> location = GetUniverseObject(location_id);
-        if (!location) {
-            ErrorLogger() << "Location is missing using production cost of " << arbitrary_large_number;
+        if (!location)
             return arbitrary_large_number;
-        }
 
         std::shared_ptr<const UniverseObject> source = Empires().GetSource(empire_id);
         if (!source && !m_production_cost->SourceInvariant()) {
@@ -273,10 +271,8 @@ int BuildingType::ProductionTime(int empire_id, int location_id) const {
         const auto arbitrary_large_number = 9999;
 
         std::shared_ptr<UniverseObject> location = GetUniverseObject(location_id);
-        if (!location) {
-            ErrorLogger() << "Location is missing using production time of " << arbitrary_large_number;
+        if (!location)
             return arbitrary_large_number;
-        }
 
         std::shared_ptr<const UniverseObject> source = Empires().GetSource(empire_id);
         if (!source && !m_production_time->SourceInvariant()) {
