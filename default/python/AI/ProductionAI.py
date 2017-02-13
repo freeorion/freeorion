f415578ff8e4e56d9923c21e893d56186337f64c
Morlic-fo <Morlic@gmx.de>
2017-02-13 12:35:36 +0100

    Reduce initial number of scouts
    2 starting scouts and 3 more built on first turn should be enough at
    game start.
    
    Building another 3 on turn 4 delays building more important stuff and
    increases fleet upkeep significantly.

--- default/python/AI/ProductionAI.py
+++ default/python/AI/ProductionAI.py
@@ -215,16 +215,12 @@ def generate_production_orders():
                 continue
             claimed_stars.setdefault(t_sys.starType, []).append(sys_id)
 
-    if (current_turn in [1, 4]) and ((production_queue.totalSpent < total_pp) or (len(production_queue) <= 3)):
-        best_design_id, best_design, build_choices = get_best_ship_info(PriorityType.PRODUCTION_EXPLORATION)
-        if len(AIstate.opponentPlanetIDs) == 0:
-            init_scouts = 3
-        else:
-            init_scouts = 0
-        if best_design_id:
-            for scout_count in range(init_scouts):
+    if current_turn == 1 and len(AIstate.opponentPlanetIDs) == 0:
+        best_design_id, _, build_choices = get_best_ship_info(PriorityType.PRODUCTION_EXPLORATION)
+        if best_design_id is not None:
+            for _ in range(3):
                 fo.issueEnqueueShipProductionOrder(best_design_id, build_choices[0])
-        fo.updateProductionQueue()
+            fo.updateProductionQueue()
 
     building_expense = 0.0
     building_ratio = foAI.foAIstate.character.preferred_building_ratio([0.4, 0.35, 0.30])
