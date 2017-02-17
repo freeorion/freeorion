ef0fd43cd33b861230849be9d2a83cb473fba1a3
Marcel Metz <mmetz@adrian-broher.net>
2017-02-17 11:00:07 +0100

    Rewrite GG unit test build to follow best practice more closely
    * Compile GG unit test into a single executable `gg_unittest`.
    * Add main.cpp for global fixture and configuration.
    * Guard GG project unittest target to avoid name clash.

---
+++ GG/test/unit/main.cpp
@@ -0,0 +1,27 @@
+/* GG is a GUI for SDL and OpenGL.
+
+   Copyright (C) 2017 Marcel Metz
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public License
+   as published by the Free Software Foundation; either version 2.1
+   of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA
+
+   If you do not wish to comply with the terms of the LGPL please
+   contact the author as other terms are available for a fee.
+
+   Zach Laine
+   whatwasthataddress@gmail.com */
+
+#define BOOST_TEST_MODULE "GG unit/coverage tests"
+#include <boost/test/unit_test.hpp>
