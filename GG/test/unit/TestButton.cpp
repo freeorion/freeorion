7bcdf06e50e7fd5c01b955de30b7b53525c3c7f6
Marcel Metz <mmetz@adrian-broher.net>
2017-02-21 14:57:20 +0100

    Use BOOST_CHECK macro for unit tests
    The test environment only provides Boost 1.58.  The boost test
    BOOST_TEST macro was only introduced with boost 1.59 or later.

--- GG/test/unit/TestButton.cpp
+++ GG/test/unit/TestButton.cpp
@@ -105,60 +105,60 @@ BOOST_AUTO_TEST_CASE( constructor )
     std::shared_ptr<GG::Font> font(new MockFont());
     InspectButton button("Test", font, GG::CLR_WHITE, GG::CLR_GREEN);
 
-    BOOST_TEST(button.Text() == "Test");
-    BOOST_TEST(button.State() == GG::Button::BN_UNPRESSED);
-    BOOST_TEST(button.MinUsableSize() == GG::Pt(GG::X(0), GG::Y(0)));
-    BOOST_TEST(button.Color() == GG::CLR_WHITE);
-    BOOST_TEST(button.GetLabel().TextColor() == GG::CLR_GREEN);
-    BOOST_TEST(button.UnpressedGraphic().Empty());
-    BOOST_TEST(button.PressedGraphic().Empty());
-    BOOST_TEST(button.RolloverGraphic().Empty());
-    BOOST_TEST(button.LeftClickedSignal.empty());
-    BOOST_TEST(button.RightClickedSignal.empty());
-    BOOST_TEST(!button.Disabled());
-    BOOST_TEST(button.Interactive());
-    BOOST_TEST(!button.RepeatKeyPress());
-    BOOST_TEST(!button.RepeatButtonDown());
-    BOOST_TEST(!button.Dragable());
-    BOOST_TEST(!button.Resizable());
-    BOOST_TEST(!button.OnTop());
-    BOOST_TEST(!button.Modal());
-    BOOST_TEST(button.GetChildClippingMode() == GG::Wnd::DontClip);
-    BOOST_TEST(!button.NonClientChild());
-    BOOST_TEST(button.Visible());
-    BOOST_TEST(button.PreRenderRequired());
-    BOOST_TEST(button.Name() == "");
-    BOOST_TEST(button.DragDropDataType() == "");
-    BOOST_TEST(button.UpperLeft() == GG::Pt(GG::X(0), GG::Y(0)));
-    BOOST_TEST(button.RelativeUpperLeft() == GG::Pt(GG::X(0), GG::Y(0)));
-    BOOST_TEST(button.ClientUpperLeft() == GG::Pt(GG::X(0), GG::Y(0)));
-    BOOST_TEST(button.Left() == GG::X(0));
-    BOOST_TEST(button.Top() == GG::Y(0));
-    BOOST_TEST(button.LowerRight() == GG::Pt(GG::X(1), GG::Y(1)));
-    BOOST_TEST(button.RelativeLowerRight() == GG::Pt(GG::X(1), GG::Y(1)));
-    BOOST_TEST(button.ClientLowerRight() == GG::Pt(GG::X(1), GG::Y(1)));
-    BOOST_TEST(button.Right() == GG::X(1));
-    BOOST_TEST(button.Bottom() == GG::Y(1));
-    BOOST_TEST(button.Width() == GG::X(1));
-    BOOST_TEST(button.ClientWidth() == GG::X(1));
-    BOOST_TEST(button.Height() == GG::Y(1));
-    BOOST_TEST(button.ClientHeight() == GG::Y(1));
-    BOOST_TEST(button.Size() == GG::Pt(GG::X(1), GG::Y(1)));
-    BOOST_TEST(button.ClientSize() == GG::Pt(GG::X(1), GG::Y(1)));
-    BOOST_TEST(button.MinSize() == GG::Pt(GG::X(0), GG::Y(0)));
-    BOOST_TEST(button.MinUsableSize() == GG::Pt(GG::X(0), GG::Y(0)));
-    BOOST_TEST(button.ScreenToWindow(GG::Pt(GG::X(0), GG::Y(0))) == GG::Pt(GG::X(0), GG::Y(0)));
-    BOOST_TEST(button.ScreenToClient(GG::Pt(GG::X(0), GG::Y(0))) == GG::Pt(GG::X(0), GG::Y(0)));
-    BOOST_TEST(button.InWindow(GG::Pt(GG::X(0), GG::Y(0))));
-    BOOST_TEST(button.InClient(GG::Pt(GG::X(0), GG::Y(0))));
-    BOOST_TEST(button.Children().size() == 1);
-    BOOST_TEST(!button.Parent());
-    BOOST_TEST(!button.RootParent());
-    BOOST_TEST(!button.GetLayout());
-    BOOST_TEST(!button.ContainingLayout());
-    BOOST_TEST(button.BrowseModes().size() == 1);
-    BOOST_TEST(button.GetStyleFactory() == gui.GetStyleFactory());
-    BOOST_TEST(button.WindowRegion(GG::Pt(GG::X(0), GG::Y(0))) == GG::WR_NONE);
+    BOOST_CHECK(button.Text() == "Test");
+    BOOST_CHECK(button.State() == GG::Button::BN_UNPRESSED);
+    BOOST_CHECK(button.MinUsableSize() == GG::Pt(GG::X(0), GG::Y(0)));
+    BOOST_CHECK(button.Color() == GG::CLR_WHITE);
+    BOOST_CHECK(button.GetLabel().TextColor() == GG::CLR_GREEN);
+    BOOST_CHECK(button.UnpressedGraphic().Empty());
+    BOOST_CHECK(button.PressedGraphic().Empty());
+    BOOST_CHECK(button.RolloverGraphic().Empty());
+    BOOST_CHECK(button.LeftClickedSignal.empty());
+    BOOST_CHECK(button.RightClickedSignal.empty());
+    BOOST_CHECK(!button.Disabled());
+    BOOST_CHECK(button.Interactive());
+    BOOST_CHECK(!button.RepeatKeyPress());
+    BOOST_CHECK(!button.RepeatButtonDown());
+    BOOST_CHECK(!button.Dragable());
+    BOOST_CHECK(!button.Resizable());
+    BOOST_CHECK(!button.OnTop());
+    BOOST_CHECK(!button.Modal());
+    BOOST_CHECK(button.GetChildClippingMode() == GG::Wnd::DontClip);
+    BOOST_CHECK(!button.NonClientChild());
+    BOOST_CHECK(button.Visible());
+    BOOST_CHECK(button.PreRenderRequired());
+    BOOST_CHECK(button.Name() == "");
+    BOOST_CHECK(button.DragDropDataType() == "");
+    BOOST_CHECK(button.UpperLeft() == GG::Pt(GG::X(0), GG::Y(0)));
+    BOOST_CHECK(button.RelativeUpperLeft() == GG::Pt(GG::X(0), GG::Y(0)));
+    BOOST_CHECK(button.ClientUpperLeft() == GG::Pt(GG::X(0), GG::Y(0)));
+    BOOST_CHECK(button.Left() == GG::X(0));
+    BOOST_CHECK(button.Top() == GG::Y(0));
+    BOOST_CHECK(button.LowerRight() == GG::Pt(GG::X(1), GG::Y(1)));
+    BOOST_CHECK(button.RelativeLowerRight() == GG::Pt(GG::X(1), GG::Y(1)));
+    BOOST_CHECK(button.ClientLowerRight() == GG::Pt(GG::X(1), GG::Y(1)));
+    BOOST_CHECK(button.Right() == GG::X(1));
+    BOOST_CHECK(button.Bottom() == GG::Y(1));
+    BOOST_CHECK(button.Width() == GG::X(1));
+    BOOST_CHECK(button.ClientWidth() == GG::X(1));
+    BOOST_CHECK(button.Height() == GG::Y(1));
+    BOOST_CHECK(button.ClientHeight() == GG::Y(1));
+    BOOST_CHECK(button.Size() == GG::Pt(GG::X(1), GG::Y(1)));
+    BOOST_CHECK(button.ClientSize() == GG::Pt(GG::X(1), GG::Y(1)));
+    BOOST_CHECK(button.MinSize() == GG::Pt(GG::X(0), GG::Y(0)));
+    BOOST_CHECK(button.MinUsableSize() == GG::Pt(GG::X(0), GG::Y(0)));
+    BOOST_CHECK(button.ScreenToWindow(GG::Pt(GG::X(0), GG::Y(0))) == GG::Pt(GG::X(0), GG::Y(0)));
+    BOOST_CHECK(button.ScreenToClient(GG::Pt(GG::X(0), GG::Y(0))) == GG::Pt(GG::X(0), GG::Y(0)));
+    BOOST_CHECK(button.InWindow(GG::Pt(GG::X(0), GG::Y(0))));
+    BOOST_CHECK(button.InClient(GG::Pt(GG::X(0), GG::Y(0))));
+    BOOST_CHECK(button.Children().size() == 1);
+    BOOST_CHECK(!button.Parent());
+    BOOST_CHECK(!button.RootParent());
+    BOOST_CHECK(!button.GetLayout());
+    BOOST_CHECK(!button.ContainingLayout());
+    BOOST_CHECK(button.BrowseModes().size() == 1);
+    BOOST_CHECK(button.GetStyleFactory() == gui.GetStyleFactory());
+    BOOST_CHECK(button.WindowRegion(GG::Pt(GG::X(0), GG::Y(0))) == GG::WR_NONE);
 }
 
 BOOST_AUTO_TEST_CASE( resize )
@@ -168,24 +168,24 @@ BOOST_AUTO_TEST_CASE( resize )
 
     button.SizeMove(GG::Pt(GG::X(10), GG::Y(10)), GG::Pt(GG::X(40), GG::Y(40)));
 
-    BOOST_TEST(button.UpperLeft() == GG::Pt(GG::X(10), GG::Y(10)));
-    BOOST_TEST(button.RelativeUpperLeft() == GG::Pt(GG::X(10), GG::Y(10)));
-    BOOST_TEST(button.ClientUpperLeft() == GG::Pt(GG::X(10), GG::Y(10)));
-    BOOST_TEST(button.Left() == GG::X(10));
-    BOOST_TEST(button.Top() == GG::Y(10));
-    BOOST_TEST(button.LowerRight() == GG::Pt(GG::X(40), GG::Y(40)));
-    BOOST_TEST(button.RelativeLowerRight() == GG::Pt(GG::X(40), GG::Y(40)));
-    BOOST_TEST(button.ClientLowerRight() == GG::Pt(GG::X(40), GG::Y(40)));
-    BOOST_TEST(button.Right() == GG::X(40));
-    BOOST_TEST(button.Bottom() == GG::Y(40));
-    BOOST_TEST(button.Width() == GG::X(30));
-    BOOST_TEST(button.ClientWidth() == GG::X(30));
-    BOOST_TEST(button.Height() == GG::Y(30));
-    BOOST_TEST(button.ClientHeight() == GG::Y(30));
-    BOOST_TEST(button.Size() == GG::Pt(GG::X(30), GG::Y(30)));
-    BOOST_TEST(button.ClientSize() == GG::Pt(GG::X(30), GG::Y(30)));
-    BOOST_TEST(button.MinSize() == GG::Pt(GG::X(0), GG::Y(0)));
-    BOOST_TEST(button.MinUsableSize() == GG::Pt(GG::X(0), GG::Y(0)));
+    BOOST_CHECK(button.UpperLeft() == GG::Pt(GG::X(10), GG::Y(10)));
+    BOOST_CHECK(button.RelativeUpperLeft() == GG::Pt(GG::X(10), GG::Y(10)));
+    BOOST_CHECK(button.ClientUpperLeft() == GG::Pt(GG::X(10), GG::Y(10)));
+    BOOST_CHECK(button.Left() == GG::X(10));
+    BOOST_CHECK(button.Top() == GG::Y(10));
+    BOOST_CHECK(button.LowerRight() == GG::Pt(GG::X(40), GG::Y(40)));
+    BOOST_CHECK(button.RelativeLowerRight() == GG::Pt(GG::X(40), GG::Y(40)));
+    BOOST_CHECK(button.ClientLowerRight() == GG::Pt(GG::X(40), GG::Y(40)));
+    BOOST_CHECK(button.Right() == GG::X(40));
+    BOOST_CHECK(button.Bottom() == GG::Y(40));
+    BOOST_CHECK(button.Width() == GG::X(30));
+    BOOST_CHECK(button.ClientWidth() == GG::X(30));
+    BOOST_CHECK(button.Height() == GG::Y(30));
+    BOOST_CHECK(button.ClientHeight() == GG::Y(30));
+    BOOST_CHECK(button.Size() == GG::Pt(GG::X(30), GG::Y(30)));
+    BOOST_CHECK(button.ClientSize() == GG::Pt(GG::X(30), GG::Y(30)));
+    BOOST_CHECK(button.MinSize() == GG::Pt(GG::X(0), GG::Y(0)));
+    BOOST_CHECK(button.MinUsableSize() == GG::Pt(GG::X(0), GG::Y(0)));
 }
 
 struct MouseClickCatcher
@@ -205,49 +205,49 @@ BOOST_AUTO_TEST_CASE( click )
 
     button.SizeMove(GG::Pt(GG::X(10), GG::Y(10)), GG::Pt(GG::X(40), GG::Y(40)));
 
-    BOOST_TEST(!button.Disabled());
+    BOOST_CHECK(!button.Disabled());
 
-    BOOST_TEST(catcher.count == 0);
+    BOOST_CHECK(catcher.count == 0);
     button.HandleEventE(mouselpress_ev);
 
-    BOOST_TEST(catcher.count == 0);
+    BOOST_CHECK(catcher.count == 0);
 
     button.HandleEventE(mouserpress_ev);
 
-    BOOST_TEST(catcher.count == 0);
+    BOOST_CHECK(catcher.count == 0);
 
     GG::Connect(button.LeftClickedSignal, &MouseClickCatcher::CountClick, &catcher);
-    BOOST_TEST(button.LeftClickedSignal.num_slots() == 1);
+    BOOST_CHECK(button.LeftClickedSignal.num_slots() == 1);
 
     button.HandleEventE(mouseenter_ev);
     button.HandleEventE(mouseleave_ev);
 
-    BOOST_TEST(catcher.count == 0);
+    BOOST_CHECK(catcher.count == 0);
 
     button.HandleEventE(mousehere_ev);
     button.HandleEventE(mouseleave_ev);
 
-    BOOST_TEST(catcher.count == 0);
+    BOOST_CHECK(catcher.count == 0);
 
     button.HandleEventE(mousehere_ev);
     button.HandleEventE(mouselpress_ev);
     button.HandleEventE(mouseldrag_ev);
     button.HandleEventE(mouseleave_ev);
 
-    BOOST_TEST(catcher.count == 0);
+    BOOST_CHECK(catcher.count == 0);
 
     button.HandleEventE(mouserpress_ev);
 
-    BOOST_TEST(catcher.count == 0);
+    BOOST_CHECK(catcher.count == 0);
 
     button.HandleEventE(mouseldrag_ev);
     button.HandleEventE(mouselrelease_ev);
 
-    BOOST_TEST(catcher.count == 0);
+    BOOST_CHECK(catcher.count == 0);
 
     button.HandleEventE(mouselclick_ev);
 
-    BOOST_TEST(catcher.count == 1);
+    BOOST_CHECK(catcher.count == 1);
 }
 
 BOOST_AUTO_TEST_CASE( stateChange )
@@ -257,33 +257,33 @@ BOOST_AUTO_TEST_CASE( stateChange )
 
     button.SizeMove(GG::Pt(GG::X(10), GG::Y(10)), GG::Pt(GG::X(40), GG::Y(40)));
 
-    BOOST_TEST(button.State() == GG::Button::BN_UNPRESSED);
+    BOOST_CHECK(button.State() == GG::Button::BN_UNPRESSED);
     button.HandleEventE(mouseenter_ev);
-    BOOST_TEST(button.State() == GG::Button::BN_ROLLOVER);
+    BOOST_CHECK(button.State() == GG::Button::BN_ROLLOVER);
     button.HandleEventE(mouseleave_ev);
-    BOOST_TEST(button.State() == GG::Button::BN_UNPRESSED);
+    BOOST_CHECK(button.State() == GG::Button::BN_UNPRESSED);
 
-    BOOST_TEST(button.State() == GG::Button::BN_UNPRESSED);
+    BOOST_CHECK(button.State() == GG::Button::BN_UNPRESSED);
     button.HandleEventE(mousehere_ev);
-    BOOST_TEST(button.State() == GG::Button::BN_ROLLOVER);
+    BOOST_CHECK(button.State() == GG::Button::BN_ROLLOVER);
     button.HandleEventE(mouseleave_ev);
-    BOOST_TEST(button.State() == GG::Button::BN_UNPRESSED);
+    BOOST_CHECK(button.State() == GG::Button::BN_UNPRESSED);
 
-    BOOST_TEST(button.State() == GG::Button::BN_UNPRESSED);
+    BOOST_CHECK(button.State() == GG::Button::BN_UNPRESSED);
     button.HandleEventE(mousehere_ev);
-    BOOST_TEST(button.State() == GG::Button::BN_ROLLOVER);
+    BOOST_CHECK(button.State() == GG::Button::BN_ROLLOVER);
     button.HandleEventE(mouselpress_ev);
-    BOOST_TEST(button.State() == GG::Button::BN_PRESSED);
+    BOOST_CHECK(button.State() == GG::Button::BN_PRESSED);
     button.HandleEventE(mouseldrag_ev);
-    BOOST_TEST(button.State() == GG::Button::BN_PRESSED);
+    BOOST_CHECK(button.State() == GG::Button::BN_PRESSED);
     button.HandleEventE(mouseleave_ev);
-    BOOST_TEST(button.State() == GG::Button::BN_UNPRESSED);
+    BOOST_CHECK(button.State() == GG::Button::BN_UNPRESSED);
 
-    BOOST_TEST(button.State() == GG::Button::BN_UNPRESSED);
+    BOOST_CHECK(button.State() == GG::Button::BN_UNPRESSED);
     button.HandleEventE(mouseldrag_ev);
-    BOOST_TEST(button.State() == GG::Button::BN_PRESSED);
+    BOOST_CHECK(button.State() == GG::Button::BN_PRESSED);
     button.HandleEventE(mouselrelease_ev);
-    BOOST_TEST(button.State() == GG::Button::BN_UNPRESSED);
+    BOOST_CHECK(button.State() == GG::Button::BN_UNPRESSED);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
