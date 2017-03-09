6f0187d86976617ac5b62562d5b697d4bef3749b
Marcel Metz <mmetz@adrian-broher.net>
2017-03-09 21:53:07 +0100

    Add declspec statements for MSVC compiler

--- util/Export.h
+++ util/Export.h
@@ -3,6 +3,10 @@
 
 #if FREEORION_BUILD_COMMON && __GNUC__
 #   define FO_COMMON_API __attribute__((__visibility__("default")))
+#elif FREEORION_BUILD_COMMON && _MSC_VER
+#   define FO_COMMON_API __declspec(dllexport)
+#elif _MSC_VER
+#   define FO_COMMON_API __declspec(dllimport)
 #else
 #   define FO_COMMON_API
 #endif
