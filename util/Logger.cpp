a586e335417e1d59602ced93d38e93b4c261e950
LGM-Doyle <LGM-Doyle@users.noreply.github.com>
2017-04-17 11:36:50 -0400

    Alphabetize and remove duplicate headers in Logger.cpp.

--- util/Logger.cpp
+++ util/Logger.cpp
@@ -5,22 +5,22 @@
 #include <boost/log/core.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/log/expressions.hpp>
-#include <boost/log/sinks/text_file_backend.hpp>
-#include <boost/log/utility/setup/file.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
-#include <boost/log/sources/severity_channel_logger.hpp>
-#include <boost/log/sources/record_ostream.hpp>
-#include <boost/log/utility/setup/formatter_parser.hpp>
-#include <boost/log/utility/setup/filter_parser.hpp>
-#include <boost/log/support/date_time.hpp>
-#include <boost/log/expressions/keyword.hpp>
-#include <boost/log/utility/manipulators/add_value.hpp>
-#include <boost/log/sources/global_logger_storage.hpp>
 #include <boost/log/attributes/value_extraction.hpp>
-#include <boost/log/utility/setup/formatter_parser.hpp>
-#include <boost/log/utility/setup/filter_parser.hpp>
+#include <boost/log/expressions/keyword.hpp>
+#include <boost/log/sinks/frontend_requirements.hpp>
+#include <boost/log/sinks/text_ostream_backend.hpp>
 #include <boost/log/sinks/text_file_backend.hpp>
 #include <boost/log/sinks/sync_frontend.hpp>
+#include <boost/log/sources/global_logger_storage.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/sources/severity_channel_logger.hpp>
+#include <boost/log/support/date_time.hpp>
+#include <boost/log/utility/manipulators/add_value.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/filter_parser.hpp>
+#include <boost/log/utility/setup/formatter_parser.hpp>
+
 #include <boost/optional.hpp>
 #include <boost/thread/thread.hpp>
 
