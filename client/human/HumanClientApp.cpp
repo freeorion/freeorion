efea294340979d93c7018bef650e16c2c7b94bfe
D Benage <dbenage.cx@gmail.com>
2017-04-27 15:05:49 -0500

    Align config options - gl version checked toggled
    checked-gl-version -> gl_version.check.enabled (inverted logic)

--- client/human/HumanClientApp.cpp
+++ client/human/HumanClientApp.cpp
@@ -177,9 +177,9 @@ namespace {
         }
 
         // only execute default option setting once
-        if (GetOptionsDB().Get<bool>("checked-gl-version"))
+        if (!GetOptionsDB().Get<bool>("gl_version.check.enabled"))
             return;
-        GetOptionsDB().Set<bool>("checked-gl-version", true);
+        GetOptionsDB().Set<bool>("gl_version.check.enabled", false);
 
         // if GL version is too low, set various map rendering options to
         // disabled, to hopefully improve frame rate.
