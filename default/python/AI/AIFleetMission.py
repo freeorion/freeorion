083f8241d01df6adab5e1f15f78781d1d86a3679
Dilvish-fo <dilvish.fo@gmail.com>
2017-05-19 11:22:51 -0700

    further restrict AI fleet mergers, to reduce fleet object churn

--- default/python/AI/AIFleetMission.py
+++ default/python/AI/AIFleetMission.py
@@ -150,6 +150,11 @@ class AIFleetMission(object):
         if system_id == INVALID_ID:
             return  # can't merge fleets in middle of starlane
         system_status = foAI.foAIstate.systemStatus[system_id]
+
+        # if a combat mission, and only have final order (so must be at final target), don't try
+        # merging if there is no local threat (it tends to lead to fleet object churn)
+        if self.type in COMBAT_MISSION_TYPES and len(self.orders) == 1 and not system_status.get('totalThreat', 0):
+            return
         destroyed_list = list(universe.destroyedObjectIDs(empire_id))
         other_fleets_here = [fid for fid in system_status.get('myFleetsAccessible', []) if fid != fleet_id and
                              fid not in destroyed_list and universe.getFleet(fid).ownedBy(empire_id)]
