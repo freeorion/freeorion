dbd78827e3396ebf43290320dc49821ce5de5d47
Dilvish-fo <dilvish.fo@gmail.com>
2017-06-12 12:17:51 -0700

    increase the AI limit on number of colonies

--- default/python/AI/character/character_module.py
+++ default/python/AI/character/character_module.py
@@ -313,7 +313,8 @@ class Aggression(Trait):
 
     def max_number_colonies(self):
         # significant growth barrier for low aggression, negligible for high aggression
-        return 2 + ((0.5 + self.aggression) ** 2) * fo.currentTurn() / 50.0
+        #TODO: consider further changes, including a dependency on galaxy size and planet density
+        return 2 + ((0.5 + 1.4*self.aggression) ** 2) * fo.currentTurn() / 50.0
 
     def may_invade(self):
         return (self.aggression > fo.aggression.turtle
