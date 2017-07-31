c9035967dc3ca01c42f71f33a9513de2f7847592
LGM-Doyle <LGM-Doyle@users.noreply.github.com>
2017-07-31 15:00:24 -0400

    Don't calculate checksums for local connections.

--- server/ServerFSM.cpp
+++ server/ServerFSM.cpp
@@ -316,7 +316,8 @@ sc::result Idle::react(const HostMPGame& msg) {
     player_connection->EstablishPlayer(host_player_id, host_player_name, Networking::CLIENT_TYPE_HUMAN_PLAYER, client_version_string);
     server.m_networking.SetHostPlayerID(host_player_id);
 
-    player_connection->SendMessage(ContentCheckSumMessage());
+    if (!player_connection->IsLocalConnection())
+        player_connection->SendMessage(ContentCheckSumMessage());
 
     DebugLogger(FSM) << "Idle::react(HostMPGame) about to send acknowledgement to host";
     player_connection->SendMessage(HostMPAckMessage(host_player_id));
@@ -358,7 +359,8 @@ sc::result Idle::react(const HostSPGame& msg) {
     int host_player_id = server.m_networking.NewPlayerID();
     player_connection->EstablishPlayer(host_player_id, host_player_name, Networking::CLIENT_TYPE_HUMAN_PLAYER, client_version_string);
     server.m_networking.SetHostPlayerID(host_player_id);
-    player_connection->SendMessage(ContentCheckSumMessage());
+    if (!player_connection->IsLocalConnection())
+        player_connection->SendMessage(ContentCheckSumMessage());
     player_connection->SendMessage(HostSPAckMessage(host_player_id));
 
     server.m_single_player_game = true;
@@ -583,7 +585,8 @@ void MPLobby::EstablishPlayer(const PlayerConnectionPtr& player_connection,
 
     // establish player with requested client type and acknowldge via connection
     player_connection->EstablishPlayer(player_id, player_name, client_type, client_version_string);
-    player_connection->SendMessage(ContentCheckSumMessage());
+    if (!player_connection->IsLocalConnection())
+        player_connection->SendMessage(ContentCheckSumMessage());
     player_connection->SendMessage(JoinAckMessage(player_id));
 
     // inform player of host
@@ -1373,7 +1376,8 @@ sc::result WaitingForSPGameJoiners::react(const JoinGame& msg) {
             // expected player
             // let the networking system know what socket this player is on
             player_connection->EstablishPlayer(expected_it->second, player_name, client_type, client_version_string);
-            player_connection->SendMessage(ContentCheckSumMessage());
+            if (!player_connection->IsLocalConnection())
+                player_connection->SendMessage(ContentCheckSumMessage());
             player_connection->SendMessage(JoinAckMessage(expected_it->second));
 
             // Inform AI of logging configuration.
@@ -1397,7 +1401,8 @@ sc::result WaitingForSPGameJoiners::react(const JoinGame& msg) {
             // unexpected but welcome human player
             int host_id = server.Networking().HostPlayerID();
             player_connection->EstablishPlayer(host_id, player_name, client_type, client_version_string);
-            player_connection->SendMessage(ContentCheckSumMessage());
+            if (!player_connection->IsLocalConnection())
+                player_connection->SendMessage(ContentCheckSumMessage());
             player_connection->SendMessage(JoinAckMessage(host_id));
 
             DebugLogger(FSM) << "Initializing new SP game...";
