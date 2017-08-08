d4620624041f929165a45650b482619851ffb80e
geoffthemedio <geoffthemedio@gmail.com>
2017-08-08 10:29:48 +0200

    grooming

--- universe/UniverseGenerator.h
+++ universe/UniverseGenerator.h
@@ -42,7 +42,8 @@ protected:
 class FO_COMMON_API MonsterFleetPlan : public FleetPlan {
 public:
     MonsterFleetPlan(const std::string& fleet_name, const std::vector<std::string>& ship_design_names,
-                     double spawn_rate = 1.0, int spawn_limit = 9999, const Condition::ConditionBase* location = nullptr,
+                     double spawn_rate = 1.0, int spawn_limit = 9999,
+                     const Condition::ConditionBase* location = nullptr,
                      bool lookup_name_userstring = false) :
         FleetPlan(fleet_name, ship_design_names, lookup_name_userstring),
         m_spawn_rate(spawn_rate),
@@ -50,10 +51,7 @@ public:
         m_location(location)
     {}
     MonsterFleetPlan() :
-        FleetPlan(),
-        m_spawn_rate(1.0),
-        m_spawn_limit(9999),
-        m_location(nullptr)
+        FleetPlan()
     {}
     virtual ~MonsterFleetPlan()
     { delete m_location; }
@@ -61,9 +59,9 @@ public:
     int                             SpawnLimit() const  { return m_spawn_limit; }
     const Condition::ConditionBase* Location() const    { return m_location; }
 protected:
-    double                          m_spawn_rate;
-    int                             m_spawn_limit;
-    const Condition::ConditionBase* m_location;
+    double                          m_spawn_rate = 1.0;
+    int                             m_spawn_limit = 9999;
+    const Condition::ConditionBase* m_location = nullptr;
 };
 
 /** Set active meter current values equal to target/max meter current
