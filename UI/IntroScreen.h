92294d38a541cb6e1057a31f2c7d8d37ce45bc84
LGM-Doyle <LGM-Doyle@users.noreply.github.com>
2017-08-12 15:10:00 -0400

    housekeeping: Remove ';' trailing comments in IntroScreen.h

--- UI/IntroScreen.h
+++ UI/IntroScreen.h
@@ -44,16 +44,16 @@ public:
     //!@}
 
 private:
-    std::shared_ptr<GG::Button>         m_single_player;//!< opens up the single player game dialog;
-    std::shared_ptr<GG::Button>         m_quick_start;  //!< starts a single-player game with the default options (no dialog);
-    std::shared_ptr<GG::Button>         m_multi_player; //!< opens up the multi player game dialog;
-    std::shared_ptr<GG::Button>         m_load_game;    //!< loads a saved single player game;
-    std::shared_ptr<GG::Button>         m_options;      //!< opens the options dialog;
-    std::shared_ptr<GG::Button>         m_pedia;        //!< shows the pedia window;
-    std::shared_ptr<GG::Button>         m_about;        //!< opens a dialog to choose to see credits or license;
-    std::shared_ptr<GG::Button>         m_website;      //!< opens web browser (or however system handles urls) to http://freeorion.org;
-    std::shared_ptr<GG::Button>         m_credits;      //!< displays credits;
-    std::shared_ptr<GG::Button>         m_exit_game;    //!< button that exits the program;
+    std::shared_ptr<GG::Button>         m_single_player;//!< opens up the single player game dialog
+    std::shared_ptr<GG::Button>         m_quick_start;  //!< starts a single-player game with the default options (no dialog)
+    std::shared_ptr<GG::Button>         m_multi_player; //!< opens up the multi player game dialog
+    std::shared_ptr<GG::Button>         m_load_game;    //!< loads a saved single player game
+    std::shared_ptr<GG::Button>         m_options;      //!< opens the options dialog
+    std::shared_ptr<GG::Button>         m_pedia;        //!< shows the pedia window
+    std::shared_ptr<GG::Button>         m_about;        //!< opens a dialog to choose to see credits or license
+    std::shared_ptr<GG::Button>         m_website;      //!< opens web browser (or however system handles urls) to http://freeorion.org
+    std::shared_ptr<GG::Button>         m_credits;      //!< displays credits
+    std::shared_ptr<GG::Button>         m_exit_game;    //!< button that exits the program
 
     std::shared_ptr<CUIWnd>             m_menu;
     std::shared_ptr<GG::StaticGraphic>  m_splash;
