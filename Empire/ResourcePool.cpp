df5047ddebb028e62f9499a3c18540ef5bfe4e16
Axel Groß <axel.gross@(iaeste|phactum).at>
2017-08-29 10:32:08 +0200

    Cleanup code as indicated by code review

--- Empire/ResourcePool.cpp
+++ Empire/ResourcePool.cpp
@@ -35,7 +35,7 @@ float ResourcePool::TotalOutput() const {
     return retval;
 }
 
-std::map<std::set<int>, float> ResourcePool::Output() const { return  m_connected_object_groups_resource_output; }
+std::map<std::set<int>, float> ResourcePool::Output() const { return m_connected_object_groups_resource_output; }
     
 float ResourcePool::GroupOutput(int object_id) const {
     // find group containing specified object
