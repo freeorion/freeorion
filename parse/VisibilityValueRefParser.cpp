73662941c9d2bd621b3b8697453ff6300f0e4a6a
geoffthemedio <geoffthemedio@gmail.com>
2017-09-06 09:20:21 +0200

    EmpireObjectVisiblity parsing

--- parse/VisibilityValueRefParser.cpp
+++ parse/VisibilityValueRefParser.cpp
@@ -12,15 +12,38 @@ namespace {
         visibility_parser_rules() :
             enum_value_ref_rules("Visibility")
         {
-            boost::spirit::qi::_val_type _val;
+            using boost::phoenix::construct;
+            using boost::phoenix::new_;
+            using boost::phoenix::push_back;
+            namespace qi = boost::spirit::qi;
+
+            qi::_1_type _1;
+            qi::_a_type _a;
+            qi::_b_type _b;
+            qi::_c_type _c;
+            qi::_d_type _d;
+            qi::_e_type _e;
+            qi::_f_type _f;
+            qi::_val_type _val;
 
             const parse::lexer& tok = parse::lexer::instance();
+            const parse::value_ref_rule<int>& simple_int = int_simple();
+
+            // variable_name left empty, as no direct object properties are
+            // available that return a visibility
 
             enum_expr
                 =   tok.Invisible_  [ _val = VIS_NO_VISIBILITY ]
                 |   tok.Basic_      [ _val = VIS_BASIC_VISIBILITY ]
                 |   tok.Partial_    [ _val = VIS_PARTIAL_VISIBILITY ]
                 |   tok.Full_       [ _val = VIS_FULL_VISIBILITY ]
+                ;
+
+            complex_expr
+                =   tok.EmpireObjectVisiblity_ [ _a = construct<std::string>(_1) ]
+                >   parse::detail::label(Empire_token)  > simple_int [ _b = _1 ]
+                >   parse::detail::label(Object_token)  > simple_int [ _c = _1 ]
+                    [ _val = new_<ValueRef::ComplexVariable<Visibility>>(_a, _b, _c, _f, _d, _e) ]
                 ;
         }
     };
