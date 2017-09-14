ed05868667765142e7862a997efff1f42c5427dd
Morlic-fo <Morlic@gmx.de>
2017-09-15 00:54:12 +0200

    Add early-exit if no path to target can possibly exist

--- default/python/AI/pathfinding.py
+++ default/python/AI/pathfinding.py
@@ -2,8 +2,17 @@ from heapq import heappush, heappop
 from collections import namedtuple
 
 import freeOrionAIInterface as fo
+from turn_state import state
 
 
+# TODO Allow short idling in system to refuel
+# TODO Consider additional optimizations:
+#    - Check if universe.shortestPath is valid before running pathfinder
+#      (seemingly not worth it - we have exact heuristic in that case)
+#    - Cut off branches that can't reach target due to supply distance
+#    - For large graphs, check existance on a simplified graph (use single node to represent supply clusters)
+#    - For large graphs, check if there are any must-visit nodes (e.g. only possible resupplying system),
+#      then try to find the shortest path between those and start/target.
 def find_path_with_resupply(start, target, fleet_id):
     path_information = namedtuple('path_information', ['distance', 'fuel', 'path'])
     universe = fo.getUniverse()
@@ -11,8 +20,22 @@ def find_path_with_resupply(start, target, fleet_id):
     fleet = universe.getFleet(fleet_id)
     supplied_systems = set(fo.getEmpire().fleetSupplyableSystemIDs)
 
-    # initialize data structures
     start_fuel = fleet.maxFuel if start in supplied_systems else fleet.fuel
+
+    # We have 1 free jump from supplied system into unsupplied systems.
+    # Thus, the target system must be at most maxFuel + 1 jumps away
+    # in order to reach the system under standard conditions.
+    # In some edge cases, we may have more supply here than what the
+    # supply graph suggests. For example, we could have recently refueled
+    # in a system that is now blockaded by an enemy or we have found
+    # the refuel special.
+    target_distance_from_supply = -state.get_systems_by_supply_tier(target)
+    if (fleet.maxFuel + 1 < target_distance_from_supply and
+            universe.jumpDistance(start, target) > start_fuel):
+        # can't possibly reach this system
+        return None
+
+    # initialize data structures
     path_cache = {}
     shortest_possible_path = universe.shortestPathDistance(start, target)
     queue = [(shortest_possible_path, path_information(distance=0, fuel=start_fuel, path=(start,)), start)]
