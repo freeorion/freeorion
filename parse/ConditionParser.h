386138ab0d24904c8bd60f51f083ab3425d76298
LGM-Doyle <LGM-Doyle@users.noreply.github.com>
2017-09-21 11:39:18 -0400

    Change parse::condition_parser into a grammar.
    Since the grammar knows its own start rule, there is no need for an
    object that persists longer than the rule.

--- parse/ConditionParser.h
+++ parse/ConditionParser.h
@@ -3,10 +3,14 @@
 
 #include "Lexer.h"
 #include "ParseImpl.h"
+#include "ValueRefParser.h"
 
 #include <boost/spirit/include/qi.hpp>
 
 namespace Condition {
+    enum SortingMethod : int;
+    enum ComparisonType : int;
+    enum ContentType : int;
     struct ConditionBase;
 }
 
@@ -17,6 +21,290 @@ namespace parse {
 
     /** Returns a const reference to the Condition parser. */
     condition_parser_rule& condition_parser();
+}
+
+namespace parse { namespace detail {
+    using condition_signature      = Condition::ConditionBase* ();
+    using condition_parser_rule    = rule<condition_signature>;
+    using condition_parser_grammar = grammar<condition_signature>;
+
+    struct condition_parser_rules_1 : public condition_parser_grammar {
+        condition_parser_rules_1(const parse::lexer& tok,
+                                 const condition_parser_grammar& condition_parser);
+
+        typedef parse::detail::rule<
+            Condition::ConditionBase* (),
+            boost::spirit::qi::locals<EmpireAffiliationType>
+        > owned_by_rule;
+
+        typedef parse::detail::rule<
+            Condition::ConditionBase* (),
+            boost::spirit::qi::locals<std::vector<Condition::ConditionBase*>>
+        > and_or_rule;
+
+        typedef parse::detail::rule<
+            Condition::ConditionBase* (),
+            boost::spirit::qi::locals<std::string>
+        > described_rule;
+
+        parse::int_arithmetic_rules     int_rules;
+        parse::condition_parser_rule    all;
+        parse::condition_parser_rule    none;
+        parse::condition_parser_rule    source;
+        parse::condition_parser_rule    root_candidate;
+        parse::condition_parser_rule    target;
+        parse::condition_parser_rule    stationary;
+        parse::condition_parser_rule    aggressive;
+        parse::condition_parser_rule    can_colonize;
+        parse::condition_parser_rule    can_produce_ships;
+        parse::condition_parser_rule    capital;
+        parse::condition_parser_rule    monster;
+        parse::condition_parser_rule    armed;
+        parse::condition_parser_rule    owned_by_1;
+        parse::condition_parser_rule    owned_by_2;
+        parse::condition_parser_rule    owned_by_3;
+        parse::condition_parser_rule    owned_by_4;
+        owned_by_rule                   owned_by_5;
+        parse::condition_parser_rule    owned_by;
+        and_or_rule                     and_;
+        and_or_rule                     or_;
+        parse::condition_parser_rule    not_;
+        described_rule                  described;
+        parse::condition_parser_rule    start;
+    };
+
+    struct condition_parser_rules_2 : public condition_parser_grammar {
+        condition_parser_rules_2(const parse::lexer& tok);
+
+        typedef parse::detail::rule<
+            Condition::ConditionBase* (),
+            boost::spirit::qi::locals<
+                ValueRef::ValueRefBase<int>*,
+                ValueRef::ValueRefBase<int>*,
+                ValueRef::ValueRefBase<int>*,
+                ValueRef::ValueRefBase<int>*,
+                ValueRef::ValueRefBase<std::string>*
+            >
+        > common_rule;
+
+        parse::int_arithmetic_rules     int_rules;
+        parse::castable_as_int_parser_rules    castable_int_rules;
+        common_rule                     has_special_since_turn;
+        common_rule                     enqueued;
+        common_rule                     enqueued1;
+        common_rule                     enqueued2;
+        common_rule                     enqueued3;
+        common_rule                     enqueued4;
+        common_rule                     design_has_part;
+        common_rule                     design_has_part_class;
+        common_rule                     in_system;
+        parse::condition_parser_rule    start;
+    };
+
+    struct condition_parser_rules_3 : public condition_parser_grammar {
+        condition_parser_rules_3(const parse::lexer& tok,
+                                 const condition_parser_grammar& condition_parser);
+
+        typedef parse::detail::rule<
+            Condition::ConditionBase* (),
+            boost::spirit::qi::locals<
+                ValueRef::ValueRefBase<double>*,
+                ValueRef::ValueRefBase<double>*,
+                ValueRef::ValueRefBase<std::string>*,
+                Condition::ComparisonType,
+                Condition::ComparisonType,
+                ValueRef::ValueRefBase<std::string>*,
+                ValueRef::ValueRefBase<int>*,
+                ValueRef::ValueRefBase<int>*
+            >
+        > double_ref_double_ref_rule;
+
+        typedef parse::detail::rule<
+            Condition::ConditionBase* (),
+            boost::spirit::qi::locals<
+                ValueRef::ValueRefBase<int>*,
+                ValueRef::ValueRefBase<int>*
+            >
+        > int_ref_int_ref_rule;
+
+        typedef parse::detail::rule<
+            Condition::ConditionBase* (),
+            boost::spirit::qi::locals<
+                ValueRef::ValueRefBase<int>*,
+                Condition::SortingMethod,
+                ValueRef::ValueRefBase<double>*
+            >
+        > int_ref_sorting_method_double_ref_rule;
+
+        typedef parse::detail::rule<
+            Condition::ConditionBase* (),
+            boost::spirit::qi::locals<
+                ResourceType,
+                ValueRef::ValueRefBase<double>*
+            >
+        > resource_type_double_ref_rule;
+
+        parse::int_arithmetic_rules             int_rules;
+        parse::castable_as_int_parser_rules     castable_int_rules;
+        parse::double_parser_rules              double_rules;
+        double_ref_double_ref_rule              has_special_capacity;
+        double_ref_double_ref_rule              within_distance;
+        int_ref_int_ref_rule                    within_starlane_jumps;
+        int_ref_int_ref_rule                    number;
+        double_ref_double_ref_rule              value_test_1;
+        double_ref_double_ref_rule              value_test_2;
+        double_ref_double_ref_rule              value_test_3;
+        double_ref_double_ref_rule              value_test_4;
+        double_ref_double_ref_rule              value_test_5;
+        double_ref_double_ref_rule              value_test_6;
+        int_ref_int_ref_rule                    turn;
+        int_ref_int_ref_rule                    created_on_turn;
+        int_ref_sorting_method_double_ref_rule  number_of;
+        int_ref_sorting_method_double_ref_rule  number_of1;
+        int_ref_sorting_method_double_ref_rule  number_of2;
+        parse::condition_parser_rule            random;
+        resource_type_double_ref_rule           owner_stockpile;
+        int_ref_int_ref_rule                    resource_supply_connected;
+        parse::condition_parser_rule            can_add_starlane;
+        parse::condition_parser_rule            start;
+    };
+
+    struct condition_parser_rules_4 : public condition_parser_grammar {
+        condition_parser_rules_4(const parse::lexer& tok);
+
+        typedef parse::detail::rule<
+            Condition::ConditionBase* (),
+            boost::spirit::qi::locals<
+                MeterType,
+                ValueRef::ValueRefBase<double>*,
+                ValueRef::ValueRefBase<double>*,
+                std::string,
+                ValueRef::ValueRefBase<std::string>*
+            >
+        > meter_value_rule;
+
+        typedef parse::detail::rule<
+            Condition::ConditionBase* (),
+            boost::spirit::qi::locals<
+                std::string,
+                ValueRef::ValueRefBase<int>*,
+                ValueRef::ValueRefBase<double>*,
+                ValueRef::ValueRefBase<double>*
+            >
+        > empire_meter_value_rule;
+
+        parse::int_arithmetic_rules     int_rules;
+        parse::double_parser_rules      double_rules;
+        meter_value_rule                meter_value;
+        meter_value_rule                ship_part_meter_value;
+        empire_meter_value_rule         empire_meter_value;
+        empire_meter_value_rule         empire_meter_value1;
+        empire_meter_value_rule         empire_meter_value2;
+        parse::condition_parser_rule    start;
+    };
+
+
+    struct condition_parser_rules_5 : public condition_parser_grammar {
+        condition_parser_rules_5(const parse::lexer& tok);
+
+        parse::int_arithmetic_rules     int_rules;
+        parse::condition_parser_rule    has_special;
+        parse::condition_parser_rule    has_tag;
+        parse::condition_parser_rule    owner_has_tech;
+        parse::condition_parser_rule    design_has_hull;
+        parse::condition_parser_rule    predefined_design;
+        parse::condition_parser_rule    design_number;
+        parse::condition_parser_rule    produced_by_empire;
+        parse::condition_parser_rule    visible_to_empire;
+        parse::condition_parser_rule    explored_by_empire;
+        parse::condition_parser_rule    resupplyable_by;
+        parse::condition_parser_rule    object_id;
+        parse::condition_parser_rule    start;
+    };
+
+    struct condition_parser_rules_6 : public condition_parser_grammar {
+        condition_parser_rules_6();
+
+        typedef parse::detail::rule<
+            std::vector<ValueRef::ValueRefBase<std::string>*> ()
+        > string_ref_vec_rule;
+
+        typedef parse::detail::rule<
+            Condition::ConditionBase* (),
+            boost::spirit::qi::locals<std::vector<ValueRef::ValueRefBase<std::string>*>>
+        > building_rule;
+
+        typedef parse::detail::rule<
+            Condition::ConditionBase* (),
+            boost::spirit::qi::locals<std::vector<ValueRef::ValueRefBase<PlanetType>*>>
+        > planet_type_rule;
+
+        typedef parse::detail::rule<
+            Condition::ConditionBase* (),
+            boost::spirit::qi::locals<std::vector<ValueRef::ValueRefBase<PlanetSize>*>>
+        > planet_size_rule;
+
+        typedef parse::detail::rule<
+            Condition::ConditionBase* (),
+            boost::spirit::qi::locals<
+                std::vector<ValueRef::ValueRefBase<PlanetEnvironment>*>,
+                ValueRef::ValueRefBase<std::string>*
+            >
+        > planet_environment_rule;
+
+        string_ref_vec_rule             string_ref_vec;
+        parse::condition_parser_rule    homeworld;
+        building_rule                   building;
+        parse::condition_parser_rule    species;
+        parse::condition_parser_rule    focus_type;
+        planet_type_rule                planet_type;
+        planet_size_rule                planet_size;
+        planet_environment_rule         planet_environment;
+        parse::condition_parser_rule    object_type;
+        parse::condition_parser_rule    start;
+    };
+
+    struct condition_parser_rules_7 : public condition_parser_grammar {
+        condition_parser_rules_7(const condition_parser_grammar& condition_parser);
+
+        typedef parse::detail::rule<
+            Condition::ConditionBase* (),
+            boost::spirit::qi::locals<std::vector<ValueRef::ValueRefBase<StarType>*>>
+        > star_type_vec_rule;
+
+        typedef parse::detail::rule<
+            Condition::ConditionBase* (),
+            boost::spirit::qi::locals<
+                Condition::ContentType,
+                ValueRef::ValueRefBase<std::string>*,
+                ValueRef::ValueRefBase<std::string>*
+            >
+        > string_ref_rule;
+
+        parse::condition_parser_rule            ordered_bombarded_by;
+        parse::condition_parser_rule            contains;
+        parse::condition_parser_rule            contained_by;
+        star_type_vec_rule                      star_type;
+        string_ref_rule                         location;
+        parse::condition_parser_rule            owner_has_shippart_available;
+        parse::condition_parser_rule            start;
+    };
+
+}}
+
+namespace parse {
+    struct conditions_parser_grammar : public detail::condition_parser_grammar {
+        conditions_parser_grammar(const parse::lexer& tok);
+
+        detail::condition_parser_rules_1 condition_parser_1;
+        detail::condition_parser_rules_2 condition_parser_2;
+        detail::condition_parser_rules_3 condition_parser_3;
+        detail::condition_parser_rules_4 condition_parser_4;
+        detail::condition_parser_rules_5 condition_parser_5;
+        detail::condition_parser_rules_6 condition_parser_6;
+        detail::condition_parser_rules_7 condition_parser_7;
+        condition_parser_rule start;
+    };
 }
 
 #endif
--- parse/ConditionParser3.cpp
+++ parse/ConditionParser3.cpp
@@ -348,4 +348,272 @@ namespace parse { namespace detail {
         static condition_parser_rules_3 retval(parse::lexer::instance(), parse::condition_parser());
         return retval.start;
     }
+
+    condition_parser_rules_3::condition_parser_rules_3(
+        const parse::lexer& tok, const condition_parser_grammar& condition_parser) :
+        condition_parser_rules_3::base_type(start, "condition_parser_rules_3"),
+        int_rules(tok),
+        castable_int_rules(tok),
+        double_rules(tok)
+    {
+        qi::_1_type _1;
+        qi::_a_type _a;
+        qi::_b_type _b;
+        qi::_c_type _c;
+        qi::_d_type _d;
+        qi::_e_type _e;
+        qi::_f_type _f;
+        qi::_g_type _g;
+        qi::_h_type _h;
+        qi::_val_type _val;
+        qi::lit_type lit;
+        using phoenix::new_;
+        using phoenix::push_back;
+
+        has_special_capacity
+            =   (   tok.HasSpecialCapacity_
+                    >       parse::detail::label(Name_token) >  parse::string_value_ref() [ _c = _1 ]
+                    >     -(parse::detail::label(Low_token)  >  double_rules.expr [ _a = _1 ] )
+                    >     -(parse::detail::label(High_token) >  double_rules.expr [ _b = _1 ] )
+                ) [ _val = new_<Condition::HasSpecial>(_c, _a, _b) ]
+            ;
+
+        within_distance
+            =   tok.WithinDistance_
+            >   parse::detail::label(Distance_token)  > double_rules.expr [ _a = _1 ]
+            >   parse::detail::label(Condition_token) > condition_parser
+            [ _val = new_<Condition::WithinDistance>(_a, _1) ]
+            ;
+
+        within_starlane_jumps
+            =   tok.WithinStarlaneJumps_
+            >   parse::detail::label(Jumps_token)     > castable_int_rules.flexible_int [ _a = _1 ]
+            >   parse::detail::label(Condition_token) > condition_parser
+            [ _val = new_<Condition::WithinStarlaneJumps>(_a, _1) ]
+            ;
+
+        number
+            =   tok.Number_
+            > -(parse::detail::label(Low_token)   >  castable_int_rules.flexible_int [ _a = _1 ])
+            > -(parse::detail::label(High_token)  >  castable_int_rules.flexible_int [ _b = _1 ])
+            >   parse::detail::label(Condition_token) > condition_parser
+            [ _val = new_<Condition::Number>(_a, _b, _1) ]
+            ;
+
+        value_test_1
+            = ('('
+               >> double_rules.expr [ _a = _1 ]
+               >> (    lit("==")   [ _d = Condition::EQUAL ]
+                      | lit('=')    [ _d = Condition::EQUAL ]
+                      | lit(">=")   [ _d = Condition::GREATER_THAN_OR_EQUAL ]
+                      | lit('>')    [ _d = Condition::GREATER_THAN ]
+                      | lit("<=")   [ _d = Condition::LESS_THAN_OR_EQUAL ]
+                      | lit('<')    [ _d = Condition::LESS_THAN ]
+                      | lit("!=")   [ _d = Condition::NOT_EQUAL ])
+                  ) > double_rules.expr // assuming the trinary form already didn't pass, can expect a (double) here, though it might be an (int) casted to (double). By matching the (int) cases first, can assume that at least one of the parameters here is not an (int) casted to double.
+                [ _val = new_<Condition::ValueTest>(_a, _d, _1) ]
+                >> ')'
+                ;
+
+            value_test_2
+                = ('('
+                >> double_rules.expr [ _a = _1 ]
+                >> (    lit("==")   [ _d = Condition::EQUAL ]
+                      | lit('=')    [ _d = Condition::EQUAL ]
+                      | lit(">=")   [ _d = Condition::GREATER_THAN_OR_EQUAL ]
+                      | lit('>')    [ _d = Condition::GREATER_THAN ]
+                      | lit("<=")   [ _d = Condition::LESS_THAN_OR_EQUAL ]
+                      | lit('<')    [ _d = Condition::LESS_THAN ]
+                      | lit("!=")   [ _d = Condition::NOT_EQUAL ])
+                >> double_rules.expr [ _b = _1 ]
+                >> (    lit("==")   [ _d = Condition::EQUAL ]
+                      | lit('=')    [ _d = Condition::EQUAL ]
+                      | lit(">=")   [ _e = Condition::GREATER_THAN_OR_EQUAL ]
+                      | lit('>')    [ _e = Condition::GREATER_THAN ]
+                      | lit("<=")   [ _e = Condition::LESS_THAN_OR_EQUAL ]
+                      | lit('<')    [ _e = Condition::LESS_THAN ]
+                      | lit("!=")   [ _e = Condition::NOT_EQUAL ])
+                  ) >  double_rules.expr    // if already seen (double) (operator) (double) (operator) can expect to see another (double). Some of these (double) may be (int) casted to double, though not all of them can be, as in that case, the (int) parser should have matched.
+                [ _val = new_<Condition::ValueTest>(_a, _d, _b, _e, _1) ]
+                >  ')'
+                ;
+
+            value_test_3
+                = ( '('
+                >> parse::string_value_ref() [ _c = _1 ]
+                >> (    lit("==")   [ _d = Condition::EQUAL ]
+                      | lit('=')    [ _d = Condition::EQUAL ]
+                      | lit("!=")   [ _d = Condition::NOT_EQUAL ])
+                  ) > parse::string_value_ref() // assuming the trinary (string) form already didn't parse, if already seen (string) (operator) can expect another (string)
+                [ _val = new_<Condition::ValueTest>(_c, _d, _1) ]
+                >> ')'
+                ;
+
+            value_test_4
+                = ( '('
+                >> parse::string_value_ref() [ _c = _1 ]
+                >> (    lit("==")   [ _d = Condition::EQUAL ]
+                      | lit('=')    [ _d = Condition::EQUAL ]
+                      | lit("!=")   [ _d = Condition::NOT_EQUAL ])
+                >> parse::string_value_ref() [ _f = _1 ]
+                >> (    lit("==")   [ _d = Condition::EQUAL ]
+                      | lit('=')    [ _d = Condition::EQUAL ]
+                      | lit("!=")   [ _e = Condition::NOT_EQUAL ])
+                ) >  parse::string_value_ref() // if already seen (string) (operator) (string) (operator) can expect to see another (string)
+                [ _val = new_<Condition::ValueTest>(_c, _d, _f, _e, _1) ]
+                >  ')'
+                ;
+
+            value_test_5
+                = '('
+                >> int_rules.expr [ _g = _1 ]
+                >> (    lit("==")   [ _d = Condition::EQUAL ]
+                      | lit('=')    [ _d = Condition::EQUAL ]
+                      | lit(">=")   [ _d = Condition::GREATER_THAN_OR_EQUAL ]
+                      | lit('>')    [ _d = Condition::GREATER_THAN ]
+                      | lit("<=")   [ _d = Condition::LESS_THAN_OR_EQUAL ]
+                      | lit('<')    [ _d = Condition::LESS_THAN ]
+                      | lit("!=")   [ _d = Condition::NOT_EQUAL ])
+                >> int_rules.expr   // can't expect an (int) here, as it could actually be a (double) comparision with the first (double) cased from an (int)
+                [ _val = new_<Condition::ValueTest>(_g, _d, _1) ]
+                >> ')'
+                ;
+
+            value_test_6
+                = ('('
+                >> int_rules.expr [ _g = _1 ]
+                >> (    lit("==")   [ _d = Condition::EQUAL ]
+                      | lit('=')    [ _d = Condition::EQUAL ]
+                      | lit(">=")   [ _d = Condition::GREATER_THAN_OR_EQUAL ]
+                      | lit('>')    [ _d = Condition::GREATER_THAN ]
+                      | lit("<=")   [ _d = Condition::LESS_THAN_OR_EQUAL ]
+                      | lit('<')    [ _d = Condition::LESS_THAN ]
+                      | lit("!=")   [ _d = Condition::NOT_EQUAL ])
+                >> int_rules.expr [ _h = _1 ]
+                >> (    lit("==")   [ _d = Condition::EQUAL ]
+                      | lit('=')    [ _d = Condition::EQUAL ]
+                      | lit(">=")   [ _e = Condition::GREATER_THAN_OR_EQUAL ]
+                      | lit('>')    [ _e = Condition::GREATER_THAN ]
+                      | lit("<=")   [ _e = Condition::LESS_THAN_OR_EQUAL ]
+                      | lit('<')    [ _e = Condition::LESS_THAN ]
+                      | lit("!=")   [ _e = Condition::NOT_EQUAL ])
+                 >> int_rules.expr   // only treat as trinary (int) comparison if all parameters are (int). otherwise fall back to (double) comparison, which allows some of the parameters to be (int) casted to (double)
+                 [ _val = new_<Condition::ValueTest>(_g, _d, _h, _e, _1) ]
+                  ) >  ')'
+                ;
+
+            turn
+                =  (tok.Turn_
+                > -(parse::detail::label(Low_token)  > (castable_int_rules.flexible_int [ _a = _1 ]))
+                > -(parse::detail::label(High_token) > (castable_int_rules.flexible_int [ _b = _1 ])))
+                [ _val = new_<Condition::Turn>(_a, _b) ]
+                ;
+
+            created_on_turn
+                =  (tok.CreatedOnTurn_
+                > -(parse::detail::label(Low_token)  > castable_int_rules.flexible_int [ _a = _1 ])
+                > -(parse::detail::label(High_token) > castable_int_rules.flexible_int [ _b = _1 ]))
+                [ _val = new_<Condition::CreatedOnTurn>(_a, _b) ]
+                ;
+
+            number_of1
+                =   tok.NumberOf_
+                >   parse::detail::label(Number_token)    > castable_int_rules.flexible_int [ _a = _1 ]
+                >   parse::detail::label(Condition_token) > condition_parser
+                [ _val = new_<Condition::SortedNumberOf>(_a, _1) ]
+                ;
+
+            number_of2
+                =   (   tok.MaximumNumberOf_ [ _b = Condition::SORT_MAX ]
+                    |   tok.MinimumNumberOf_ [ _b = Condition::SORT_MIN ]
+                    |   tok.ModeNumberOf_    [ _b = Condition::SORT_MODE ]
+                    )
+                >   parse::detail::label(Number_token)    > castable_int_rules.flexible_int [ _a = _1 ]
+                >   parse::detail::label(SortKey_token)   > double_rules.expr [ _c = _1 ]
+                >   parse::detail::label(Condition_token) > condition_parser
+                [ _val = new_<Condition::SortedNumberOf>(_a, _c, _b, _1) ]
+                ;
+
+            number_of
+                =   number_of1
+                |   number_of2
+                ;
+
+            random
+                =   tok.Random_
+                >   parse::detail::label(Probability_token) > double_rules.expr
+                [ _val = new_<Condition::Chance>(_1) ]
+                ;
+
+            owner_stockpile
+                =   tok.OwnerTradeStockpile_ [ _a = RE_TRADE ]
+                >   parse::detail::label(Low_token)  > double_rules.expr [ _b = _1 ]
+                >   parse::detail::label(High_token) > double_rules.expr
+                [ _val = new_<Condition::EmpireStockpileValue>(_a, _b, _1) ]
+                ;
+
+            resource_supply_connected
+                =   tok.ResourceSupplyConnected_
+                >   parse::detail::label(Empire_token)    > int_rules.expr [ _a = _1 ]
+                >   parse::detail::label(Condition_token) > condition_parser
+                [ _val = new_<Condition::ResourceSupplyConnectedByEmpire>(_a, _1) ]
+                ;
+
+            can_add_starlane
+                =   tok.CanAddStarlanesTo_
+                >   parse::detail::label(Condition_token) > condition_parser
+                [ _val = new_<Condition::CanAddStarlaneConnection>(_1) ]
+                ;
+
+            start
+                =   has_special_capacity
+                |   within_distance
+                |   within_starlane_jumps
+                |   number
+                |   value_test_5    // more complicated format that is strict extension of value_test_3 format, so needs to be tested before it
+                |   value_test_6    //  first int ...
+                |   value_test_2    // more complicated format that is strict extension of value_test_1 format, so needs to be tested before it
+                |   value_test_1    //  ... then double (which may include int(s) casted to double(s))...
+                |   value_test_4    // more complicated format that is strict extension of value_test_3 format, so needs to be tested before it
+                |   value_test_3    //  ... then string
+                |   turn
+                |   created_on_turn
+                |   number_of
+                |   random
+                |   owner_stockpile
+                |   resource_supply_connected
+                |   can_add_starlane
+                ;
+
+            has_special_capacity.name("HasSpecialCapacity");
+            within_distance.name("WithinDistance");
+            within_starlane_jumps.name("WithinStarlaneJumps");
+            number.name("Number");
+            value_test_1.name("ValueTest Binary");
+            value_test_2.name("ValueTest Trinary");
+            turn.name("Turn");
+            created_on_turn.name("CreatedOnTurn");
+            number_of.name("NumberOf");
+            random.name("Random");
+            owner_stockpile.name("OwnerStockpile");
+            resource_supply_connected.name("ResourceSupplyConnected");
+            can_add_starlane.name("CanAddStarlanesTo");
+
+#if DEBUG_CONDITION_PARSERS
+            debug(has_special_capacity);
+            debug(within_distance);
+            debug(within_starlane_jumps);
+            debug(number);
+            debug(value_test_1);
+            debug(value_test_2);
+            debug(turn);
+            debug(created_on_turn);
+            debug(number_of);
+            debug(random);
+            debug(owner_stockpile);
+            debug(resource_supply_connected);
+            debug(can_add_starlane);
+#endif
+        }
+
 } }
