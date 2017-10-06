661e32046663ed819c66c21025673b9870771cc3
D Benage <dbenage.cx@gmail.com>
2017-10-05 22:49:03 -0500

    Return unique_ptr when parsing HullTypes

--- parse/ShipHullsParser.cpp
+++ parse/ShipHullsParser.cpp
@@ -21,8 +21,8 @@
 namespace std {
     inline ostream& operator<<(ostream& os, const std::vector<HullType::Slot>&) { return os; }
     inline ostream& operator<<(ostream& os, const std::vector<std::shared_ptr<Effect::EffectsGroup>>&) { return os; }
-    inline ostream& operator<<(ostream& os, const std::map<std::string, HullType*>&) { return os; }
-    inline ostream& operator<<(ostream& os, const std::pair<const std::string, HullType*>&) { return os; }
+    inline ostream& operator<<(ostream& os, const std::map<std::string, std::unique_ptr<HullType>>&) { return os; }
+    inline ostream& operator<<(ostream& os, const std::pair<const std::string, std::unique_ptr<HullType>>&) { return os; }
     inline ostream& operator<<(ostream& os, const HullType::Slot&) { return os; }
     inline ostream& operator<<(ostream& os, const HullTypeStats&) { return os; }
 }
@@ -31,7 +31,18 @@ namespace std {
 namespace {
     const boost::phoenix::function<parse::detail::is_unique> is_unique_;
 
-    const boost::phoenix::function<parse::detail::insert> insert_;
+    void insert_hulltype(std::map<std::string, std::unique_ptr<HullType>>& hulltypes,
+                         const HullTypeStats& stats, const CommonParams& common_params,
+                         const MoreCommonParams& more_common_params,
+                         const std::vector<HullType::Slot>& slots,
+                         const std::string& icon, const std::string& graphic)
+    {
+        auto hulltype = std::unique_ptr<HullType>(
+            new HullType(stats, common_params, more_common_params, slots, icon, graphic));
+        hulltypes.emplace(hulltype->Name(), std::move(hulltype));
+    }
+
+    BOOST_PHOENIX_ADAPT_FUNCTION(void, insert_hulltype_, insert_hulltype, 7)
 
     struct rules {
         rules() {
@@ -39,9 +50,7 @@ namespace {
             namespace qi = boost::spirit::qi;
 
             using phoenix::construct;
-            using phoenix::new_;
             using phoenix::push_back;
-            using phoenix::insert;
 
             qi::_1_type _1;
             qi::_2_type _2;
@@ -96,8 +105,7 @@ namespace {
                 >   parse::detail::common_params_parser()       [ _d = _1 ]
                 >   parse::detail::label(Icon_token)    > tok.string    [ _f = _1 ]
                 >   parse::detail::label(Graphic_token) > tok.string
-                [ insert_(_r1, phoenix::bind(&MoreCommonParams::name, _a),
-                          new_<HullType>(_c, _d, _a, _e, _f, _1)) ]
+                [ insert_hulltype_(_r1, _c, _d, _a, _e, _f, _1) ]
                 ;
 
             start
@@ -144,7 +152,7 @@ namespace {
         > slots_rule;
 
         typedef parse::detail::rule<
-            void (std::map<std::string, HullType*>&),
+            void (std::map<std::string, std::unique_ptr<HullType>>&),
             boost::spirit::qi::locals<
                 MoreCommonParams,
                 std::string,    // dummy
@@ -156,7 +164,7 @@ namespace {
         > hull_rule;
 
         typedef parse::detail::rule<
-            void (std::map<std::string, HullType*>&)
+            void (std::map<std::string, std::unique_ptr<HullType>>&)
         > start_rule;
 
         hull_stats_rule                             hull_stats;
@@ -168,11 +176,11 @@ namespace {
 }
 
 namespace parse {
-    bool ship_hulls(std::map<std::string, HullType*>& hulls) {
+    bool ship_hulls(std::map<std::string, std::unique_ptr<HullType>>& hulls) {
         bool result = true;
 
         for (const boost::filesystem::path& file : ListScripts("scripting/ship_hulls")) {
-            result &= detail::parse_file<rules, std::map<std::string, HullType*>>(file, hulls);
+            result &= detail::parse_file<rules, std::map<std::string, std::unique_ptr<HullType>>>(file, hulls);
         }
 
         return result;
