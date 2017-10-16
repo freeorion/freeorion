82135144255a2e38d31841a74c7705a79ed5fc4d
D Benage <dbenage.cx@gmail.com>
2017-10-16 13:41:50 -0500

    Rename shadowing var in UniverseObject::Dump

--- universe/UniverseObject.cpp
+++ universe/UniverseObject.cpp
@@ -184,13 +184,13 @@ std::string UniverseObject::Dump() const {
     } else {
         os << "  at: (" << this->X() << ", " << this->Y() << ")";
         int near_id = GetPathfinder()->NearestSystemTo(this->X(), this->Y());
-        auto system = GetSystem(near_id);
-        if (system) {
-            const std::string& sys_name = system->Name();
+        auto near_system = GetSystem(near_id);
+        if (near_system) {
+            const std::string& sys_name = near_system->Name();
             if (sys_name.empty())
-                os << " nearest (System " << system->ID() << ")";
+                os << " nearest (System " << near_system->ID() << ")";
             else
-                os << " nearest " << system->Name();
+                os << " nearest " << near_system->Name();
         }
     }
     if (Unowned()) {
