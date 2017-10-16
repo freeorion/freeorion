e3039cad6cec05b46dcd05f1bf4255e3e2999ae9
LGM-Doyle <LGM-Doyle@users.noreply.github.com>
2017-10-16 10:36:04 -0400

    Allow 1 FPS max-fps and max-fps-no_focus
    This will allow better debugging reports of GUI issues like flicker.

--- UI/ClientUI.cpp
+++ UI/ClientUI.cpp
@@ -473,9 +473,11 @@ namespace {
     void AddOptions(OptionsDB& db) {
         db.Add("show-fps",              UserStringNop("OPTIONS_DB_SHOW_FPS"),              false);
         db.Add("limit-fps",             UserStringNop("OPTIONS_DB_LIMIT_FPS"),             true);
-        db.Add("max-fps",               UserStringNop("OPTIONS_DB_MAX_FPS"),               60.0,    RangedStepValidator<double>(1.0, 10.0, 240.0));
+        db.Add("max-fps",               UserStringNop("OPTIONS_DB_MAX_FPS"),               60.0,
+               RangedStepValidator<double>(1.0, 1.0, 240.0));
         db.Add("limit-fps-no-focus",    UserStringNop("OPTIONS_DB_LIMIT_FPS_NO_FOCUS"),    true);
-        db.Add("max-fps-no_focus",      UserStringNop("OPTIONS_DB_MAX_FPS_NO_FOCUS"),      15.0,    RangedStepValidator<double>(1.0, 1.0, 30.0));
+        db.Add("max-fps-no_focus",      UserStringNop("OPTIONS_DB_MAX_FPS_NO_FOCUS"),      15.0,
+               RangedStepValidator<double>(1.0, 1.0, 30.0));
 
         // sound and music
         db.Add<std::string>("UI.sound.bg-music",                UserStringNop("OPTIONS_DB_BG_MUSIC"),                          (GetRootDataDir() / "default" / "data" / "sound" / "artificial_intelligence_v3.ogg").string());
