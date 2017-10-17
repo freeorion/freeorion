7c990db8471f2d9b1db1089b70410bf90020d9e6
LGM-Doyle <LGM-Doyle@users.noreply.github.com>
2017-10-17 08:34:15 -0400

    Refactor parse_file warnings into parse_file_end_of_file_warnings

--- parse/ParseImpl.h
+++ parse/ParseImpl.h
@@ -131,6 +131,14 @@ namespace parse { namespace detail {
                            text_iterator& last,
                            token_iterator& it);
 
+    /** Report warnings about unparsed end of file and return true for a good
+        parse. */
+    bool parse_file_end_of_file_warnings(const boost::filesystem::path& path,
+                                         bool parser_success,
+                                         std::string& file_contents,
+                                         text_iterator& first,
+                                         text_iterator& last);
+
     template <typename Grammar, typename Arg1>
     bool parse_file(const lexer& lexer, const boost::filesystem::path& path, Arg1& arg1) {
         SectionedScopedTimer timer("parse_file \"" + path.filename().string()  + "\"", std::chrono::microseconds(1000));
@@ -152,25 +160,8 @@ namespace parse { namespace detail {
         bool success = boost::spirit::qi::phrase_parse(
             it, lexer.end(), grammar(boost::phoenix::ref(arg1)), in_state("WS")[lexer.self]);
 
-        if (!success)
-            WarnLogger() << "A parser failed while parsing " << path;
-
-        auto length_of_unparsed_file = std::distance(first, last);
-        bool parse_length_good = ((length_of_unparsed_file == 0)
-                                  || (length_of_unparsed_file == 1 && *first == '\n'));
-
-        if (!parse_length_good
-            && length_of_unparsed_file > 0
-            && static_cast<std::string::size_type>(length_of_unparsed_file) <= file_contents.size())
-        {
-            auto unparsed_section = file_contents.substr(file_contents.size() - std::abs(length_of_unparsed_file));
-            std::copy(first, last, std::back_inserter(unparsed_section));
-            WarnLogger() << "File \"" << path << "\" was incompletely parsed. " << std::endl
-                         << "Unparsed section of file, " << length_of_unparsed_file <<" characters:" << std::endl
-                         << unparsed_section;
-        }
-
-        return success && parse_length_good;
+        return parse_file_end_of_file_warnings(path, success, file_contents, first, last);
+    }
     }
 
 } }
