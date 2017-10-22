2d51fb63f5275e1b3e3916a2997a1b89ed8326e4
geoffthemedio <geoffthemedio@gmail.com>
2017-10-22 17:25:45 +0200

    Fix rebasing issues.

--- UI/FleetWnd.cpp
+++ UI/FleetWnd.cpp
@@ -855,10 +855,6 @@ namespace {
             return;
         }
 
-        } else if (entry.first == METER_SECONDARY_STAT ||
-                   entry.first == METER_TERTIARY_STAT)
-        {
-
         int empire_id = HumanClientApp::GetApp()->EmpireID();
 
 
@@ -904,7 +900,9 @@ namespace {
                                                    TroopIcon(), UserString("SHIP_TROOPS_TITLE"),
                                                    UserString("SHIP_TROOPS_STAT")));
 
-            } else if (entry.first == METER_SECONDARY_STAT) {
+            } else if (entry.first == METER_SECONDARY_STAT ||
+                       entry.first == METER_TERTIARY_STAT)
+            {
                 entry.second->SetBrowseInfoWnd(GG::Wnd::Create<ShipFightersBrowseWnd>(
                                                    m_ship_id, entry.first));
                 entry.second->SetBrowseModeTime(GetOptionsDB().Get<int>("ui.tooltip.extended.delay"), 1);
