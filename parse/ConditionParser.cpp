daa1ea8e44b161619cbc5e777d99f02470c1f89c
LGM-Doyle <LGM-Doyle@users.noreply.github.com>
2017-11-30 08:41:10 -0500

    Add ValueRef include to ConditionParser.cpp
    This fixes issue #1890 clang implicit instantiation of undefined
    template 'ValueRef::ValueRefBase<int>'

--- parse/ConditionParser.cpp
+++ parse/ConditionParser.cpp
@@ -9,6 +9,7 @@
 #include "ConditionParser7.h"
 
 #include "../universe/Condition.h"
+#include "../universe/ValueRef.h"
 
 //TODO: replace with std::make_unique when transitioning to C++14
 #include <boost/smart_ptr/make_unique.hpp>
