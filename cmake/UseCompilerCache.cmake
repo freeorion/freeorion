da19b564a24dcf1b83d3fe7c3d299a6791d7ff77
LGM-Doyle <LGM-Doyle@users.noreply.github.com>
2017-12-06 17:25:04 -0500

    Split USE_COMPILER_CACHE into before and after project() parts.
    The Xcode portions of USE_COMPILER_CACHE require project() to have
    already found the compilers.

--- cmake/UseCompilerCache.cmake
+++ cmake/UseCompilerCache.cmake
@@ -7,7 +7,7 @@
 #
 # This module provides a function to setup a compiler cache tool (currently ``ccache``):
 #
-#   USE_COMPILER_CACHE([PROGRAM <ccache_name>] [QUIET] [REQUIRED])
+#   FIND_COMPILER_CACHE([PROGRAM <ccache_name>] [QUIET] [REQUIRED])
 #   -- Add the compiler cache tool (default to look for ccache on the path)
 #      to your build through CMAKE_<LANG>_COMPILER_LAUNCHER variables. Also
 #      supports XCode. Uses a wrapper for XCode and CCache < 3.3.
@@ -16,7 +16,7 @@
 # TODO: Remove this module when cmake 3.11 is the minimum required version
 #       FreeOrion included this file directly from CMake master.  
 
-function(USE_COMPILER_CACHE)
+function(FIND_COMPILER_CACHE)
     set(options
         QUIET
         REQUIRED
@@ -55,6 +55,7 @@ function(USE_COMPILER_CACHE)
             string(REGEX REPLACE "ccache version ([\\.0-9]+)$" "\\1" version "${line}")
             if(version AND NOT "x${line}" STREQUAL "x${version}")
                 set(COMPILER_CACHE_VERSION ${version})
+                set(COMPILER_CACHE_VERSION ${version} PARENT_SCOPE)
                 break()
             endif()
         endforeach()
@@ -65,67 +66,7 @@ function(USE_COMPILER_CACHE)
 
         # TODO: set PARENT_SCOPE with COMPILER_CACHE_VERSION
 
-        # This wrapper only is needed for CCache < 3.3 or XCode
-        if("${CMAKE_GENERATOR}" STREQUAL Xcode OR "${COMPILER_CACHE_VERSION}" VERSION_LESS 3.2.0)
-            file(WRITE "${CMAKE_BINARY_DIR}/launch-c" ""
-                "#!/bin/sh\n"
-                "\n"
-                "# Xcode generator doesn't include the compiler as the\n"
-                "# first argument, Ninja and Makefiles do. Handle both cases.\n"
-                "if [[ \"$1\" = \"${CMAKE_C_COMPILER}\" ]] ; then\n"
-                "    shift\n"
-                "fi\n"
-                "\n"
-                "export CCACHE_CPP2=true\n"
-                "echo using \"${RULE_LAUNCH_COMPILE}\" to compile\n"
-                "exec \"${RULE_LAUNCH_COMPILE}\" \"${CMAKE_C_COMPILER}\" \"$@\"\n"
-                )
-
-            file(WRITE "${CMAKE_BINARY_DIR}/launch-cxx" ""
-                "#!/bin/sh\n"
-                "\n"
-                "# Xcode generator doesn't include the compiler as the\n"
-                "# first argument, Ninja and Makefiles do. Handle both cases.\n"
-                "if [[ \"$1\" = \"${CMAKE_CXX_COMPILER}\" ]] ; then\n"
-                "    shift\n"
-                "fi\n"
-                "\n"
-                "export CCACHE_CPP2=true\n"
-                "echo using \"${RULE_LAUNCH_COMPILE}\" to compile\n"
-                "exec \"${RULE_LAUNCH_COMPILE}\" \"${CMAKE_CXX_COMPILER}\" \"$@\"\n"
-                )
-
-            # Cuda support only added in CMake 3.10
-            file(WRITE "${CMAKE_BINARY_DIR}/launch-cuda" ""
-                "#!/bin/sh\n"
-                "\n"
-                "# Xcode generator doesn't include the compiler as the\n"
-                "# first argument, Ninja and Makefiles do. Handle both cases.\n"
-                "if [[ \"$1\" = \"${CMAKE_CUDA_COMPILER}\" ]] ; then\n"
-                "    shift\n"
-                "fi\n"
-                "\n"
-                "export CCACHE_CPP2=true\n"
-                "echo using \"${RULE_LAUNCH_COMPILE}\" to compile\n"
-                "exec \"${RULE_LAUNCH_COMPILE}\" \"${CMAKE_CUDA_COMPILER}\" \"$@\"\n"
-                )
-
-
-            execute_process(COMMAND chmod a+rx
-                             "${CMAKE_BINARY_DIR}/launch-c"
-                             "${CMAKE_BINARY_DIR}/launch-cxx"
-                             "${CMAKE_BINARY_DIR}/launch-cuda"
-            )
-
-            # Set Xcode project attributes to route compilation and linking
-            # through our scripts
-            set(CMAKE_XCODE_ATTRIBUTE_CC         "${CMAKE_BINARY_DIR}/launch-c")
-            set(CMAKE_XCODE_ATTRIBUTE_CXX        "${CMAKE_BINARY_DIR}/launch-cxx")
-            set(CMAKE_XCODE_ATTRIBUTE_LD         "${CMAKE_BINARY_DIR}/launch-c")
-            set(CMAKE_XCODE_ATTRIBUTE_LDPLUSPLUS "${CMAKE_BINARY_DIR}/launch-cxx")
-            message(STATUS "CCache configured for XCode: ${CCACHE_PROGRAM}")
-
-        else()
+        if(NOT ("${CMAKE_GENERATOR}" STREQUAL Xcode OR "${COMPILER_CACHE_VERSION}" VERSION_LESS 3.2.0))
             # Support Unix Makefiles and Ninja
             set(CMAKE_C_COMPILER_LAUNCHER   "${CCACHE_PROGRAM}")
             set(CMAKE_CXX_COMPILER_LAUNCHER "${CMAKE_PROGRAM}")
@@ -133,4 +74,71 @@ function(USE_COMPILER_CACHE)
             message(STATUS "CCache configured for linux: ${CCACHE_PROGRAM}")
         endif()
     endif()
+endfunction()
+
+# This module provides a function to setup a compiler cache tool for XCode (currently ``ccache``):
+#
+#   USE_COMPILER_CACHE_WITH_XCODE()
+#   -- Confiure XCode to use ccache.  This must be called after the project()
+#   statement so that CMAKE_C_COMPILER is already set.
+
+function(USE_COMPILER_CACHE_WITH_XCODE)
+  get_property(RULE_LAUNCH_COMPILE_VAR GLOBAL PROPERTY RULE_LAUNCH_COMPILE)
+  message(STATUS "Use CCache for ${CMAKE_GENERATOR} called with: ccache program ${RULE_LAUNCH_COMPILE_VAR} ${COMPILER_CACHE_VERSION}")
+  # This wrapper only is needed for CCache < 3.3 or XCode
+  if(RULE_LAUNCH_COMPILE_VAR AND ("${CMAKE_GENERATOR}" STREQUAL Xcode OR "${COMPILER_CACHE_VERSION}" VERSION_LESS 3.2.0))
+      file(WRITE "${CMAKE_BINARY_DIR}/launch-c" ""
+          "#!/bin/sh\n"
+          "\n"
+          "# Xcode generator doesn't include the compiler as the\n"
+          "# first argument, Ninja and Makefiles do. Handle both cases.\n"
+          "if [[ \"$1\" = \"${CMAKE_C_COMPILER}\" ]] ; then\n"
+          "    shift\n"
+          "fi\n"
+          "\n"
+          "export CCACHE_CPP2=true\n"
+          "echo using \"${RULE_LAUNCH_COMPILE_VAR}\" to compile\n"
+          "exec \"${RULE_LAUNCH_COMPILE_VAR}\" \"${CMAKE_C_COMPILER}\" \"$@\"\n"
+          )
+       file(WRITE "${CMAKE_BINARY_DIR}/launch-cxx" ""
+          "#!/bin/sh\n"
+          "\n"
+          "# Xcode generator doesn't include the compiler as the\n"
+          "# first argument, Ninja and Makefiles do. Handle both cases.\n"
+          "if [[ \"$1\" = \"${CMAKE_CXX_COMPILER}\" ]] ; then\n"
+          "    shift\n"
+          "fi\n"
+          "\n"
+          "export CCACHE_CPP2=true\n"
+          "echo using \"${RULE_LAUNCH_COMPILE_VAR}\" to compile\n"
+          "exec \"${RULE_LAUNCH_COMPILE_VAR}\" \"${CMAKE_CXX_COMPILER}\" \"$@\"\n"
+          )
+       # Cuda support only added in CMake 3.10
+      file(WRITE "${CMAKE_BINARY_DIR}/launch-cuda" ""
+          "#!/bin/sh\n"
+          "\n"
+          "# Xcode generator doesn't include the compiler as the\n"
+          "# first argument, Ninja and Makefiles do. Handle both cases.\n"
+          "if [[ \"$1\" = \"${CMAKE_CUDA_COMPILER}\" ]] ; then\n"
+          "    shift\n"
+          "fi\n"
+          "\n"
+          "export CCACHE_CPP2=true\n"
+          "echo using \"${RULE_LAUNCH_COMPILE_VAR}\" to compile\n"
+          "exec \"${RULE_LAUNCH_COMPILE_VAR}\" \"${CMAKE_CUDA_COMPILER}\" \"$@\"\n"
+          )
+        execute_process(COMMAND chmod a+rx
+          "${CMAKE_BINARY_DIR}/launch-c"
+          "${CMAKE_BINARY_DIR}/launch-cxx"
+          "${CMAKE_BINARY_DIR}/launch-cuda"
+          )
+                       
+      # Set Xcode project attributes to route compilation and linking
+      # through our scripts
+      set(CMAKE_XCODE_ATTRIBUTE_CC         "${CMAKE_BINARY_DIR}/launch-c" PARENT_SCOPE)
+      set(CMAKE_XCODE_ATTRIBUTE_CXX        "${CMAKE_BINARY_DIR}/launch-cxx" PARENT_SCOPE)
+      set(CMAKE_XCODE_ATTRIBUTE_LD         "${CMAKE_BINARY_DIR}/launch-c" PARENT_SCOPE)
+      set(CMAKE_XCODE_ATTRIBUTE_LDPLUSPLUS "${CMAKE_BINARY_DIR}/launch-cxx" PARENT_SCOPE)
+      message(STATUS "CCache configured for XCode: ${RULE_LAUNCH_COMPILE_VAR}")
+  endif()  
 endfunction()
