6c251b64a1f06992d58650e4b350c541bd4628c5
Marcel Metz <mmetz@adrian-broher.net>
2017-12-10 01:08:03 +0100

    Use lambdas as `System` signal handlers
    The `boost::bind` library, while being helpful during C++03 days, uses
    an awkward syntax and probably cannot be optimized as well as a builtin
    language feature.

--- UI/MapWnd.cpp
+++ UI/MapWnd.cpp
@@ -2792,9 +2792,9 @@ void MapWnd::InitTurn() {
     // connect system fleet add and remove signals
     for (auto& system : objects.FindObjects<System>()) {
         m_system_fleet_insert_remove_signals[system->ID()].push_back(system->FleetsInsertedSignal.connect(
-            boost::bind(&MapWnd::FleetsInsertedSignalHandler, this, _1)));
+            [this](const std::vector<std::shared_ptr<Fleet>>& fleets){ FleetsInsertedSignalHandler(fleets); }));
         m_system_fleet_insert_remove_signals[system->ID()].push_back(system->FleetsRemovedSignal.connect(
-            boost::bind(&MapWnd::FleetsRemovedSignalHandler, this, _1)));
+            [this](const std::vector<std::shared_ptr<Fleet>>& fleets){ FleetsRemovedSignalHandler(fleets); }));
     }
 
     RefreshFleetSignals();
