2fee91341c789a31505c538d9ae8d99632e09b89
Cjkjvfnby <Cjkjvfnby@gmail.com>
2018-02-18 11:18:40 +0300

    Clarify comment

--- default/python/tests/conftest.py
+++ default/python/tests/conftest.py
@@ -12,7 +12,11 @@ sys.path.append(this_dir)
 sys.path.append(os.path.join(this_dir, '..', '..', 'python', 'AI/'))
 sys.path.append(os.path.join(this_dir, '..', '..', 'python'))
 
-# This import put module to memory, after that we can import code form AI module without problem,
-#  because module named freeOrionAIInterface is already in memory and imports in AI just use it.
+# Since the 'true' freeOrionAIInterface is not available during testing, this import loads the 'mock' freeOrionInterface
+# module into memory.  For testing purposes the mock interface provides values for certain constants which various
+# AI modules rely upon.  Since any later imports of the freeOrionInterface module will simply refer back the first-
+# loaded module of the same name, code from the AI modules can then be imported and tested, provided that such
+# AI code only relies upon constants from the freeOrionAIInterface (and specifically does not depend on any of the
+# various dynamic galaxy-state queries provided by the 'true' freeOrionAIInterface).
 # noinspection PyUnresolvedReferences
 import freeOrionAIInterface
