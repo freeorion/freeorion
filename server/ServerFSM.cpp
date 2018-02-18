5ffb8e0699f52c06420fce5ebdc8fa35e2a74e25
LGM-Doyle <LGM-Doyle@users.noreply.github.com>
2018-02-18 16:27:06 -0500

    housekeeping: ServerFSM change DebugLogger to ErrorLogger for exceptions.

--- server/ServerFSM.cpp
+++ server/ServerFSM.cpp
@@ -2412,8 +2412,9 @@ sc::result WaitingForSaveData::react(const ClientSaveData& msg) {
 
     try {
         ExtractClientSaveDataMessageData(message, received_orders, ui_data_available, *ui_data, save_state_string_available, save_state_string);
-    } catch (const std::exception& e) {
-        DebugLogger(FSM) << "WaitingForSaveData::react(const ClientSaveData& msg) received invalid save data from player " << player_connection->PlayerName();
+    } catch (const std::exception& error) {
+        ErrorLogger(FSM) << "WaitingForSaveData::react(const ClientSaveData& msg) received invalid save data from player "
+                         << player_connection->PlayerName() << ". Error: " << error.what();
         player_connection->SendMessage(ErrorMessage(UserStringNop("INVALID_CLIENT_SAVE_DATA_RECEIVED"), false));
 
         // TODO: use whatever portion of message data was extracted, and leave the rest as defaults.
@@ -2467,8 +2468,8 @@ sc::result WaitingForSaveData::react(const ClientSaveData& msg) {
                                      GetCombatLogManager(),             server.m_galaxy_setup_data,
                                      !server.m_single_player_game);
 
-        } catch (const std::exception&) {
-            DebugLogger(FSM) << "Catch std::exception&";
+        } catch (const std::exception& error) {
+            ErrorLogger(FSM) << "Catch std::exception: " << error.what();
             SendMessageToAllPlayers(ErrorMessage(UserStringNop("UNABLE_TO_WRITE_SAVE_FILE"), false));
         }
 
