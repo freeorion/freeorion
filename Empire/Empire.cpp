freeorion/freeorion

commit bee799f5205caa4e92dc5e96cacf69ccec70dba9

Move GameRules class into dedicated files

Empire/Empire.cpp
@@ -1,7 +1,7 @@
 #include "Empire.h"
 
 #include "../util/i18n.h"
-#include "../util/MultiplayerCommon.h"
+#include "../util/GameRules.h"
 #include "../util/ScopedTimer.h"
 #include "../util/Random.h"
 #include "../util/Logger.h"

AI/AIInterface.cpp
@@ -1,6 +1,5 @@
 #include "AIInterface.h"
 
-#include "../util/MultiplayerCommon.h"
 #include "../network/ClientNetworking.h"
 #include "../client/AI/AIClientApp.h"
 

Empire/EmpireManager.cpp
@@ -3,7 +3,6 @@
 #include "Empire.h"
 #include "../util/Directories.h"
 #include "../util/Logger.h"
-#include "../util/MultiplayerCommon.h"
 #include "../util/XMLDoc.h"
 #include "../universe/Enums.h"
 

UI/ChatWnd.cpp
@@ -19,7 +19,6 @@
 #include "../util/i18n.h"
 #include "../util/Logger.h"
 #include "../util/OptionsDB.h"
-#include "../util/MultiplayerCommon.h"
 
 #include <GG/GUI.h>
 

UI/EncyclopediaDetailPanel.cpp
@@ -29,6 +29,7 @@
 #include "../util/i18n.h"
 #include "../util/Logger.h"
 #include "../util/OptionsDB.h"
+#include "../util/GameRules.h"
 #include "../util/Directories.h"
 #include "../util/VarText.h"
 #include "../util/ScopedTimer.h"

UI/GalaxySetupWnd.cpp
@@ -8,7 +8,7 @@
 #include "../client/human/HumanClientApp.h"
 #include "../util/i18n.h"
 #include "../util/Logger.h"
-#include "../util/MultiplayerCommon.h"
+#include "../util/GameRules.h"
 #include "../util/OptionsDB.h"
 #include "../util/Directories.h"
 #include "../util/AppInterface.h"

UI/InGameMenu.cpp
@@ -8,7 +8,6 @@
 #include "../client/human/HumanClientApp.h"
 #include "../network/Networking.h"
 #include "../util/i18n.h"
-#include "../util/MultiplayerCommon.h"
 #include "../util/OptionsDB.h"
 #include "../util/Directories.h"
 #include "../util/Logger.h"

UI/MapWnd.cpp
@@ -28,7 +28,7 @@
 #include "../util/Directories.h"
 #include "../util/i18n.h"
 #include "../util/Logger.h"
-#include "../util/MultiplayerCommon.h"
+#include "../util/GameRules.h"
 #include "../util/OptionsDB.h"
 #include "../util/Order.h"
 #include "../util/Random.h"

commit 2e7ef2bce8809f844e43eeb03044eeae47cbcb2e

Add missing boost filesystem include
Fixes bug introduced with bee799f5205caa4e92dc5e96cacf69ccec70dba9.

Empire/Empire.cpp
@@ -22,6 +22,7 @@
 #include "EmpireManager.h"
 #include "Supply.h"
 
+#include <boost/filesystem/fstream.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/range/numeric.hpp>
 #include <boost/range/adaptor/map.hpp>