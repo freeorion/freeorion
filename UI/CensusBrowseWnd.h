a0ff6faebd713d42532ad53ee10e8d1534e01a6d
st.pa <stephan.pakebusch@zollsoft.de>
2018-03-10 19:47:18 +0100

    Census now also shows the numbers of populated worlds per species / per tag (and typo correction in a comment)

--- UI/CensusBrowseWnd.h
+++ UI/CensusBrowseWnd.h
@@ -8,8 +8,13 @@
 /** A popup tooltop for display when mousing over in-game icons.  A title and some detail text.*/
 class CensusBrowseWnd : public GG::BrowseInfoWnd {
 public:
-    CensusBrowseWnd(const std::string& title_text, const std::map<std::string, float>& population_counts,
-                    const std::map<std::string, float>& tag_counts, const std::vector<std::string>& census_order);
+    CensusBrowseWnd(const std::string& title_text,
+					const std::map<std::string, float>& population_counts,
+					const std::map<std::string, int>&   population_worlds,
+                    const std::map<std::string, float>& tag_counts,
+					const std::map<std::string, int>&   tag_worlds,
+					const std::vector<std::string>&     census_order
+	);
 
     void CompleteConstruction() override;
     bool WndHasBrowseInfo(const Wnd* wnd, std::size_t mode) const override;
@@ -27,8 +32,10 @@ private:
     std::shared_ptr<GG::Label>      m_tags_text;
     std::shared_ptr<GG::ListBox>    m_tags_list;
     GG::Pt          m_offset;
-    std::map<std::string, float>    m_population_counts;
+	std::map<std::string, float>    m_population_counts;
+	std::map<std::string, int>      m_population_worlds;
     std::map<std::string, float>    m_tag_counts;
+	std::map<std::string, int>      m_tag_worlds;
     std::vector<std::string>        m_census_order;
 
     void InitRowSizes();
