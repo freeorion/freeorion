e1a7ad469b57e2b01e03c2b697bb5d5591fee749
O01eg <o01eg@yandex.ru>
2018-03-11 23:30:14 +0300

    Fail if server is already running.

--- test/game/TestGame.cpp
+++ test/game/TestGame.cpp
@@ -1,6 +1,7 @@
 #include <boost/test/unit_test.hpp>
 
 #include "client/ClientApp.h"
+#include "network/ClientNetworking.h"
 
 struct GameFixture : public ClientApp {
     int EffectsProcessingThreads() const
@@ -10,6 +11,8 @@ struct GameFixture : public ClientApp {
 BOOST_FIXTURE_TEST_SUITE(TestGame, GameFixture)
 
 BOOST_AUTO_TEST_CASE(host_server) {
+    if (m_networking->PingLocalHostServer(std::chrono::milliseconds(100)))
+        BOOST_FAIL("Local server already running");
 
 }
 
