df07ad187bf1d907896e755bf2e71f3876ce0e8f
O01eg <o01eg@yandex.ru>
2018-03-11 23:20:52 +0300

    Start to implement ClientApp.

--- test/game/TestGame.cpp
+++ test/game/TestGame.cpp
@@ -1,6 +1,13 @@
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_SUITE(TestGame)
+#include "client/ClientApp.h"
+
+struct GameFixture : public ClientApp {
+    int EffectsProcessingThreads() const
+    { return GetOptionsDB().Get<int>("effects.ai.threads"); }
+};
+
+BOOST_FIXTURE_TEST_SUITE(TestGame, GameFixture)
 
 BOOST_AUTO_TEST_CASE(host_server) {
 
