freeorion/freeorion

commit 7d75b362a4e53be98b99fc1d473ca086de71674b

Use initializer lists to create std::pair instances where approciate

parse/BuildingsParser.cpp
@@ -37,7 +37,7 @@ namespace {
         auto building_type = boost::make_unique<BuildingType>(
             name, description, *common_params.OpenEnvelope(pass), capture_result, icon);
 
-        building_types.insert(std::make_pair(building_type->Name(), std::move(building_type)));
+        building_types.insert({building_type->Name(), std::move(building_type)});
     }
 
     BOOST_PHOENIX_ADAPT_FUNCTION(void, insert_building_, insert_building, 7)

Empire/Supply.cpp
@@ -511,18 +511,18 @@ void SupplyManager::Update() {
                 // and set any traversals going to this system as obstructed
                 for (const auto& lane : lane_traversals_initial) {
                     if (lane.first == sys_id) {
-                        lane_traversals.erase(std::make_pair(sys_id, lane.second));
+                        lane_traversals.erase({sys_id, lane.second});
                     }
                     if (lane.second == sys_id) {
-                        lane_traversals.erase(std::make_pair(lane.first, sys_id));
-                        obstructed_traversals.insert(std::make_pair(lane.first, sys_id));
+                        lane_traversals.erase({lane.first, sys_id});
+                        obstructed_traversals.insert({lane.first, sys_id});
                     }
                 }
 
                 // remove obstructed traverals departing from this system
                 for (const auto& lane : obstrcuted_traversals_initial) {
                     if (lane.first == sys_id)
-                        obstructed_traversals.erase(std::make_pair(lane.first, lane.second));
+                        obstructed_traversals.erase({lane.first, lane.second});
                 }
             }
 
@@ -597,7 +597,7 @@ void SupplyManager::Update() {
 
                     // if so, add a blocked traversal and continue
                     if (range_after_one_more_jump <= other_empire_biggest_range) {
-                        m_supply_starlane_obstructed_traversals[empire_id].insert(std::make_pair(system_id, lane_end_sys_id));
+                        m_supply_starlane_obstructed_traversals[empire_id].insert({system_id, lane_end_sys_id});
                         //DebugLogger() << "Added obstructed traversal from " << system_id << " to " << lane_end_sys_id << " due to other empire biggest range being " << other_empire_biggest_range;
                         continue;
                     }
@@ -635,15 +635,15 @@ void SupplyManager::Update() {
                     //DebugLogger() << "Added traversal from " << system_id << " to " << lane_end_sys_id;
 
                     // erase any previous obstructed traversal that just succeeded
-                    if (m_supply_starlane_obstructed_traversals[empire_id].count(std::make_pair(system_id, lane_end_sys_id)))
+                    if (m_supply_starlane_obstructed_traversals[empire_id].count({system_id, lane_end_sys_id}))
                     {
                         //DebugLogger() << "Removed obstructed traversal from " << system_id << " to " << lane_end_sys_id;
-                        m_supply_starlane_obstructed_traversals[empire_id].erase(std::make_pair(system_id, lane_end_sys_id));
+                        m_supply_starlane_obstructed_traversals[empire_id].erase({system_id, lane_end_sys_id});
                     }
-                    if (m_supply_starlane_obstructed_traversals[empire_id].count(std::make_pair(lane_end_sys_id, system_id)))
+                    if (m_supply_starlane_obstructed_traversals[empire_id].count({lane_end_sys_id, system_id}))
                     {
                         //DebugLogger() << "Removed obstructed traversal from " << lane_end_sys_id << " to " << system_id;
-                        m_supply_starlane_obstructed_traversals[empire_id].erase(std::make_pair(lane_end_sys_id, system_id));
+                        m_supply_starlane_obstructed_traversals[empire_id].erase({lane_end_sys_id, system_id});
                     }
                 }
             }

UI/DesignWnd.cpp
@@ -552,8 +552,7 @@ namespace {
 
             const auto save_path = CreateSaveFileNameForDesign(design);
 
-            m_saved_designs.insert(
-                std::make_pair(design.UUID(), std::make_pair(std::move(design_copy), save_path)));
+            m_saved_designs.insert({design.UUID(), std::make_pair(std::move(design_copy), save_path)});
             SaveDesign(design.UUID());
         }
 
@@ -877,7 +876,7 @@ namespace {
         design_ids_and_obsoletes.clear();
         for (const auto id : m_ordered_design_ids) {
             try {
-                design_ids_and_obsoletes.push_back(std::make_pair(id, m_id_to_obsolete_and_loc.at(id).first));
+                design_ids_and_obsoletes.push_back({id, m_id_to_obsolete_and_loc.at(id).first});
             } catch (const std::out_of_range&) {
                 ErrorLogger() << "CurrentShipDesignManager::Save missing id = " << id;
                 continue;
@@ -887,7 +886,7 @@ namespace {
         hulls_and_obsoletes.clear();
         for (const auto name : m_ordered_hulls) {
             try {
-               hulls_and_obsoletes.push_back(std::make_pair(name, m_hull_to_obsolete_and_loc.at(name).first));
+               hulls_and_obsoletes.push_back({name, m_hull_to_obsolete_and_loc.at(name).first});
             } catch (const std::out_of_range&) {
                 ErrorLogger() << "CurrentShipDesignManager::Save missing hull = " << name;
                 continue;

UI/OptionsWnd.cpp
@@ -481,7 +481,7 @@ void OptionsWnd::CompleteConstruction() {
     MusicVolumeOption(current_page, 0, m_sound_feedback);
     VolumeOption(current_page, 0, "audio.effects.enabled", "audio.effects.volume", UserString("OPTIONS_UI_SOUNDS"), UI_sound_enabled, m_sound_feedback);
     FileOption(current_page, 0, "audio.music.path", UserString("OPTIONS_BACKGROUND_MUSIC"), ClientUI::SoundDir(),
-               std::make_pair(UserString("OPTIONS_MUSIC_FILE"), "*" + MUSIC_FILE_SUFFIX),
+               {UserString("OPTIONS_MUSIC_FILE"), "*" + MUSIC_FILE_SUFFIX},
                ValidMusicFile);
 
     CreateSectionHeader(current_page, 0, UserString("OPTIONS_SOUNDS"));
@@ -534,8 +534,7 @@ void OptionsWnd::CompleteConstruction() {
 
     FileOption(current_page, 0, "resource.stringtable.path",    UserString("OPTIONS_LANGUAGE"),
                GetRootDataDir() / "default" / "stringtables",
-               std::make_pair(UserString("OPTIONS_LANGUAGE_FILE"),
-               "*" + STRINGTABLE_FILE_SUFFIX),
+               {UserString("OPTIONS_LANGUAGE_FILE"), "*" + STRINGTABLE_FILE_SUFFIX},
                &ValidStringtableFile);
 
     // flush stringtable button
@@ -1091,8 +1090,8 @@ void OptionsWnd::FileOption(GG::ListBox* page, int indentation_level, const std:
 { FileOptionImpl(page, indentation_level, option_name, text, path, filters, string_validator, false, false, false); }
 
 void OptionsWnd::SoundFileOption(GG::ListBox* page, int indentation_level, const std::string& option_name, const std::string& text) {
-    FileOption(page, indentation_level, option_name, text, ClientUI::SoundDir(), std::make_pair(UserString("OPTIONS_SOUND_FILE"),
-               "*" + SOUND_FILE_SUFFIX), ValidSoundFile);
+    FileOption(page, indentation_level, option_name, text, ClientUI::SoundDir(),
+               {UserString("OPTIONS_SOUND_FILE"), "*" + SOUND_FILE_SUFFIX}, ValidSoundFile);
 }
 
 void OptionsWnd::DirectoryOption(GG::ListBox* page, int indentation_level, const std::string& option_name,
@@ -1127,7 +1126,7 @@ void OptionsWnd::ColorOption(GG::ListBox* page, int indentation_level, const std
 
 void OptionsWnd::FontOption(GG::ListBox* page, int indentation_level, const std::string& option_name, const std::string& text) {
     FileOption(page, indentation_level, option_name, text, GetRootDataDir() / "default",
-               std::make_pair<std::string, std::string>(std::string(option_name), "*" + FONT_FILE_SUFFIX),
+               {std::string(option_name), "*" + FONT_FILE_SUFFIX},
                &ValidFontFile);
 }
 

UI/PopulationPanel.cpp
@@ -52,18 +52,21 @@ void PopulationPanel::CompleteConstruction() {
     }
 
     // small meter indicators - for use when panel is collapsed
-    m_meter_stats.push_back(
-        std::make_pair(METER_POPULATION, GG::Wnd::Create<StatisticIcon>(ClientUI::SpeciesIcon(pop->SpeciesName()),
-                                                                        obj->InitialMeterValue(METER_POPULATION), 3, false,
-                                                                        MeterIconSize().x, MeterIconSize().y)));
-    m_meter_stats.push_back(
-        std::make_pair(METER_HAPPINESS, GG::Wnd::Create<StatisticIcon>(ClientUI::MeterIcon(METER_HAPPINESS),
-                                                                       obj->InitialMeterValue(METER_HAPPINESS), 3, false,
-                                                                       MeterIconSize().x, MeterIconSize().y)));
-    m_meter_stats.push_back(
-        std::make_pair(METER_CONSTRUCTION, GG::Wnd::Create<StatisticIcon>(ClientUI::MeterIcon(METER_CONSTRUCTION),
-                                                                          obj->InitialMeterValue(METER_CONSTRUCTION), 3, false,
-                                                                          MeterIconSize().x, MeterIconSize().y)));
+    m_meter_stats.push_back({
+        METER_POPULATION,
+        GG::Wnd::Create<StatisticIcon>(ClientUI::SpeciesIcon(pop->SpeciesName()),
+                                       obj->InitialMeterValue(METER_POPULATION), 3, false,
+                                       MeterIconSize().x, MeterIconSize().y)});
+    m_meter_stats.push_back({
+        METER_HAPPINESS,
+        GG::Wnd::Create<StatisticIcon>(ClientUI::MeterIcon(METER_HAPPINESS),
+                                       obj->InitialMeterValue(METER_HAPPINESS), 3, false,
+                                       MeterIconSize().x, MeterIconSize().y)});
+    m_meter_stats.push_back({
+        METER_CONSTRUCTION,
+        GG::Wnd::Create<StatisticIcon>(ClientUI::MeterIcon(METER_CONSTRUCTION),
+                                       obj->InitialMeterValue(METER_CONSTRUCTION), 3, false,
+                                       MeterIconSize().x, MeterIconSize().y)});
 
     // meter and production indicators
     std::vector<std::pair<MeterType, MeterType>> meters;
@@ -71,7 +74,7 @@ void PopulationPanel::CompleteConstruction() {
     for (auto& meter_stat : m_meter_stats) {
         meter_stat.second->InstallEventFilter(shared_from_this());
         AttachChild(meter_stat.second);
-        meters.push_back(std::make_pair(meter_stat.first, AssociatedMeterType(meter_stat.first)));
+        meters.push_back({meter_stat.first, AssociatedMeterType(meter_stat.first)});
     }
 
     // attach and show meter bars and large resource indicators

UI/SidePanel.cpp
@@ -2504,7 +2504,7 @@ void SidePanel::PlanetPanelContainer::SetPlanets(const std::vector<int>& planet_
             ErrorLogger() << "PlanetPanelContainer::SetPlanets couldn't find system of planet" << planet->Name();
             continue;
         }
-        orbits_planets.insert(std::make_pair(system->OrbitOfPlanet(planet_id), planet_id));
+        orbits_planets.insert({system->OrbitOfPlanet(planet_id), planet_id});
     }
 
     // create new panels and connect their signals
@@ -3143,7 +3143,7 @@ void SidePanel::RefreshSystemNames() {
     for (auto& system : Objects().FindObjects<System>()) {
         // Skip rows for systems that aren't known to this client, except the selected system
         if (!system->Name().empty() || system->ID() == s_system_id)
-            sorted_systems.insert(std::make_pair(system->Name(), system->ID()));
+            sorted_systems.insert({system->Name(), system->ID()});
     }
 
     auto system_name_font(ClientUI::GetBoldFont(SystemNameFontSize()));

UI/Sound.cpp
@@ -563,7 +563,7 @@ void Sound::Impl::PlaySound(const boost::filesystem::path& path, bool is_ui_soun
 
                         current_buffer = sound_handle;
                         found_buffer = true;
-                        m_buffers.insert(std::make_pair(filename, sound_handle));
+                        m_buffers.insert({filename, sound_handle});
                     }
                     else
                     {

UI/SystemResourceSummaryBrowseWnd.cpp
@@ -215,7 +215,7 @@ void SystemResourceSummaryBrowseWnd::UpdateProduction(GG::Y& top) {
         value->Resize(GG::Pt(ValueWidth(), row_height));
         AttachChild(value);
 
-        m_production_labels_and_amounts.push_back(std::make_pair(label, value));
+        m_production_labels_and_amounts.push_back({label, value});
 
         top += row_height;
     }
@@ -233,7 +233,7 @@ void SystemResourceSummaryBrowseWnd::UpdateProduction(GG::Y& top) {
         value->Resize(GG::Pt(ValueWidth(), row_height));
         AttachChild(value);
 
-        m_production_labels_and_amounts.push_back(std::make_pair(label, value));
+        m_production_labels_and_amounts.push_back({label, value});
 
         top += row_height;
     }
@@ -315,7 +315,7 @@ void SystemResourceSummaryBrowseWnd::UpdateAllocation(GG::Y& top) {
         value->Resize(GG::Pt(ValueWidth(), row_height));
         AttachChild(value);
 
-        m_allocation_labels_and_amounts.push_back(std::make_pair(label, value));
+        m_allocation_labels_and_amounts.push_back({label, value});
 
         top += row_height;
     }
@@ -333,7 +333,7 @@ void SystemResourceSummaryBrowseWnd::UpdateAllocation(GG::Y& top) {
         value->Resize(GG::Pt(ValueWidth(), row_height));
         AttachChild(value);
 
-        m_allocation_labels_and_amounts.push_back(std::make_pair(label, value));
+        m_allocation_labels_and_amounts.push_back({label, value});
 
         top += row_height;
     }
@@ -441,7 +441,7 @@ void SystemResourceSummaryBrowseWnd::UpdateImportExport(GG::Y& top) {
     value->Resize(GG::Pt(ValueWidth(), row_height));
     AttachChild(value);
 
-    m_import_export_labels_and_amounts.push_back(std::make_pair(label, value));
+    m_import_export_labels_and_amounts.push_back({label, value});
 
     top += row_height;
 }

commit 07d97eb7def2fe05c7e6fb916c0426431a56ad2c

Partially revert bug introduced with commit 7d75b362a4e53be98b99fc1d473ca086de71674b

parse/BuildingsParser.cpp
@@ -37,7 +37,7 @@ namespace {
         auto building_type = boost::make_unique<BuildingType>(
             name, description, *common_params.OpenEnvelope(pass), capture_result, icon);
 
-        building_types.insert({building_type->Name(), std::move(building_type)});
+        building_types.insert(std::make_pair(building_type->Name(), std::move(building_type)));
     }
 
     BOOST_PHOENIX_ADAPT_FUNCTION(void, insert_building_, insert_building, 7)