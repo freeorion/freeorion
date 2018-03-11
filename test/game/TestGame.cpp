3805ff6395934c35de535ac9ff65a476c4f07dd0
O01eg <o01eg@yandex.ru>
2018-03-11 23:40:09 +0300

    Get server's binary path.

--- test/game/TestGame.cpp
+++ test/game/TestGame.cpp
@@ -2,6 +2,21 @@
 
 #include "client/ClientApp.h"
 #include "network/ClientNetworking.h"
+#include "util/Directories.h"
+
+namespace {
+    std::string ServerClientExe() {
+#ifdef FREEORION_WIN32
+        return PathToString(GetBinDir() / "freeoriond.exe");
+#else
+        return (GetBinDir() / "freeoriond").string();
+#endif
+    }
+}
+
+#ifdef FREEORION_MACOSX
+#include <stdlib.h>
+#endif
 
 struct GameFixture : public ClientApp {
     int EffectsProcessingThreads() const
@@ -13,6 +28,17 @@ BOOST_FIXTURE_TEST_SUITE(TestGame, GameFixture)
 BOOST_AUTO_TEST_CASE(host_server) {
     if (m_networking->PingLocalHostServer(std::chrono::milliseconds(100)))
         BOOST_FAIL("Local server already running");
+
+    std::string SERVER_CLIENT_EXE = ServerClientExe();
+
+#ifdef FREEORION_MACOSX
+    // On OSX set environment variable DYLD_LIBRARY_PATH to python framework folder
+    // bundled with app, so the dynamic linker uses the bundled python library.
+    // Otherwise the dynamic linker will look for a correct python lib in system
+    // paths, and if it can't find it, throw an error and terminate!
+    // Setting environment variable here, spawned child processes will inherit it.
+    setenv("DYLD_LIBRARY_PATH", GetPythonHome().string().c_str(), 1);
+#endif
 
 }
 
