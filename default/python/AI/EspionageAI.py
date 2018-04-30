c4e9d75a9f093b6e0fbf480863c2f56f09059a2a
Dilvish <dilvish.fo@gmail.com>
2018-04-29 23:48:03 -0700

    Warn on AI failure to retrieve empire

--- default/python/AI/EspionageAI.py
+++ default/python/AI/EspionageAI.py
@@ -1,4 +1,4 @@
-from logging import error
+from logging import error, warn
 
 import freeOrionAIInterface as fo  # pylint: disable=import-error
 import AIDependencies
@@ -30,6 +30,7 @@ def get_empire_detection(empire_id):
     if empire:
         return empire.getMeter(EmpireMeters.DETECTION_STRENGTH).initial
     else:
+        warn("AI failed to retrieve empire ID %d, in game with %d empires." % (empire_id, len(fo.allEmpireIDs())))
         return default_empire_detection_strength()
 
 
