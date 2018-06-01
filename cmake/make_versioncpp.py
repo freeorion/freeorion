3beafbe8610a114ea26c4c42bd1886e9be5f0010
Vezzra <vezzra@gmx.net>
2018-06-01 15:49:15 +0200

    Stop regeneration of Version.cpp in cases where it shouldn't
    When the commit SHA could not be determined by the build scripts,
    Version.cpp got regenerated whether an appropriate Version.cpp was
    present or not.
    
    This patch fixes that: now, when the commit SHA can't be
    determined, but a Version.cpp is present, the version number is
    checked against the one in the existing Version.cpp (instead of the
    build number, which is the abbreviated commit SHA), and if the
    version numbers match, Version.cpp is not regenerated.

--- cmake/make_versioncpp.py
+++ cmake/make_versioncpp.py
@@ -46,9 +46,15 @@ class Generator(object):
         if build_no == INVALID_BUILD_NO:
             print "WARNING: Can't determine git commit!"
 
-        if os.path.isfile(self.outfile) and not build_no == INVALID_BUILD_NO:
+        if os.path.isfile(self.outfile):
             with open(self.outfile) as check_file:
-                if build_no in check_file.read():
+                check_file_contents = check_file.read()
+                if build_no == INVALID_BUILD_NO:
+                    if version in check_file_contents:
+                        print "Version matches version in existing Version.cpp, skip regenerating it"
+                        return
+                elif build_no in check_file_contents:
+                    print "Build number matches build number in existing Version.cpp, skip regenerating it"
                     return
 
         try:
