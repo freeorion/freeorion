863b7a016afae852c8566873de9090c7529f4323
geoffthemedio <geoffthemedio@gmail.com>
2018-06-27 19:41:21 +0200

    Don't call .back() on empty vectors.

--- universe/ValueRef.cpp
+++ universe/ValueRef.cpp
@@ -576,7 +576,7 @@ if (m_ref_type == EFFECT_TARGET_VALUE_REFERENCE) {                     \
 template <>
 PlanetSize Variable<PlanetSize>::Eval(const ScriptingContext& context) const
 {
-    const std::string& property_name = m_property_name.back();
+    const std::string& property_name = m_property_name.empty() ? "" : m_property_name.back();
 
     IF_CURRENT_VALUE(PlanetSize)
 
@@ -609,7 +609,7 @@ PlanetSize Variable<PlanetSize>::Eval(const ScriptingContext& context) const
 template <>
 PlanetType Variable<PlanetType>::Eval(const ScriptingContext& context) const
 {
-    const std::string& property_name = m_property_name.back();
+    const std::string& property_name = m_property_name.empty() ? "" : m_property_name.back();
 
     IF_CURRENT_VALUE(PlanetType)
 
@@ -648,7 +648,7 @@ PlanetType Variable<PlanetType>::Eval(const ScriptingContext& context) const
 template <>
 PlanetEnvironment Variable<PlanetEnvironment>::Eval(const ScriptingContext& context) const
 {
-    const std::string& property_name = m_property_name.back();
+    const std::string& property_name = m_property_name.empty() ? "" : m_property_name.back();
 
     IF_CURRENT_VALUE(PlanetEnvironment)
 
@@ -675,7 +675,7 @@ PlanetEnvironment Variable<PlanetEnvironment>::Eval(const ScriptingContext& cont
 template <>
 UniverseObjectType Variable<UniverseObjectType>::Eval(const ScriptingContext& context) const
 {
-    const std::string& property_name = m_property_name.back();
+    const std::string& property_name = m_property_name.empty() ? "" : m_property_name.back();
 
     IF_CURRENT_VALUE(UniverseObjectType)
 
@@ -707,7 +707,7 @@ UniverseObjectType Variable<UniverseObjectType>::Eval(const ScriptingContext& co
 template <>
 StarType Variable<StarType>::Eval(const ScriptingContext& context) const
 {
-    const std::string& property_name = m_property_name.back();
+    const std::string& property_name = m_property_name.empty() ? "" : m_property_name.back();
 
     IF_CURRENT_VALUE(StarType)
 
@@ -740,8 +740,6 @@ StarType Variable<StarType>::Eval(const ScriptingContext& context) const
 template <>
 Visibility Variable<Visibility>::Eval(const ScriptingContext& context) const
 {
-    // const std::string& property_name = m_property_name.back();
-
     IF_CURRENT_VALUE(Visibility)
 
     // As of this writing, there are no properties of objects that directly
@@ -756,7 +754,7 @@ Visibility Variable<Visibility>::Eval(const ScriptingContext& context) const
 template <>
 double Variable<double>::Eval(const ScriptingContext& context) const
 {
-    const std::string& property_name = m_property_name.back();
+    const std::string& property_name = m_property_name.empty() ? "" : m_property_name.back();
 
     IF_CURRENT_VALUE(float)
 
@@ -860,7 +858,7 @@ double Variable<double>::Eval(const ScriptingContext& context) const
 template <>
 int Variable<int>::Eval(const ScriptingContext& context) const
 {
-    const std::string& property_name = m_property_name.back();
+    const std::string& property_name = m_property_name.empty() ? "" : m_property_name.back();
 
     IF_CURRENT_VALUE(int)
 
@@ -1097,10 +1095,9 @@ template <>
 std::vector<std::string> Variable<std::vector<std::string>>::Eval(
     const ScriptingContext& context) const
 {
-    const std::string& property_name = m_property_name.back();
+    const std::string& property_name = m_property_name.empty() ? "" : m_property_name.back();
 
     IF_CURRENT_VALUE(std::vector<std::string>)
-
 
     if (m_ref_type == NON_OBJECT_REFERENCE) {
         // add more non-object reference int functions here
@@ -1162,7 +1159,7 @@ std::vector<std::string> Variable<std::vector<std::string>>::Eval(
 template <>
 std::string Variable<std::string>::Eval(const ScriptingContext& context) const
 {
-    const std::string& property_name = m_property_name.back();
+    const std::string& property_name = m_property_name.empty() ? "" : m_property_name.back();
 
     IF_CURRENT_VALUE(std::string)
 
