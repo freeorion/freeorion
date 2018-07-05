0a258c9bb393928d2bd4a250331e86d0c42a7183
geoffthemedio <geoffthemedio@gmail.com>
2018-07-05 22:36:29 +0200

    Use generic_wstring() in PathToString on Windows to genericize path separators for cross-platform compatibility with different OS servers.

--- util/Directories.cpp
+++ util/Directories.cpp
@@ -502,7 +502,7 @@ fs::path RelativePath(const fs::path& from, const fs::path& to) {
 #if defined(FREEORION_WIN32)
 
 std::string PathToString(const fs::path& path) {
-    fs::path::string_type native_string = path.native();
+    fs::path::string_type native_string = path.generic_wstring();// path.native();
     std::string retval;
     utf8::utf16to8(native_string.begin(), native_string.end(), std::back_inserter(retval));
     return retval;
