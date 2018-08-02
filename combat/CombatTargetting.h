13e2976ad4e4e46367f2455ba19f28a456d5d968
Axel Gross <axel.gross@iaeste.at>
2018-08-02 22:46:53 +0200

    Add some design info

--- combat/CombatTargetting.h
+++ combat/CombatTargetting.h
@@ -3,9 +3,29 @@
 
 #include "../universe/UniverseObject.h"
 
-// possible target, prey
-// hit a mark
-// wanted, sight, prey, target, preferredX, sought, aim, triggerCondition
+//  Big Picture:
+//    Hunters have a precision and preferred types of prey. This means that
+//    hunters with a higher precisions are more likely to hit the preferred
+//    prey in combat.
+//
+//  Design Choices:
+//    Combat Interpretation - Precision means number of rolls to find a preferred target
+//       Precision 1 or NoPreference means the old behavior (doesnt change chances to hit certain prey)
+//
+//    Content Setup - in CombatTargetting.cpp
+//       Flak (SR_WEAPON_0_1) - Precision 3 hunting space boats
+//       Bombers (FT_HANGAR_3) - Precision 2 hunting space ship
+//       Interceptors (FT_HANGAR_1) - Precision 2 hunting space boats
+//       Everything else - no preferences
+//    Attacking planets should be reconsidered (E.g. let ships prefer ships with precision 2); also glass cannon
+//       
+//    Content Setup Language
+//       Hardcoded TriggerConditions and PreyTypes in CombatTargetting.cpp
+//       Alternatives
+//         - specify TriggerCondition as FOCS condition on weapon system / hangar   
+//         - specify TriggerCondition and PreyType as ~tags in FOCS on weapons/hangar and resp. ShipDesigns
+//    The content may actually not be hardcoded, so this needs to change before merge.
+
 namespace Targetting {
     enum PreyType { NoPreference = 7, AllPrey = 7, PlanetPrey = 1, ShipPrey = 2, BoatPrey = 4 };
     enum class HunterType { Unknown, Planet, Boat, Ship }; 
