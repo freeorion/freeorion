62edc03cb63064f2116a7d2cb40cbde389581331
Axel Gross <axel.gross@iaeste.at>
2018-10-26 00:13:14 +0200

    Add optional combatTargets condition for ship parts

--- parse/Tokens.h
+++ parse/Tokens.h
@@ -57,6 +57,7 @@
     (ClockwiseNextPlanetType)                   \
     (Colony)                                    \
     (Colour)                                    \
+    (CombatTargets)                             \
     (Condition)                                 \
     (Construction)                              \
     (Consumption)                               \
