c831caa10bf38fc1d403c08d58ef0703edc96a73
Ingo Schwarze <schwarze@openbsd.org>
2018-11-20 16:44:52 +0100

    pre-warp era: make Remote Colonization researchable
    Re-enable researching of the CON_REMOTE_COL technology.
    Let it depend on GRO_PLANET_ECOL
    to encourage research together with organic hulls.
    Raise the research cost to the minimum of 8 RP;
    no other technology is cheaper than that.
    Update the syntax of its FOCS file to current standards.
    
    Let GRO_LIFECYCLE_MAN depend on it because that unlocks
    the subsequent upgrade to the colony pod.

--- default/python/AI/TechsListsAI.py
+++ default/python/AI/TechsListsAI.py
@@ -108,6 +108,7 @@ class TechGroup1(TechGroup):
         self.economy.extend([
             "LRN_PHYS_BRAIN",
             "GRO_PLANET_ECOL",
+            "CON_REMOTE_COL",
             "LRN_ALGO_ELEGANCE",
             "GRO_SUBTER_HAB",
             "LRN_ARTIF_MINDS",
@@ -126,8 +127,9 @@ class TechGroup1(TechGroup):
         self.hull.extend([
             "SHP_MIL_ROBO_CONT",
         ])
-        # always start with the same first 7 techs; leaves 2 econ, 3 weap, 1 hull
+        # always start with the same first 8 techs; leaves 2 econ, 3 weap, 1 hull
         self.enqueue(
+            self.economy,
             self.economy,
             self.economy,
             self.economy,
