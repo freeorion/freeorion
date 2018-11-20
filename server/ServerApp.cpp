6123dd5a2eaebcfbfe3723e1a85554291ec116cc
Ingo Schwarze <schwarze@openbsd.org>
2018-11-20 15:40:17 +0100

    preparation for tutorial: starting_unlocks for STARTING_ERA_PREWARP
    Provide separate files items-prewarp.inf and buildings-prewarp.inf
    to be used with STARTING_ERA_PREWARP.  For now, they are copies of
    items.inf and buildings.inf, but they will diverge when the era gets
    fleshed out.
    
    Parse them into the Universe object just like the corresponding
    files for the Classical era.

--- server/ServerApp.cpp
+++ server/ServerApp.cpp
@@ -165,8 +165,12 @@ void ServerApp::StartBackgroundParsing() {
     const auto& rdir = GetResourceDir();
     m_universe.SetInitiallyUnlockedItems(
         Pending::StartParsing(parse::items, rdir / "scripting/starting_unlocks/items.inf"));
+    m_universe.SetInitiallyUnlockedItemsPrewarp(
+        Pending::StartParsing(parse::items, rdir / "scripting/starting_unlocks/items-prewarp.inf"));
     m_universe.SetInitiallyUnlockedBuildings(
         Pending::StartParsing(parse::starting_buildings, rdir / "scripting/starting_unlocks/buildings.inf"));
+    m_universe.SetInitiallyUnlockedBuildingsPrewarp(
+        Pending::StartParsing(parse::starting_buildings, rdir / "scripting/starting_unlocks/buildings-prewarp.inf"));
     m_universe.SetInitiallyUnlockedFleetPlans(
         Pending::StartParsing(parse::fleet_plans, rdir / "scripting/starting_unlocks/fleets.inf"));
     m_universe.SetMonsterFleetPlans(
