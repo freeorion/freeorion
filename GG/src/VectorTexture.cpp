7081d204e3b899cfb0895347fd81f28ebdb39daf
geoffthemedio <geoffthemedio@gmail.com>
2019-02-17 02:00:39 +0100

    Rack code license text.

--- GG/src/VectorTexture.cpp
+++ GG/src/VectorTexture.cpp
@@ -27,10 +27,25 @@ namespace {
         }
         return s_vg;
     }
+}
 
-
-    //// Begin code adapted from https://github.com/VCVRack/Rack/blob/v0.6/src/widgets/SVGWidget.cpp
-
+namespace {
+    // Begin code adapted from https://github.com/VCVRack/Rack/blob/v0.6/src/widgets/SVGWidget.cpp
+    //
+    // "licensed under BSD-3-Clause by Andrew Belt."
+    //
+    // Copyright 2016 Andrew Belt
+    //
+    // Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+    //
+    // 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    //
+    // 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    //
+    // 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+    //
+    // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    //
     struct Vec {
         float x = 0.f;
         float y = 0.f;
@@ -101,6 +116,7 @@ namespace {
             //          << "  fillrule: " << shape->fillRule
             //          << "  from (" << shape->bounds[0] << ", " << shape->bounds[1]
             //          << ") to (" << shape->bounds[2] << ", " << shape->bounds[3] << ")" << std::endl;
+
             // Visibility
             if (!(shape->flags & NSVG_FLAGS_VISIBLE))
                 continue;
@@ -211,7 +227,6 @@ namespace {
             nvgRestore(vg);
         }
     }
-
     //// End code from https://github.com/VCVRack/Rack/blob/v0.6/src/widgets/SVGWidget.cpp
 }
 
