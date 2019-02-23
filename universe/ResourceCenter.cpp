12eadd0fd0b7b1ab7786fe88aea7bf0419821deb
dimhotepus <dimhotepus@users.noreply.github.com>
2019-02-23 03:43:16 +0300

    Make setting focus faster
    Just need to check focus presense in available ones. std::count was
    counting all occurrences instead of stopping on the first one.

--- universe/ResourceCenter.cpp
+++ universe/ResourceCenter.cpp
@@ -101,7 +101,7 @@ void ResourceCenter::SetFocus(const std::string& focus) {
         return;
     }
     auto avail_foci = AvailableFoci();
-    if (std::count(avail_foci.begin(), avail_foci.end(), focus)) {
+    if (std::find(avail_foci.begin(), avail_foci.end(), focus) != avail_foci.end()) {
         m_focus = focus;
         if (m_focus == m_focus_turn_initial)
             m_last_turn_focus_changed = m_last_turn_focus_changed_turn_initial;
