1d1cfaef7d4c505411c841b239bd0a6ef9fc00e7
Axel Gross <axel.gross@iaeste.at>
2019-03-10 22:11:08 +0100

    Optimize application of OrderedAlternativesOf for NON_MATCHES domain

--- universe/Condition.cpp
+++ universe/Condition.cpp
@@ -10031,41 +10031,37 @@ void OrderedAlternativesOf::Eval(const ScriptingContext& parent_context,
         // check all operand conditions on all objects in the non matches set
         // stop if the operand condition has at least one match
         // Return non-matches if there are any
-        ObjectSet temp_non_matches;
-        temp_non_matches.reserve(non_matches.size());
+        ObjectSet temp_matches;
+        temp_matches.reserve(non_matches.size());
         // try matching operand conditions until one matches
         bool found_sth = false;
         for (auto& operand : m_operands) {
-            // move those matching the current non-negated operand from non_matches to temp_non_matches
-            operand->Eval(local_context, non_matches, temp_non_matches, MATCHES);
-            if (!non_matches.empty()) {
-                // i.e. the current operand should be used negated to move
+            operand->Eval(local_context, temp_matches, non_matches, NON_MATCHES);
+            if (!temp_matches.empty()) {
+                // have selected the current operand condition to use, so apply it to the NON_MATCHES candidate set.
                 found_sth = true;
-            }
-            // recreate previous state by moving back temp_non matches to non_matches
-            FCMoveContent(temp_non_matches, non_matches);
-
-            if (!found_sth) {
-                 // Also have to look at the other local candidates if there was a match
-                ObjectSet temp_matches;
-                temp_matches.reserve(matches.size());
-                operand->Eval(local_context, temp_matches, matches, NON_MATCHES);
-                if (!temp_matches.empty()) {
-                    // i.e. the current operand should be used negated to move
-                    found_sth = true;
-                }
-                // recreate previous state
+                // in this case we alread did the application, so we use the results
                 FCMoveContent(temp_matches, matches);
-            }
-
-            if (found_sth) {
-                // descent into subcondition for NON_MATCHES
-                operand->Eval(local_context, matches, non_matches, NON_MATCHES);
                 break;
             }
+            // Also have to look at the other local candidates if there was a match
+            ObjectSet temp_non_matches;
+            temp_non_matches.reserve(matches.size());
+            operand->Eval(local_context, matches, temp_non_matches, MATCHES);
+            if (!matches.empty()) {
+                // have selected the current operand condition to use, so apply it to the NON_MATCHES candidate set.
+                found_sth = true;
+                // We already did the application before, but there were no matches. Move all objects to non_matches.
+                FCMoveContent(matches, non_matches);
+                FCMoveContent(temp_non_matches, non_matches);
+                break;
+            }
+            // recreate previous state
+            FCMoveContent(temp_non_matches, matches);
+
             // try the next operand
         }
-        // If NON_MATCHES could not find any matches it should match all
+        // If no operand condition was selected, there are no matches. Move all objects to non_matches
         if (!found_sth)
             FCMoveContent(matches, non_matches);
     } else /*(search_domain == MATCHES)*/ {
