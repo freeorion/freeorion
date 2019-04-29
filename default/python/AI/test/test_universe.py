4cf0a6ad468f0c59b466ec11877b4fe45b9713bc
Morlic-fo <Morlic@gmx.de>
2019-04-29 21:05:43 +0200

    Refactor unittest setup: Generate a properly named test case for each property to test

--- default/python/AI/test/test_universe.py
+++ default/python/AI/test/test_universe.py
@@ -1,4 +1,5 @@
 import unittest
+from copy import deepcopy
 
 import freeOrionAIInterface as fo
 
@@ -24,14 +25,22 @@ class PropertyTester(unittest.TestCase):
         if self.__class__ == PropertyTester:
             self.run = lambda self, *args, **kwargs: None
 
-    def test_properties(self):
-        self.assertTrue(self.properties)
-        self.assertTrue(self.objects_to_test)
-        for property, kwargs in self.properties.items():
+    @classmethod
+    def generate_tests(cls):
+        for property in cls.properties:
+            setattr(cls, "test_property_%s" % property,
+                    PropertyTester._test_case_generator(property, cls.properties[property]))
+
+    @staticmethod
+    def _test_case_generator(property, kwargs):
+        def _test_case(self):
             for obj in self.objects_to_test:
                 self.__test_property(property, obj, **kwargs)
+        return _test_case
 
     def __test_property(self, property_to_test, obj, type=None, read_only=None, additional_tests=[]):
+        self.assertIn(property_to_test, dir(obj))
+        property_to_test = getattr(self.class_to_test, property_to_test)
         return_value = property_to_test.fget(obj)
         if type is not None:
             self.assertIsInstance(return_value, type)
@@ -43,158 +52,281 @@ class PropertyTester(unittest.TestCase):
 
         if read_only is False:
             property_to_test.fset(obj, type())
-            self.assertIsEqual(property_to_test.fget(obj), type())
+            self.assertEqual(property_to_test.fget(obj), type())
             property_to_test.fset(obj, return_value)
-            self.assertIsEqual(property_to_test.fget(obj), return_value)
+            self.assertEqual(property_to_test.fget(obj), return_value)
 
         for test in additional_tests:
-            test[0](return_value, *test[1:])
+            test[0](self, return_value, *test[1:])
 
 
 class UniverseObjectTester(PropertyTester):
     # hack: we don't want unittest to run this abstract test base.
     # Therefore, we override this classes run function to do nothing.
+    properties = {
+            "id": {
+                TYPE: int,
+                READ_ONLY: True,
+            },
+            "name": {
+                TYPE: str,
+                READ_ONLY: True,
+            },
+            "x": {
+                TYPE: float,
+                READ_ONLY: True,
+            },
+            "y": {
+                TYPE: float,
+                READ_ONLY: True,
+            },
+            "systemID": {
+                TYPE: int,
+                READ_ONLY: True,
+            },
+            "unowned": {
+                TYPE: bool,
+                READ_ONLY: True,
+            },
+            "owner": {
+                TYPE: int,
+                READ_ONLY: True,
+            },
+            "creationTurn": {
+                TYPE: int,
+                READ_ONLY: True,
+            },
+            "ageInTurns": {
+                TYPE: int,
+                READ_ONLY: True,
+            },
+            "containedObjects": {
+                TYPE: fo.IntSet,
+                READ_ONLY: True,
+            },
+            "containerObject": {
+                TYPE: int,
+                READ_ONLY: True,
+            },
+            "tags": {
+                TYPE: fo.StringSet,
+                READ_ONLY: True,
+            },
+            "meters": {
+                TYPE: fo.MeterTypeMeterMap,
+                READ_ONLY: True,
+            },
+    }
+
     def __init__(self, *args, **kwargs):
         super(UniverseObjectTester, self).__init__(*args, **kwargs)
         if self.__class__ == UniverseObjectTester:
             self.run = lambda self, *args, **kwargs: None
 
-    def setUp(self):
-        self.properties = {
-            fo.universeObject.id: {
-                TYPE: int,
-                READ_ONLY: True,
-            },
-            fo.universeObject.name: {
-                TYPE: str,
-                READ_ONLY: True,
-            },
-            fo.universeObject.x: {
-                TYPE: float,
-                READ_ONLY: True,
-            },
-            fo.universeObject.y: {
-                TYPE: float,
-                READ_ONLY: True,
-            },
-            fo.universeObject.systemID: {
-                TYPE: int,
-                READ_ONLY: True,
-            },
-            fo.universeObject.unowned: {
-                TYPE: bool,
-                READ_ONLY: True,
-            },
-            fo.universeObject.owner: {
-                TYPE: int,
-                READ_ONLY: True,
-            },
-            fo.universeObject.creationTurn: {
-                TYPE: int,
-                READ_ONLY: True,
-            },
-            fo.universeObject.ageInTurns: {
-                TYPE: int,
-                READ_ONLY: True,
-            },
-            fo.universeObject.containedObjects: {
-                TYPE: fo.IntSet,
-                READ_ONLY: True,
-            },
-            fo.universeObject.containerObject: {
-                TYPE: int,
-                READ_ONLY: True,
-            },
-            fo.universeObject.tags: {
-                TYPE: fo.StringSet,
-                READ_ONLY: True,
-            },
-            fo.universeObject.meters: {
-                TYPE: fo.MeterTypeMeterMap,
-                READ_ONLY: True,
-            },
-        }
+
+def greater_or_equal(arg):
+    return unittest.TestCase.assertGreaterEqual, arg
+
+
+def is_equal(arg):
+    return unittest.TestCase.assertGreaterEqual, arg
+
+
+def is_false():
+    return (unittest.TestCase.assertFalse,)
 
 
 class FleetTester(UniverseObjectTester):
+    class_to_test = fo.fleet
+    properties = deepcopy(UniverseObjectTester.properties)
+    properties.update({
+        "fuel": {
+            TYPE: float,
+            READ_ONLY: True,
+            ADDITIONAL_TESTS: [greater_or_equal(0)],
+        },
+        "maxFuel": {
+            TYPE: float,
+            READ_ONLY: True,
+            ADDITIONAL_TESTS: [greater_or_equal(0)],
+        },
+        "finalDestinationID": {
+            TYPE: int,
+            READ_ONLY: True,
+            ADDITIONAL_TESTS: [is_equal(INVALID_ID)],
+        },
+        "previousSystemID": {
+            TYPE: int,
+            READ_ONLY: True,
+            ADDITIONAL_TESTS: [is_equal(INVALID_ID)],
+        },
+        "nextSystemID": {
+            TYPE: int,
+            READ_ONLY: True,
+            ADDITIONAL_TESTS: [is_equal(INVALID_ID)],
+        },
+        "aggressive": {
+            TYPE: bool,
+            READ_ONLY: True,
+        },
+        "speed": {
+            TYPE: float,
+            READ_ONLY: True,
+        },
+        "hasMonsters": {
+            TYPE: bool,
+            READ_ONLY: True,
+        },
+        "hasFighterShips": {
+            TYPE: bool,
+            READ_ONLY: True,
+        },
+        "hasColonyShips": {
+            TYPE: bool,
+            READ_ONLY: True,
+        },
+        "hasOutpostShips": {
+            TYPE: bool,
+            READ_ONLY: True,
+        },
+        "hasTroopShips": {
+            TYPE: bool,
+            READ_ONLY: True,
+        },
+        "numShips": {
+            TYPE: int,
+            READ_ONLY: True,
+        },
+        "empty": {
+            TYPE: int,
+            READ_ONLY: True,
+            ADDITIONAL_TESTS: [(unittest.TestCase.assertFalse,)],
+        },
+        "shipIDs": {
+            TYPE: fo.IntSet,
+            READ_ONLY: True,
+        },
+    })
 
     def setUp(self):
         super(FleetTester, self).setUp()
         universe = fo.getUniverse()
         fleet_ids = list(universe.fleetIDs)
         self.objects_to_test = map(universe.getFleet, fleet_ids)
+        assert all(isinstance(obj, self.class_to_test) for obj in self.objects_to_test)
 
-        def greater_or_equal(x):
-            return (self.assertGreaterEqual, x)
 
-        def is_equal(x):
-            return (self.assertEqual, x)
+class ShipTester(UniverseObjectTester):
+    class_to_test = fo.ship
+    properties = deepcopy(UniverseObjectTester.properties)
+    properties.update({
+        "design": {
+            TYPE: fo.shipDesign,
+            READ_ONLY: True,
+        },
+        "designID": {
+            TYPE: int,
+            READ_ONLY: True,
+            ADDITIONAL_TESTS: [greater_or_equal(0)],
+        },
+        "fleetID": {
+            TYPE: int,
+            READ_ONLY: True,
+            ADDITIONAL_TESTS: [greater_or_equal(0)],
+        },
+        "producedByEmpireID": {
+            TYPE: int,
+            READ_ONLY: True,
+            ADDITIONAL_TESTS: [greater_or_equal(INVALID_ID)],
+        },
+        "arrivedOnTurn": {
+            TYPE: int,
+            READ_ONLY: True,
+        },
+        "lastResuppliedOnTurn": {
+            TYPE: int,
+            READ_ONLY: True,
+        },
+        "lastTurnActiveInCombat": {
+            TYPE: int,
+            READ_ONLY: True,
+        },
+        "isMonster": {
+            TYPE: bool,
+            READ_ONLY: True,
+        },
+        "isArmed": {
+            TYPE: bool,
+            READ_ONLY: True,
+        },
+        "hasFighters": {
+            TYPE: bool,
+            READ_ONLY: True,
+        },
+        "canColonize": {
+            TYPE: bool,
+            READ_ONLY: True,
+        },
+        "canInvade": {
+            TYPE: bool,
+            READ_ONLY: True,
+        },
+        "canBombard": {
+            TYPE: bool,
+            READ_ONLY: True,
+        },
+        "speciesName": {
+            TYPE: str,
+            READ_ONLY: True,
+        },
+        "speed": {
+            TYPE: float,
+            READ_ONLY: True,
+            ADDITIONAL_TESTS: [greater_or_equal(0)],
+        },
+        "colonyCapacity": {
+            TYPE: float,
+            READ_ONLY: True,
+            ADDITIONAL_TESTS: [greater_or_equal(0)],
+        },
+        "troopCapacity": {
+            TYPE: float,
+            READ_ONLY: True,
+            ADDITIONAL_TESTS: [greater_or_equal(0)],
+        },
+        "orderedScrapped": {
+            TYPE: bool,
+            READ_ONLY: True,
+            ADDITIONAL_TESTS: [is_false()],
+        },
+        "orderedColonizePlanet": {
+            TYPE: int,
+            READ_ONLY: True,
+            ADDITIONAL_TESTS: [is_equal(INVALID_ID)],
+        },
+        "orderedInvadePlanet": {
+            TYPE: int,
+            READ_ONLY: True,
+            ADDITIONAL_TESTS: [is_equal(INVALID_ID)],
+        },
+        "partMeters": {
+            TYPE: fo.ShipPartMeterMap,
+            READ_ONLY: True,
+        },
+    })
 
-        self.properties.update({
-            fo.fleet.fuel: {
-                TYPE: float,
-                READ_ONLY: True,
-                ADDITIONAL_TESTS: [greater_or_equal(0)],
-            },
-            fo.fleet.maxFuel: {
-                TYPE: float,
-                READ_ONLY: True,
-                ADDITIONAL_TESTS: [greater_or_equal(0)],
-            },
-            fo.fleet.finalDestinationID: {
-                TYPE: int,
-                READ_ONLY: True,
-                ADDITIONAL_TESTS: [is_equal(INVALID_ID)],
-            },
-            fo.fleet.previousSystemID: {
-                TYPE: int,
-                READ_ONLY: True,
-                ADDITIONAL_TESTS: [is_equal(INVALID_ID)],
-            },
-            fo.fleet.nextSystemID: {
-                TYPE: int,
-                READ_ONLY: True,
-                ADDITIONAL_TESTS: [is_equal(INVALID_ID)],
-            },
-            fo.fleet.aggressive: {
-                TYPE: bool,
-                READ_ONLY: True,
-            },
-            fo.fleet.speed: {
-                TYPE: float,
-                READ_ONLY: True,
-            },
-            fo.fleet.hasMonsters: {
-                TYPE: bool,
-                READ_ONLY: True,
-            },
-            fo.fleet.hasFighterShips: {
-                TYPE: bool,
-                READ_ONLY: True,
-            },
-            fo.fleet.hasColonyShips: {
-                TYPE: bool,
-                READ_ONLY: True,
-            },
-            fo.fleet.hasOutpostShips: {
-                TYPE: bool,
-                READ_ONLY: True,
-            },
-            fo.fleet.hasTroopShips: {
-                TYPE: bool,
-                READ_ONLY: True,
-            },
-            fo.fleet.numShips: {
-                TYPE: int,
-                READ_ONLY: True,
-            },
-            fo.fleet.empty: {
-                TYPE: int,
-                READ_ONLY: True,
-                ADDITIONAL_TESTS: [(self.assertFalse,)],
-            },
-            fo.fleet.shipIDs: {
-                TYPE: fo.IntSet,
-                READ_ONLY: False,  # FIXME: Intentional failure of unittest!
-            },
-        })
+    def setUp(self):
+        super(ShipTester, self).setUp()
+        universe = fo.getUniverse()
+        fleet_ids = list(universe.fleetIDs)
+        self.objects_to_test = []
+        for fid in fleet_ids:
+            self.objects_to_test.extend(map(universe.getShip, universe.getFleet(fid).shipIDs))
+
+
+
+
+
+
+FleetTester.generate_tests()
+ShipTester.generate_tests()
