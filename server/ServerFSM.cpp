c09e4e325fc0bf2c11adcafc408d80d010150ebe
O01eg <o01eg@yandex.ru>
2019-07-26 20:51:40 +0300

    Don't save private messages

--- server/ServerFSM.cpp
+++ server/ServerFSM.cpp
@@ -1808,7 +1808,9 @@ sc::result MPLobby::react(const PlayerChat& msg) {
 
     boost::posix_time::ptime timestamp = boost::posix_time::second_clock::universal_time();
 
-    if (sender->GetClientType() != Networking::CLIENT_TYPE_AI_PLAYER) {
+    if (receiver == Networking::INVALID_PLAYER_ID &&
+        sender->GetClientType() != Networking::CLIENT_TYPE_AI_PLAYER)
+    {
         GG::Clr text_color(255, 255, 255, 255);
         for (const auto& player : m_lobby_data->m_players) {
             if (player.first != sender->PlayerID())
@@ -2519,7 +2521,9 @@ sc::result PlayingGame::react(const PlayerChat& msg) {
 
     boost::posix_time::ptime timestamp = boost::posix_time::second_clock::universal_time();
 
-    if (sender->GetClientType() != Networking::CLIENT_TYPE_AI_PLAYER) {
+    if (receiver == Networking::INVALID_PLAYER_ID &&
+        sender->GetClientType() != Networking::CLIENT_TYPE_AI_PLAYER)
+    {
         GG::Clr text_color(255, 255, 255, 255);
         if (auto empire = GetEmpire(sender->PlayerID()))
             text_color = empire->Color();
