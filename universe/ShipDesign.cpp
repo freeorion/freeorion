68097cd42b1bd1f8b1ca7cf5aed4a81268ef7d4c
geoffthemedio <geoffthemedio@gmail.com>
2019-09-12 00:26:42 +0200

    A ShipDesign must have both a fighter bay and a hangar to be considered as having fighters, rather than needing either.

--- universe/ShipDesign.cpp
+++ universe/ShipDesign.cpp
@@ -1300,6 +1300,9 @@ void ShipDesign::BuildStatCaches() {
     m_structure =       hull->Structure();
     m_speed =           hull->Speed();
 
+    bool has_fighter_bays = false;
+    bool has_fighter_hangars = false;
+
     for (const std::string& part_name : m_parts) {
         if (part_name.empty())
             continue;
@@ -1320,8 +1323,10 @@ void ShipDesign::BuildStatCaches() {
             m_is_armed = true;
             break;
         case PC_FIGHTER_BAY:
+            has_fighter_bays = true;
+            break;
         case PC_FIGHTER_HANGAR:
-            m_has_fighters = true;
+            has_fighter_hangars = true;
             break;
         case PC_COLONY:
             m_colony_capacity += part->Capacity();
@@ -1365,6 +1370,7 @@ void ShipDesign::BuildStatCaches() {
         default:
             break;
         }
+        m_has_fighters = has_fighter_bays && has_fighter_hangars;
 
         m_num_part_types[part_name]++;
         if (part_class > INVALID_SHIP_PART_CLASS && part_class < NUM_SHIP_PART_CLASSES)
