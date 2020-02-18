192f41330fd57fdd9d0368a6ae044fddb5507a55
O01eg <o01eg@yandex.ru>
2020-02-18 07:19:03 +0300

    Provide a str to json decoder
    Fixes json.decoder.JSONDecodeError: Expecting value: line 1 column 1 (char 0)

--- default/python/AI/savegame_codec/_decoder.py
+++ default/python/AI/savegame_codec/_decoder.py
@@ -44,7 +44,7 @@ def load_savegame_string(string):
     except zlib.error:
         pass  # probably an uncompressed (or wrongly base64 decompressed) string
     try:
-        decoded_state = decode(new_string)
+        decoded_state = decode(six.ensure_str(new_string, 'utf-8'))
         debug("Decoded a zlib-compressed and apparently base64-encoded save-state string.")
         return decoded_state
     except (InvalidSaveGameException, ValueError, TypeError) as e:
@@ -60,7 +60,7 @@ def load_savegame_string(string):
     # We need to check uncompressed string, since zip archive of the empty string is not the empty string.
     if not string:
         raise Exception("Save game string is empty")
-    return decode(string)
+    return decode(six.ensure_str(string, 'utf-8'))
 
 
 def decode(obj):
