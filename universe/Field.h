7f12a954822ba5e0ba372fb9fee06ca4b21f54ba
geoffthemedio <geoffthemedio@gmail.com>
2020-02-24 21:59:30 +0100

    grooming

--- universe/Field.h
+++ universe/Field.h
@@ -14,35 +14,33 @@ namespace Effect {
 class FO_COMMON_API Field : public UniverseObject {
 public:
     /** \name Accessors */ //@{
-    std::set<std::string> Tags() const override;
+    std::set<std::string>   Tags() const override;
+    bool                    HasTag(const std::string& name) const override;
 
-    bool HasTag(const std::string& name) const override;
+    UniverseObjectType  ObjectType() const override;
 
-    UniverseObjectType ObjectType() const override;
+    std::string         Dump(unsigned short ntabs = 0) const override;
 
-    std::string Dump(unsigned short ntabs = 0) const override;
+    int                 ContainerObjectID() const override;
+    bool                ContainedBy(int object_id) const override;
 
-    int ContainerObjectID() const override;
+    const std::string&  PublicName(int empire_id) const override;
+    const std::string&  FieldTypeName() const { return m_type_name; }
 
-    bool ContainedBy(int object_id) const override;
+    /* Field is (presently) the only distributed UniverseObject that isn't just
+     * location at a single point in space. These functions check if locations
+     * or objecs are within this field's area. */
+    bool                InField(std::shared_ptr<const UniverseObject> obj) const;
+    bool                InField(double x, double y) const;
 
     std::shared_ptr<UniverseObject> Accept(const UniverseObjectVisitor& visitor) const override;
-
-    const std::string& PublicName(int empire_id) const override;
-
-    void ClampMeters() override;
-
-    const std::string&          FieldTypeName() const { return m_type_name; }
-
-    bool InField(std::shared_ptr<const UniverseObject> obj) const;
-
-    bool                        InField(double x, double y) const;
     //@}
 
     /** \name Mutators */ //@{
     void Copy(std::shared_ptr<const UniverseObject> copied_object, int empire_id = ALL_EMPIRES) override;
 
     void ResetTargetMaxUnpairedMeters() override;
+    void ClampMeters() override;
     //@}
 
 protected:
@@ -52,20 +50,17 @@ protected:
 
 public:
     Field(const std::string& field_type, double x, double y, double radius);
+    ~Field();
 
 protected:
     template <class T> friend void boost::python::detail::value_destroyer<false>::execute(T const volatile* p);
 
-public:
-    ~Field();
-
-protected:
     /** Returns new copy of this Field. */
     Field* Clone(int empire_id = ALL_EMPIRES) const override;
     //@}
 
 private:
-    std::string     m_type_name;
+    std::string m_type_name;
 
     friend class boost::serialization::access;
     template <class Archive>
