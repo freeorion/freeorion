11155ebf1edbe5479d5bedbb93ec180c9aa48b5a
Marcel Metz <mmetz@adrian-broher.net>
2020-04-18 15:08:31 +0200

    Use old serialization tag names to preserve save file compat

--- util/SerializeEmpire.cpp
+++ util/SerializeEmpire.cpp
@@ -176,7 +176,7 @@ void Empire::serialize(Archive& ar, const unsigned int version)
             & boost::serialization::make_nvp("m_research_progress", empty_research_progress)
             & boost::serialization::make_nvp("m_production_queue", empty_production_queue)
             & boost::serialization::make_nvp("m_available_building_types", empty_string_set)
-            & boost::serialization::make_nvp("m_available_ship_parts", empty_string_set)
+            & boost::serialization::make_nvp("m_available_part_types", empty_string_set)
             & boost::serialization::make_nvp("m_available_hull_types", empty_string_set);
     } else {
         // processing all data on deserialization, saving to savegame,
@@ -186,7 +186,7 @@ void Empire::serialize(Archive& ar, const unsigned int version)
             & BOOST_SERIALIZATION_NVP(m_research_progress)
             & BOOST_SERIALIZATION_NVP(m_production_queue)
             & BOOST_SERIALIZATION_NVP(m_available_building_types)
-            & BOOST_SERIALIZATION_NVP(m_available_ship_parts)
+            & boost::serialization::make_nvp("m_available_part_types", m_available_ship_parts)
             & BOOST_SERIALIZATION_NVP(m_available_hull_types);
     }
 
@@ -205,7 +205,7 @@ void Empire::serialize(Archive& ar, const unsigned int version)
 
             & BOOST_SERIALIZATION_NVP(m_species_ships_owned)
             & BOOST_SERIALIZATION_NVP(m_ship_designs_owned)
-            & BOOST_SERIALIZATION_NVP(m_ship_parts_owned)
+            & boost::serialization::make_nvp("m_ship_part_types_owned", m_ship_parts_owned)
             & BOOST_SERIALIZATION_NVP(m_ship_part_class_owned)
             & BOOST_SERIALIZATION_NVP(m_species_colonies_owned)
             & BOOST_SERIALIZATION_NVP(m_outposts_owned)
