1ab1f4519d2e6f363b7b529e9f654476f6c4d9c8
O01eg <o01eg@yandex.ru>
2020-06-22 09:36:14 +0300

    Don't shutdown dedicated server.
    Fixes accidental server restart on DoS attack.

--- server/ServerFSM.cpp
+++ server/ServerFSM.cpp
@@ -693,7 +693,7 @@ sc::result Idle::react(const ShutdownServer& msg) {
 
 sc::result Idle::react(const Error& msg) {
     auto fatal = HandleErrorMessage(msg, Server());
-    if (fatal)
+    if (fatal && !Server().IsHostless())
         return transit<ShuttingDownServer>();
     return discard_event();
 }
@@ -1960,7 +1960,7 @@ sc::result MPLobby::react(const Hostless& msg) {
 
 sc::result MPLobby::react(const Error& msg) {
     auto fatal = HandleErrorMessage(msg, Server());
-    if (fatal)
+    if (fatal && !Server().IsHostless())
         return transit<ShuttingDownServer>();
     return discard_event();
 }
@@ -2495,7 +2495,7 @@ sc::result WaitingForMPGameJoiners::react(const ShutdownServer& msg) {
 
 sc::result WaitingForMPGameJoiners::react(const Error& msg) {
     auto fatal = HandleErrorMessage(msg, Server());
-    if (fatal)
+    if (fatal && !Server().IsHostless())
         return transit<ShuttingDownServer>();
     return discard_event();
 }
@@ -2873,7 +2873,7 @@ sc::result PlayingGame::react(const EliminateSelf& msg) {
 
 sc::result PlayingGame::react(const Error& msg) {
     auto fatal = HandleErrorMessage(msg, Server());
-    if (fatal) {
+    if (fatal && !Server().IsHostless()) {
         DebugLogger(FSM) << "Fatal received.";
         return transit<ShuttingDownServer>();
     }
