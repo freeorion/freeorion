1d4ed052c88aedddfa613b34e8caf54afc079112
Marcel Metz <mmetz@adrian-broher.net>
2020-08-02 13:55:06 +0200

    Format Empire/InfluenceQueue.{h,cpp}

--- Empire/InfluenceQueue.h
+++ Empire/InfluenceQueue.h
@@ -17,19 +17,25 @@ FO_COMMON_API extern const int INVALID_OBJECT_ID;
 FO_COMMON_API extern const int ALL_EMPIRES;
 
 
-struct FO_COMMON_API InfluenceQueue {
-    /** The type of a single element in the Influence queue. */
-    struct FO_COMMON_API Element {
+struct FO_COMMON_API InfluenceQueue
+{
+    //! The type of a single element in the Influence queue.
+    struct FO_COMMON_API Element
+    {
         explicit Element() = default;
+
         Element(int empire_id_, std::string name_, bool paused_ = false) :
             name(std::move(name_)),
             empire_id(empire_id_),
             paused(paused_)
         {}
 
-        std::string     name;                       ///< name of influence project
+        //! Name of influence project
+        std::string     name;
         int             empire_id = ALL_EMPIRES;
-        float           allocated_ip = 0.0f;        ///< IP allocated to this InfluenceQueue Element by Empire Influence update
+        //! IP allocated to this InfluenceQueue Element by Empire Influence
+        //! update
+        float           allocated_ip = 0.0f;
         bool            paused = false;
 
         std::string Dump() const;
@@ -42,59 +48,83 @@ struct FO_COMMON_API InfluenceQueue {
 
     typedef std::deque<Element> QueueType;
 
-    /** The InfluenceQueue iterator type.  Dereference yields a Element. */
+    //! The InfluenceQueue iterator type.  Dereference yields a Element.
     typedef QueueType::iterator iterator;
-    /** The const InfluenceQueue iterator type.  Dereference yields a Element. */
+
+    //! The const InfluenceQueue iterator type.  Dereference yields a Element.
     typedef QueueType::const_iterator const_iterator;
 
     explicit InfluenceQueue(int empire_id) :
         m_empire_id(empire_id)
     {}
 
-    bool    InQueue(const std::string& name) const;
+    auto InQueue(const std::string& name) const -> bool;
 
-    int     ProjectsInProgress() const { return m_projects_in_progress; }
-    float   TotalIPsSpent() const { return m_total_IPs_spent; };
-    int     EmpireID() const { return m_empire_id; }
+    auto ProjectsInProgress() const -> int
+    { return m_projects_in_progress; }
 
-    /** Returns amount of stockpile IP allocated to Influence queue elements. */
-    float AllocatedStockpileIP() const;
+    auto TotalIPsSpent() const -> float
+    { return m_total_IPs_spent; };
 
-    /** Returns the value expected for the Influence Stockpile for the next
-      * turn, based on the current InfluenceQueue allocations. */
-    float ExpectedNewStockpileAmount() const { return m_expected_new_stockpile_amount; }
+    auto EmpireID() const -> int
+    { return m_empire_id; }
 
+    //! Returns amount of stockpile IP allocated to Influence queue elements.
+    auto AllocatedStockpileIP() const -> float;
 
-    // STL container-like interface
-    bool            empty() const { return m_queue.empty(); }
-    unsigned int    size() const { return m_queue.size(); }
-    const_iterator  begin() const { return m_queue.begin(); }
-    const_iterator  end() const { return m_queue.end(); }
-    const_iterator  find(const std::string& item_name) const;
-    const Element&  operator[](std::size_t i) const;
-    const Element&  operator[](int i) const;
+    //! Returns the value expected for the Influence Stockpile for the next
+    //! turn, based on the current InfluenceQueue allocations.
+    auto ExpectedNewStockpileAmount() const -> float
+    { return m_expected_new_stockpile_amount; }
 
-
-    /** Recalculates the PPs spent on and number of turns left for each project in the queue.  Also
-      * determines the number of projects in progress, and the industry consumed by projects
-      * in each resource-sharing group of systems.  Does not actually "spend" the PP; a later call to
-      * empire->CheckInfluenceProgress() will actually spend PP, remove items from queue and create them
-      * in the universe. */
+    //! Recalculates the PPs spent on and number of turns left for each project
+    //! in the queue.  Also determines the number of projects in progress, and
+    //! the industry consumed by projects * in each resource-sharing group of
+    //! systems.  Does not actually "spend" the PP; a later call to
+    //! empire->CheckInfluenceProgress() will actually spend PP, remove items
+    //! from queue and create them in the universe.
     void Update();
 
+    auto empty() const -> bool
+    { return m_queue.empty(); }
 
-    // STL container-like interface
-    void        push_back(const Element& element) { m_queue.push_back(element); }
-    void        insert(iterator it, const Element& element) { m_queue.insert(it, element); }
-    void        erase(int i);
-    iterator    erase(iterator it) { return m_queue.erase(it); }
+    auto size() const -> std::size_t
+    { return m_queue.size(); }
 
-    iterator    begin() { return m_queue.begin(); }
-    iterator    end() { return m_queue.end(); }
-    iterator    find(const std::string& item_name);
-    Element&    operator[](int i);
+    auto begin() const -> const_iterator
+    { return m_queue.begin(); }
 
-    void        clear();
+    auto end() const -> const_iterator
+    { return m_queue.end(); }
+
+    auto find(const std::string& item_name) const -> const_iterator;
+
+    auto operator[](std::size_t i) const -> const Element&;
+
+    auto operator[](int i) const -> const Element&;
+
+    void push_back(const Element& element)
+    { m_queue.push_back(element); }
+
+    void insert(iterator it, const Element& element)
+    { m_queue.insert(it, element); }
+
+    void erase(int i);
+
+    auto erase(iterator it) -> iterator
+    { return m_queue.erase(it); }
+
+    auto begin() -> iterator
+    { return m_queue.begin(); }
+
+    auto end() -> iterator
+    { return m_queue.end(); }
+
+    auto find(const std::string& item_name) -> iterator;
+
+    auto operator[](int i) -> Element&;
+
+    void clear();
 
     mutable boost::signals2::signal<void ()> InfluenceQueueChangedSignal;
 
