491a90be9dede27b9c15c80c85b3568d3ff430c5
Andrey <Cjkjvfnby@gmail.com>
2020-08-05 23:46:37 +0300

    Rename tests

--- default/python/tests/AI/test_parse_ai_tag_grade.py
+++ default/python/tests/AI/test_parse_ai_tag_grade.py
@@ -1,17 +1,44 @@
+from pytest import mark
+
 from freeorion_tools import get_ai_tag_grade
 
 
-def test_parse_weapon_tag_positive1():
-    assert 'GREAT' == get_ai_tag_grade(['GREAT_WEAPONS'], 'WEAPONS')
+@mark.parametrize(
+    "tags,tags_type",
+    (
+            (['GREAT_WEAPONS'], 'WEAPONS'),
+            (['GREAT_WEAPONS'], 'weapons'),
+            (['GOOD_HULLS', 'GREAT_WEAPONS'], 'WEAPONS'),
+            (['GREAT_WEAPONS', 'GOOD_WEAPONS'], 'WEAPONS'),
+    ))
+def test_tags_search_by_tag_type_returns_grade(tags, tags_type):
+    assert 'GREAT' == get_ai_tag_grade(tags, tags_type)
 
 
-def test_parse_weapon_tag_positive2():
-    assert 'GREAT' == get_ai_tag_grade(['GREAT_MULTI_WORD_TAG'], 'MULTI_WORD_TAG')
+def test_tags_search_by_tag_type_with_underscore_returns_first_grade():
+    assert 'GREAT' == get_ai_tag_grade(['GREAT_WEAPONS_TEST'], 'WEAPONS_TEST')
 
 
-def test_parse_weapon_tag_negative1():
-    assert '' == get_ai_tag_grade(['WEAPONS'], 'WEAPONS')
+def test_tags_search_by_not_matching_tag_type_returns_empty_string():
+    assert '' == get_ai_tag_grade(['GOOD_HULL'], 'WEAPONS')
 
 
-def test_parse_weapon_tag_negative2():
-    assert '' == get_ai_tag_grade(['GREAT_WEAPONS'], 'WEAPON')
+@mark.parametrize(
+    "tags,tags_type",
+    (
+            (['GREAT_WEAPONS'], 'WEAPON'),
+            (['GREAT_WEAPONS'], 'EAPONS'),
+            (['GREAT_WEAPONS'], 'EAPON'),
+    ))
+def test_tags_search_by_substring_tag_type_returns_empty_string(tags, tags_type):
+    assert '' == get_ai_tag_grade(tags, tags_type)
+
+
+@mark.parametrize(
+    "tags,tags_type",
+    (
+            (['great_weapons'], 'weapons'),
+            (['great_weapons'], 'WEAPONS'),
+    ))
+def test_lowercase_tags_search_by_tag_type_returns_empty_string(tags, tags_type):
+    assert '' == get_ai_tag_grade(tags, tags_type)
--- default/python/tests/AI/test_save_game_decoding.py
+++ default/python/tests/AI/test_save_game_decoding.py
@@ -1,44 +1,99 @@
+from collections import OrderedDict
+
 import pytest
-from pytest import fixture
-from savegame_codec import build_savegame_string, load_savegame_string
+from pytest import fixture, raises, mark
+
 import aistate_interface
+from ColonisationAI import OrbitalColonizationManager
+from EnumsAI import MissionType
+from savegame_codec import build_savegame_string, load_savegame_string, CanNotSaveGameException
 from savegame_codec._decoder import SaveDecompressException
 
 
 @fixture
-def state():
-    return {1: "2"}
+def pack_object(monkeypatch):
+    def do_decoding(obj):
+        def get_aistate():
+            return obj
+        monkeypatch.setattr(aistate_interface, "get_aistate", get_aistate)
+        return build_savegame_string().decode('utf-8')
+    return do_decoding
 
 
 @fixture
-def save_string(monkeypatch, state):
-    def get_aistate():
-        return state
-
-    monkeypatch.setattr(aistate_interface, "get_aistate", get_aistate)
-    return build_savegame_string().decode('utf-8')
-
-
-def test_save_load_game_from_string(save_string, state):
-    result = load_savegame_string(save_string)
-    assert result == state
+def patch_orb_col_management(monkeypatch):
+    def eq(self, other):
+        self_tuple = self._colonization_plans, self.num_enqueued_bases
+        other_tuple = other._colonization_plans, other.num_enqueued_bases
+        return self_tuple == other_tuple
+    monkeypatch.setattr(OrbitalColonizationManager, '__eq__', eq)
 
 
-def test_save_load_game_from_bytes(save_string, state):
-    result = load_savegame_string(save_string.encode("utf-8"))
-    assert result == state
+@fixture(params=[
+    True,
+    False,
+    None,
+    1,
+    1.0,
+    tuple(),
+    tuple("abc"),
+    ("1", ("2", ("3",))),
+    "hello 'world'",
+    [],
+    [1, 2, 3],
+    {},
+    {1: 2, 3: 4},
+    OrderedDict({}),
+    OrderedDict({1: 2}),
+    set(),
+    {1, 2, 3},
+    MissionType.COLONISATION,
+    OrbitalColonizationManager(),
+    [[[OrbitalColonizationManager(), MissionType.COLONISATION, 1], "2", 3.0, {1: 2}]],
+    {1: {2:  {3: OrbitalColonizationManager()}}}
+]
+)
+def serialized_object(request, pack_object):
+    obj = request.param
+    yield obj,  pack_object(obj)
 
 
-def test_decode_not_base64():
+def test_inherited_trusted_object_is_not_encoded(pack_object):
+    class Inherited(OrbitalColonizationManager):
+        pass
+
+    with raises(CanNotSaveGameException, match='Class .*.Inherited is not trusted'):
+        pack_object(Inherited())
+
+
+@mark.parametrize("obj", ['"', '\\', '\n', '\t'])
+def test_string_with_sepcial_caracters_encoded_and_decoded_back(obj, pack_object):
+    encoded = pack_object(obj)
+    assert obj == load_savegame_string(encoded)
+
+
+@mark.usefixtures('patch_orb_col_management')
+def test_string_decode_to_object(serialized_object):
+    obj, serialized_string = serialized_object
+    assert obj == load_savegame_string(serialized_string)
+
+
+@mark.usefixtures('patch_orb_col_management')
+def test_bytes_decode_to_object(serialized_object):
+    obj, serialized_bytes = serialized_object
+    assert obj == load_savegame_string(serialized_bytes.encode("utf-8"))
+
+
+def test_not_base64_decode_raises_error():
     with pytest.raises(SaveDecompressException, match="Fail to decode base64 savestate"):
         load_savegame_string("///")
 
 
-def test_decode_not_gziped():
+def test_not_gziped_decode_raises_error():
     with pytest.raises(SaveDecompressException, match="Fail to decompress savestate"):
         load_savegame_string("aaaa")
 
 
-def test_decode_not_ascii():
+def test_not_unicode_decode_raises_error():
     with pytest.raises(SaveDecompressException, match="Fail to decode base64 savestate"):
         load_savegame_string("шишка")
