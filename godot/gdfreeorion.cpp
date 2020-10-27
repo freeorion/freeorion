4eb57e9505562f90ae980a01b95042424d90dbac
O01eg <o01eg@yandex.ru>
2020-10-27 22:04:02 +0300

    Parse resources

--- godot/gdfreeorion.cpp
+++ godot/gdfreeorion.cpp
@@ -40,6 +40,29 @@ void GDFreeOrion::_init() {
 
     CompleteXDGMigration();
 
+    // Handle the case where the resource.path does not exist anymore
+    // gracefully by resetting it to the standard path into the
+    // application bundle.  This may happen if a previous installed
+    // version of FreeOrion was residing in a different directory.
+    if (!boost::filesystem::exists(GetResourceDir()) ||
+        !boost::filesystem::exists(GetResourceDir() / "credits.xml") ||
+        !boost::filesystem::exists(GetResourceDir() / "data" / "art" / "misc" / "missing.png"))
+    {
+        DebugLogger() << "Resources directory from config.xml missing or does not contain expected files. Resetting to default.";
+
+        GetOptionsDB().Set<std::string>("resource.path", "");
+
+        // double-check that resetting actually fixed things...
+        if (!boost::filesystem::exists(GetResourceDir()) ||
+            !boost::filesystem::exists(GetResourceDir() / "credits.xml") ||
+            !boost::filesystem::exists(GetResourceDir() / "data" / "art" / "misc" / "missing.png"))
+        {
+            DebugLogger() << "Default Resources directory missing or does not contain expected files. Cannot start game.";
+            throw std::runtime_error("Unable to load game resources at default location: " +
+                                     PathToString(GetResourceDir()) + " : Install may be broken.");
+        }
+    }
+
     // initialize any variables here
     time_passed = 0.0;
     t = std::thread(do_the_ping, this);
