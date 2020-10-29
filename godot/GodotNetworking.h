aab398a44e9a3c374a8934de213267717fec5f54
O01eg <o01eg@yandex.ru>
2020-10-29 16:37:53 +0300

    Initial networking

---
+++ godot/GodotNetworking.h
@@ -0,0 +1,26 @@
+#ifndef _GODOT_NETWORKING_H_
+#define _GODOT_NETWORKING_H_
+
+#include <Godot.hpp>
+#include <Object.hpp>
+
+namespace godot {
+    class GodotNetworking : public Object {
+        GODOT_CLASS(GodotNetworking, Object)
+
+    private:
+    public:
+        static void _register_methods();
+
+        GodotNetworking();
+        ~GodotNetworking();
+
+        void _init(); // our initializer called by Godot
+
+        bool _is_connected() const;
+        bool _connect_to_server(String dest);
+    };
+}
+
+#endif
+
