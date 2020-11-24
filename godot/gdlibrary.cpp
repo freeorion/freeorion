ce954015785b4a2f619fd891c2caefff4440bf03
O01eg <o01eg@yandex.ru>
2020-11-24 20:08:25 +0300

    Get JNIEnv to get access to Android directories

--- godot/gdlibrary.cpp
+++ godot/gdlibrary.cpp
@@ -6,6 +6,17 @@
 
 extern "C" void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *o) {
     godot::Godot::gdnative_init(o);
+
+
+    // ToDo: use future OS::get_native_handle instead
+#if defined(FREEORION_ANDROID)
+    for (unsigned int i = 0; i < o->api_struct->num_extensions; i++) {
+        if (o->api_struct->extensions[i]->type == GDNATIVE_EXT_ANDROID) {
+            godot::GDFreeOrion::set_android_api_struct(reinterpret_cast<const godot_gdnative_ext_android_api_struct *>(o->api_struct->extensions[i]));
+            break;
+        }
+    }
+#endif
 }
 
 extern "C" void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_options *o) {
