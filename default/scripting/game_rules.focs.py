9c0ee46f8700bcc2f593b774d649b759463d3413
O01eg <o01eg@yandex.ru>
2020-12-29 20:03:41 +0300

    Add game rules python script.
    Check format of game fules python script.
    Test parsing.

---
+++ default/scripting/game_rules.focs.py
@@ -0,0 +1,134 @@
+GameRule(
+    name="RULE_SHIP_HULL_COST_FACTOR",
+    description="RULE_SHIP_HULL_COST_FACTOR_DESC",
+    category="BALANCE",
+    type=float,
+    default=1.0,
+    min=0.1,
+    max=10.0)
+
+GameRule(
+    name="RULE_SHIP_PART_COST_FACTOR",
+    description="RULE_SHIP_PART_COST_FACTOR_DESC",
+    category="BALANCE",
+    type=float,
+    default=1.0,
+    min=0.1,
+    max=10.0)
+
+GameRule(
+    name="RULE_TECH_COST_FACTOR",
+    description="RULE_TECH_COST_FACTOR_DESC",
+    category="BALANCE",
+    type=float,
+    default=2.0,
+    min=0.1,
+    max=10.0)
+
+GameRule(
+    name="RULE_BUILDING_COST_FACTOR",
+    description="RULE_BUILDING_COST_FACTOR_DESC",
+    category="BALANCE",
+    type=float,
+    default=1.0,
+    min=0.1,
+    max=10.0)
+
+GameRule(
+    name="RULE_ENABLE_EXPERIMENTORS",
+    description="RULE_ENABLE_EXPERIMENTORS_DESC",
+    category="CONTENT",
+    type=bool,
+    default=True)
+
+GameRule(
+    name="RULE_ENABLE_SUPER_TESTER",
+    description="RULE_ENABLE_SUPER_TESTER_DESC",
+    category="CONTENT",
+    type=bool,
+    default=True)
+
+GameRule(
+    name="RULE_TEST_STRING",
+    description="RULE_TEST_STRING_DESC",
+    category="TEST",
+    type=str,
+    default="PLAYER",
+    allowed=["MODERATOR", "OBSERVER", "PLAYER", "AI_PLAYER"])
+
+GameRule(
+    name="RULE_HABITABLE_SIZE_TINY",
+    description="RULE_HABITABLE_SIZE_DESC",
+    category="PLANET_SIZE",
+    type=int,
+    default=1,
+    min=0,
+    max=999)
+
+GameRule(
+    name="RULE_HABITABLE_SIZE_SMALL",
+    description="RULE_HABITABLE_SIZE_DESC",
+    category="PLANET_SIZE",
+    type=int,
+    default=2,
+    min=0,
+    max=999)
+
+GameRule(
+    name="RULE_HABITABLE_SIZE_MEDIUM",
+    description="RULE_HABITABLE_SIZE_DESC",
+    category="PLANET_SIZE",
+    type=int,
+    default=3,
+    min=0,
+    max=999)
+
+GameRule(
+    name="RULE_HABITABLE_SIZE_LARGE",
+    description="RULE_HABITABLE_SIZE_DESC",
+    category="PLANET_SIZE",
+    type=int,
+    default=4,
+    min=0,
+    max=999)
+
+GameRule(
+    name="RULE_HABITABLE_SIZE_HUGE",
+    description="RULE_HABITABLE_SIZE_DESC",
+    category="PLANET_SIZE",
+    type=int,
+    default=5,
+    min=0,
+    max=999)
+
+GameRule(
+    name="RULE_HABITABLE_SIZE_ASTEROIDS",
+    description="RULE_HABITABLE_SIZE_DESC",
+    category="PLANET_SIZE",
+    type=int,
+    default=3,
+    min=0,
+    max=999)
+
+GameRule(
+    name="RULE_HABITABLE_SIZE_GASGIANT",
+    description="RULE_HABITABLE_SIZE_DESC",
+    category="PLANET_SIZE",
+    type=int,
+    default=6,
+    min=0,
+    max=999)
+
+GameRule(
+    name="RULE_SHIP_PART_BASED_UPKEEP",
+    description="RULE_SHIP_PART_BASED_UPKEEP_DESC",
+    category="BALANCE",
+    type=bool,
+    default=False)
+
+GameRule(
+    name="RULE_ENABLE_ALLIED_REPAIR",
+    description="RULE_ENABLE_ALLIED_REPAIR_DESC",
+    category="MULTIPLAYER",
+    type=bool,
+    default=False)
--- test/parse/TestPythonParser.cpp
+++ test/parse/TestPythonParser.cpp
@@ -1,7 +1,9 @@
 #include <boost/test/unit_test.hpp>
 
+#include "parse/Parse.h"
 #include "parse/PythonParser.h"
 #include "util/Directories.h"
+#include "util/GameRules.h"
 #include "util/PythonCommon.h"
 
 #include "ParserAppFixture.h"
@@ -10,6 +12,11 @@ BOOST_FIXTURE_TEST_SUITE(TestPythonParser, ParserAppFixture)
 
 BOOST_AUTO_TEST_CASE(parse0) {
     PythonParser parser(m_python);
+
+    GameRules game_rules = parse::game_rules("default/scripting/game_rules.focs.txt");
+    BOOST_REQUIRE(!game_rules.Empty());
+    BOOST_REQUIRE(game_rules.RuleExists("RULE_HABITABLE_SIZE_MEDIUM"));
+    BOOST_REQUIRE(GameRules::Type::TOGGLE == game_rules.GetType("RULE_ENABLE_ALLIED_REPAIR"));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
