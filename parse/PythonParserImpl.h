919a125e9edcd2abe12a09ed69c39e4fac5c5834
O01eg <o01eg@yandex.ru>
2020-12-30 01:24:32 +0300

    Show python parsing speed up.

--- parse/PythonParserImpl.h
+++ parse/PythonParserImpl.h
@@ -8,7 +8,7 @@ namespace py_parse { namespace detail {
 
     template <typename Grammar, typename Arg1>
     bool parse_file(const PythonParser& parser, const boost::filesystem::path& path, Arg1& arg1) {
-        ScopedTimer timer("parse_file \"" + path.filename().string()  + "\"", std::chrono::milliseconds(10));
+        ScopedTimer timer("parse_file \"" + path.filename().string()  + "\"", std::chrono::milliseconds(1));
 
         std::string filename;
         std::string file_contents;
@@ -20,7 +20,7 @@ namespace py_parse { namespace detail {
 
     template <typename Grammar, typename Arg1, typename Arg2>
     bool parse_file(const PythonParser& parser, const boost::filesystem::path& path, Arg1& arg1, Arg2& arg2) {
-        ScopedTimer timer("parse_file \"" + path.filename().string()  + "\"", std::chrono::milliseconds(10));
+        ScopedTimer timer("parse_file \"" + path.filename().string()  + "\"", std::chrono::milliseconds(1));
 
         std::string filename;
         std::string file_contents;
