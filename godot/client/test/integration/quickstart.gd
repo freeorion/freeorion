c3e8031a796d2401febf7a2ed1c07337398410af
O01eg <o01eg@yandex.ru>
2020-12-30 19:45:35 +0300

    Update GUT to 7.1.0 to fix GDNative support

--- godot/client/test/integration/quickstart.gd
+++ godot/client/test/integration/quickstart.gd
@@ -1,5 +1,14 @@
 extends "res://addons/gut/test.gd"
 
+var fo = preload("res://gdcpptest.gdns")
 
 func test_quickstart():
-    pass
+    assert_not_null(fo)
+    assert_true(is_instance_valid(fo))
+    
+    assert_not_null(fo.library)
+    assert_true(is_instance_valid(fo.library))
+    
+    var freeorion = fo.new()
+    assert_not_null(freeorion)
+    assert_true(is_instance_valid(freeorion))
