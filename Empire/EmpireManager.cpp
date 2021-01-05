c7b65799c1b5d20e0cf2951db63d2234489db465
geoffthemedio <geoffthemedio@gmail.com>
2021-01-05 12:46:03 +0100

    -added getters to EmpireManager for containers of Empire pointers -added getter for DiploStatusMap to EmpireManager

--- Empire/EmpireManager.cpp
+++ Empire/EmpireManager.cpp
@@ -30,6 +30,9 @@ EmpireManager& EmpireManager::operator=(EmpireManager&& other) noexcept {
 EmpireManager::~EmpireManager()
 {}
 
+const EmpireManager::const_container_type& EmpireManager::GetEmpires() const
+{ return m_const_empire_map; }
+
 std::shared_ptr<const Empire> EmpireManager::GetEmpire(int id) const {
     auto it = m_const_empire_map.find(id);
     return it == m_const_empire_map.end() ? nullptr : it->second;
@@ -94,6 +97,9 @@ std::string EmpireManager::DumpDiplomacy() const {
     return retval;
 }
 
+const EmpireManager::container_type& EmpireManager::GetEmpires()
+{ return m_empire_map; }
+
 std::shared_ptr<Empire> EmpireManager::GetEmpire(int id) {
     iterator it = m_empire_map.find(id);
     return it == end() ? nullptr : it->second;
@@ -147,6 +153,9 @@ void EmpireManager::Clear() {
     m_empire_map.clear();
     m_empire_diplomatic_statuses.clear();
 }
+
+const EmpireManager::DiploStatusMap& EmpireManager::GetDiplomaticStatuses() const
+{ return m_empire_diplomatic_statuses; }
 
 DiplomaticStatus EmpireManager::GetDiplomaticStatus(int empire1, int empire2) const {
     if (empire1 == ALL_EMPIRES || empire2 == ALL_EMPIRES || empire1 == empire2)
--- Empire/EmpireManager.h
+++ Empire/EmpireManager.h
@@ -24,10 +24,13 @@ public:
     using const_container_type = std::map<int, std::shared_ptr<const Empire>>;
     using const_iterator = const_container_type::const_iterator;
 
+    using DiploStatusMap = std::map<std::pair<int, int>, DiplomaticStatus>;
+
     EmpireManager() = default;
     EmpireManager& operator=(EmpireManager&& other) noexcept;
     virtual ~EmpireManager();
 
+    const const_container_type&           GetEmpires() const;
     std::shared_ptr<const Empire>         GetEmpire(int id) const;  //!< Returns the empire whose ID is \a id, or nullptr if none exist
     const std::string&                    GetEmpireName(int id) const;
     std::shared_ptr<const UniverseObject> GetSource(int id) const;  //!< Return the empire source or nullptr if the empire or source doesn't exist
@@ -35,6 +38,7 @@ public:
     int                         NumEmpires() const;
     int                         NumEliminatedEmpires() const;
 
+    const DiploStatusMap&       GetDiplomaticStatuses() const;
     DiplomaticStatus            GetDiplomaticStatus(int empire1, int empire2) const;
     std::set<int>               GetEmpireIDsWithDiplomaticStatusWithEmpire(int empire_id,
                                                                            DiplomaticStatus diplo_status) const;
@@ -44,6 +48,7 @@ public:
     std::string                 Dump() const;
 
     std::shared_ptr<Empire>     GetEmpire(int id);  //!< Returns the empire whose ID is \a id, or nullptr if none exist
+    const container_type&       GetEmpires();
 
     const_iterator begin() const;
     const_iterator end() const;
@@ -84,7 +89,7 @@ private:
 
     container_type                                   m_empire_map;
     const_container_type                             m_const_empire_map;
-    std::map<std::pair<int, int>, DiplomaticStatus>  m_empire_diplomatic_statuses;
+    DiploStatusMap                                   m_empire_diplomatic_statuses;
     std::map<std::pair<int, int>, DiplomaticMessage> m_diplomatic_messages;
 
     friend class ClientApp;
