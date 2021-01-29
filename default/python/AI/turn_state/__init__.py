d68a86a7b9bf255f9c95ef094cc22c38512df1ca
Andrey <Cjkjvfnby@gmail.com>
2021-01-29 19:40:32 +0300

    Update module documentation

--- default/python/AI/turn_state/__init__.py
+++ default/python/AI/turn_state/__init__.py
@@ -1,22 +1,30 @@
 """
 This module contains tools to work with pre-aggregated information about AI state.
-This information is not stored in the save game and recalculated each turn.
+This information is not stored in the save game, so it recalculated each turn.
 
-The main purpose of this module is to separate code that aggregates information and code that makes a decision.
-Decision-making code is smart and this is the dumb module,
-that means that we try to make this module API as comfortable as possible.
+The main purpose of this module is to separate code that aggregates information and decision-making code.
+Decision-making code is smart, and this is the dumb module,
+which means that we will try to make this module API to cover all needs of decision-making code, in order to reduce extra manipulation.
+If getting information requires more than one line it might be a candidate to a function in this module.
 
 Module API:
 Module API is represented as several functions.
-All public functions just lazy objects.
-They initialized with the first request and cache its value till the end of the turn.
+All public functions are just lazy objects.
+They are initialized upon the first request each turn, and the value is then cached until the end of that turn.
 
 Avoiding classes that are exposed to outer scope requires some additional calls inside functions,
  but this price is not too high to get a simple API.
 
-The module is split into submodules by the idea that each submodule works with the same set of data.
-This allows to achieve the following:
-- Each submodule is relatively small, so it is easy to understand and easy to extended;
+The module is split into sub-modules, based on the idea that each submodule should work with the same set of data.
+This allows achieving the following: each submodule is relatively small and is therefore easy to understand and extend.
+
+What should be put to the module:
+If your code works with freeOrionAIInterface only and data can be queried each time, this code is a candidate for this module.
+
+How to organize data inside that module:
+Simple rules: keep files small and dependencies between sub-modules under control.
+If you don't sure which sub-modules should be chosen - consider creating a new one.
+If your code adds new sub-modules dependency to existing sub-modules - consider creating a new one.
 """
 
 from turn_state._dry_dock_state import get_empire_drydocks
