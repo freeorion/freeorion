6ce6d76dc39b2516761d3b360a4e9724a8cc3d09
Andrey <Cjkjvfnby@gmail.com>
2021-02-06 16:02:43 +0300

    Revert queue removal

---
+++ Empire/InfluenceQueue.cpp
@@ -0,0 +1,170 @@
+#include "InfluenceQueue.h"
+
+#include "Empire.h"
+#include "Government.h"
+#include "../universe/ValueRef.h"
+#include "../util/AppInterface.h"
+#include "../util/GameRules.h"
+#include "../util/ScopedTimer.h"
+
+#include <boost/range/numeric.hpp>
+#include <boost/range/adaptor/map.hpp>
+
+
+namespace {
+    //const float EPSILON = 0.01f;
+
+    //void AddRules(GameRules& rules)
+    //{}
+    //bool temp_bool = RegisterGameRules(&AddRules);
+
+    //float CalculateNewInfluenceStockpile(int empire_id, float starting_stockpile, float project_transfer_to_stockpile,
+    //                                     float available_IP, float allocated_IP, float allocated_stockpile_IP)
+    //{
+    //    TraceLogger() << "CalculateNewInfluenceStockpile for empire " << empire_id;
+    //    const Empire* empire = GetEmpire(empire_id);
+    //    if (!empire) {
+    //        ErrorLogger() << "CalculateNewInfluenceStockpile() passed null empire.  doing nothing.";
+    //        return 0.0f;
+    //    }
+    //    TraceLogger() << " ... stockpile used: " << allocated_stockpile_IP;
+    //    float new_contributions = available_IP - allocated_IP;
+    //    return starting_stockpile + new_contributions + project_transfer_to_stockpile - allocated_stockpile_IP;
+    //}
+
+    const InfluenceQueue::Element EMPTY_ELEMENT;
+
+    /** Sets the allocated_IP value for each Element in the passed
+      * InfluenceQueue \a queue. Elements are allocated IP based on their need,
+      * the limits they can be given per turn, and the amount available to the
+      * empire. Also checks if elements will be completed this turn. */
+    //void SetInfluenceQueueElementSpending(
+    //    float available_IP, float available_stockpile,
+    //    InfluenceQueue::QueueType& queue,
+    //    float& allocated_IP, float& allocated_stockpile_IP,
+    //    int& projects_in_progress, bool simulating)
+    //{
+    //    projects_in_progress = 0;
+    //    allocated_IP = 0.0f;
+    //    allocated_stockpile_IP = 0.0f;
+
+    //    float dummy_IP_source = 0.0f;
+    //    float stockpile_transfer = 0.0f;
+
+    //    //DebugLogger() << "queue size: " << queue.size();
+    //    int i = 0;
+    //    for (auto& queue_element : queue) {
+    //        queue_element.allocated_ip = 0.0f;  // default, to be updated below...
+    //        if (queue_element.paused) {
+    //            TraceLogger() << "allocation: " << queue_element.allocated_ip
+    //                          << "  to: " << queue_element.name
+    //                          << "  due to it being paused";
+    //            ++i;
+    //            continue;
+    //        }
+
+    //        ++i;
+    //    }
+    //}
+}
+
+
+/////////////////////////////
+// InfluenceQueue::Element //
+/////////////////////////////
+std::string InfluenceQueue::Element::Dump() const {
+    std::stringstream retval;
+    retval << "InfluenceQueue::Element: name: " << name << "  empire id: " << empire_id;
+    retval << "  allocated: " << allocated_ip;
+    if (paused)
+        retval << "  (paused)";
+    retval << "\n";
+    return retval.str();
+}
+
+
+////////////////////
+// InfluenceQueue //
+////////////////////
+bool InfluenceQueue::InQueue(const std::string& name) const {
+    return std::any_of(m_queue.begin(), m_queue.end(),
+                       [name](const Element& e){ return e.name == name; });
+}
+
+float InfluenceQueue::AllocatedStockpileIP() const
+{ return 0.0f; } // todo
+
+InfluenceQueue::const_iterator InfluenceQueue::find(const std::string& item_name) const
+{ return std::find_if(begin(), end(), [&](const auto& e) { return e.name == item_name; }); }
+
+const InfluenceQueue::Element& InfluenceQueue::operator[](std::size_t i) const {
+    if (i >= m_queue.size())
+        return EMPTY_ELEMENT;
+    return m_queue[i];
+}
+
+const InfluenceQueue::Element& InfluenceQueue::operator[](int i) const
+{ return operator[](static_cast<std::size_t>(i)); }
+
+void InfluenceQueue::Update(const ObjectMap& objects) {
+    const Empire* empire = GetEmpire(m_empire_id);
+    if (!empire) {
+        ErrorLogger() << "InfluenceQueue::Update passed null empire.  doing nothing.";
+        m_projects_in_progress = 0;
+        return;
+    }
+
+    ScopedTimer update_timer("InfluenceQueue::Update");
+
+    float available_IP = empire->ResourceOutput(ResourceType::RE_INFLUENCE);
+    float stockpiled_IP = empire->ResourceStockpile(ResourceType::RE_INFLUENCE);
+
+    float spending_on_policy_adoption_ip = 0.0f;
+    for (auto policy_turn : empire->TurnsPoliciesAdopted()) {
+        if (policy_turn.second != CurrentTurn())
+            continue;
+        auto policy = GetPolicy(policy_turn.first);
+        if (!policy) {
+            ErrorLogger() << "InfluenceQueue::Update couldn't get policy supposedly adopted this turn: " << policy_turn.first;
+            continue;
+        }
+        spending_on_policy_adoption_ip += policy->AdoptionCost(m_empire_id, objects);
+    }
+
+    m_total_IPs_spent = spending_on_policy_adoption_ip;
+
+    m_expected_new_stockpile_amount = stockpiled_IP + available_IP - m_total_IPs_spent;
+
+    DebugLogger() << "InfluenceQueue::Update : available IP: " << available_IP << "  stockpiled: "
+                  << stockpiled_IP << "  new expected: " << m_expected_new_stockpile_amount << "\n";
+
+    //// cache Influence item costs and times
+    //// initialize Influence queue item completion status to 'never'
+    //boost::posix_time::ptime sim_time_start;
+    //boost::posix_time::ptime sim_time_end;
+
+    //DebugLogger() << "InfluenceQueue::Update: Projections took "
+    //              << ((sim_time_end - sim_time_start).total_microseconds()) << " microseconds with "
+    //              << empire->ResourceOutput(ResourceType::RE_INFLUENCE) << " influence output";
+    InfluenceQueueChangedSignal();
+}
+
+void InfluenceQueue::erase(int i) {
+    if (i > 0 && i < static_cast<int>(m_queue.size()))
+        m_queue.erase(begin() + i);
+}
+
+InfluenceQueue::iterator InfluenceQueue::find(const std::string& item_name)
+{ return std::find_if(begin(), end(), [&](const auto& e) { return e.name == item_name; }); }
+
+InfluenceQueue::Element& InfluenceQueue::operator[](int i) {
+    assert(0 <= i && i < static_cast<int>(m_queue.size()));
+    return m_queue[i];
+}
+
+void InfluenceQueue::clear() {
+    m_queue.clear();
+    m_projects_in_progress = 0;
+    InfluenceQueueChangedSignal();
+}
+
---
+++ Empire/InfluenceQueue.h
@@ -0,0 +1,114 @@
+#ifndef _InfluenceQueue_h_
+#define _InfluenceQueue_h_
+
+#include "../util/AppInterface.h"
+#include "../util/Export.h"
+
+#include <deque>
+#include <map>
+#include <memory>
+#include <set>
+#include <string>
+#include <boost/serialization/access.hpp>
+#include <boost/signals2/signal.hpp>
+
+class ResourcePool;
+FO_COMMON_API extern const int INVALID_DESIGN_ID;
+FO_COMMON_API extern const int ALL_EMPIRES;
+
+
+struct FO_COMMON_API InfluenceQueue {
+    /** The type of a single element in the Influence queue. */
+    struct FO_COMMON_API Element {
+        explicit Element() = default;
+        Element(int empire_id_, std::string name_, bool paused_ = false) :
+            name(std::move(name_)),
+            empire_id(empire_id_),
+            paused(paused_)
+        {}
+
+        std::string     name;                       ///< name of influence project
+        int             empire_id = ALL_EMPIRES;
+        float           allocated_ip = 0.0f;        ///< IP allocated to this InfluenceQueue Element by Empire Influence update
+        bool            paused = false;
+
+        std::string Dump() const;
+
+    private:
+        friend class boost::serialization::access;
+        template <class Archive>
+        void serialize(Archive& ar, const unsigned int version);
+    };
+
+    typedef std::deque<Element> QueueType;
+
+    /** The InfluenceQueue iterator type.  Dereference yields a Element. */
+    typedef QueueType::iterator iterator;
+    /** The const InfluenceQueue iterator type.  Dereference yields a Element. */
+    typedef QueueType::const_iterator const_iterator;
+
+    explicit InfluenceQueue(int empire_id) :
+        m_empire_id(empire_id)
+    {}
+
+    bool    InQueue(const std::string& name) const;
+
+    int     ProjectsInProgress() const { return m_projects_in_progress; }
+    float   TotalIPsSpent() const { return m_total_IPs_spent; };
+    int     EmpireID() const { return m_empire_id; }
+
+    /** Returns amount of stockpile IP allocated to Influence queue elements. */
+    float AllocatedStockpileIP() const;
+
+    /** Returns the value expected for the Influence Stockpile for the next
+      * turn, based on the current InfluenceQueue allocations. */
+    float ExpectedNewStockpileAmount() const { return m_expected_new_stockpile_amount; }
+
+
+    // STL container-like interface
+    bool            empty() const { return m_queue.empty(); }
+    unsigned int    size() const { return m_queue.size(); }
+    const_iterator  begin() const { return m_queue.begin(); }
+    const_iterator  end() const { return m_queue.end(); }
+    const_iterator  find(const std::string& item_name) const;
+    const Element&  operator[](std::size_t i) const;
+    const Element&  operator[](int i) const;
+
+
+    /** Recalculates the PPs spent on and number of turns left for each project in the queue.  Also
+      * determines the number of projects in progress, and the industry consumed by projects
+      * in each resource-sharing group of systems.  Does not actually "spend" the PP; a later call to
+      * empire->CheckInfluenceProgress() will actually spend PP, remove items from queue and create them
+      * in the universe. */
+    void Update(const ObjectMap& objects = Objects());
+
+
+    // STL container-like interface
+    void        push_back(const Element& element) { m_queue.push_back(element); }
+    void        insert(iterator it, const Element& element) { m_queue.insert(it, element); }
+    void        erase(int i);
+    iterator    erase(iterator it) { return m_queue.erase(it); }
+
+    iterator    begin() { return m_queue.begin(); }
+    iterator    end() { return m_queue.end(); }
+    iterator    find(const std::string& item_name);
+    Element&    operator[](int i);
+
+    void        clear();
+
+    mutable boost::signals2::signal<void ()> InfluenceQueueChangedSignal;
+
+private:
+    QueueType   m_queue;
+    int         m_projects_in_progress = 0;
+    float       m_total_IPs_spent = 0.0f;
+    float       m_expected_new_stockpile_amount = 0.0f;
+    int         m_empire_id = ALL_EMPIRES;
+
+    friend class boost::serialization::access;
+    template <class Archive>
+    void serialize(Archive& ar, const unsigned int version);
+};
+
+
+#endif
