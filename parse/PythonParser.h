e63355199cfb20a5885c382ad6fa08266917c447
O01eg <o01eg@yandex.ru>
2021-03-04 12:38:07 +0300

    Make python parser unmoveble and uncopyable

--- parse/PythonParser.h
+++ parse/PythonParser.h
@@ -9,6 +9,14 @@ class FO_PARSE_API PythonParser {
 public:
     PythonParser(PythonCommon& _python);
     ~PythonParser();
+
+    PythonParser(const PythonParser&) = delete;
+
+    PythonParser(PythonParser&&) = delete;
+
+    const PythonParser& operator=(const PythonParser&) = delete;
+
+    PythonParser& operator=(PythonParser&&) = delete;
 private:
     PythonCommon& python;
 };
