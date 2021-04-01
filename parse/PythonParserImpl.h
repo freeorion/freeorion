278ec0f8048a7c9c6a97389226226f39e0774062
O01eg <o01eg@yandex.ru>
2021-04-01 14:42:20 +0300

    Refactor grammar usages

--- parse/PythonParserImpl.h
+++ parse/PythonParserImpl.h
@@ -10,25 +10,21 @@
 namespace py_parse { namespace detail {
 
     template <typename Grammar, typename Arg1>
-    bool parse_file(const PythonParser& parser, const boost::filesystem::path& path, Arg1& arg1) {
+    bool parse_file(const PythonParser& parser, const boost::filesystem::path& path, const Grammar& grammar, Arg1& arg1) {
         ScopedTimer timer("parse_file \"" + path.filename().string()  + "\"", std::chrono::milliseconds(1));
 
         std::string filename;
         std::string file_contents;
-
-        Grammar grammar(parser);
 
         return parser.ParseFileCommon(path, grammar(arg1), filename, file_contents);
     }
 
     template <typename Grammar, typename Arg1, typename Arg2>
-    bool parse_file(const PythonParser& parser, const boost::filesystem::path& path, Arg1& arg1, Arg2& arg2) {
+    bool parse_file(const PythonParser& parser, const boost::filesystem::path& path, const Grammar& grammar, Arg1& arg1, Arg2& arg2) {
         ScopedTimer timer("parse_file \"" + path.filename().string()  + "\"", std::chrono::milliseconds(1));
 
         std::string filename;
         std::string file_contents;
-
-        Grammar grammar(parser);
 
         return parser.ParseFileCommon(path, grammar(arg1, arg2), filename, file_contents);
     }
