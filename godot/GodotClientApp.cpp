236469ea3caeb051318f4afbe74e4bfca7eb3f9e
O01eg <o01eg@yandex.ru>
2021-04-14 21:07:38 +0300

    Fix update of background parsing

--- godot/GodotClientApp.cpp
+++ godot/GodotClientApp.cpp
@@ -80,7 +80,15 @@ GodotClientApp::GodotClientApp() {
 
     SetStringtableDependentOptionDefaults();
 
-    StartBackgroundParsing();
+    // Start parsing content
+    std::promise<void> barrier;
+    std::future<void> barrier_future = barrier.get_future();
+    std::thread background([this] (auto b) {
+        DebugLogger() << "Started background parser thread";
+        StartBackgroundParsing(std::move(b));
+    }, std::move(barrier));
+    background.detach();
+    barrier_future.wait();
 }
 
 GodotClientApp::~GodotClientApp() {
