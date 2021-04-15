7db29e582ff0beac29cd5e26a4387f1b1ef3e414
O01eg <o01eg@yandex.ru>
2021-04-15 08:39:49 +0300

    Attach dialogs to popup just before show

--- godot/client/FreeOrionClient.gd
+++ godot/client/FreeOrionClient.gd
@@ -9,7 +9,6 @@ var thread: Thread
 # Called when the node enters the scene tree for the first time.
 func _ready():
     game_setup_dlg = preload("res://GameSetup.tscn").instance()
-    $Popup.add_child(game_setup_dlg)
     game_setup_dlg.connect("ok", self, "_on_GameSetupDlg_ok")
     game_setup_dlg.connect("cancel", self, "_on_GameSetupDlg_cancel")
     
@@ -31,6 +30,7 @@ func _on_QuitBtn_pressed():
 
 
 func _on_SinglePlayerBtn_pressed():
+    $Popup.add_child(game_setup_dlg)
     var pos_x = ($Popup.get_rect().size.x - game_setup_dlg.get_rect().size.x) / 2
     var pos_y = ($Popup.get_rect().size.y - game_setup_dlg.get_rect().size.y) / 2
     game_setup_dlg.set_position(Vector2(pos_x, pos_y))
