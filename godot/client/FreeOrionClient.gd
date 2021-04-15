b6db7c891610a1a68189365ee9b5c51d1d5e580e
O01eg <o01eg@yandex.ru>
2021-04-16 00:04:13 +0300

    Try to scale UI on mobile devices

--- godot/client/FreeOrionClient.gd
+++ godot/client/FreeOrionClient.gd
@@ -1,6 +1,8 @@
 extends Control
 
 
+onready var viewport = get_viewport()
+
 var game_setup_dlg: FOWindow
 var multiplayer_setup_dlg: FOWindow
 var auth_password_setup_dlg: FOWindow
@@ -31,6 +33,26 @@ func _ready():
     print("Systems from optionsDB ", systems)
     network_thread = Thread.new()
     network_thread.start($Node, "network_thread")
+    
+    var scale = 1
+    if OS.get_name() == "Android":
+        scale = 1.5
+    
+    var minimum_size = Vector2(1600, 900) / scale
+    
+    var current_size = OS.get_window_size()
+    
+    var scale_factor = minimum_size.y/current_size.y
+    var new_size = Vector2(current_size.x*scale_factor, minimum_size.y)
+
+    if new_size.y < minimum_size.y:
+        scale_factor = minimum_size.y/new_size.y
+        new_size = Vector2(new_size.x*scale_factor, minimum_size.y)
+    if new_size.x < minimum_size.x:
+        scale_factor = minimum_size.x/new_size.x
+        new_size = Vector2(minimum_size.x, new_size.y*scale_factor)
+
+    viewport.set_size_override(true, new_size)
 
 func _exit_tree():
     network_thread.wait_to_finish()
