65b56263604101560a51c7c0a1d2deb680d40961
O01eg <o01eg@yandex.ru>
2021-04-15 10:18:34 +0300

    Add dialog window to connect to different servers

---
+++ godot/client/MultiplayerSetup.gd
@@ -0,0 +1,39 @@
+extends FOWindow
+
+
+signal ok
+signal cancel
+
+
+
+var player_name: String
+var client_type: int
+var server_name: String
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    randomize()
+
+func _on_CloseWidget_pressed():
+    emit_signal("cancel")
+
+func _on_RefreshBtn_pressed():
+    pass
+
+func _on_OKBtn_pressed():
+    player_name = $LeftContainer/PlayerName/Label.text
+    client_type = $LeftContainer/JoinGame/ClientType.get_selected_id()
+    if $LeftContainer/JoinGame/CheckButton.pressed:
+        server_name = $LeftContainer/ServerName.text
+    else:
+        server_name = "HOST GAME SELECTED"
+    emit_signal("ok")
+
+
+func _on_CancelBtn_pressed():
+    emit_signal("cancel")
+
+
+func _on_JoinCheckButton_toggled(button_pressed: bool):
+    $LeftContainer/JoinGame/ClientType.disabled = !button_pressed
