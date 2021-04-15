624b6122b0d0131472e8a4016f94ca637a97485d
O01eg <o01eg@yandex.ru>
2021-04-15 10:18:34 +0300

    Add dialog window to connect to different servers

--- godot/client/FreeOrionClient.gd
+++ godot/client/FreeOrionClient.gd
@@ -1,7 +1,8 @@
 extends Control
 
 
-var game_setup_dlg: Control
+var game_setup_dlg: FOWindow
+var multiplayer_setup_dlg: FOWindow
 var pings: int = 0
 var network_thread: Thread
 
@@ -11,7 +12,11 @@ func _ready():
     game_setup_dlg = preload("res://GameSetup.tscn").instance()
     game_setup_dlg.connect("ok", self, "_on_GameSetupDlg_ok")
     game_setup_dlg.connect("cancel", self, "_on_GameSetupDlg_cancel")
-    
+
+    multiplayer_setup_dlg = preload("res://MultiplayerSetup.tscn").instance()
+    multiplayer_setup_dlg.connect("ok", self, "_on_MultiplayerSetupDlg_ok")
+    multiplayer_setup_dlg.connect("cancel", self, "_on_MultiplayerSetupDlg_cancel")
+
     $Node.connect("ping", self, "_on_Node_ping")
     $Node.connect("auth_request", self, "_on_Node_auth_request")
     $Node.connect("empire_status", self, "_on_Node_empire_status")
@@ -47,6 +52,23 @@ func _on_GameSetupDlg_ok():
 func _on_GameSetupDlg_cancel():
     $Popup.hide()
 
+func _on_MultiplayerSetupDlg_ok():
+    $Popup.hide()
+
+    var connected: bool = $Node.networking._is_connected()
+    if connected:
+        print("Already connected")
+    else:
+        print("Connecting to ", multiplayer_setup_dlg.server_name)
+        connected = $Node.networking._connect_to_server(multiplayer_setup_dlg.server_name, multiplayer_setup_dlg.player_name)
+        if not connected:
+            print("Cannot connect")
+        else:
+            pass
+
+
+func _on_MultiplayerSetupDlg_cancel():
+    $Popup.hide()
 
 func _on_Node_ping(message: String):
     pings += 1
@@ -64,12 +86,8 @@ func _on_Node_start_game(is_new_game: bool):
     get_tree().change_scene("res://GalaxyMap.tscn")
 
 func _on_MultiplayerBtn_pressed():
-    var connected: bool = $Node.networking._is_connected()
-    if connected:
-        print("Already connected")
-    else:
-        connected = $Node.networking._connect_to_server("localhost", "GodotPlayer")
-        if not connected:
-            print("Cannot connect")
-        else:
-            pass
+    $Popup.add_child(multiplayer_setup_dlg)
+    var pos_x = ($Popup.get_rect().size.x - multiplayer_setup_dlg.get_rect().size.x) / 2
+    var pos_y = ($Popup.get_rect().size.y - multiplayer_setup_dlg.get_rect().size.y) / 2
+    multiplayer_setup_dlg.set_position(Vector2(pos_x, pos_y))
+    $Popup.popup()
