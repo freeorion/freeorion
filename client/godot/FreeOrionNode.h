4c603bbf1ed114b4b99d32cfdc2be1cc834115e0
O01eg <o01eg@yandex.ru>
2021-04-23 08:38:30 +0300

    Add documentation for class

--- client/godot/FreeOrionNode.h
+++ client/godot/FreeOrionNode.h
@@ -5,20 +5,25 @@
 #include <Node.hpp>
 #include <String.hpp>
 
+/** Node for GDNative library of FreeOrion Godot client. */
 class FreeOrionNode : public godot::Node {
     GODOT_CLASS(FreeOrionNode, godot::Node)
 
 public:
-    static void _register_methods();
+    static void _register_methods(); ///< Registers GDNative methods, properties and signals
 
     FreeOrionNode();
     ~FreeOrionNode();
 
+    /** Calls when Godot initializes Node.
+      * Should be used instead of constructor. */
     void _init();
 private:
+    /** Calls when Godot remove Node from scene at finalization.
+      * Should be used instead of destructor. */
     void _exit_tree();
 
-    godot::String get_version() const;
+    godot::String get_version() const; ///< Returns FreeOrion version
 };
 
 #endif
