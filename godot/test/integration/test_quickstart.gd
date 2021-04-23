05f13cf9cdfdb7a3ccffc78094320ad3dc03a32e
O01eg <o01eg@yandex.ru>
2021-04-23 08:15:44 +0300

    Test GDNative library

--- godot/test/integration/test_quickstart.gd
+++ godot/test/integration/test_quickstart.gd
@@ -12,8 +12,14 @@ func _quickstart() -> int:
 	assert_not_null(fo.library)
 	assert_true(is_instance_valid(fo.library))
 
+	var freeorion = add_child_autoqfree(fo.new())
+
+	assert_not_null(freeorion.get_version())
+	assert_typeof(freeorion.get_version(), TYPE_STRING)
+
 	return PASS
 
 
 func test_quickstart():
 	assert_eq(_quickstart(), PASS)
+	assert_no_new_orphans()
