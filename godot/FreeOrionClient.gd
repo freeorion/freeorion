b62f1e9ba696e875ad10ea68bae7a872664c715d
oleg <o01eg@yandex.ru>
2021-04-24 16:13:00 +0300

    Attach signals to buttons

--- godot/FreeOrionClient.gd
+++ godot/FreeOrionClient.gd
@@ -4,3 +4,19 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
+
+
+func _on_SinglePlayerBtn_pressed():
+	pass  # Replace with function body.
+
+
+func _on_QuickstartBtn_pressed():
+	pass  # Replace with function body.
+
+
+func _on_MultiplayerBtn_pressed():
+	pass  # Replace with function body.
+
+
+func _on_QuitBtn_pressed():
+	get_tree().quit()
