b3a57fbef14e2da5252d5bae37b2bfd726554270
O01eg <o01eg@yandex.ru>
2021-04-27 15:48:37 +0300

    Add single player options window

--- godot/FreeOrionClient.gd
+++ godot/FreeOrionClient.gd
@@ -7,11 +7,17 @@ var auth_password_setup_dlg: FOWindow
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	game_setup_dlg = preload("res://GameSetup.tscn").instance()
+	game_setup_dlg.connect("ok", self, "_on_GameSetupDlg_ok")
+	game_setup_dlg.connect("cancel", self, "_on_GameSetupDlg_cancel")
 
 
 func _on_SinglePlayerBtn_pressed():
-	pass  # Replace with function body.
+	$Popup.add_child(game_setup_dlg)
+	var pos_x = ($Popup.get_rect().size.x - game_setup_dlg.get_rect().size.x) / 2
+	var pos_y = ($Popup.get_rect().size.y - game_setup_dlg.get_rect().size.y) / 2
+	game_setup_dlg.set_position(Vector2(pos_x, pos_y))
+	$Popup.popup()
 
 
 func _on_QuickstartBtn_pressed():
@@ -24,3 +30,13 @@ func _on_MultiplayerBtn_pressed():
 
 func _on_QuitBtn_pressed():
 	get_tree().quit()
+
+
+func _on_GameSetupDlg_ok():
+	$Popup.hide()
+	$Popup.remove_child(game_setup_dlg)
+
+
+func _on_GameSetupDlg_cancel():
+	$Popup.hide()
+	$Popup.remove_child(game_setup_dlg)
---
+++ godot/global.gd
@@ -0,0 +1,49 @@
+extends Node
+
+#const LETTER_UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#const LETTER_LOWER = "abcdefghijklmnopqrstuvwxyz"
+#const LETTER_DIGITS = "1234567890"
+
+const GO_NONE = 0
+const GO_LOW = 1
+const GO_MEDIUM = 2
+const GO_HIGH = 3
+const GO_RANDOM = 4
+
+const GS_SPIRAL2 = 0
+const GS_SPIRAL3 = 1
+const GS_SPIRAL4 = 2
+const GS_CLUSTER = 3
+const GS_ELLIPTICAL = 4
+const GS_DISC = 5
+const GS_BOX = 6
+const GS_IRREGULAR = 7
+const GS_RING = 8
+const GS_RANDOM = 9
+
+const GA_YOUNG = 0
+const GA_MATURE = 1
+const GA_ANCIENT = 2
+const GA_RANDOM = 3
+
+const AIA_BEGINNER = 0
+const AIA_TURTLE = 1
+const AIA_CAUTIOUS = 2
+const AIA_TYPICAL = 3
+const AIA_AGGRESSIVE = 4
+const AIA_MANIACAL = 5
+
+const SP_HUMAN = 0
+const SP_LAENFA = 1
+const SP_SCYLIOR = 2
+const SP_EGASSEM = 3
+const SP_TRITH = 4
+
+#const MIN_SYS_DIST = 1
+#const MAX_STARLANE_LENGTH = 15
+
+#const INVALID_CLIENT_TYPE = -1
+#const CLIENT_TYPE_AI_PLAYER = 0
+#const CLIENT_TYPE_HUMAN_PLAYER = 1
+#const CLIENT_TYPE_HUMAN_OBSERVER = 2
+#const CLIENT_TYPE_HUMAN_MODERATOR = 3
