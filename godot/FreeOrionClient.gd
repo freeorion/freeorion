1dc27940088215799762c58a77f42f89a879209f
O01eg <o01eg@yandex.ru>
2021-04-27 16:14:40 +0300

    Scale UI for Android

--- godot/FreeOrionClient.gd
+++ godot/FreeOrionClient.gd
@@ -4,6 +4,8 @@ var game_setup_dlg: FOWindow
 var multiplayer_setup_dlg: FOWindow
 var auth_password_setup_dlg: FOWindow
 
+onready var viewport = get_viewport()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,6 +16,26 @@ func _ready():
 	multiplayer_setup_dlg = preload("res://MultiplayerSetup.tscn").instance()
 	multiplayer_setup_dlg.connect("ok", self, "_on_MultiplayerSetup_ok")
 	multiplayer_setup_dlg.connect("cancel", self, "_on_MultiplayerSetup_cancel")
+
+	var scale = 1
+	if OS.get_name() == "Android":
+		scale = 1.4
+
+	var minimum_size = Vector2(1600, 900) / scale
+
+	var current_size = OS.get_window_size()
+
+	var scale_factor = minimum_size.y / current_size.y
+	var new_size = Vector2(current_size.x * scale_factor, minimum_size.y)
+
+	if new_size.y < minimum_size.y:
+		scale_factor = minimum_size.y / new_size.y
+		new_size = Vector2(new_size.x * scale_factor, minimum_size.y)
+	if new_size.x < minimum_size.x:
+		scale_factor = minimum_size.x / new_size.x
+		new_size = Vector2(minimum_size.x, new_size.y * scale_factor)
+
+	viewport.set_size_override(true, new_size)
 
 
 func _on_SinglePlayerBtn_pressed():
