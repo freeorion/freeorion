1abb7d1474b90c6f87ce059e98f6496e0d49fb5a
Andrey <Cjkjvfnby@gmail.com>
2021-05-30 16:31:57 +0300

    Extract planet supply to a s separate file (#3470)

--- default/python/AI/colonization/__init__.py
+++ default/python/AI/colonization/__init__.py
@@ -1,1 +1,2 @@
+from colonization.planet_supply import get_planet_supply, update_planet_supply
 from colonization.rate_pilots import rate_piloting_tag, rate_planetary_piloting
---
+++ default/python/AI/colonization/planet_supply.py
@@ -0,0 +1,23 @@
+from typing import Dict
+
+
+_planet_supply_map: Dict[int, int] = {}
+
+
+def get_planet_supply(pid: int, default: int) -> int:
+    """
+    Return planet supply.
+
+    Note:
+    This method has temporal coupling with the `update_planet_supply`.
+    Supply map is persistent during session, so if you try to access it
+    before planet was updated you will get old/default value.
+    """
+    return _planet_supply_map.get(pid, default)
+
+
+def update_planet_supply(pid: int, value: int) -> None:
+    """
+    Update the planet supply.
+    """
+    _planet_supply_map[pid] = value
