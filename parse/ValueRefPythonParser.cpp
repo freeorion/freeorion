2330aecab3505901150ed712e6a4dcfae7465591
O01eg <o01eg@yandex.ru>
2021-10-19 20:13:01 +0300

    Move value refs to separate file

--- parse/ValueRefPythonParser.cpp
+++ parse/ValueRefPythonParser.cpp
@@ -2,7 +2,11 @@
 
 #include <stdexcept>
 
+#include <boost/python/extract.hpp>
+#include <boost/python/raw_function.hpp>
+
 #include "../universe/ValueRefs.h"
+#include "../universe/NamedValueRefManager.h"
 #include "../universe/Conditions.h"
 
 value_ref_wrapper<double> operator*(int lhs, const value_ref_wrapper<double>& rhs) {
@@ -64,5 +68,21 @@ condition_wrapper operator==(const value_ref_wrapper<int>& lhs, const value_ref_
             Condition::ComparisonType::EQUAL,
             ValueRef::CloneUnique(rhs.value_ref))
     );
+}
+
+
+namespace {
+    template<typename T>
+    value_ref_wrapper<T> insert_named_lookup_(const boost::python::tuple& args, const boost::python::dict& kw) {
+        auto name = boost::python::extract<std::string>(kw["name"])();
+
+        return value_ref_wrapper<T>(std::make_shared<ValueRef::NamedRef<T>>(name, true));
+    }
+}
+
+void RegisterGlobalsValueRefs(boost::python::dict& globals) {
+    globals["NamedRealLookup"] = boost::python::raw_function(insert_named_lookup_<double>);
+    globals["Value"] = value_ref_wrapper<double>(std::make_shared<ValueRef::Variable<double>>(ValueRef::ReferenceType::EFFECT_TARGET_VALUE_REFERENCE));
+    globals["CurrentTurn"] = value_ref_wrapper<int>(std::make_shared<ValueRef::Variable<int>>(ValueRef::ReferenceType::NON_OBJECT_REFERENCE, "CurrentTurn"));
 }
 
