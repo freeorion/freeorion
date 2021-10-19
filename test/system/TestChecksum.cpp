1e21ec232c419d44c962857ece0cc09147264424
O01eg <o01eg@yandex.ru>
2021-10-20 00:03:23 +0300

    Test techs checksum

--- test/system/TestChecksum.cpp
+++ test/system/TestChecksum.cpp
@@ -7,7 +7,8 @@ BOOST_FIXTURE_TEST_SUITE(TestChecksum, ClientAppFixture)
 BOOST_AUTO_TEST_CASE(compare_checksum) {
     auto checksums = CheckSumContent();
 
-    BOOST_WARN_EQUAL(checksums["NamedValueRefManager"], 1042814);
+    BOOST_WARN_EQUAL(checksums["NamedValueRefManager"], 42021);
+    BOOST_WARN_EQUAL(checksums["TechManager"], 9095847);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
