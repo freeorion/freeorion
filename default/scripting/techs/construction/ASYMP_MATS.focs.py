91f46c8d86e26cbf1158ee40d5f97027b168d26b
O01eg <o01eg@yandex.ru>
2021-10-20 21:51:18 +0300

    Convert techs/construction/ASYMP_MATS.focs

---
+++ default/scripting/techs/construction/ASYMP_MATS.focs.py
@@ -0,0 +1,12 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="CON_ASYMP_MATS",
+    description="CON_ASYMP_MATS_DESC",
+    short_description="THEORY_SHORT_DESC",
+    category="CONSTRUCTION_CATEGORY",
+    researchcost=10 * TECH_COST_MULTIPLIER,
+    researchturns=2,
+    tags=["PEDIA_CONSTRUCTION_CATEGORY", "THEORY"],
+    graphic="icons/tech/asymptotic_materials.png",
+)
