668476b2f197c4bd810bdd8f9895417027028625
O01eg <o01eg@yandex.ru>
2021-10-20 21:41:45 +0300

    Convert techs/construction/ART_HEAVEN.focs

---
+++ default/scripting/techs/construction/ART_HEAVEN.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="CON_ART_HEAVENLY",
+    description="CON_ART_HEAVENLY_DESC",
+    short_description="BUILDING_UNLOCK_SHORT_DESC",
+    category="CONSTRUCTION_CATEGORY",
+    researchcost=100 * TECH_COST_MULTIPLIER,
+    researchturns=6,
+    tags=["PEDIA_CONSTRUCTION_CATEGORY"],
+    prerequisites=["CON_CONTGRAV_ARCH", "SHP_CONTGRAV_MAINT"],
+    unlock=Item(type=Building, name="BLD_ART_MOON"),
+    graphic="icons/tech/artificial_heavenly_bodies.png",
+)
