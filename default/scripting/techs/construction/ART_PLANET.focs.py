1c7396a4b7565216297fc9fc96fd938d5853392b
O01eg <o01eg@yandex.ru>
2021-10-20 21:48:20 +0300

    Convert techs/construction/ART_PLANET.focs

---
+++ default/scripting/techs/construction/ART_PLANET.focs.py
@@ -0,0 +1,18 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="CON_ART_PLANET",
+    description="CON_ART_PLANET_DESC",
+    short_description="BUILDING_UNLOCK_SHORT_DESC",
+    category="CONSTRUCTION_CATEGORY",
+    researchcost=300 * TECH_COST_MULTIPLIER,
+    researchturns=8,
+    tags=["PEDIA_CONSTRUCTION_CATEGORY"],
+    prerequisites=["CON_ART_HEAVENLY", "LRN_SPATIAL_DISTORT_GEN"],
+    unlock=[
+        Item(type=Building, name="BLD_ART_PLANET"),
+        Item(type=Building, name="BLD_ART_FACTORY_PLANET"),
+        Item(type=Building, name="BLD_ART_PARADISE_PLANET"),
+    ],
+    graphic="icons/tech/artificial_planet.png",
+)
