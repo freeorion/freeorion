0905b2f094cc18ab1145e1cc23331d30257410b3
geoffthemedio <geoffthemedio@gmail.com>
2021-12-10 19:07:56 +0100

    remove effectively duplicated code in LinkText generators

--- UI/LinkText.cpp
+++ UI/LinkText.cpp
@@ -573,22 +573,11 @@ void TextLinker::MarkLinks() {
     SetLinkedText(std::move(marked_text));
 }
 
-std::string LinkTaggedText(std::string_view tag, std::string_view stringtable_entry) {
-    std::string retval;
-    const auto& us{UserString(stringtable_entry)};
-    retval.reserve(10 + stringtable_entry.length() + 2*tag.length() + us.length());
-    retval.append("<").append(tag).append(" ").append(stringtable_entry)
-          .append(">").append(us).append("</").append(tag).append(">");
-    return retval;
-}
+std::string LinkTaggedText(std::string_view tag, std::string_view stringtable_entry)
+{ return LinkTaggedPresetText(tag, stringtable_entry, UserString(stringtable_entry)); }
 
-std::string LinkTaggedIDText(std::string_view tag, int id, std::string_view text) {
-    std::string retval;
-    retval.reserve(22 + text.length() + 2*tag.length());
-    retval.append("<").append(tag).append(" ").append(std::to_string(id))
-          .append(">").append(text).append("</").append(tag).append(">");
-    return retval;
-}
+std::string LinkTaggedIDText(std::string_view tag, int id, std::string_view display_text)
+{ return LinkTaggedPresetText(tag, std::to_string(id), display_text); }
 
 std::string LinkTaggedPresetText(std::string_view tag, std::string_view stringtable_entry,
                                  std::string_view display_text)
