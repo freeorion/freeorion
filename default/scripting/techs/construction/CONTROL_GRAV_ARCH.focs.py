e487b6bbdc72cd0f88efe3232b11e3a5b072efc0
oleg <o01eg@yandex.ru>
2021-12-26 21:44:20 +0300

    Convert techs/construction/CONTROL_GRAV_ARCH.focs

---
+++ default/scripting/techs/construction/CONTROL_GRAV_ARCH.focs.py
@@ -0,0 +1,17 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="CON_CONTGRAV_ARCH",
+    description="CON_CONTGRAV_ARCH_DESC",
+    short_description="SUPPLY_SHORT_DESC",
+    category="CONSTRUCTION_CATEGORY",
+    researchcost=100 * TECH_COST_MULTIPLIER,
+    researchturns=4,
+    tags=["PEDIA_CONSTRUCTION_CATEGORY"],
+    prerequisites="CON_ARCH_MONOFILS",
+    effectsgroups=EffectsGroup(
+        scope=Planet() & OwnedBy(empire=Source.Owner) & Focus(type="FOCUS_LOGISTICS"),
+        effects=SetMaxSupply(value=Value + 1),
+    ),
+    graphic="icons/tech/controlled_gravity_architecture.png",
+)
