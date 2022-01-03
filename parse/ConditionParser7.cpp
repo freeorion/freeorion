0a71f9c5d9718aac92c86f84a004d6ba92df107c
geoffthemedio <geoffthemedio@gmail.com>
2022-01-04 00:25:53 +0100

    fix parser capitalization

--- parse/ConditionParser7.cpp
+++ parse/ConditionParser7.cpp
@@ -73,7 +73,7 @@ namespace parse { namespace detail {
                 tok.Building_   [ _val = Condition::ContentType::CONTENT_BUILDING ]
             |   tok.Species_    [ _val = Condition::ContentType::CONTENT_SPECIES ]
             |   tok.Hull_       [ _val = Condition::ContentType::CONTENT_SHIP_HULL ]
-            |   tok.part_       [ _val = Condition::ContentType::CONTENT_SHIP_PART ]
+            |   tok.Part_       [ _val = Condition::ContentType::CONTENT_SHIP_PART ]
             |   tok.Special_    [ _val = Condition::ContentType::CONTENT_SPECIAL ]
             |   tok.Focus_      [ _val = Condition::ContentType::CONTENT_FOCUS ];
 
