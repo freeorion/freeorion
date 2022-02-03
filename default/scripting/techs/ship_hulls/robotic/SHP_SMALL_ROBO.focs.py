8e880ca758e1ac7d0d2a0758d5245d051ecc4787
MichaelCourtney <wobbly@fredvisions.com>
2022-02-04 02:33:18 +1100

    Add Small Robotic Hull

---
+++ default/scripting/techs/ship_hulls/robotic/SHP_SMALL_ROBO.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_SMALL_ROBO",
+    description="SHP_SMALL_ROBO_DESC",
+    short_description="SHIP_HULL_UNLOCK_SHORT_DESC",
+    category="SHIP_HULLS_CATEGORY",
+    researchcost=8 * TECH_COST_MULTIPLIER,
+    researchturns=2,
+    tags=["PEDIA_ROBOTIC_HULL_TECHS"],
+    prerequisites=["SHP_MIL_ROBO_CONT", "PRO_NANOTECH_PROD"],
+    unlock=Item(type=UnlockShipHull, name="SH_SMALL_ROBOTIC"),
+    graphic="icons/ship_hulls/generic-medium-hull_small.png",
+)
