bb82da1287443cb89129388dc1dd985cb338be69
Michael Courtney <43386669+MichaelCourtney@users.noreply.github.com>
2022-02-07 05:32:00 +1100

    Flux bubble should decrease colony building time (#3690)
    * Flux bubble should decrease colony building time
    * Fix pedia cateogorization of spatial flux hulls

--- default/scripting/buildings/colonies/col_bld_gen.py
+++ default/scripting/buildings/colonies/col_bld_gen.py
@@ -183,17 +183,17 @@ t_buildtime_stat_cond_extinct = string.Template(
     """condition = And [
                 Planet
                 OwnedBy empire = Source.Owner
-            Or [
-               And [
-                   Species name = "${id}"
-                   Population low = [[MIN_RECOLONIZING_SIZE]]
-                   Happiness low = 5
+                Or [
+                   And [
+                       Species name = "${id}"
+                       Population low = [[MIN_RECOLONIZING_SIZE]]
+                       Happiness low = 5
+                    ]
+                    And [
+                        HasSpecial name = "EXTINCT_${name}_SPECIAL"
+                        Contains Building name = "BLD_XENORESURRECTION_LAB"
+                    ]
                 ]
-                And [
-                    HasSpecial name = "EXTINCT_${name}_SPECIAL"
-                    Contains Building name = "BLD_XENORESURRECTION_LAB"
-                ]
-            ]
                 ResourceSupplyConnected empire = Source.Owner condition = Target
             ]"""
 )
@@ -206,6 +206,7 @@ t_buildtime = string.Template(
         ) / (60
              + 20 * (Statistic If condition = Or [
                  And [ Source OwnerHasTech name = "SHP_MIL_ROBO_CONT" ]
+                 And [ Source OwnerHasTech name = "SHP_SPACE_FLUX_BUBBLE" ]
                  And [ Source OwnerHasTech name = "SHP_ORG_HULL" ]
                  And [ Source OwnerHasTech name = "SHP_QUANT_ENRG_MAG" ]
              ])
