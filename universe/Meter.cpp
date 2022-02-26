3494cc2bafa93b0672381fd927d81063f6a542c8
geoffthemedio <geoffthemedio@gmail.com>
2022-02-26 15:23:35 +0100

    use sprintf in buffer instead of cocatenating result of std::to_string, when std::to_chars is unavailable

--- universe/Meter.cpp
+++ universe/Meter.cpp
@@ -1,10 +1,12 @@
 #include "Meter.h"
 
 #include <algorithm>
+#include <array>
 
 #if __has_include(<charconv>)
-#include <array>
 #include <charconv>
+#else
+#include <stdio.h>
 #endif
 
 
@@ -20,13 +22,18 @@ float Meter::Initial() const
 { return m_initial_value; }
 
 std::string Meter::Dump(unsigned short ntabs) const {
-#if defined(__cpp_lib_to_chars)
     std::array<std::string::value_type, 64> buffer{"Cur: "}; // rest should be nulls
+#if defined(__cpp_lib_to_chars)
     auto ToChars = [buf_end{buffer.data() + buffer.size()}](char* buf_start, float num) -> char * {
         int precision = num < 10 ? 2 : 1;
         return std::to_chars(buf_start, buf_end, num, std::chars_format::fixed, precision).ptr;
     };
-
+#else
+    auto ToChars = [buf_end{buffer.data() + buffer.size()}](char* buf_start, float num) -> char * {
+        auto count = sprintf(buf_start, num < 10 ? "%1.2f" : "%5.1f", num);
+        return buf_start + std::max(0, count);
+    };
+#endif
     auto result_ptr = ToChars(buffer.data() + 5, m_current_value);
     // the biggest result of to_chars should be like "-65535.999" or 10 chars per number
     *result_ptr = ' ';
@@ -39,10 +46,6 @@ std::string Meter::Dump(unsigned short ntabs) const {
     ToChars(result_ptr + 1, m_initial_value);
 
     return buffer.data();
-#else
-    return std::string{"Cur: "}.append(std::to_string(m_current_value))
-             .append(" Init: ").append(std::to_string(m_initial_value));
-#endif
 }
 
 void Meter::SetCurrent(float current_value)
