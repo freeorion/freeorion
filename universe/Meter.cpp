506d91aa2ad4984b9fa7909851ac49e56ea40828
geoffthemedio <geoffthemedio@gmail.com>
2022-02-26 15:55:00 +0100

    [[nodiscard]] and constexpr Meter

--- universe/Meter.cpp
+++ universe/Meter.cpp
@@ -9,18 +9,6 @@
 #include <stdio.h>
 #endif
 
-
-Meter::Meter(float current_value, float initial_value) :
-    m_current_value(current_value),
-    m_initial_value(initial_value)
-{}
-
-float Meter::Current() const
-{ return m_current_value; }
-
-float Meter::Initial() const
-{ return m_initial_value; }
-
 std::string Meter::Dump(unsigned short ntabs) const {
     std::array<std::string::value_type, 64> buffer{"Cur: "}; // rest should be nulls
 #if defined(__cpp_lib_to_chars)
@@ -48,27 +36,5 @@ std::string Meter::Dump(unsigned short ntabs) const {
     return buffer.data();
 }
 
-void Meter::SetCurrent(float current_value)
-{ m_current_value = current_value; }
-
-void Meter::Set(float current_value, float initial_value) {
-    m_current_value = current_value;
-    m_initial_value = initial_value;
-}
-
-void Meter::ResetCurrent()
-{ m_current_value = DEFAULT_VALUE; } // initial unchanged
-
-void Meter::Reset() {
-    m_current_value = DEFAULT_VALUE;
-    m_initial_value = DEFAULT_VALUE;
-}
-
-void Meter::AddToCurrent(float adjustment)
-{ m_current_value += adjustment; }
-
-void Meter::ClampCurrentToRange(float min/* = DEFAULT_VALUE*/, float max/* = LARGE_VALUE*/)
+void Meter::ClampCurrentToRange(float min, float max)
 { m_current_value = std::max(std::min(m_current_value, max), min); }
-
-void Meter::BackPropagate()
-{ m_initial_value = m_current_value; }
--- universe/Meter.h
+++ universe/Meter.h
@@ -9,49 +9,50 @@
 #include "../util/Export.h"
 
 
-/** A Meter is a value with an associated maximum value.  A typical example is
-  * the population meter.  The max represents the max pop for a planet, and the
-  * current represents the current pop there.  The max may be adjusted upwards
-  * or downwards, and the current may be as well. */
+/** A Meter is a value and associated initial value that is used to track information
+  * about gamestate. A typical example is the population meter of a planet. */
 class FO_COMMON_API Meter {
 public:
-    /** Creates a new meter with both initial and current value set to
-        DEFAULT_VALUE. */
-    Meter() = default;
+    [[nodiscard]] constexpr float Current() const { return m_current_value; };
+    [[nodiscard]] constexpr float Initial() const { return m_initial_value; };
 
-    /** Creates a new meter with the current value set to @p current_value and
-        the initial value set to @p initial_value. */
-    Meter(float current_value, float initial_value);
+    [[nodiscard]] std::string Dump(unsigned short ntabs = 0) const;   ///< returns text of meter values
 
-    float Current() const;                  ///< returns the current value of the meter
-    float Initial() const;                  ///< returns the value of the meter as it was at the beginning of the turn
-
-    std::string Dump(unsigned short ntabs = 0) const;   ///< returns text of meter values
-
-    bool operator==(const Meter& rhs) const
+    [[nodiscard]] constexpr bool operator==(const Meter& rhs) const
     { return m_current_value == rhs.m_current_value && m_initial_value == rhs.m_initial_value; }
 
-    bool operator<(const Meter& rhs) const
+    [[nodiscard]] constexpr bool operator<(const Meter& rhs) const
     { return m_current_value < rhs.m_current_value || (m_current_value == rhs.m_current_value && m_initial_value < rhs.m_initial_value); }
 
-    void SetCurrent(float current_value);   ///< sets current value, leaving initial value unchanged
-    void Set(float current_value, float initial_value); ///< sets current and initial values
-    void ResetCurrent();                    ///< sets current value to DEFAULT_VALUE
-    void Reset();                           ///< sets current and initial values to DEFAULT_VALUE
+    constexpr void SetCurrent(float current_value) { m_current_value = current_value; }
 
-    void AddToCurrent(float adjustment);    ///< adds \a current to the current value of the Meter
+    constexpr void Set(float current_value, float initial_value) {
+        m_current_value = current_value;
+        m_initial_value = initial_value;
+    }
+
+    constexpr void ResetCurrent() { m_current_value = DEFAULT_VALUE; } // initial unchanged
+
+    constexpr void Reset() {
+        m_current_value = DEFAULT_VALUE;
+        m_initial_value = DEFAULT_VALUE;
+    }
+
+    constexpr void AddToCurrent(float adjustment) { m_current_value += adjustment; }
+
     void ClampCurrentToRange(float min = DEFAULT_VALUE, float max = LARGE_VALUE);   ///< ensures the current value falls in the range [\a min, \a max]
 
-    void BackPropagate();                   ///< sets previous equal to initial, then sets initial equal to current
+    constexpr void BackPropagate() { m_initial_value = m_current_value; }
+
 
     static constexpr float DEFAULT_VALUE = 0.0f;                        ///< value assigned to current or initial when resetting or when no value is specified in a constructor
     static constexpr float LARGE_VALUE = static_cast<float>(2 << 15);   ///< a very large number, which is useful to set current to when it will be later clamped, to ensure that the result is the max value in the clamp range
     static constexpr float INVALID_VALUE = -LARGE_VALUE;                ///< sentinel value to indicate no valid value for this meter
 
-private:
     float m_current_value = DEFAULT_VALUE;
     float m_initial_value = DEFAULT_VALUE;
 
+private:
     friend class boost::serialization::access;
     template <typename Archive>
     void serialize(Archive& ar, const unsigned int version);
@@ -63,14 +64,16 @@ BOOST_CLASS_VERSION(Meter, 1)
 template <typename Archive>
 void Meter::serialize(Archive& ar, const unsigned int version)
 {
-    if (Archive::is_loading::value && version < 1) {
-        ar  & BOOST_SERIALIZATION_NVP(m_current_value)
-            & BOOST_SERIALIZATION_NVP(m_initial_value);
-    } else {
-        // use minimum size NVP label to reduce archive size bloat for very-often serialized meter values...
-        ar  & boost::serialization::make_nvp("c", m_current_value)
-            & boost::serialization::make_nvp("i", m_initial_value);
+    if constexpr (Archive::is_loading::value) {
+        if (version < 1) {
+            ar  & BOOST_SERIALIZATION_NVP(m_current_value)
+                & BOOST_SERIALIZATION_NVP(m_initial_value);
+            return;
+        }
     }
+    // use minimum size NVP label to reduce archive size bloat for very-often serialized meter values...
+    ar  & boost::serialization::make_nvp("c", m_current_value)
+        & boost::serialization::make_nvp("i", m_initial_value);
 }
 
 
