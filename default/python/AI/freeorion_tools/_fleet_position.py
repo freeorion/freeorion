b3df30744f90006c632bbb184303f8732e3058d9
Grummel7 <muzz@albatross.pond.sub.org>
2022-03-07 22:18:50 +0100

    Fix tag parsing (#3723)
    * Fix tag parser, added influence to species roster
    * Fix mis-handling of SystemID 0
    * Fix get_ai_tag_grade, added a test

--- default/python/AI/freeorion_tools/_fleet_position.py
+++ default/python/AI/freeorion_tools/_fleet_position.py
@@ -1,9 +1,10 @@
 import freeOrionAIInterface as fo
 
+from AIDependencies import INVALID_ID
 from common.fo_typing import FleetId, SystemId
 
 
 def get_fleet_position(fleet_id: FleetId) -> SystemId:
     """Return system where fleet is located or moving to."""
     fleet = fo.getUniverse().getFleet(fleet_id)
-    return fleet.systemID if fleet.systemID > 0 else fleet.nextSystemID
+    return fleet.systemID if fleet.systemID != INVALID_ID else fleet.nextSystemID
