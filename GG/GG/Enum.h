6650459f1724b498a114d4cdab874b97c1b6eaca
geoffthemedio <geoffthemedio@gmail.com>
2022-03-11 22:00:34 +0100

    -move EnumMap definitions into declaration -reimplement EnumMapInsert with string_view -WIP since the call in BuildEnumMap to Insert passes a temporary string

--- GG/GG/Enum.h
+++ GG/GG/Enum.h
@@ -35,16 +35,72 @@ namespace GG {
 template <typename EnumType>
 class EnumMap {
 public:
-    [[nodiscard]] std::string_view operator[](EnumType value) const;
-    [[nodiscard]] EnumType operator[](std::string_view name) const;
+    [[nodiscard]] std::string_view operator[](EnumType value) const
+    {
+        auto value_it = std::find(m_values.cbegin(), m_values.cend(), value);
+        if (value_it == m_values.cend())
+            return "None";
+        auto dist = std::distance(m_values.cbegin(), value_it);
+        auto name_it = m_names.cbegin();
+        std::advance(name_it, dist);
+        return *name_it;
+    }
 
-    void Insert(const std::string& entry);
+    [[nodiscard]] EnumType operator[](std::string_view name) const
+    {
+        auto name_it = std::find(m_names.cbegin(), m_names.cend(), name);
+        if (name_it == m_names.cend())
+            return std::numeric_limits<EnumType>::max();
+        auto dist = std::distance(m_names.cbegin(), name_it);
+        auto value_it = m_values.cbegin();
+        std::advance(value_it, dist);
+        return *value_it;
+    }
+
+    void Insert(std::string_view entry)
+    {
+        // entires passed as series of "SYMBOL = 0x1b" formatted key-value pairs
+        // parse into key name and value
+        auto [name, value_str] = Split(entry);
+        EnumType value = EnumType(strtol(value_str.data(), nullptr, 0));
+
+        const auto place_idx = m_size++;
+        if (m_size >= CAPACITY)
+            throw std::runtime_error("Too many entries inserted into EnumMap.");
+
+        m_names[place_idx] = name;
+        m_values[place_idx] = value;
+    }
 
     [[nodiscard]] size_t size() const { return m_size; }
     [[nodiscard]] bool empty() const { return m_size == 0; }
 
 private:
     static constexpr size_t CAPACITY = std::numeric_limits<unsigned char>::max();
+
+    [[nodiscard]] static constexpr std::pair<std::string_view, std::string_view>
+        Split(std::string_view entry, char separator = '=')
+    {
+        constexpr char* whitespace = " \b\f\n\r\t\v";
+        auto name_start = entry.find_first_not_of(whitespace);
+        auto eq_pos = entry.find_first_of(separator, name_start);
+        auto name_to_eq = entry.substr(name_start, eq_pos - name_start + 1);
+        auto name_to_before_eq = name_to_eq.substr(0, name_to_eq.length() - 1);
+        auto name_end = name_to_before_eq.find_last_not_of(whitespace);
+        auto name = name_to_before_eq.substr(0, name_end + 1);
+        auto after_eq = entry.substr(eq_pos + 1);
+        auto value_start = after_eq.find_first_not_of(whitespace);
+        auto value_to_end = after_eq.substr(value_start);
+        auto value_end = value_to_end.find_first_of(whitespace);
+        auto value = after_eq.substr(value_start, value_end);
+
+        return {name, value};
+    };
+
+    static constexpr std::string_view test_text = "   something = \t 0x42\b\t  ";
+    static constexpr auto test_name_value = Split(test_text);
+    static_assert(test_name_value.first == "something" && test_name_value.second == "0x42");
+
 
     size_t                            m_size = 0;
     std::array<std::string, CAPACITY> m_names{};
@@ -141,54 +197,7 @@ void BuildEnumMap(EnumMap<EnumType>& map, const char* comma_separated_names)
 /////////////
 // EnumMap //
 /////////////
-template <typename EnumType>
-std::string_view EnumMap<EnumType>::operator[](EnumType value) const
-{
-    auto value_it = std::find(m_values.cbegin(), m_values.cend(), value);
-    if (value_it == m_values.cend())
-        return "None";
-    auto dist = std::distance(m_values.cbegin(), value_it);
-    auto name_it = m_names.cbegin();
-    std::advance(name_it, dist);
-    return *name_it;
-}
 
-template <typename EnumType>
-EnumType EnumMap<EnumType>::operator[](std::string_view name) const
-{
-    auto name_it = std::find(m_names.cbegin(), m_names.cend(), name);
-    if (name_it == m_names.cend())
-        return std::numeric_limits<EnumType>::max();
-    auto dist = std::distance(m_names.cbegin(), name_it);
-    auto value_it = m_values.cbegin();
-    std::advance(value_it, dist);
-    return *value_it;
-}
-
-template <typename EnumType>
-void EnumMap<EnumType>::Insert(const std::string& entry)
-{
-    std::stringstream name_and_value(entry);
-
-    // entires passed as series of "SYMBOL = 0x1b" formatted key-value pairs
-    // parse into key name and value
-    std::string name, value_str;
-    std::getline(name_and_value, name, '=');
-    bool no_error{std::getline(name_and_value, value_str)};
-
-    EnumType value{0};
-    if (no_error)
-        value = EnumType(strtol(value_str.c_str(), nullptr, 0));
-
-    boost::trim(name);
-
-    const auto place_idx = m_size++;
-    if (m_size >= CAPACITY)
-        throw std::runtime_error("Too many entries inserted into EnumMap.");
-
-    m_names[place_idx] = std::move(name);
-    m_values[place_idx] = value;
-}
 
 }
 
