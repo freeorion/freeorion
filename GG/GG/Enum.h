f7547dad4fcce9f613d51908fc062a313e43a9a3
geoffthemedio <geoffthemedio@gmail.com>
2022-03-12 15:49:29 +0100

    -remove default delimeter argument -implement constexpr string_view to EnumType via decimal or two-digit hex parsing

--- GG/GG/Enum.h
+++ GG/GG/Enum.h
@@ -58,16 +58,17 @@ public:
     }
 
     [[nodiscard]] constexpr size_t size() const { return m_size; }
+
     [[nodiscard]] constexpr bool empty() const { return m_size == 0; }
 
     /** Do not call this function directly.
       * Instead, rely on the functions generated
       * by the GG_ENUM or GG_CLASS_ENUM macro invocations. */
-    void Build(const char* comma_separated_names)
+    constexpr void Build(const char* comma_separated_names)
     {
         if (Count(comma_separated_names) > CAPACITY)
             throw std::invalid_argument("too many comma separated enum vals to build map");
-        auto count_names = SplitApply(comma_separated_names, Trim);
+        auto count_names = SplitApply(comma_separated_names, Trim, ',');
         for (size_t i = 0; i < count_names.first; ++i)
             Insert(count_names.second[i]);
     }
@@ -75,7 +76,7 @@ public:
 private:
     // Formats entries passed as series of "SYMBOL = 0x1b" into key-value pairs
     // and inserts into this map
-    void Insert(std::string_view entry)
+    constexpr void Insert(std::string_view entry)
     {
         auto count_trimmed_name_value_str = SplitApply<2>(entry, Trim, '=');
         static_assert(std::is_same_v<decltype(count_trimmed_name_value_str.first), size_t>);
@@ -87,10 +88,10 @@ private:
             return;
         auto [name, value_str] = count_trimmed_name_value_str.second;
 
-        EnumType value = EnumType(strtol(value_str.data(), nullptr, 0));
+        EnumType value = ToEnumType(value_str);
         //std::cout << "inserting entry: " << entry << "  as name: " << name
         //          << "  value string: " << value_str
-        //          << "  value: " << static_cast<std::underlying_type_t<EnumType>>(value) << std::endl;
+        //          << "  value: " << static_cast<int>(value) << std::endl;
 
         const auto place_idx = m_size++;
         if (m_size >= CAPACITY)
@@ -104,7 +105,7 @@ private:
 
 
     [[nodiscard]] static constexpr std::pair<std::string_view, std::string_view> Split(
-        std::string_view delim_separated_vals, const char delim = ',')
+        std::string_view delim_separated_vals, const char delim)
     {
         auto comma_idx = delim_separated_vals.find_first_of(delim);
         if (comma_idx == std::string_view::npos)
@@ -114,10 +115,10 @@ private:
     }
 
     static constexpr std::string_view test_cs_names = "123 = 7  , next_thing =   124, last thing  =-1";
-    static constexpr auto first_and_rest = Split(test_cs_names);
-    static constexpr auto second_and_rest = Split(first_and_rest.second);
-    static constexpr auto third_and_rest = Split(second_and_rest.second);
-    static constexpr auto fourth_and_rest = Split(third_and_rest.second);
+    static constexpr auto first_and_rest = Split(test_cs_names, ',');
+    static constexpr auto second_and_rest = Split(first_and_rest.second, ',');
+    static constexpr auto third_and_rest = Split(second_and_rest.second, ',');
+    static constexpr auto fourth_and_rest = Split(third_and_rest.second, ',');
     static_assert(fourth_and_rest.first.empty());
     static constexpr std::string_view third_result_expected = " last thing  =-1";
     static_assert(third_and_rest.first == third_result_expected);
@@ -137,7 +138,7 @@ private:
 
     template <size_t RETVAL_CAP = CAPACITY, typename F>
     [[nodiscard]] static constexpr std::pair<size_t, std::array<std::string_view, RETVAL_CAP>>
-        SplitApply(std::string_view comma_separated_names, F&& fn, char delim = ',')
+        SplitApply(std::string_view comma_separated_names, F&& fn, char delim)
     {
         size_t count = 0;
         std::array<std::string_view, RETVAL_CAP> retval;
@@ -151,6 +152,7 @@ private:
         return {count, retval};
     }
 
+
     [[nodiscard]] static constexpr std::string_view Trim(std::string_view padded)
     {
         constexpr std::string_view whitespace = " \b\f\n\r\t\v";
@@ -163,11 +165,86 @@ private:
     static_assert(Trim(test_text) == trimmed_text_expected);
 
 
-    static constexpr auto split_count_vals = SplitApply(test_cs_names, Trim);
+    static constexpr auto split_count_vals = SplitApply(test_cs_names, Trim, ',');
     static_assert(split_count_vals.first == 3);
     static_assert(!split_count_vals.second[0].empty());
     static constexpr std::string_view expected_first_trimmed_name = "123 = 7";
     static_assert(split_count_vals.second[0] == expected_first_trimmed_name);
+
+
+    static constexpr std::string_view bin = "1";
+    static constexpr std::string_view hex = "0x4f";
+    static constexpr std::string_view dec_neg = "-39";
+    static constexpr std::string_view dec_big = "237534";
+    static constexpr std::string_view dec_sml = "123";
+    static constexpr std::string_view alpha = "xxxx";
+
+    [[nodiscard]] static constexpr bool IsDecChar(char c)
+    { return c >= '0' && c <= '9'; }
+    [[nodiscard]] static constexpr bool IsDec(std::string_view txt)
+    {
+        return txt.length() >= 1 &&
+            (IsDecChar(txt[0]) || (txt[0] == '-' && txt.length() >= 2)) &&
+            txt.substr(1).find_first_not_of("0123456789") == std::string_view::npos;
+    }
+    static_assert(IsDec(dec_neg) && IsDec(dec_big) && IsDec(dec_sml) &&
+                  !IsDec(hex) && IsDec(bin) && !IsDec(alpha));
+
+
+    [[nodiscard]] static constexpr bool IsAtoF(char c)
+    { return c >= 'a' && c <= 'f'; }
+    [[nodiscard]] static constexpr bool IsHexChar(char c)
+    { return IsDecChar(c) || IsAtoF(c); }
+    [[nodiscard]] static constexpr bool IsHex(std::string_view txt)
+    {
+        return txt.length() == 4 && txt[0] == '0' && txt[1] == 'x' &&
+            IsHexChar(txt[2]) && IsHexChar(txt[3]);
+    }
+    static_assert(IsHex(hex) && !IsHex(bin) && !IsHex(dec_neg) && IsHex("0x1d"));
+
+
+    [[nodiscard]] static constexpr unsigned int Base(std::string_view txt) {
+        if (IsHex(txt))
+            return 16;
+        if (IsDec(txt))
+            return 10;
+        return 0;
+    }
+    static_assert(Base("124") == 10);
+    static_assert(Base("0x5d") == 16);
+
+
+    [[nodiscard]] static constexpr int ToInt(std::string_view txt) {
+        using C = std::string_view::value_type;
+        static_assert(std::is_same_v<C, char>);
+        constexpr std::string_view dec_chars = "0123456789";
+        static_assert(dec_chars.find('5') == 5);
+        static_assert(dec_chars.find('0') == 0);
+        static_assert(dec_chars.find('b') == std::string_view::npos);
+        constexpr std::string_view hex_chars = "0123456789abcdef";
+        static_assert(hex_chars.find('b') == 11);
+
+        unsigned char base = Base(txt);
+        if (base == 0)
+            return 0;
+
+        std::string_view valid_chars = base == 10 ? dec_chars :
+            base == 16 ? hex_chars : "";
+
+        bool negative = txt[0] == '-';
+        size_t idx = negative;
+        int retval = valid_chars.find(txt[idx++]);
+
+        for (; idx < txt.length(); ++idx) {
+            retval *= base;
+            retval += valid_chars.find(txt[idx]);
+        }
+
+        return retval * (1 - 2*negative);
+    }
+
+    [[nodiscard]] static constexpr EnumType ToEnumType(std::string_view str)
+    { return EnumType(ToInt(str)); }
 
 
     size_t                            m_size = 0;
