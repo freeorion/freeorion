87e2ed33415ad0c7cabc053815ce0856667ccc54
Andrey <Cjkjvfnby@gmail.com>
2022-03-17 00:56:37 +0300

    [AI] Ensure colony_buildes are initialized before usage (#3744)

---
+++ default/python/AI/freeorion_tools/lazy_initializer.py
@@ -0,0 +1,32 @@
+import freeOrionAIInterface as fo
+from functools import wraps
+from typing import Callable
+
+
+class InitializerLock:
+    """
+    Class that ensure that it's properly initialized before the first use of data.
+    """
+
+    def __init__(self, name):
+        self.__initialized = False
+        self.__name = name
+
+    def lock(self):
+        self.__initialized = False
+
+    def unlock(self):
+        self.__initialized = True
+
+    def __call__(self, function: Callable):
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            print("use zzzxxcc3", fo.currentTurn())
+            if self.__initialized:
+                return function(*args, **kwargs)
+            else:
+                raise ValueError(
+                    f"Call of '{function.__name__}' is forbidden before {self.__class__.__name__}('{self.__name}') is initialized"
+                )
+
+        return wrapper
