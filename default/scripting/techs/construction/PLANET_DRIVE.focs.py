c50b379c3c142be616108b046c4e85fb5de98c72
O01eg <o01eg@yandex.ru>
2022-04-03 21:09:26 +0300

    Convert techs/construction/PLANET_DRIVE.focs

---
+++ default/scripting/techs/construction/PLANET_DRIVE.focs.py
@@ -0,0 +1,18 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="CON_PLANET_DRIVE",
+    description="CON_PLANET_DRIVE_DESC",
+    short_description="BUILDING_UNLOCK_SHORT_DESC",
+    category="CONSTRUCTION_CATEGORY",
+    researchcost=250 * TECH_COST_MULTIPLIER,
+    researchturns=6,
+    tags=["PEDIA_CONSTRUCTION_CATEGORY"],
+    prerequisites="LRN_SPATIAL_DISTORT_GEN",
+    unlock=[
+        Item(type=Building, name="BLD_PLANET_DRIVE"),
+        Item(type=Building, name="BLD_PLANET_BEACON"),
+        Item(type=ShipPart, name="SP_PLANET_BEACON"),
+    ],
+    graphic="icons/tech/planetary_stardrive.png",
+)
