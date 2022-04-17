f5ebf69dd97f7c032f8db98e3410544fd8a66be9
geoffthemedio <geoffthemedio@gmail.com>
2022-04-17 12:18:56 +0200

    -store Meter internally as int -add specialized Meter::serializae specializations for XML archives

--- python/UniverseWrapper.cpp
+++ python/UniverseWrapper.cpp
@@ -344,8 +344,8 @@ namespace FreeOrionPython {
         //   Meter   //
         ///////////////
         py::class_<Meter, boost::noncopyable>("meter", py::no_init)
-            .add_property("current",            &Meter::cur)
-            .add_property("initial",            &Meter::init)
+            .add_property("current",            +[](const Meter& m) -> float { return m.Current(); })
+            .add_property("initial",            +[](const Meter& m) -> float { return m.Initial(); })
             .def("dump",                        +[](const Meter& m) -> std::string { return m.Dump(0).data(); },
                                                 py::return_value_policy<py::return_by_value>(),
                                                 "Returns string with debug information.")
