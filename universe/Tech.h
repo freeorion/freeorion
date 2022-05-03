188ef34e629269cf0262f1221bf64b72bed95b83
geoffthemedio <geoffthemedio@gmail.com>
2022-05-03 11:53:13 +0200

    [[nodiscard]]

--- universe/Tech.h
+++ universe/Tech.h
@@ -73,35 +73,29 @@ public:
     bool operator!=(const Tech& rhs) const
     { return !(*this == rhs); }
 
-    const std::string&  Name() const                { return m_name; }              //!< returns name of this tech
-    const std::string&  Description() const         { return m_description; }       //!< Returns the text description of this tech
-    const std::string&  ShortDescription() const    { return m_short_description; } //!< Returns the single-line short text description of this tech
-    std::string         Dump(unsigned short ntabs = 0) const;                               //!< Returns a text representation of this object
-    const std::string&  Category() const            { return m_category; }                  //!< retursn the name of the category to which this tech belongs
-    float               ResearchCost(int empire_id, const ScriptingContext& context) const; //!< returns the total research cost in RPs required to research this tech
-    float               PerTurnCost(int empire_id, const ScriptingContext& context) const;  //!< returns the maximum number of RPs per turn allowed to be spent on researching this tech
-    int                 ResearchTime(int empire_id, const ScriptingContext& context) const; //!< returns the number of turns required to research this tech, if ResearchCost() RPs are spent per turn
-    bool                Researchable() const        { return m_researchable; }              //!< returns whether this tech is researchable by players and appears on the tech tree
+    [[nodiscard]] const std::string&  Name() const                { return m_name; }              //!< returns name of this tech
+    [[nodiscard]] const std::string&  Description() const         { return m_description; }       //!< Returns the text description of this tech
+    [[nodiscard]] const std::string&  ShortDescription() const    { return m_short_description; } //!< Returns the single-line short text description of this tech
+    [[nodiscard]] std::string         Dump(unsigned short ntabs = 0) const;                               //!< Returns a text representation of this object
+    [[nodiscard]] const std::string&  Category() const            { return m_category; }                  //!< retursn the name of the category to which this tech belongs
+    [[nodiscard]] float               ResearchCost(int empire_id, const ScriptingContext& context) const; //!< returns the total research cost in RPs required to research this tech
+    [[nodiscard]] float               PerTurnCost(int empire_id, const ScriptingContext& context) const;  //!< returns the maximum number of RPs per turn allowed to be spent on researching this tech
+    [[nodiscard]] int                 ResearchTime(int empire_id, const ScriptingContext& context) const; //!< returns the number of turns required to research this tech, if ResearchCost() RPs are spent per turn
+    [[nodiscard]] bool                Researchable() const        { return m_researchable; }              //!< returns whether this tech is researchable by players and appears on the tech tree
 
-    const std::set<std::string>&    Tags() const    { return m_tags; }
+    [[nodiscard]] const std::set<std::string>& Tags() const { return m_tags; }
 
     /** returns the effects that are applied to the discovering empire's capital
       * when this tech is researched; not all techs have effects, in which case
       * this returns 0 */
-    const std::vector<std::shared_ptr<Effect::EffectsGroup>>& Effects() const
-    { return m_effects; }
+    [[nodiscard]] const std::vector<std::shared_ptr<Effect::EffectsGroup>>& Effects() const { return m_effects; }
 
-    const std::set<std::string>&    Prerequisites() const { return m_prerequisites; }   //!< returns the set of names of all techs required before this one can be researched
-    const std::string&              Graphic() const       { return m_graphic; }         //!< returns the name of the grapic file for this tech
-
-    //! Returns the set all items that are unlocked by researching this tech
-    const std::vector<UnlockableItem>&    UnlockedItems() const
-    { return m_unlocked_items; }
-
-    const ValueRef::ValueRef<double>* ResearchCostRef() const { return m_research_cost.get(); } //!< return value ref of research cost
-    const ValueRef::ValueRef<int>*    ResearchTurnsRef() const { return m_research_turns.get(); } //!< return value ref of research turns
-
-    const std::set<std::string>&    UnlockedTechs() const { return m_unlocked_techs; }  //!< returns the set of names of all techs for which this one is a prerequisite
+    [[nodiscard]] const std::set<std::string>&       Prerequisites() const   { return m_prerequisites; }       //!< returns the set of names of all techs required before this one can be researched
+    [[nodiscard]] const std::string&                 Graphic() const         { return m_graphic; }             //!< returns the name of the grapic file for this tech
+    [[nodiscard]] const std::vector<UnlockableItem>& UnlockedItems() const   { return m_unlocked_items; }      //! Returns the set all items that are unlocked by researching this tech
+    [[nodiscard]] const ValueRef::ValueRef<double>*  ResearchCostRef() const { return m_research_cost.get(); } //!< return value ref of research cost
+    [[nodiscard]] const ValueRef::ValueRef<int>*     ResearchTurnsRef() const{ return m_research_turns.get(); }//!< return value ref of research turns
+    [[nodiscard]] const std::set<std::string>&       UnlockedTechs() const   { return m_unlocked_techs; }      //!< returns the set of names of all techs for which this one is a prerequisite
 
     /** Returns a number, calculated from the contained data, which should be
       * different for different contained data, and must be the same for
@@ -109,7 +103,7 @@ public:
       * and executions of the program and the function. Useful to verify that
       * the parsed content is consistent without sending it all between
       * clients and server. */
-    unsigned int                    GetCheckSum() const;
+    [[nodiscard]] unsigned int                      GetCheckSum() const;
 
 private:
     Tech(const Tech&) = delete;
@@ -138,7 +132,7 @@ private:
 struct FO_COMMON_API TechCategory {
     TechCategory() = default;
     TechCategory(std::string name_, std::string&& graphic_,
-                 const std::array<unsigned char, 4>& colour_):
+                 std::array<unsigned char, 4> colour_):
         name(std::move(name_)),
         graphic(std::move(graphic_)),
         colour(colour_)
