e25e476e59c81024889d5244948bbbc9807aa56e
O01eg <o01eg@yandex.ru>
2022-05-30 10:35:17 +0300

    Convert default/scripting/techs/growth/ADV_ECOMAN.focs

---
+++ default/scripting/techs/growth/ADV_ECOMAN.focs.py
@@ -0,0 +1,13 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="GRO_ADV_ECOMAN",
+    description="GRO_ADV_ECOMAN_DESC",
+    short_description="THEORY_SHORT_DESC",
+    category="GROWTH_CATEGORY",
+    researchcost=50 * TECH_COST_MULTIPLIER,
+    researchturns=3,
+    tags=["PEDIA_GROWTH_CATEGORY", "THEORY"],
+    prerequisites="GRO_SUBTER_HAB",
+    graphic="icons/tech/advanced_eco_man.png",
+)
