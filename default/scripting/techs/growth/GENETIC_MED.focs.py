5b6efc4759b9217e343e0fc7eec46b8c8b1402d7
O01eg <o01eg@yandex.ru>
2022-06-02 12:59:17 +0300

    Convert default/scripting/techs/growth/GENETIC_MED.focs

---
+++ default/scripting/techs/growth/GENETIC_MED.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="GRO_GENETIC_MED",
+    description="GRO_GENETIC_MED_DESC",
+    short_description="BUILDING_UNLOCK_SHORT_DESC",
+    category="GROWTH_CATEGORY",
+    researchcost=12 * TECH_COST_MULTIPLIER,
+    researchturns=4,
+    tags=["PEDIA_GROWTH_CATEGORY"],
+    prerequisites="GRO_GENETIC_ENG",
+    unlock=Item(type=Building, name="BLD_GENOME_BANK"),
+    graphic="icons/tech/genetic_medicine.png",
+)
