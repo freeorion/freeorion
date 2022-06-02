015bd6673dac5c7afe4742198b8cd8f88be9a834
O01eg <o01eg@yandex.ru>
2022-06-02 12:36:02 +0300

    Convert default/scripting/techs/growth/GENETIC_ENG.focs

---
+++ default/scripting/techs/growth/GENETIC_ENG.focs.py
@@ -0,0 +1,12 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="GRO_GENETIC_ENG",
+    description="GRO_GENETIC_ENG_DESC",
+    short_description="THEORY_SHORT_DESC",
+    category="GROWTH_CATEGORY",
+    researchcost=4 * TECH_COST_MULTIPLIER,
+    researchturns=3,
+    tags=["PEDIA_GROWTH_CATEGORY", "THEORY"],
+    graphic="icons/tech/genetic_engineering.png",
+)
