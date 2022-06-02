13484ffbc74e5e80510abb2441ee14614512b6b3
O01eg <o01eg@yandex.ru>
2022-06-02 16:51:54 +0300

    Convert default/scripting/techs/growth/NANOTECH_MED.focs

---
+++ default/scripting/techs/growth/NANOTECH_MED.focs.py
@@ -0,0 +1,17 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="GRO_NANOTECH_MED",
+    description="GRO_NANOTECH_MED_DESC",
+    short_description="THEORY_SHORT_DESC",
+    category="GROWTH_CATEGORY",
+    researchcost=40 * TECH_COST_MULTIPLIER,
+    researchturns=5,
+    tags=["PEDIA_GROWTH_CATEGORY", "THEORY"],
+    prerequisites=[
+        "GRO_GENETIC_MED",
+        "PRO_NANOTECH_PROD",
+    ],
+    unlock=Item(type=Policy, name="PLC_POPULATION"),
+    graphic="icons/tech/nanotech_medicine.png",
+)
