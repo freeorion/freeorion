b42604d6b368aa8104cb405407985a5444ca90eb
O01eg <o01eg@yandex.ru>
2022-06-02 11:49:41 +0300

    Convert default/scripting/techs/growth/GAIA_TRANS.focs

---
+++ default/scripting/techs/growth/GAIA_TRANS.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="GRO_GAIA_TRANS",
+    description="GRO_GAIA_TRANS_DESC",
+    short_description="GRO_GAIA_TRANS_SHORT_DESC",
+    category="GROWTH_CATEGORY",
+    researchcost=250 * TECH_COST_MULTIPLIER,
+    researchturns=7,
+    tags=["PEDIA_GROWTH_CATEGORY"],
+    prerequisites="GRO_TRANSORG_SENT",
+    unlock=Item(type=Building, name="BLD_GAIA_TRANS"),
+    graphic="icons/tech/the_living_planet.png",
+)
