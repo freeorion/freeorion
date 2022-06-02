24c66edc9cff9315671f6a7e1edc5c57d8e04969
O01eg <o01eg@yandex.ru>
2022-06-02 16:25:20 +0300

    Convert default/scripting/techs/growth/NANO_CYBERNET.focs

---
+++ default/scripting/techs/growth/NANO_CYBERNET.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="GRO_NANO_CYBERNET",
+    description="GRO_NANO_CYBERNET_DESC",
+    short_description="SHIP_PART_UNLOCK_SHORT_DESC",
+    category="GROWTH_CATEGORY",
+    researchcost=75 * TECH_COST_MULTIPLIER,
+    researchturns=5,
+    tags=["PEDIA_GROWTH_CATEGORY"],
+    prerequisites="GRO_NANOTECH_MED",
+    unlock=[Item(type=ShipPart, name="GT_TROOP_POD_2"), Item(type=Policy, name="PLC_AUGMENTATION")],
+    graphic="icons/tech/nanotech_cybernetics.png",
+)
