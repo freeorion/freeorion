68af557305d7888b0a2e84a517c23ad9cd987df7
O01eg <o01eg@yandex.ru>
2022-06-03 09:09:29 +0300

    Convert default/scripting/techs/growth/SUBTER_HAB.focs

---
+++ default/scripting/techs/growth/SUBTER_HAB.focs.py
@@ -0,0 +1,22 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+from common.priorities import TARGET_POPULATION_AFTER_SCALING_PRIORITY
+
+Tech(
+    name="GRO_SUBTER_HAB",
+    description="GRO_SUBTER_HAB_DESC",
+    short_description="POPULATION_SHORT_DESC",
+    category="GROWTH_CATEGORY",
+    researchcost=12 * TECH_COST_MULTIPLIER,
+    researchturns=2,
+    tags=["PEDIA_GROWTH_CATEGORY"],
+    prerequisites="GRO_PLANET_ECOL",
+    effectsgroups=[
+        EffectsGroup(
+            scope=Species & OwnedBy(empire=Source.Owner),
+            accountinglabel="GRO_TECH_ACCOUNTING_LABEL",
+            priority=TARGET_POPULATION_AFTER_SCALING_PRIORITY,
+            effects=SetTargetPopulation(value=Value + 1 * Target.HabitableSize),
+        )
+    ],
+    graphic="icons/tech/subterranean_construction.png",
+)
