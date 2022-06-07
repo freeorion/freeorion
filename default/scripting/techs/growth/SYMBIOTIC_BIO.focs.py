54ed6cae101f8e2630a345fc376c464b4c6717a1
O01eg <o01eg@yandex.ru>
2022-06-07 15:28:03 +0300

    Convert default/scripting/techs/growth/SYMBIOTIC_BIO.focs

---
+++ default/scripting/techs/growth/SYMBIOTIC_BIO.focs.py
@@ -0,0 +1,34 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+from common.priorities import TARGET_POPULATION_BEFORE_SCALING_PRIORITY
+
+Tech(
+    name="GRO_SYMBIOTIC_BIO",
+    description="GRO_SYMBIOTIC_BIO_DESC",
+    short_description="POPULATION_SHORT_DESC",
+    category="GROWTH_CATEGORY",
+    researchcost=50 * TECH_COST_MULTIPLIER,
+    researchturns=6,
+    tags=["PEDIA_GROWTH_CATEGORY"],
+    prerequisites="GRO_PLANET_ECOL",
+    unlock=[
+        Item(type=Policy, name="PLC_DIVERSITY"),
+        Item(type=Policy, name="PLC_BLACK_MARKET"),
+    ],
+    effectsgroups=[
+        EffectsGroup(
+            scope=Planet()
+            & OwnedBy(empire=Source.Owner)
+            & Planet(
+                environment=[
+                    Good,
+                    Adequate,
+                    Poor,
+                ]
+            ),
+            accountinglabel="GRO_TECH_ACCOUNTING_LABEL",
+            priority=TARGET_POPULATION_BEFORE_SCALING_PRIORITY,
+            effects=SetTargetPopulation(value=Value + 1 * Target.HabitableSize),
+        ),
+    ],
+    graphic="icons/tech/symbiosis_biology.png",
+)
