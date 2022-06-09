e5c73aeebb32091b7424291a7e7b1184e1cd83fb
O01eg <o01eg@yandex.ru>
2022-06-09 23:46:16 +0300

    Convert default/scripting/techs/learning/GRAVITONICS.focs

---
+++ default/scripting/techs/learning/GRAVITONICS.focs.py
@@ -0,0 +1,17 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="LRN_GRAVITONICS",
+    description="LRN_GRAVITONICS_DESC",
+    short_description="BUILDING_UNLOCK_SHORT_DESC",
+    category="LEARNING_CATEGORY",
+    researchcost=80 * TECH_COST_MULTIPLIER,
+    researchturns=5,
+    tags=["PEDIA_LEARNING_CATEGORY"],
+    prerequisites="LRN_FORCE_FIELD",
+    unlock=[
+        Item(type=Building, name="BLD_STARLANE_BORE"),
+        Item(type=Building, name="BLD_SCRYING_SPHERE"),
+    ],
+    graphic="icons/tech/gravitonics.png",
+)
