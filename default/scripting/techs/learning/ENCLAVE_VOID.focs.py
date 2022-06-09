a727a260f99f47fc14f026ee19c4b33125d6280c
O01eg <o01eg@yandex.ru>
2022-06-09 20:31:50 +0300

    Convert default/scripting/techs/learning/ENCLAVE_VOID.focs

---
+++ default/scripting/techs/learning/ENCLAVE_VOID.focs.py
@@ -0,0 +1,17 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="LRN_ENCLAVE_VOID",
+    description="LRN_ENCLAVE_VOID_DESC",
+    short_description="BUILDING_UNLOCK_SHORT_DESC",
+    category="LEARNING_CATEGORY",
+    researchcost=150 * TECH_COST_MULTIPLIER,
+    researchturns=5,
+    tags=["PEDIA_LEARNING_CATEGORY"],
+    prerequisites=[
+        "LRN_GATEWAY_VOID",
+        "LRN_EVERYTHING",
+    ],
+    unlock=Item(type=Building, name="BLD_ENCLAVE_VOID"),
+    graphic="icons/tech/enclave_of_the_void.png",
+)
