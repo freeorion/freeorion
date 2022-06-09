ef2113ef1873618c1e0996347032189b201baf6e
O01eg <o01eg@yandex.ru>
2022-06-09 23:37:35 +0300

    Convert default/scripting/techs/learning/GATEWAY_VOID.focs

---
+++ default/scripting/techs/learning/GATEWAY_VOID.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="LRN_GATEWAY_VOID",
+    description="LRN_GATEWAY_VOID_DESC",
+    short_description="BUILDING_UNLOCK_SHORT_DESC",
+    category="LEARNING_CATEGORY",
+    researchcost=150 * TECH_COST_MULTIPLIER,
+    researchturns=5,
+    tags=["PEDIA_LEARNING_CATEGORY"],
+    prerequisites=["CON_STARGATE"],
+    unlock=Item(type=Building, name="BLD_GATEWAY_VOID"),
+    graphic="icons/tech/monument_to_exodus.png",
+)
