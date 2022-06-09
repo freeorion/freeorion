5518830f9c3ab78255901c6fb8c4660e25c91f54
O01eg <o01eg@yandex.ru>
2022-06-09 09:46:49 +0300

    Convert default/scripting/techs/learning/COLLAPSER.focs

---
+++ default/scripting/techs/learning/COLLAPSER.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="LRN_COLLAPSER",
+    description="LRN_COLLAPSER_DESC",
+    short_description="BUILDING_UNLOCK_SHORT_DESC",
+    category="LEARNING_CATEGORY",
+    researchcost=1500 * TECH_COST_MULTIPLIER,
+    researchturns=8,
+    tags=["PEDIA_LEARNING_CATEGORY"],
+    prerequisites=["LRN_ART_BLACK_HOLE", "LRN_STELLAR_TOMOGRAPHY", "LRN_SPATIAL_DISTORT_GEN"],
+    unlock=Item(type=Building, name="BLD_BLACK_HOLE_COLLAPSER"),
+    graphic="icons/tech/black_hole_collapse.png",
+)
