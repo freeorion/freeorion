6d6a7fb66996718bf5ae0272d851f49fca86537d
O01eg <o01eg@yandex.ru>
2022-06-10 11:22:15 +0300

    Convert default/scripting/techs/learning/PHYSICAL_BRAIN.focs

---
+++ default/scripting/techs/learning/PHYSICAL_BRAIN.focs.py
@@ -0,0 +1,13 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="LRN_PHYS_BRAIN",
+    description="LRN_PHYS_BRAIN_DESC",
+    short_description="BUILDING_UNLOCK_SHORT_DESC",
+    category="LEARNING_CATEGORY",
+    researchcost=4 * TECH_COST_MULTIPLIER,
+    researchturns=2,
+    tags=["PEDIA_LEARNING_CATEGORY"],
+    unlock=[Item(type=Policy, name="PLC_LIBERTY"), Item(type=Building, name="BLD_AUTO_HISTORY_ANALYSER")],
+    graphic="icons/tech/the_physical_brain.png",
+)
