8113dd5bd52501b6fcd8419f320fa7c8e6740985
O01eg <o01eg@yandex.ru>
2022-06-10 10:40:25 +0300

    Convert default/scripting/techs/learning/N_DIM_SUBSPACE.focs

---
+++ default/scripting/techs/learning/N_DIM_SUBSPACE.focs.py
@@ -0,0 +1,13 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="LRN_NDIM_SUBSPACE",
+    description="LRN_NDIM_SUBSPACE_DESC",
+    short_description="THEORY_SHORT_DESC",
+    category="LEARNING_CATEGORY",
+    researchcost=80 * TECH_COST_MULTIPLIER,
+    researchturns=5,
+    tags=["PEDIA_LEARNING_CATEGORY", "THEORY"],
+    prerequisites="LRN_FORCE_FIELD",
+    graphic="icons/tech/n-dimensional_subspace.png",
+)
