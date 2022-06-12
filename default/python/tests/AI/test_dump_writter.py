59e73d02ecb203254bef2e2cf57038c081fa5ec3
Andrey <Cjkjvfnby@gmail.com>
2022-06-12 11:09:47 +0300

    Fix test

--- default/python/tests/AI/test_dump_writter.py
+++ default/python/tests/AI/test_dump_writter.py
@@ -1,11 +1,11 @@
 from unittest.mock import Mock
 
-from freeorion_tools.log_dumper.dump_interface import DumpKey
-from freeorion_tools.log_dumper.dumper.dumper import Dumper
+from freeorion_tools.log_dumper import DumpKey
+from freeorion_tools.log_dumper.dumper import Dumper
 
 
 def test_dump_key():
     writer = Mock()
     d = Dumper(writer=writer)
-    d.dump(DumpKey.AI_NAME, "hello")
-    writer.assert_called_once_with("**ai_name:str:hello")
+    d.dump(DumpKey.SHIP_CONT, 11)
+    writer.assert_called_once_with("##ShipCount:11")
