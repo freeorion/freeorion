b25ea9f7fb768dfc4a6a05d50e1e465473c6dfea
geoffthemedio <geoffthemedio@gmail.com>
2022-06-16 12:04:59 +0200

    -move workarounds for Boost 1.79 include error into Parsers StdAfx.h -restore MSVC2022 Parsers project to C++20 standard

--- msvc2022/Parsers/StdAfx.h
+++ msvc2022/Parsers/StdAfx.h
@@ -18,6 +18,16 @@
 #include <unordered_map>
 #include <vector>
 
+#include <boost/version.hpp>
+#if BOOST_VERSION == 107900 && defined(_MSC_VER)
+#include <cmath>
+namespace boost::core {
+    // workaround for error in boost/lexical_cast/detail/inf_nan.hpp : error C2039: '_copysign': is not a member of 'boost::core'
+    inline float _copysign(float x, float y)
+    { return std::copysignf(x, y); }
+}
+#endif
+
 #include <boost/algorithm/string/case_conv.hpp>
 #include <boost/algorithm/string/classification.hpp>
 #include <boost/algorithm/string/find_iterator.hpp>
@@ -50,3 +60,4 @@
 #include <boost/preprocessor/control/if.hpp>
 #include <boost/preprocessor/facilities/is_empty_variadic.hpp>
 #endif
+
