31751a2a3732c1fcb8b0016014d3e5aef24809a6
O01eg <o01eg@yandex.ru>
2022-06-17 11:53:59 +0300

    Convert default/scripting/techs/learning/TEMPORAL_MECH.focs

---
+++ default/scripting/techs/learning/TEMPORAL_MECH.focs.py
@@ -0,0 +1,18 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="LRN_TIME_MECH",
+    description="LRN_TIME_MECH_DESC",
+    short_description="BUILDING_UNLOCK_SHORT_DESC",
+    category="LEARNING_CATEGORY",
+    researchcost=75 * TECH_COST_MULTIPLIER,
+    researchturns=10,
+    tags=["PEDIA_LEARNING_CATEGORY"],
+    prerequisites="LRN_EVERYTHING",
+    unlock=Item(type=Building, name="BLD_STARLANE_NEXUS"),
+    effectsgroups=EffectsGroup(
+        scope=Source,
+        effects=SetEmpireMeter(empire=Source.Owner, meter="MILITARY_CATEGORY_NUM_POLICY_SLOTS", value=Value + 1),
+    ),
+    graphic="icons/tech/temporal_mechanics.png",
+)
