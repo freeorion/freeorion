af5dc3fc72d0f22660e1f6dc76075711aaeacf4b
O01eg <o01eg@yandex.ru>
2022-06-29 10:20:14 +0300

    Convert default/scripting/techs/production/INTERSTELLAR_ENTANGLEMENT_FACTORY.focs

---
+++ default/scripting/techs/production/INTERSTELLAR_ENTANGLEMENT_FACTORY.focs.py
@@ -0,0 +1,25 @@
+from common.base_prod import STOCKPILE_PER_POP, TECH_COST_MULTIPLIER
+
+Tech(
+    name="PRO_INTERSTELLAR_ENTANGLEMENT_FACTORY",
+    description="PRO_INTERSTELLAR_ENTANGLEMENT_FACTORY_DESC",
+    short_description="IMPERIAL_STOCKPILE_SHORT_DESC",
+    category="PRODUCTION_CATEGORY",
+    researchcost=100 * TECH_COST_MULTIPLIER,
+    researchturns=5,
+    tags=["PEDIA_PRODUCTION_CATEGORY"],
+    prerequisites=["PRO_GENERIC_SUPPLIES", "PRO_ADAPTIVE_AUTOMATION", "LRN_GRAVITONICS"],
+    unlock=Item(type=Building, name="BLD_STOCKPILING_CENTER"),
+    effectsgroups=[
+        EffectsGroup(
+            scope=ProductionCenter & OwnedBy(empire=Source.Owner) & Species,
+            accountinglabel="INTERSTELLAR_ENTANGLEMENT_FACTORY_POPULATION_BONUS_LABEL",
+            effects=SetMaxStockpile(value=Value + 1.0 * Target.Population * STOCKPILE_PER_POP),
+        ),
+        EffectsGroup(
+            scope=ProductionCenter & OwnedBy(empire=Source.Owner) & Focus(type="FOCUS_STOCKPILE"),
+            accountinglabel="INTERSTELLAR_ENTANGLEMENT_FACTORY_FIXED_BONUS_LABEL",
+            effects=SetMaxStockpile(value=Value + 6),
+        ),
+    ],
+)
