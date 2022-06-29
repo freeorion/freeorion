9e33c2b867c6d0e6a65fe5b1c4f3d9d491a36d8d
O01eg <o01eg@yandex.ru>
2022-06-29 09:24:01 +0300

    Convert default/scripting/techs/production/INDUSTRY_CENTER_I.focs

---
+++ default/scripting/techs/production/INDUSTRY_CENTER_I.focs.py
@@ -0,0 +1,17 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="PRO_INDUSTRY_CENTER_I",
+    description="PRO_INDUSTRY_CENTER_I_DESC",
+    short_description="BUILDING_UNLOCK_SHORT_DESC",
+    category="PRODUCTION_CATEGORY",
+    researchcost=30 * TECH_COST_MULTIPLIER,
+    researchturns=4,
+    tags=["PEDIA_PRODUCTION_CATEGORY"],
+    prerequisites="PRO_ROBOTIC_PROD",
+    unlock=[
+        Item(type=Building, name="BLD_INDUSTRY_CENTER"),
+        Item(type=Policy, name="PLC_INDUSTRIALISM"),
+    ],
+    graphic="icons/tech/industrial_centre_i.png",
+)
