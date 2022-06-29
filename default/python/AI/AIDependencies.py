c913ba76cd45f2f076870f23d79ef9436f8fd007
Grummel7 <muzz@albatross.pond.sub.org>
2022-06-29 21:06:49 +0200

    [AI] calculate_production.py (#3934)

--- default/python/AI/AIDependencies.py
+++ default/python/AI/AIDependencies.py
@@ -216,6 +216,22 @@ for metab, boosts in metabolismBoostMap.items():
         metabolismBoosts[boost] = metab
 # </editor-fold>
 
+# <editor-fold desc="Industry boosting specials">
+# Each adds INDUSTRY_PER_POP before production is multiplied by species skill modifier
+industry_boost_specials_modified = {
+    "TIDAL_LOCK_SPECIAL",
+}
+# Each adds INDUSTRY_PER_POP after all multipliers have been applied
+industry_boost_specials_unmodified = {
+    "CRYSTALS_SPECIAL",
+    "ELERIUM_SPECIAL",
+    "MINERALS_SPECIAL",
+    "MONOPOLE_SPECIAL",
+    "POSITRONIUM_SPECIAL",
+    "SUPERCONDUCTOR_SPECIAL",
+}
+# </editor-fold>
+
 # <editor-fold desc="Other Population changing specials">
 # Please see the Note at top of this file regarding PlanetSize-Dependent-Lookup
 # Regardless of whether the sub-dictionary here has PlanetSize keys, the final
@@ -246,6 +262,7 @@ POP_PROPORTIONAL_MOD_SPECIALS = {
 # </editor-fold>
 
 # <editor-fold desc="Industry related specials">
+HONEYCOMB_SPECIAL = "HONEYCOMB_SPECIAL"
 HONEYCOMB_IND_MULTIPLIER = 1.0
 # </editor-fold>
 
