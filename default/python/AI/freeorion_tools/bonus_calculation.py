2c2921e07c39a3ee76e832be0153d291f8e90c5e
Andrey <Cjkjvfnby@gmail.com>
2022-07-03 13:51:33 +0300

    Fix stability check for bonus

--- default/python/AI/freeorion_tools/bonus_calculation.py
+++ default/python/AI/freeorion_tools/bonus_calculation.py
@@ -9,4 +9,4 @@ class Bonus(NamedTuple):
     def get_bonus(self, stability: float) -> float:
         if not self.available:
             return 0.0
-        return self.value if stability > self.min_stability else 0.0
+        return self.value if stability >= self.min_stability else 0.0
--- default/python/tests/AI/bonus_calculator_tests.py
+++ default/python/tests/AI/bonus_calculator_tests.py
@@ -5,7 +5,7 @@ from freeorion_tools.bonus_calculation import Bonus
 @pytest.mark.parametrize(
     ("bonus", "expected"),
     (
-        (Bonus(True, 1, 5.5), 0),
+        (Bonus(True, 1, 5.5), 5.5),
         (Bonus(True, 2, 5.5), 0),
         (Bonus(True, 0, 5.5), 5.5),
         (Bonus(False, 0, 5.5), 0),
