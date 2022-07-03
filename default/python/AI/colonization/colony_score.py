7ff9ca081afadc45f918299dc522cb1d9f9d5eb5
Grummel7 <muzz@albatross.pond.sub.org>
2022-07-03 20:03:12 +0200

    typo

--- default/python/AI/colonization/colony_score.py
+++ default/python/AI/colonization/colony_score.py
@@ -5,7 +5,7 @@ from common.option_tools import check_bool, get_option_dict
 from freeorion_tools.caching import cache_for_session
 
 # Minimum evaluation score that a planet must reach to it is considered for outposting or colonizing.
-MINIMUM_COLONY_SCORE = 609
+MINIMUM_COLONY_SCORE = 600
 # Resource production is evaluated as production * priority_for_the_resource * RESOURCE_PRIORITY_MULTIPLIER.
 # So raising this makes it easier for planets to reach the minimum score and in relation reduces the value of
 # other scores, which are rated as factor * MINIMUM_COLONY_SCORE
