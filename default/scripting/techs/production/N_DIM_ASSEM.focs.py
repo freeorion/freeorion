13e2560fb8b0a5b2a4d8251388797f9c00bb45f8
O01eg <o01eg@yandex.ru>
2022-07-08 13:04:10 +0300

    Convert default/scripting/techs/production/N_DIM_ASSEM.focs

---
+++ default/scripting/techs/production/N_DIM_ASSEM.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="PRO_NDIM_ASSMB",
+    description="PRO_NDIM_ASSMB_DESC",
+    short_description="BUILDING_UNLOCK_SHORT_DESC",
+    category="PRODUCTION_CATEGORY",
+    researchcost=200 * TECH_COST_MULTIPLIER,
+    researchturns=5,
+    tags=["PEDIA_PRODUCTION_CATEGORY"],
+    prerequisites="LRN_NDIM_SUBSPACE",
+    unlock=Item(type=Building, name="BLD_HYPER_DAM"),
+    graphic="icons/tech/n-dimensional_assembly.png",
+)
