85441970931c7f347cab2d0e612ec8a865790911
O01eg <o01eg@yandex.ru>
2022-07-08 16:39:48 +0300

    Convert default/scripting/techs/production/SOLAR_ORB_GEN.focs

---
+++ default/scripting/techs/production/SOLAR_ORB_GEN.focs.py
@@ -0,0 +1,20 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="PRO_SOL_ORB_GEN",
+    description="PRO_SOL_ORB_GEN_DESC",
+    short_description="BUILDING_UNLOCK_SHORT_DESC",
+    category="PRODUCTION_CATEGORY",
+    researchcost=125
+    * TECH_COST_MULTIPLIER
+    * (
+        1
+        - 0.25
+        * StatisticIf(float, condition=Source & EmpireHasAdoptedPolicy(empire=Source.Owner, name="PLC_INDUSTRIALISM"))
+    ),
+    researchturns=8,
+    tags=["PEDIA_PRODUCTION_CATEGORY"],
+    prerequisites="PRO_ORBITAL_GEN",
+    unlock=Item(type=Building, name="BLD_SOL_ORB_GEN"),
+    graphic="icons/building/miniature_sun.png",
+)
