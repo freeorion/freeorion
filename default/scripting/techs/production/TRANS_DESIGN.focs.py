1499ee2348de71166da6babc381b049d181b1dcf
O01eg <o01eg@yandex.ru>
2022-07-08 16:44:53 +0300

    Convert default/scripting/techs/production/TRANS_DESIGN.focs

---
+++ default/scripting/techs/production/TRANS_DESIGN.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="PRO_TRANS_DESIGN",
+    description="PRO_TRANS_DESIGN_DESC",
+    short_description="BUILDING_UNLOCK_SHORT_DESC",
+    category="PRODUCTION_CATEGORY",
+    researchcost=75 * TECH_COST_MULTIPLIER,
+    researchturns=5,
+    tags=["PEDIA_PRODUCTION_CATEGORY"],
+    prerequisites=["LRN_PSIONICS", "CON_ARCH_PSYCH"],
+    unlock=Item(type=Building, name="BLD_INTERSPECIES_ACADEMY"),
+    graphic="icons/tech/transcendent_architecture.png",
+)
