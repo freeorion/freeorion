db10cb5ea7ea20621dc650ff34c26e6be1467a06
O01eg <o01eg@yandex.ru>
2022-07-08 16:03:39 +0300

    Convert default/scripting/techs/production/ROBOTIC_PROD.focs

---
+++ default/scripting/techs/production/ROBOTIC_PROD.focs.py
@@ -0,0 +1,27 @@
+from common.base_prod import INDUSTRY_PER_POP, TECH_COST_MULTIPLIER
+from common.priorities import TARGET_AFTER_SCALING_PRIORITY
+
+Tech(
+    name="PRO_ROBOTIC_PROD",
+    description="PRO_ROBOTIC_PROD_DESC",
+    short_description="INDUSTRY_SHORT_DESC",
+    category="PRODUCTION_CATEGORY",
+    researchcost=12 * TECH_COST_MULTIPLIER,
+    researchturns=3,
+    tags=["PEDIA_PRODUCTION_CATEGORY"],
+    effectsgroups=[
+        EffectsGroup(
+            scope=ProductionCenter
+            & OwnedBy(empire=Source.Owner)
+            & Focus(type="FOCUS_INDUSTRY")
+            & Happiness(low=NamedReal(name="PRO_ROBOTIC_PROD_MIN_STABILITY", value=5)),
+            priority=TARGET_AFTER_SCALING_PRIORITY,
+            effects=SetTargetIndustry(
+                value=Value
+                + Target.Population
+                * NamedReal(name="PRO_ROBOTIC_PROD_TARGET_INDUSTRY_PERPOP", value=0.25 * INDUSTRY_PER_POP)
+            ),
+        )
+    ],
+    graphic="icons/tech/robotic_production.png",
+)
