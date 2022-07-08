8677731e747e13b005b05fd5361265ad11747ee7
O01eg <o01eg@yandex.ru>
2022-07-08 14:22:41 +0300

    Convert default/scripting/techs/production/NEUTRONIUM_EXTRACTION.focs

---
+++ default/scripting/techs/production/NEUTRONIUM_EXTRACTION.focs.py
@@ -0,0 +1,18 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="PRO_NEUTRONIUM_EXTRACTION",
+    description="PRO_NEUTRONIUM_EXTRACTION_DESC",
+    short_description="BUILDING_UNLOCK_SHORT_DESC",
+    category="PRODUCTION_CATEGORY",
+    researchcost=150 * TECH_COST_MULTIPLIER,
+    researchturns=5,
+    tags=["PEDIA_PRODUCTION_CATEGORY"],
+    prerequisites=["PRO_ZERO_GEN", "LRN_STELLAR_TOMOGRAPHY"],
+    unlock=[
+        Item(type=Building, name="BLD_NEUTRONIUM_EXTRACTOR"),
+        Item(type=Building, name="BLD_NEUTRONIUM_FORGE"),
+        Item(type=ShipPart, name="AR_NEUTRONIUM_PLATE"),
+    ],
+    graphic="icons/tech/neutronium_extraction.png",
+)
