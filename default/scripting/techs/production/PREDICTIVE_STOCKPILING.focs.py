8aacf26c4a262da2967767216166cf39a090c166
O01eg <o01eg@yandex.ru>
2022-07-08 14:40:43 +0300

    Convert default/scripting/techs/production/PREDICTIVE_STOCKPILING.focs

---
+++ default/scripting/techs/production/PREDICTIVE_STOCKPILING.focs.py
@@ -0,0 +1,16 @@
+Tech(
+    name="PRO_PREDICTIVE_STOCKPILING",
+    description="PRO_PREDICTIVE_STOCKPILING_DESC",
+    short_description="IMPERIAL_STOCKPILE_SHORT_DESC",
+    category="PRODUCTION_CATEGORY",
+    researchcost=1,
+    researchturns=1,
+    tags=["PEDIA_PRODUCTION_CATEGORY"],
+    effectsgroups=[
+        # Set initial meters
+        EffectsGroup(
+            scope=ProductionCenter & OwnedBy(empire=Source.Owner) & Focus(type="FOCUS_STOCKPILE"),
+            effects=SetMaxStockpile(value=Value + 1),
+        )
+    ],
+)
