4c72c827913af7fd10afb428a1aa3528806bb6ef
O01eg <o01eg@yandex.ru>
2022-07-08 14:33:06 +0300

    Convert default/scripting/techs/production/ORBITAL_GEN.focs

---
+++ default/scripting/techs/production/ORBITAL_GEN.focs.py
@@ -0,0 +1,20 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="PRO_ORBITAL_GEN",
+    description="PRO_ORBITAL_GEN_DESC",
+    short_description="BUILDING_UNLOCK_SHORT_DESC",
+    category="PRODUCTION_CATEGORY",
+    researchcost=80
+    * TECH_COST_MULTIPLIER
+    * (
+        1
+        - 0.25
+        * StatisticIf(float, condition=Source & EmpireHasAdoptedPolicy(empire=Source.Owner, name="PLC_INDUSTRIALISM"))
+    ),
+    researchturns=4,
+    tags=["PEDIA_PRODUCTION_CATEGORY"],
+    prerequisites=["PRO_FUSION_GEN", "CON_ORBITAL_CON"],
+    unlock=Item(type=Building, name="BLD_GAS_GIANT_GEN"),
+    graphic="icons/tech/orbital_generation.png",
+)
