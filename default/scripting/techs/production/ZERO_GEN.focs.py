3f2914969e81f4bf58623f0cd9b72e6b0fd8a329
O01eg <o01eg@yandex.ru>
2022-07-08 17:36:53 +0300

    Convert default/scripting/techs/production/ZERO_GEN.focs

---
+++ default/scripting/techs/production/ZERO_GEN.focs.py
@@ -0,0 +1,19 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="PRO_ZERO_GEN",
+    description="PRO_ZERO_GEN_DESC",
+    short_description="THEORY_SHORT_DESC",
+    category="PRODUCTION_CATEGORY",
+    researchcost=640
+    * TECH_COST_MULTIPLIER
+    * (
+        1
+        - 0.25
+        * StatisticIf(float, condition=Source & EmpireHasAdoptedPolicy(empire=Source.Owner, name="PLC_INDUSTRIALISM"))
+    ),
+    researchturns=7,
+    tags=["PEDIA_PRODUCTION_CATEGORY", "THEORY"],
+    prerequisites=["PRO_NDIM_ASSMB", "PRO_SINGULAR_GEN"],
+    graphic="icons/tech/zero_point_energy.png",
+)
