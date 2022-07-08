8b11c7557e70b14431f2d0eff31396bf8ac2809f
O01eg <o01eg@yandex.ru>
2022-07-08 11:35:44 +0300

    Convert default/scripting/techs/production/NANOTECH_PROD.focs

---
+++ default/scripting/techs/production/NANOTECH_PROD.focs.py
@@ -0,0 +1,13 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="PRO_NANOTECH_PROD",
+    description="PRO_NANOTECH_PROD_DESC",
+    short_description="THEORY_SHORT_DESC",
+    category="PRODUCTION_CATEGORY",
+    researchcost=45 * TECH_COST_MULTIPLIER,
+    researchturns=4,
+    tags=["PEDIA_PRODUCTION_CATEGORY", "THEORY"],
+    prerequisites="PRO_ROBOTIC_PROD",
+    graphic="icons/tech/nanotech_production.png",
+)
