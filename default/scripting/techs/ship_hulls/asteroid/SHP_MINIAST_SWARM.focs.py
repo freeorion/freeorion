33989937ef72b59e9b5217c1e882628de87b0d8d
O01eg <o01eg@yandex.ru>
2022-07-18 10:02:48 +0300

    Convert default/scripting/techs/ship_hulls/asteroid/SHP_MINIAST_SWARM.focs

---
+++ default/scripting/techs/ship_hulls/asteroid/SHP_MINIAST_SWARM.focs.py
@@ -0,0 +1,16 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_MINIAST_SWARM",
+    description="SHP_MINIAST_SWARM_DESC",
+    short_description="SHIP_HULL_UNLOCK_SHORT_DESC",
+    category="SHIP_HULLS_CATEGORY",
+    researchcost=80 * TECH_COST_MULTIPLIER,
+    researchturns=4,
+    tags=["PEDIA_ASTEROID_HULL_TECHS"],
+    prerequisites="SHP_ASTEROID_REFORM",
+    unlock=[
+        Item(type=UnlockShipHull, name="SH_MINIASTEROID_SWARM"),
+    ],
+    graphic="icons/ship_hulls/mini_asteroid_swarm_small.png",
+)
