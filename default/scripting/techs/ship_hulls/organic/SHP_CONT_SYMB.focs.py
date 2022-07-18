12b234e698fee7f19bed4269c353ec2b8b73bb91
O01eg <o01eg@yandex.ru>
2022-07-18 15:26:46 +0300

    Convert default/scripting/techs/ship_hulls/organic/SHP_CONT_SYMB.focs

---
+++ default/scripting/techs/ship_hulls/organic/SHP_CONT_SYMB.focs.py
@@ -0,0 +1,17 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_CONT_SYMB",
+    description="SHP_CONT_SYMB_DESC",
+    short_description="SHIP_HULL_UNLOCK_SHORT_DESC",
+    category="SHIP_HULLS_CATEGORY",
+    researchcost=16 * TECH_COST_MULTIPLIER,
+    researchturns=8,
+    tags=["PEDIA_ORGANIC_HULL_TECHS"],
+    prerequisites=["SHP_ORG_HULL"],
+    unlock=[
+        Item(type=UnlockShipHull, name="SH_SYMBIOTIC"),
+        Item(type=UnlockBuilding, name="BLD_SHIPYARD_ORG_CELL_GRO_CHAMB"),
+    ],
+    graphic="icons/ship_hulls/symbiotic_hull_small.png",
+)
