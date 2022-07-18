2004ef264459b7899bdcb9de27931ecd754ca567
O01eg <o01eg@yandex.ru>
2022-07-18 09:55:54 +0300

    Convert default/scripting/techs/ship_hulls/asteroid/SHP_HEAVY_AST_HULL.focs.py

---
+++ default/scripting/techs/ship_hulls/asteroid/SHP_HEAVY_AST_HULL.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_HEAVY_AST_HULL",
+    description="SHP_HEAVY_AST_HULL_DESC",
+    short_description="SHIP_HULL_UNLOCK_SHORT_DESC",
+    category="SHIP_HULLS_CATEGORY",
+    researchcost=80 * TECH_COST_MULTIPLIER,
+    researchturns=8,
+    tags=["PEDIA_ASTEROID_HULL_TECHS"],
+    prerequisites="SHP_ASTEROID_REFORM",
+    unlock=[Item(type=UnlockShipHull, name="SH_HEAVY_ASTEROID")],
+    graphic="icons/ship_hulls/heavy_asteroid_hull_small.png",
+)
