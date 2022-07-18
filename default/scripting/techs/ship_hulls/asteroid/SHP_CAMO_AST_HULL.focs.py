77dea533b3d5941cef01bfe4c530ec981b469b73
O01eg <o01eg@yandex.ru>
2022-07-18 09:50:29 +0300

    Convert default/scripting/techs/ship_hulls/asteroid/SHP_CAMO_AST_HULL.focs

---
+++ default/scripting/techs/ship_hulls/asteroid/SHP_CAMO_AST_HULL.focs.py
@@ -0,0 +1,17 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_CAMO_AST_HULL",
+    description="SHP_CAMO_AST_HULL_DESC",
+    short_description="SHIP_HULL_UNLOCK_SHORT_DESC",
+    category="SHIP_HULLS_CATEGORY",
+    researchcost=26 * TECH_COST_MULTIPLIER,
+    researchturns=2,
+    tags=["PEDIA_ASTEROID_HULL_TECHS"],
+    prerequisites="SHP_ASTEROID_HULLS",
+    unlock=[
+        Item(type=UnlockShipHull, name="SH_SMALL_CAMOUFLAGE_ASTEROID"),
+        Item(type=UnlockShipHull, name="SH_CAMOUFLAGE_ASTEROID"),
+    ],
+    graphic="icons/ship_hulls/camouflage_asteroid_hull_small.png",
+)
