b3a1ca87a3b4237c686dc2f9a699e56cec927f14
O01eg <o01eg@yandex.ru>
2022-07-18 15:03:31 +0300

    Convert default/scripting/techs/ship_hulls/energy/SHP_SOLAR_CONT.focs

---
+++ default/scripting/techs/ship_hulls/energy/SHP_SOLAR_CONT.focs.py
@@ -0,0 +1,17 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_SOLAR_CONT",
+    description="SHP_SOLAR_CONT_DESC",
+    short_description="SHIP_HULL_UNLOCK_SHORT_DESC",
+    category="SHIP_HULLS_CATEGORY",
+    researchcost=950 * TECH_COST_MULTIPLIER,
+    researchturns=7,
+    tags=["PEDIA_ENERGY_HULL_TECHS"],
+    prerequisites=["SHP_QUANT_ENRG_MAG", "SHP_ENRG_BOUND_MAN", "PRO_ZERO_GEN"],
+    unlock=[
+        Item(type=UnlockShipHull, name="SH_SOLAR")
+        # Item type = Building name = "BLD_SHIPYARD_ENRG_SOLAR"
+    ],
+    graphic="icons/ship_hulls/solar_hull_small.png",
+)
