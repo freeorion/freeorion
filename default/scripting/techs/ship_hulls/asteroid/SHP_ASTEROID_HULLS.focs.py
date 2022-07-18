1a34c63466d6f9c7d6e2821272b3bda9d6d504b2
O01eg <o01eg@yandex.ru>
2022-07-18 09:23:51 +0300

    Convert default/scripting/techs/ship_hulls/asteroid/SHP_ASTEROID_HULLS.focs

---
+++ default/scripting/techs/ship_hulls/asteroid/SHP_ASTEROID_HULLS.focs.py
@@ -0,0 +1,18 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_ASTEROID_HULLS",
+    description="SHP_ASTEROID_HULLS_DESC",
+    short_description="SHIP_HULL_UNLOCK_SHORT_DESC",
+    category="SHIP_HULLS_CATEGORY",
+    researchcost=25 * TECH_COST_MULTIPLIER,
+    researchturns=3,
+    tags=["PEDIA_ASTEROID_HULL_TECHS"],
+    prerequisites="PRO_MICROGRAV_MAN",
+    unlock=[
+        Item(type=UnlockShipHull, name="SH_SMALL_ASTEROID"),
+        Item(type=UnlockShipHull, name="SH_ASTEROID"),
+        Item(type=UnlockBuilding, name="BLD_SHIPYARD_AST"),
+    ],
+    graphic="icons/ship_hulls/asteroid_hull_small.png",
+)
