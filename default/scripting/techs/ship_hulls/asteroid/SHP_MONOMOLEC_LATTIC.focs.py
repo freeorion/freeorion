c390721dc451e65e58a4842e109ba65572fdf9b5
O01eg <o01eg@yandex.ru>
2022-07-18 11:29:26 +0300

    Convert default/scripting/techs/ship_hulls/asteroid/SHP_MONOMOLEC_LATTIC.focs

---
+++ default/scripting/techs/ship_hulls/asteroid/SHP_MONOMOLEC_LATTIC.focs.py
@@ -0,0 +1,17 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_MONOMOLEC_LATTICE",
+    description="SHP_MONOMOLEC_LATTICE_DESC",
+    short_description="SHIP_PART_UNLOCK_SHORT_DESC",
+    category="SHIP_HULLS_CATEGORY",
+    researchcost=800 * TECH_COST_MULTIPLIER,
+    researchturns=5,
+    tags=["PEDIA_ASTEROID_HULL_TECHS"],
+    prerequisites="SHP_ASTEROID_REFORM",
+    unlock=[
+        Item(type=UnlockShipHull, name="SH_CRYSTALLIZED_ASTEROID"),
+        Item(type=UnlockShipPart, name="AR_CRYSTAL_PLATE"),
+    ],
+    graphic="icons/ship_parts/crystal_plating.png",
+)
