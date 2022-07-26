6437f49da6f2b240ec25490766dda5e3f2600b76
O01eg <o01eg@yandex.ru>
2022-07-26 12:14:00 +0300

    Convert default/scripting/techs/ship_hulls/organic/SHP_ENDOCRINE_SYSTEMS.focs

---
+++ default/scripting/techs/ship_hulls/organic/SHP_ENDOSYMB_HULL.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_ENDOSYMB_HULL",
+    description="SHP_ENDOSYMB_HULL_DESC",
+    short_description="SHIP_HULL_UNLOCK_SHORT_DESC",
+    category="SHIP_HULLS_CATEGORY",
+    researchcost=72 * TECH_COST_MULTIPLIER,
+    researchturns=4,
+    tags=["PEDIA_ORGANIC_HULL_TECHS"],
+    prerequisites=["SHP_MONOCELL_EXP", "SHP_ENDOCRINE_SYSTEMS"],
+    unlock=[Item(type=UnlockShipHull, name="SH_ENDOSYMBIOTIC")],
+    graphic="icons/ship_hulls/endosymbiotic_hull_small.png",
+)
