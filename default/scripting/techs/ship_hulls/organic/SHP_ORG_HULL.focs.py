449409b6c8022d825e5a096acfcf0828e260a3d4
O01eg <o01eg@yandex.ru>
2022-07-26 14:45:31 +0300

    Convert default/scripting/techs/ship_hulls/organic/SHP_ORG_HULL.focs

---
+++ default/scripting/techs/ship_hulls/organic/SHP_ORG_HULL.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_ORG_HULL",
+    description="SHP_ORG_HULL_DESC",
+    short_description="SHIP_HULL_UNLOCK_SHORT_DESC",
+    category="SHIP_HULLS_CATEGORY",
+    researchcost=8 * TECH_COST_MULTIPLIER,
+    researchturns=5,
+    tags=["PEDIA_ORGANIC_HULL_TECHS"],
+    prerequisites="SHP_DOMESTIC_MONSTER",
+    unlock=[Item(type=UnlockShipHull, name="SH_ORGANIC"), Item(type=UnlockBuilding, name="BLD_SHIPYARD_ORG_ORB_INC")],
+    graphic="hulls_design/organic_hull.png",
+)
