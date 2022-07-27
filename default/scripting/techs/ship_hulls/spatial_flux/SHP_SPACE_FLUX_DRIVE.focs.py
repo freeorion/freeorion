ee7ea2bdd7373435493c18add8f6e84ac5f4de11
O01eg <o01eg@yandex.ru>
2022-07-27 12:43:27 +0300

    Convert default/scripting/techs/ship_hulls/spatial_flux/SHP_SPACE_FLUX_DRIVE.focs

---
+++ default/scripting/techs/ship_hulls/spatial_flux/SHP_SPACE_FLUX_DRIVE.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_SPACE_FLUX_DRIVE",
+    description="SHP_SPACE_FLUX_DRIVE_DESC",
+    short_description="SHIP_HULL_UNLOCK_SHORT_DESC",
+    category="SHIP_HULLS_CATEGORY",
+    researchcost=20 * TECH_COST_MULTIPLIER,
+    researchturns=4,
+    tags=["PEDIA_FLUX_HULL_TECHS"],
+    prerequisites=["SHP_SPACE_FLUX_BASICS"],
+    unlock=Item(type=UnlockShipHull, name="SH_SPATIAL_FLUX"),
+    graphic="icons/ship_hulls/spatial_flux_hull_small.png",
+)
