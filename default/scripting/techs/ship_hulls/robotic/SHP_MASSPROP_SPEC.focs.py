d207d5c934286819832d91ba830df46f2e14f5c6
O01eg <o01eg@yandex.ru>
2022-07-27 10:28:03 +0300

    Convert default/scripting/techs/ship_hulls/robotic/SHP_MASSPROP_SPEC.focs

---
+++ default/scripting/techs/ship_hulls/robotic/SHP_MASSPROP_SPEC.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_MASSPROP_SPEC",
+    description="SHP_MASSPROP_SPEC_DESC",
+    short_description="SHIP_HULL_UNLOCK_SHORT_DESC",
+    category="SHIP_HULLS_CATEGORY",
+    researchcost=800 * TECH_COST_MULTIPLIER,
+    researchturns=10,
+    tags=["PEDIA_ROBOTIC_HULL_TECHS"],
+    prerequisites="SHP_CONTGRAV_MAINT",
+    unlock=[Item(type=UnlockShipHull, name="SH_TITANIC")],
+    graphic="icons/ship_hulls/titanic_hull_small.png",
+)
