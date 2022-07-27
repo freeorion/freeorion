28af354dfc604afc450ce6c77b13a7ccefd2d404
O01eg <o01eg@yandex.ru>
2022-07-27 10:36:54 +0300

    Convert default/scripting/techs/ship_hulls/robotic/SHP_MIDCOMB_LOG.focs

---
+++ default/scripting/techs/ship_hulls/robotic/SHP_MIDCOMB_LOG.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_MIDCOMB_LOG",
+    description="SHP_MIDCOMB_LOG_DESC",
+    short_description="SHIP_HULL_UNLOCK_SHORT_DESC",
+    category="SHIP_HULLS_CATEGORY",
+    researchcost=800 * TECH_COST_MULTIPLIER,
+    researchturns=8,
+    tags=["PEDIA_ROBOTIC_HULL_TECHS"],
+    prerequisites=["SHP_MASSPROP_SPEC", "SHP_TRANSSPACE_DRIVE", "SHP_INTSTEL_LOG"],
+    unlock=[Item(type=UnlockShipHull, name="SH_LOGISTICS_FACILITATOR")],
+    graphic="icons/ship_hulls/logistics_facilitator_hull_small.png",
+)
