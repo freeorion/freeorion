a8fb9b000781be426b30c897450ac2eef0cf7741
O01eg <o01eg@yandex.ru>
2022-07-27 12:17:16 +0300

    Convert default/scripting/techs/ship_hulls/spatial_flux/SHP_SPACE_FLUX_COMPOSITION.focs

---
+++ default/scripting/techs/ship_hulls/spatial_flux/SHP_SPACE_FLUX_COMPOSITION.focs.py
@@ -0,0 +1,15 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_SPACE_FLUX_COMPOSITION",
+    description="SHP_SPACE_FLUX_COMPOSITION_DESC",
+    short_description="SHIP_HULL_UNLOCK_SHORT_DESC",
+    category="SHIP_HULLS_CATEGORY",
+    researchcost=60 * TECH_COST_MULTIPLIER,
+    researchturns=6,
+    tags=["PEDIA_FLUX_HULL_TECHS"],
+    prerequisites=["SHP_SPACE_FLUX_BUBBLE", "SHP_SPACE_FLUX_DRIVE"],
+    unlock=Item(type=UnlockShipHull, name="SH_SPACE_FLUX_COMPOSITE"),
+    # FIXME graphic
+    graphic="icons/ship_hulls/agregate_asteroid_hull_small.png",
+)
