9baca6ddb6829b96e556980ce1900a2ec029a2d3
O01eg <o01eg@yandex.ru>
2022-07-27 12:10:30 +0300

    Convert default/scripting/techs/ship_hulls/spatial_flux/SHP_SPACE_FLUX_BUBBLE.focs

---
+++ default/scripting/techs/ship_hulls/spatial_flux/SHP_SPACE_FLUX_BUBBLE.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_SPACE_FLUX_BUBBLE",
+    description="SHP_SPACE_FLUX_BUBBLE_DESC",
+    short_description="SHIP_HULL_UNLOCK_SHORT_DESC",
+    category="SHIP_HULLS_CATEGORY",
+    researchcost=12 * TECH_COST_MULTIPLIER,
+    researchturns=3,
+    tags=["PEDIA_FLUX_HULL_TECHS"],
+    prerequisites=["SHP_SPACE_FLUX_BASICS"],
+    unlock=Item(type=UnlockShipHull, name="SH_SPACE_FLUX_BUBBLE"),
+    graphic="icons/ship_hulls/bulk_freighter_hull_small.png",
+)
