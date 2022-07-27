7bf2d4961d1dd5f081d686443deffa47f1d23174
O01eg <o01eg@yandex.ru>
2022-07-27 10:57:13 +0300

    Convert default/scripting/techs/ship_hulls/robotic/SHP_MIL_ROBO_CONT.focs

---
+++ default/scripting/techs/ship_hulls/robotic/SHP_MIL_ROBO_CONT.focs.py
@@ -0,0 +1,17 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_MIL_ROBO_CONT",
+    description="SHP_MIL_ROBO_CONT_DESC",
+    short_description="SHIP_HULL_UNLOCK_SHORT_DESC",
+    category="SHIP_HULLS_CATEGORY",
+    researchcost=12 * TECH_COST_MULTIPLIER,
+    researchturns=3,
+    tags=["PEDIA_ROBOTIC_HULL_TECHS"],
+    prerequisites="PRO_ROBOTIC_PROD",
+    unlock=[
+        Item(type=UnlockShipHull, name="SH_ROBOTIC"),
+        Item(type=UnlockShipPart, name="SH_ROBOTIC_INTERFACE_SHIELDS"),
+    ],
+    graphic="icons/ship_hulls/robotic_hull_small.png",
+)
