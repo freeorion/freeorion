2e355f1f823f5d34d9a698432d722d1939614d4d
O01eg <o01eg@yandex.ru>
2022-07-27 10:15:28 +0300

    Convert default/scripting/techs/ship_hulls/organic/SHP_SENT_HULL.focs

---
+++ default/scripting/techs/ship_hulls/organic/SHP_SENT_HULL.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_SENT_HULL",
+    description="SHP_SENT_HULL_DESC",
+    short_description="SHIP_HULL_UNLOCK_SHORT_DESC",
+    category="SHIP_HULLS_CATEGORY",
+    researchcost=200 * TECH_COST_MULTIPLIER,
+    researchturns=5,
+    tags=["PEDIA_ORGANIC_HULL_TECHS"],
+    prerequisites=["SHP_BIOADAPTIVE_SPEC", "SHP_CONT_BIOADAPT"],
+    unlock=[Item(type=UnlockShipHull, name="SH_SENTIENT")],
+    graphic="icons/ship_hulls/sentient_hull_small.png",
+)
