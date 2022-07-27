86186db05b1ca1b1d76dac0c6acd70118f624a3d
O01eg <o01eg@yandex.ru>
2022-07-27 14:21:42 +0300

    Convert default/scripting/techs/ship_parts/armor/SHP_DIAMOND_PLATE.focs

---
+++ default/scripting/techs/ship_parts/armor/SHP_DIAMOND_PLATE.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_DIAMOND_PLATE",
+    description="SHP_DIAMOND_PLATE_DESC",
+    short_description="SHIP_PART_UNLOCK_SHORT_DESC",
+    category="SHIP_PARTS_CATEGORY",
+    researchcost=150 * TECH_COST_MULTIPLIER,
+    researchturns=4,
+    tags=["PEDIA_ARMOR_PART_TECHS"],
+    prerequisites="SHP_ZORTRIUM_PLATE",
+    unlock=Item(type=UnlockShipPart, name="AR_DIAMOND_PLATE"),
+    graphic="icons/tech/armor_plating.png",
+)
