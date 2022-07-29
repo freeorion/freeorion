fa1ce5763dcb861b3deb96695de88d29d5c67159
O01eg <o01eg@yandex.ru>
2022-07-29 09:06:41 +0300

    Convert default/scripting/techs/ship_parts/damage_control/SHP_REINFORCED_HULL.focs

---
+++ default/scripting/techs/ship_parts/damage_control/SHP_REINFORCED_HULL.focs.py
@@ -0,0 +1,17 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_REINFORCED_HULL",
+    description="SHP_REINFORCED_HULL_DESC",
+    short_description="STRUCTURE_SHORT_DESC",
+    category="SHIP_PARTS_CATEGORY",
+    researchcost=36 * TECH_COST_MULTIPLIER,
+    researchturns=3,
+    tags=["PEDIA_DAMAGE_CONTROL_PART_TECHS"],
+    prerequisites="CON_ARCH_MONOFILS",
+    effectsgroups=EffectsGroup(
+        scope=Ship & OwnedBy(empire=Source.Owner),
+        effects=SetMaxStructure(value=Value + NamedRealLookup(name="SHP_REINFORCED_HULL_BONUS")),
+    ),
+    graphic="icons/tech/structural_integrity_fields.png",
+)
