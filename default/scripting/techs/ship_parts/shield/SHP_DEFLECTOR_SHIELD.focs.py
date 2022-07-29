2cbc86affd0d2eb6247336a0bda8d7b6e31225b1
O01eg <o01eg@yandex.ru>
2022-07-29 10:40:56 +0300

    Convert default/scripting/techs/ship_parts/shield/SHP_DEFLECTOR_SHIELD.focs

---
+++ default/scripting/techs/ship_parts/shield/SHP_DEFLECTOR_SHIELD.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_DEFLECTOR_SHIELD",
+    description="SHP_DEFLECTOR_SHIELD_DESC",
+    short_description="SHIP_PART_UNLOCK_SHORT_DESC",
+    category="SHIP_PARTS_CATEGORY",
+    researchcost=150 * TECH_COST_MULTIPLIER,
+    researchturns=10,
+    tags=["PEDIA_SHIELD_PART_TECHS"],
+    prerequisites="LRN_FORCE_FIELD",
+    unlock=Item(type=UnlockShipPart, name="SH_DEFLECTOR"),
+    graphic="icons/ship_parts/deflector_shield.png",
+)
