e651fa6082d864c0729e32bfe1f5b824b675f0d6
O01eg <o01eg@yandex.ru>
2022-07-29 10:35:31 +0300

    default/scripting/techs/ship_parts/shield/SHP_BLACKSHIELD.focs

---
+++ default/scripting/techs/ship_parts/shield/SHP_BLACKSHIELD.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_BLACKSHIELD",
+    description="SHP_BLACKSHIELD_DESC",
+    short_description="SHIP_PART_UNLOCK_SHORT_DESC",
+    category="SHIP_PARTS_CATEGORY",
+    researchcost=3750 * TECH_COST_MULTIPLIER,
+    researchturns=10,
+    tags=["PEDIA_SHIELD_PART_TECHS"],
+    prerequisites="SHP_PLASMA_SHIELD",
+    unlock=Item(type=UnlockShipPart, name="SH_BLACK"),
+    graphic="icons/ship_parts/blackshield.png",
+)
