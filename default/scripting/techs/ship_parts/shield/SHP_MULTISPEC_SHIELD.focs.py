70715d6b2cd8e79eac9d434139fa3d43c22c355e
O01eg <o01eg@yandex.ru>
2022-07-29 10:46:32 +0300

    Convert default/scripting/techs/ship_parts/shield/SHP_MULTISPEC_SHIELD.focs

---
+++ default/scripting/techs/ship_parts/shield/SHP_MULTISPEC_SHIELD.focs.py
@@ -0,0 +1,15 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_MULTISPEC_SHIELD",
+    description="SHP_MULTISPEC_SHIELD_DESC",
+    short_description="SHIP_PART_UNLOCK_SHORT_DESC",
+    category="SHIP_PARTS_CATEGORY",
+    researchcost=2000 * TECH_COST_MULTIPLIER,
+    researchturns=10,
+    # Unresearchable
+    tags=["PEDIA_SHIELD_PART_TECHS"],
+    prerequisites=["SHP_PLASMA_SHIELD", "SPY_DIST_MOD"],
+    unlock=Item(type=UnlockShipPart, name="SH_MULTISPEC"),
+    graphic="icons/ship_parts/multi-spectral.png",
+)
