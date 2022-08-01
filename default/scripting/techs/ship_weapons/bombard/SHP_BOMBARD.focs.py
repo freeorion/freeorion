94c606d76ccf9807a56bb882dbe6f9a35de4cbf4
O01eg <o01eg@yandex.ru>
2022-08-01 15:59:00 +0300

    Convert default/scripting/techs/ship_weapons/bombard/SHP_BOMBARD.focs

---
+++ default/scripting/techs/ship_weapons/bombard/SHP_BOMBARD.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_BOMBARD",
+    description="SHP_BOMBARD_DESC",
+    short_description="SHIP_WEAPON_UNLOCK_SHORT_DESC",
+    category="SHIP_WEAPONS_CATEGORY",
+    researchcost=100 * TECH_COST_MULTIPLIER,
+    researchturns=5,
+    tags=["PEDIA_BOMBARD_WEAPON_TECHS"],
+    prerequisites="SHP_ROOT_AGGRESSION",
+    unlock=Item(type=UnlockPolicy, name="PLC_TERROR_SUPPRESSION"),
+    graphic="",
+)
