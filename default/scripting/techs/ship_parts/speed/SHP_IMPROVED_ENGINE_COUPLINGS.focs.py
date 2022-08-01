0698a7d7df680a515b05423313bec16ccbdb6948
O01eg <o01eg@yandex.ru>
2022-08-01 11:55:35 +0300

    Convert default/scripting/techs/ship_parts/speed/SHP_IMPROVED_ENGINE_COUPLINGS.focs

---
+++ default/scripting/techs/ship_parts/speed/SHP_IMPROVED_ENGINE_COUPLINGS.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_IMPROVED_ENGINE_COUPLINGS",
+    description="SHP_IMPROVED_ENGINE_COUPLINGS_DESC",
+    short_description="SHIP_PART_UNLOCK_SHORT_DESC",
+    category="SHIP_PARTS_CATEGORY",
+    researchcost=24 * TECH_COST_MULTIPLIER,
+    researchturns=3,
+    tags=["PEDIA_ENGINE_PART_TECHS"],
+    prerequisites="SHP_GAL_EXPLO",
+    unlock=[Item(type=UnlockShipPart, name="FU_IMPROVED_ENGINE_COUPLINGS"), Item(type=UnlockPolicy, name="PLC_CHARGE")],
+    graphic="icons/ship_parts/engine-1.png",
+)
