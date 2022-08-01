78a82c83084b275fbea962a1593dafd4824d2d29
O01eg <o01eg@yandex.ru>
2022-08-01 16:17:41 +0300

    Convert default/scripting/techs/ship_weapons/bombard/SHP_CHAOS_WAVE.focs

---
+++ default/scripting/techs/ship_weapons/bombard/SHP_CHAOS_WAVE.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_CHAOS_WAVE",
+    description="SHP_CHAOS_WAVE_DESC",
+    short_description="SHIP_PART_UNLOCK_SHORT_DESC",
+    category="SHIP_WEAPONS_CATEGORY",
+    researchcost=1500 * TECH_COST_MULTIPLIER,
+    researchturns=5,
+    tags=["PEDIA_BOMBARD_WEAPON_TECHS"],
+    prerequisites=["SHP_BIOTERM", "SHP_GRV", "SHP_EMO", "SHP_VOID_SHADOW"],
+    unlock=Item(type=UnlockShipPart, name="SP_CHAOS_WAVE"),
+    graphic="icons/ship_parts/chaos-wave.png",
+)
