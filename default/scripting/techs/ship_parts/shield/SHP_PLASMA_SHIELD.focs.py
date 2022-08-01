29f26e9b38fed78432a3c2131d94c49e00bf17be
O01eg <o01eg@yandex.ru>
2022-08-01 11:37:39 +0300

    Convert default/scripting/techs/ship_parts/shield/SHP_PLASMA_SHIELD.focs

---
+++ default/scripting/techs/ship_parts/shield/SHP_PLASMA_SHIELD.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_PLASMA_SHIELD",
+    description="SHP_PLASMA_SHIELD_DESC",
+    short_description="SHIP_PART_UNLOCK_SHORT_DESC",
+    category="SHIP_PARTS_CATEGORY",
+    researchcost=750 * TECH_COST_MULTIPLIER,
+    researchturns=10,
+    tags=["PEDIA_SHIELD_PART_TECHS"],
+    prerequisites="SHP_DEFLECTOR_SHIELD",
+    unlock=Item(type=UnlockShipPart, name="SH_PLASMA"),
+    graphic="icons/ship_parts/plasma_shield.png",
+)
