8c264c0575bd3426c418bb622ebcc08d19de3545
O01eg <o01eg@yandex.ru>
2022-08-02 15:39:59 +0300

    Convert default/scripting/techs/ship_weapons/fighters/SHP_FIGHTERS_1.focs

---
+++ default/scripting/techs/ship_weapons/fighters/SHP_FIGHTERS_1.focs.py
@@ -0,0 +1,20 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_FIGHTERS_1",
+    description="SHP_FIGHTERS_1_DESC",
+    short_description="SHIP_WEAPON_UNLOCK_SHORT_DESC",
+    category="SHIP_WEAPONS_CATEGORY",
+    researchcost=6 * TECH_COST_MULTIPLIER,
+    researchturns=3,
+    tags=["PEDIA_FIGHTER_TECHS"],
+    prerequisites="SHP_ROOT_AGGRESSION",
+    unlock=[
+        Item(type=UnlockShipPart, name="FT_BAY_1"),
+        Item(type=UnlockShipPart, name="FT_HANGAR_1"),
+        Item(type=UnlockShipPart, name="FT_HANGAR_2"),
+        Item(type=UnlockShipPart, name="FT_HANGAR_3"),
+        Item(type=UnlockShipPart, name="FT_HANGAR_4"),
+    ],
+    graphic="icons/ship_parts/fighter05.png",
+)
