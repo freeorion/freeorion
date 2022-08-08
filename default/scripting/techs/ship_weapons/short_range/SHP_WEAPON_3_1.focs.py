c612e368aaa1e538fae332a6583f5b04b13b4b4c
O01eg <o01eg@yandex.ru>
2022-08-08 11:34:11 +0300

    Convert default/scripting/techs/ship_weapons/short_range/SHP_WEAPON_3_1.focs

---
+++ default/scripting/techs/ship_weapons/short_range/SHP_WEAPON_3_1.focs.py
@@ -0,0 +1,16 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+from techs.ship_weapons.ship_weapons import WEAPON_BASE_EFFECTS
+
+Tech(
+    name="SHP_WEAPON_3_1",
+    description="SHP_WEAPON_3_1_DESC",
+    short_description="SHIP_WEAPON_UNLOCK_SHORT_DESC",
+    category="SHIP_WEAPONS_CATEGORY",
+    researchcost=150 * TECH_COST_MULTIPLIER,
+    researchturns=8,
+    tags=["PEDIA_SR_WEAPON_TECHS"],
+    prerequisites="SHP_WEAPON_2_1",
+    unlock=Item(type=UnlockShipPart, name="SR_WEAPON_3_1"),
+    effectsgroups=WEAPON_BASE_EFFECTS("SR_WEAPON_3_1"),
+    graphic="icons/ship_parts/plasma-1.png",
+)
