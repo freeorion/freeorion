44dbd54076ad13ac79b665e2f3b7a4f0423ee069
O01eg <o01eg@yandex.ru>
2022-08-08 11:02:03 +0300

    Convert default/scripting/techs/ship_weapons/short_range/SHP_WEAPON_2_4.focs

---
+++ default/scripting/techs/ship_weapons/short_range/SHP_WEAPON_2_4.focs.py
@@ -0,0 +1,15 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+from techs.ship_weapons.ship_weapons import WEAPON_UPGRADE_CAPACITY_EFFECTS
+
+Tech(
+    name="SHP_WEAPON_2_4",
+    description="SHP_WEAPON_2_4_DESC",
+    short_description="SHIP_WEAPON_IMPROVE_SHORT_DESC",
+    category="SHIP_WEAPONS_CATEGORY",
+    researchcost=50 * TECH_COST_MULTIPLIER,
+    researchturns=2,
+    tags=["PEDIA_SR_WEAPON_TECHS"],
+    prerequisites="SHP_WEAPON_2_3",
+    effectsgroups=WEAPON_UPGRADE_CAPACITY_EFFECTS("SR_WEAPON_2_1", 2),
+    graphic="icons/ship_parts/laser-4.png",
+)
