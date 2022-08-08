a2b71b99ec17ec85a49c2ed600e1cc9e1d624c00
O01eg <o01eg@yandex.ru>
2022-08-08 12:02:59 +0300

    Convert default/scripting/techs/ship_weapons/short_range/SHP_WEAPON_3_2.focs

---
+++ default/scripting/techs/ship_weapons/short_range/SHP_WEAPON_3_2.focs.py
@@ -0,0 +1,15 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+from techs.ship_weapons.ship_weapons import WEAPON_UPGRADE_CAPACITY_EFFECTS
+
+Tech(
+    name="SHP_WEAPON_3_2",
+    description="SHP_WEAPON_3_2_DESC",
+    short_description="SHIP_WEAPON_IMPROVE_SHORT_DESC",
+    category="SHIP_WEAPONS_CATEGORY",
+    researchcost=100 * TECH_COST_MULTIPLIER,
+    researchturns=2,
+    tags=["PEDIA_SR_WEAPON_TECHS"],
+    prerequisites="SHP_WEAPON_3_1",
+    effectsgroups=WEAPON_UPGRADE_CAPACITY_EFFECTS("SR_WEAPON_3_1", 3),
+    graphic="icons/ship_parts/plasma-2.png",
+)
