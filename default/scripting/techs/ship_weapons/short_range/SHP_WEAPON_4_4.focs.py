f2af2328443c09f9bbd40bd47dda07e1d9cf7154
O01eg <o01eg@yandex.ru>
2022-08-09 09:01:02 +0300

    Convert default/scripting/techs/ship_weapons/short_range/SHP_WEAPON_4_4.focs

---
+++ default/scripting/techs/ship_weapons/short_range/SHP_WEAPON_4_4.focs.py
@@ -0,0 +1,15 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+from techs.ship_weapons.ship_weapons import WEAPON_UPGRADE_CAPACITY_EFFECTS
+
+Tech(
+    name="SHP_WEAPON_4_4",
+    description="SHP_WEAPON_4_4_DESC",
+    short_description="SHIP_WEAPON_IMPROVE_SHORT_DESC",
+    category="SHIP_WEAPONS_CATEGORY",
+    researchcost=1250 * TECH_COST_MULTIPLIER,
+    researchturns=3,
+    tags=["PEDIA_SR_WEAPON_TECHS"],
+    prerequisites="SHP_WEAPON_4_3",
+    effectsgroups=WEAPON_UPGRADE_CAPACITY_EFFECTS("SR_WEAPON_4_1", 5),
+    graphic="icons/ship_parts/death-ray-4.png",
+)
