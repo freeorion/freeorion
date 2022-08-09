1254a8be812d57dfc85edf5c6080815dac9a740e
O01eg <o01eg@yandex.ru>
2022-08-09 12:36:52 +0300

    Convert default/scripting/techs/specials/COL_BANFORO.focs

---
+++ default/scripting/techs/specials/COL_BANFORO.focs.py
@@ -0,0 +1,11 @@
+Tech(
+    name="TECH_COL_BANFORO",
+    description="TECH_COL_BANFORO_DESC",
+    short_description="EXTINCT_UNLOCK_SHORT_DESC",
+    category="SPY_CATEGORY",
+    researchcost=1,
+    researchturns=1,
+    researchable=False,
+    tags=["PEDIA_EXTINCT_SPECIES_TECHS", "SP_BANFORO", "CTRL_EXTINCT"],
+    graphic="icons/species/banforo.png",
+)
