d002d1e1bad1a151ca9ec18639dbe2037dc44899
O01eg <o01eg@yandex.ru>
2022-08-09 12:45:28 +0300

    Convert default/scripting/techs/specials/COL_KILANDOW.focs

---
+++ default/scripting/techs/specials/COL_KILANDOW.focs.py
@@ -0,0 +1,11 @@
+Tech(
+    name="TECH_COL_KILANDOW",
+    description="TECH_COL_KILANDOW_DESC",
+    short_description="EXTINCT_UNLOCK_SHORT_DESC",
+    category="SPY_CATEGORY",
+    researchcost=1,
+    researchturns=1,
+    researchable=False,
+    tags=["PEDIA_EXTINCT_SPECIES_TECHS", "SP_KILANDOW", "CTRL_EXTINCT"],
+    graphic="icons/species/kilandow.png",
+)
