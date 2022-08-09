fcfb616a31ad473a410e13223964da6cd77e6ff1
O01eg <o01eg@yandex.ru>
2022-08-09 11:54:14 +0300

    Convert default/scripting/techs/ship_weapons/SHP_NOVA_BOMB.focs

---
+++ default/scripting/techs/ship_weapons/SHP_NOVA_BOMB.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SHP_NOVA_BOMB",
+    description="SHP_NOVA_BOMB_DESC",
+    short_description="SHIP_PART_UNLOCK_SHORT_DESC",
+    category="SHIP_WEAPONS_CATEGORY",
+    researchcost=750 * TECH_COST_MULTIPLIER,
+    researchturns=12,
+    tags=["PEDIA_SHIP_WEAPONS_CATEGORY"],
+    prerequisites=["LRN_STELLAR_TOMOGRAPHY", "PRO_ZERO_GEN"],
+    unlock=[Item(type=UnlockShipPart, name="SP_NOVA_BOMB"), Item(type=UnlockBuilding, name="BLD_NOVA_BOMB_ACTIVATOR")],
+    graphic="icons/ship_parts/nova-bomb.png",
+)
