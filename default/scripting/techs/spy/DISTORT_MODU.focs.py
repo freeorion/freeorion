99f1bf4d1b5d8de2cf0a4c4ddbedb186a21654fe
O01eg <o01eg@yandex.ru>
2022-08-10 14:02:45 +0300

    Convert default/scripting/techs/spy/DISTORT_MODU.focs

---
+++ default/scripting/techs/spy/DISTORT_MODU.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SPY_DIST_MOD",
+    description="SPY_DIST_MOD_DESC",
+    short_description="SHIP_PART_UNLOCK_SHORT_DESC",
+    category="SPY_CATEGORY",
+    researchcost=75 * TECH_COST_MULTIPLIER,
+    researchturns=4,
+    tags=["PEDIA_DETECTION_PART_TECHS"],
+    prerequisites="LRN_EVERYTHING",
+    unlock=Item(type=UnlockShipPart, name="SP_DISTORTION_MODULATOR"),
+    graphic="icons/ship_parts/distortion_modulator.png",
+)
