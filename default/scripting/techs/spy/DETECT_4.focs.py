96239c7d4ccc4a9e32bdeb3d04191f99c94af76c
O01eg <o01eg@yandex.ru>
2022-08-10 12:51:47 +0300

    Convert default/scripting/techs/spy/DETECT_4.focs

---
+++ default/scripting/techs/spy/DETECT_4.focs.py
@@ -0,0 +1,25 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SPY_DETECT_4",
+    description="SPY_DETECT_4_DESC",
+    short_description="DETECTION_SHORT_DESC",
+    category="SPY_CATEGORY",
+    researchcost=500 * TECH_COST_MULTIPLIER,
+    researchturns=7,
+    tags=["PEDIA_SPY_CATEGORY"],
+    prerequisites="SPY_DETECT_3",
+    unlock=Item(type=UnlockShipPart, name="DT_DETECTOR_4"),
+    effectsgroups=[
+        EffectsGroup(
+            scope=Planet() & OwnedBy(empire=Source.Owner),
+            activation=~OwnerHasTech(name="SPY_DETECT_5"),
+            effects=SetDetection(value=Value + NamedReal(name="SPY_DETECT_4_RANGE", value=125.0)),
+        ),
+        EffectsGroup(
+            scope=Source,
+            activation=~OwnerHasTech(name="SPY_DETECT_5"),
+            effects=SetEmpireMeter(empire=Source.Owner, meter="METER_DETECTION_STRENGTH", value=Value + 70),
+        ),
+    ],
+)
