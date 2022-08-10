cdeb5827ed11919e166c57b188edba2a2d86a5ea
O01eg <o01eg@yandex.ru>
2022-08-10 14:50:16 +0300

    Convert default/scripting/techs/spy/LIGHTHOUSE.focs

---
+++ default/scripting/techs/spy/LIGHTHOUSE.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SPY_LIGHTHOUSE",
+    description="SPY_LIGHTHOUSE_DESC",
+    short_description="BUILDING_UNLOCK_SHORT_DESC",
+    category="SPY_CATEGORY",
+    researchcost=75 * TECH_COST_MULTIPLIER,
+    researchturns=4,
+    tags=["PEDIA_SPY_CATEGORY"],
+    prerequisites="SPY_DETECT_2",
+    unlock=Item(type=UnlockBuilding, name="BLD_LIGHTHOUSE"),
+    graphic="icons/tech/interstellar_lighthouse.png",
+)
