2bcce69d8450ce9b33f7e25b6b6146090e6e8eda
O01eg <o01eg@yandex.ru>
2022-08-11 15:49:22 +0300

    Convert default/scripting/techs/spy/PLANET_STEALTH_MOD.focs

---
+++ default/scripting/techs/spy/PLANET_STEALTH_MOD.focs.py
@@ -0,0 +1,17 @@
+Tech(
+    name="SPY_PLANET_STEALTH_MOD",
+    description="SPY_PLANET_STEALTH_MOD_DESC",
+    short_description="GAME_MAKE_WORK_SHORT_DESC",
+    category="SPY_CATEGORY",
+    researchcost=1,
+    researchturns=1,
+    researchable=False,
+    tags=["PEDIA_SPY_CATEGORY"],
+    effectsgroups=EffectsGroup(
+        scope=Building() | Planet(),
+        stackinggroup="PLANET_STEALTH_MOD_STACK",
+        accountinglabel="BASIC_PLANET_STEALTH",
+        effects=SetStealth(value=Value + NamedReal(name="PLANET_BASIC_STEALTH", value=5.0)),
+    ),
+    graphic="",
+)
