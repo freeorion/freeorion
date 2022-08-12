e2e86f18059b6ac9fb8a03a447178d3142b4a725
O01eg <o01eg@yandex.ru>
2022-08-12 09:55:22 +0300

    Convert default/scripting/techs/spy/STEALTH_3.focs

---
+++ default/scripting/techs/spy/STEALTH_3.focs.py
@@ -0,0 +1,27 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SPY_STEALTH_3",
+    description="SPY_STEALTH_3_DESC",
+    short_description="PLANET_PROTECT_SHORT_DESC",
+    category="SPY_CATEGORY",
+    researchcost=(
+        500
+        - (250 * StatisticIf(float, condition=Source & OwnerHasTech(name="SPY_STEALTH_PART_3")))
+        * TECH_COST_MULTIPLIER
+        / (1 + StatisticIf(float, condition=OwnedBy(empire=Source.Owner) & HasTag(name="SNEAKY")))
+    ),
+    researchturns=7,
+    tags=["PEDIA_SPY_CATEGORY"],
+    prerequisites="SPY_STEALTH_2",
+    effectsgroups=EffectsGroup(
+        scope=OwnedBy(empire=Source.Owner) & Planet(),
+        activation=~OwnerHasTech(name="SPY_STEALTH_4"),
+        effects=[
+            AddSpecial(name="DIM_RIFT_SLAVE_SPECIAL"),
+            RemoveSpecial(name="VOLCANIC_ASH_SLAVE_SPECIAL"),
+            RemoveSpecial(name="CLOUD_COVER_SLAVE_SPECIAL"),
+        ],
+    ),
+    graphic="icons/specials_huge/dim_rift.png",
+)
