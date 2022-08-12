d94d75670fd1ecb46e454ceec5fb5e5f2520f67e
O01eg <o01eg@yandex.ru>
2022-08-12 13:43:49 +0300

    Convert default/scripting/techs/spy/STEALTH_PART_3.focs

---
+++ default/scripting/techs/spy/STEALTH_PART_3.focs.py
@@ -0,0 +1,14 @@
+from common.base_prod import TECH_COST_MULTIPLIER
+
+Tech(
+    name="SPY_STEALTH_PART_3",
+    description="SPY_STEALTH_PART_3_DESC",
+    short_description="SHIP_PART_UNLOCK_SHORT_DESC",
+    category="SPY_CATEGORY",
+    researchcost=250 * TECH_COST_MULTIPLIER,
+    researchturns=5,
+    tags=["PEDIA_STEALTH_PART_TECHS"],
+    prerequisites="SPY_DETECT_4",
+    unlock=Item(type=UnlockShipPart, name="ST_CLOAK_3"),
+    graphic="icons/ship_parts/cloak-3.png",
+)
