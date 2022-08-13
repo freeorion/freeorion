761f0e2845dcb1566be06c9b19f1c6eb1d15dfaa
Grummel7 <muzz@albatross.pond.sub.org>
2022-08-13 14:56:51 +0200

    Disable chat reaction while debug chat is used

--- default/python/AI/freeorion_tools/chat_handler/base_chat_handler.py
+++ default/python/AI/freeorion_tools/chat_handler/base_chat_handler.py
@@ -6,6 +6,9 @@ class ChatHandlerBase(metaclass=ABCMeta):
     Base class for chat handling.
     """
 
+    def __init__(self):
+        self.debug_active = False  # switched to true when any AI is debugged to stop normal chat reactions
+
     @abstractmethod
     def process_message(self, sender_id: int, message: str) -> bool:
         """
