23f5d843882a7e1501d56ad84e5c6e3122ffcbe4
O01eg <o01eg@yandex.ru>
2022-08-15 18:49:40 +0300

    Remove old FOCS tech parser

--- parse/TechsParser.cpp
+++ parse/TechsParser.cpp
@@ -1,11 +1,3 @@
-#include "Parse.h"
-
-#include "ParseImpl.h"
-#include "EnumParser.h"
-#include "EffectParser.h"
-#include "ValueRefParser.h"
-#include "MovableEnvelope.h"
-
 #include "PythonParserImpl.h"
 #include "ValueRefPythonParser.h"
 #include "ConditionPythonParser.h"
@@ -15,9 +7,6 @@
 
 #include "../universe/Tech.h"
 #include "../util/Directories.h"
-
-#include <boost/phoenix.hpp>
-#include <boost/spirit/include/qi_as.hpp>
 
 #include <boost/python/import.hpp>
 #include <boost/python/raw_function.hpp>
@@ -52,149 +41,12 @@ namespace {
         return retval;
     }
 
-    void insert_tech(TechManager::TechContainer& techs,
-                     parse::detail::MovableEnvelope<Tech::TechInfo>& tech_info,
-                     boost::optional<parse::effects_group_payload>& effects,
-                     boost::optional<std::set<std::string>>& prerequisites,
-                     boost::optional<std::vector<UnlockableItem>>& unlocked_items,
-                     boost::optional<std::string>& graphic,
-                     bool& pass)
-    {
-        auto tech_ptr = std::make_unique<Tech>(
-            std::move(*tech_info.OpenEnvelope(pass)),
-            (effects ? parse::detail::OpenEnvelopes(*effects, pass) : std::vector<std::unique_ptr<Effect::EffectsGroup>>{}),
-            (prerequisites ? std::move(*prerequisites) : std::set<std::string>{}),
-            (unlocked_items ? std::move(*unlocked_items) : std::vector<UnlockableItem>{}),
-            (graphic ? std::move(*graphic) : std::string{}));
-
-        if (check_tech(techs, tech_ptr)) {
-            g_categories_seen->emplace(tech_ptr->Category());
-            techs.emplace(std::move(tech_ptr));
-        }
-    }
-
-    BOOST_PHOENIX_ADAPT_FUNCTION(void, insert_tech_, insert_tech, 7)
-
     void insert_category(std::map<std::string, std::unique_ptr<TechCategory>, std::less<>>& categories,
                          std::string& name, std::string& graphic, const std::array<unsigned char, 4>& color)
     {
         auto category_ptr = std::make_unique<TechCategory>(name, std::move(graphic), color);
         categories.emplace(std::move(name), std::move(category_ptr));
     }
-
-    using start_rule_signature = void(TechManager::TechContainer&);
-
-    struct grammar : public parse::detail::grammar<start_rule_signature> {
-        grammar(const parse::lexer& tok,
-                const std::string& filename,
-                const parse::text_iterator& first, const parse::text_iterator& last) :
-            grammar::base_type(start),
-            one_or_more_string_tokens(tok),
-            condition_parser(tok, label),
-            string_grammar(tok, label, condition_parser),
-            castable_int_rules(tok, label, condition_parser, string_grammar),
-            double_rules(tok, label, condition_parser, string_grammar),
-            effects_group_grammar(tok, label, condition_parser, string_grammar),
-            tags_parser(tok, label),
-            unlockable_item_parser(tok, label),
-            one_or_more_unlockable_items(unlockable_item_parser),
-            color_parser(tok)
-        {
-            namespace phoenix = boost::phoenix;
-            namespace qi = boost::spirit::qi;
-
-            using phoenix::new_;
-            using phoenix::construct;
-            using phoenix::insert;
-            using phoenix::push_back;
-
-            qi::_1_type _1;
-            qi::_2_type _2;
-            qi::_3_type _3;
-            qi::_4_type _4;
-            qi::_5_type _5;
-            qi::_6_type _6;
-            qi::_7_type _7;
-            qi::_8_type _8;
-            qi::_pass_type _pass;
-            qi::_r1_type _r1;
-            qi::_val_type _val;
-            qi::eps_type eps;
-            qi::omit_type omit_;
-            qi::as_string_type as_string_;
-            const boost::phoenix::function<parse::detail::construct_movable> construct_movable_;
-            const boost::phoenix::function<parse::detail::deconstruct_movable> deconstruct_movable_;
-
-            researchable =
-                    tok.Unresearchable_ [ _val = false ]
-                |   tok.Researchable_ [ _val = true ]
-                |   eps [ _val = true ]
-                ;
-
-            tech_info
-                = ( label(tok.name_)                > tok.string
-                >   label(tok.description_)         > tok.string
-                >   label(tok.short_description_)   > tok.string  // TODO: Get rid of underscore.
-                >   label(tok.category_)            > tok.string
-                >   label(tok.researchcost_)        > double_rules.expr
-                >   label(tok.researchturns_)       > castable_int_rules.flexible_int
-                >   researchable
-                >   tags_parser
-                ) [ _val = construct_movable_(new_<Tech::TechInfo>(
-                    _1, _2, _3, _4, deconstruct_movable_(_5, _pass),
-                    deconstruct_movable_(_6, _pass), _7, _8)) ]
-                ;
-
-            tech
-                = ( omit_[tok.Tech_]
-                >   tech_info
-                >  -(label(tok.prerequisites_) > one_or_more_string_tokens)
-                >  -(label(tok.unlock_)        > one_or_more_unlockable_items)
-                >  -(label(tok.effectsgroups_) > effects_group_grammar)
-                >  -as_string_[(label(tok.graphic_) > tok.string)]
-                  ) [ insert_tech_(_r1, _1, _4, _2, _3, _5, _pass) ]
-                ;
-
-            start
-                = +(tech(_r1));
-
-            researchable.name("Researchable");
-            tech_info.name("Tech info");
-            tech.name("Tech");
-            start.name("start");
-
-#if DEBUG_PARSERS
-            debug(tech_info_name_desc);
-            debug(tech_info);
-            debug(tech);
-#endif
-
-            qi::on_error<qi::fail>(start, parse::report_error(filename, first, last, _1, _2, _3, _4));
-        }
-
-        using tech_info_rule = parse::detail::rule<parse::detail::MovableEnvelope<Tech::TechInfo> ()>;
-        using tech_rule = parse::detail::rule<void (TechManager::TechContainer&)>;
-        using category_rule = parse::detail::rule<void (std::map<std::string, std::unique_ptr<TechCategory>>&)>;
-        using start_rule = parse::detail::rule<void (TechManager::TechContainer&)>;
-
-        parse::detail::Labeller                 label;
-        parse::detail::single_or_repeated_string<std::set<std::string>>
-                                                one_or_more_string_tokens;
-        parse::conditions_parser_grammar        condition_parser;
-        const parse::string_parser_grammar      string_grammar;
-        parse::castable_as_int_parser_rules     castable_int_rules;
-        parse::double_parser_rules              double_rules;
-        parse::effects_group_grammar            effects_group_grammar;
-        parse::detail::tags_grammar             tags_parser;
-        parse::detail::unlockable_item_grammar  unlockable_item_parser;
-        parse::detail::single_or_bracketed_repeat<parse::detail::unlockable_item_grammar>
-                                                one_or_more_unlockable_items;
-        parse::detail::color_parser_grammar     color_parser;
-        parse::detail::rule<bool()>             researchable;
-        tech_info_rule                          tech_info;
-        tech_rule                               tech;
-        start_rule                              start;
-    };
 
     boost::python::object insert_category_(const boost::python::tuple& args, const boost::python::dict& kw) {
         auto name = boost::python::extract<std::string>(kw["name"])();
@@ -347,8 +199,6 @@ namespace parse {
 
         py_grammar_techs p = py_grammar_techs(parser, techs_);
 
-        for (const auto& file : ListDir(path, IsFOCScript))
-            detail::parse_file<grammar, TechManager::TechContainer>(lexer::tok, file, techs_);
         for (const auto& file : ListDir(path, IsFOCPyScript))
             py_parse::detail::parse_file<py_grammar_techs>(parser, file, p);
 
