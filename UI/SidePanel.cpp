e9b0a798254ee19b647b6fd89457473f67e3ceb9
geoffthemedio <geoffthemedio@gmail.com>
2022-08-18 20:58:21 +0200

    grooming

--- UI/SidePanel.cpp
+++ UI/SidePanel.cpp
@@ -2127,7 +2127,7 @@ void SidePanel::PlanetPanel::RClick(const GG::Pt& pt, GG::Flags<GG::ModKey> mod_
     if (planet->OwnedBy(GGHumanClientApp::GetApp()->EmpireID()) && m_order_issuing_enabled
         && m_planet_name->InClient(pt))
     {
-        auto rename_action = [this, planet]() { // rename planet
+        auto rename_action = [context, this, planet]() mutable { // rename planet
             auto edit_wnd = GG::Wnd::Create<CUIEditWnd>(
                 GG::X(350), UserString("SP_ENTER_NEW_PLANET_NAME"), planet->Name());
             edit_wnd->Run();
@@ -2135,14 +2135,15 @@ void SidePanel::PlanetPanel::RClick(const GG::Pt& pt, GG::Flags<GG::ModKey> mod_
             if (!m_order_issuing_enabled)
                 return;
 
-            ScriptingContext context;
+            auto result = edit_wnd->Result();
+
             if (!RenameOrder::Check(GGHumanClientApp::GetApp()->EmpireID(),
-                                    planet->ID(), edit_wnd->Result(), context))
+                                    planet->ID(), result, context))
             { return; }
 
             GGHumanClientApp::GetApp()->Orders().IssueOrder(
                 std::make_shared<RenameOrder>(GGHumanClientApp::GetApp()->EmpireID(),
-                                              planet->ID(), edit_wnd->Result(), context),
+                                              planet->ID(), std::move(result), context),
                 context);
             Refresh(context);
         };
@@ -2161,10 +2162,9 @@ void SidePanel::PlanetPanel::RClick(const GG::Pt& pt, GG::Flags<GG::ModKey> mod_
 
         // submenus for each available recipient empire
         GG::MenuItem give_away_menu(UserString("ORDER_GIVE_PLANET_TO_EMPIRE"), false, false);
-        for (auto& entry : Empires()) {
+        for (auto& entry : context.Empires()) {
             int recipient_empire_id = entry.first;
-            auto gift_action = [recipient_empire_id, client_empire_id, planet]() {
-                ScriptingContext context;
+            auto gift_action = [context, recipient_empire_id, client_empire_id, planet]() mutable {
                 GGHumanClientApp::GetApp()->Orders().IssueOrder(
                     std::make_shared<GiveObjectToEmpireOrder>(
                         client_empire_id, planet->ID(), recipient_empire_id, context),
@@ -2177,9 +2177,8 @@ void SidePanel::PlanetPanel::RClick(const GG::Pt& pt, GG::Flags<GG::ModKey> mod_
         popup->AddMenuItem(std::move(give_away_menu));
 
         if (planet->OrderedGivenToEmpire() != ALL_EMPIRES) {
-            auto ungift_action = [planet]() { // cancel give away order for this fleet
+            auto ungift_action = [context, planet]() mutable { // cancel give away order for this fleet
                 const OrderSet orders = GGHumanClientApp::GetApp()->Orders();
-                ScriptingContext context;
                 for (const auto& id_and_order : orders) {
                     if (auto order = std::dynamic_pointer_cast<
                         GiveObjectToEmpireOrder>(id_and_order.second))
