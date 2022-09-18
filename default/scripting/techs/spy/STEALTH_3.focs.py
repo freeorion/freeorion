e3b5a88cff33c59149ebabf3a366a4fbdceb5a1e
Grummel7 <muzz@albatross.pond.sub.org>
2022-09-18 19:19:20 +0200

    Simplified cost reduction of stealth techs

--- default/scripting/techs/spy/STEALTH_3.focs.py
+++ default/scripting/techs/spy/STEALTH_3.focs.py
@@ -6,8 +6,9 @@ Tech(
     short_description="PLANET_PROTECT_SHORT_DESC",
     category="SPY_CATEGORY",
     researchcost=(
-        (700 - (350 * StatisticIf(float, condition=Source & OwnerHasTech(name="SPY_STEALTH_PART_3"))))
+        700
         * TECH_COST_MULTIPLIER
+        / (1 + StatisticIf(float, condition=Source & OwnerHasTech(name="SPY_STEALTH_PART_3")))
         / (1 + StatisticIf(float, condition=OwnedBy(empire=Source.Owner) & HasTag(name="SNEAKY")))
     ),
     researchturns=7,
