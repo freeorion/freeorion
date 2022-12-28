c236bd2832bc0b4d5a27ae050460dafb3e12a44d
geoffthemedio <geoffthemedio@gmail.com>
2022-12-28 10:47:48 +0100

    combine is_capital lambdas

--- universe/Conditions.cpp
+++ universe/Conditions.cpp
@@ -1770,29 +1770,24 @@ void Capital::Eval(const ScriptingContext& parent_context, ObjectSet& matches,
                    ObjectSet& non_matches, SearchDomain search_domain) const
 {
     auto sz = (search_domain == SearchDomain::MATCHES) ? matches.size() : non_matches.size();
-    const auto& empires{parent_context.Empires()};
-    const auto& capitals = empires.CapitalIDs();
+    auto is_capital = [capitals{parent_context.Empires().CapitalIDs()}](const UniverseObject* obj) {
+    return std::any_of(
+        capitals.begin(), capitals.end(),
+        [obj_id{obj->ID()}](const int cap_id) noexcept { return cap_id == obj_id; });
+    };
 
     if (sz == 1) { // in testing, this was faster for a single candidate than setting up the loop stuff
         const bool test_val = search_domain == SearchDomain::MATCHES;
         auto& from = test_val ? matches : non_matches;
         auto& to = test_val ? non_matches : matches;
-        auto obj = from.front();
+        auto* obj = from.front();
 
-        bool is_capital = std::any_of(capitals.begin(), capitals.end(),
-                                      [obj_id{obj->ID()}](int cap_id) { return cap_id == obj_id; });
-
-        if (is_capital != test_val) {
+        if (is_capital(obj) != test_val) {
             to.push_back(obj);
             from.clear();
         }
 
     } else {
-        auto is_capital = [capitals](const UniverseObject* obj) {
-            return std::any_of(capitals.begin(), capitals.end(),
-                               [obj_id{obj->ID()}](int cap_id) { return cap_id == obj_id; });
-        };
-
         EvalImpl(matches, non_matches, search_domain, is_capital);
     }
 }
