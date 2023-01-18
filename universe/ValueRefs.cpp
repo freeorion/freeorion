e7c57d10757e1c5eb3d78b2c355bd28054773e8a
geoffthemedio <geoffthemedio@gmail.com>
2023-01-18 20:19:28 +0100

    fix compile error with GCC 13

--- universe/ValueRefs.cpp
+++ universe/ValueRefs.cpp
@@ -2191,9 +2191,9 @@ double ComplexVariable<double>::Eval(const ScriptingContext& context) const
     std::function<const std::map<int, float>& ()> property_int_key{nullptr};
 
     if (variable_name == "PropagatedSystemSupplyRange") // int_ref2 is system ID
-        property_int_key = [&context]() { return context.supply.PropagatedSupplyRanges(); };
+        property_int_key = [&context]() -> const auto& { return context.supply.PropagatedSupplyRanges(); };
     else if (variable_name == "PropagatedSystemSupplyDistance") // int_ref2 is system ID
-        property_int_key = [&context]() { return context.supply.PropagatedSupplyDistances(); };
+        property_int_key = [&context]() -> const auto& { return context.supply.PropagatedSupplyDistances(); };
 
     if (property_int_key) {
         if (!m_int_ref2)
