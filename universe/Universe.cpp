freeorion/freeorion

commit 46341d578a9ecdbb3603a47dec59112e591e6514

dynamic -> static pointer casts

universe/Universe.cpp
@@ -2995,10 +2995,11 @@ std::set<int> Universe::RecursiveDestroy(int object_id, const std::vector<int>& 
 
     auto system = m_objects->get<System>(obj->SystemID());
 
-    if (auto ship = std::dynamic_pointer_cast<Ship>(obj)) { // TODO: static cast after checking ObjectType
-        // if a ship is being deleted, and it is the last ship in its fleet, then the empty fleet should also be deleted
-        auto fleet = m_objects->get<Fleet>(ship->FleetID());
-        if (fleet) {
+    if (obj->ObjectType() == UniverseObjectType::OBJ_SHIP) {
+        auto ship = std::static_pointer_cast<Ship>(std::move(obj));
+        if (auto fleet = m_objects->get<Fleet>(ship->FleetID())) {
+            // if a ship is being deleted, and it is the last ship in
+            // its fleet, then the empty fleet should also be deleted
             fleet->RemoveShips({ship->ID()});
             if (fleet->Empty()) {
                 if (system)
@@ -3012,7 +3013,8 @@ std::set<int> Universe::RecursiveDestroy(int object_id, const std::vector<int>& 
         Destroy(object_id, empire_ids);
         retval.insert(object_id);
 
-    } else if (auto obj_fleet = std::dynamic_pointer_cast<Fleet>(obj)) {
+    } else if (obj->ObjectType() == UniverseObjectType::OBJ_SHIP) {
+        auto obj_fleet = std::static_pointer_cast<Fleet>(std::move(obj));
         for (int ship_id : obj_fleet->ShipIDs()) {
             if (system)
                 system->Remove(ship_id);
@@ -3024,7 +3026,8 @@ std::set<int> Universe::RecursiveDestroy(int object_id, const std::vector<int>& 
         Destroy(object_id, empire_ids);
         retval.insert(object_id);
 
-    } else if (auto obj_planet = std::dynamic_pointer_cast<Planet>(obj)) {
+    } else if (obj->ObjectType() == UniverseObjectType::OBJ_PLANET) {
+        auto obj_planet = std::static_pointer_cast<Planet>(std::move(obj));
         for (int building_id : obj_planet->BuildingIDs()) {
             if (system)
                 system->Remove(building_id);
@@ -3036,7 +3039,8 @@ std::set<int> Universe::RecursiveDestroy(int object_id, const std::vector<int>& 
         Destroy(object_id, empire_ids);
         retval.insert(object_id);
 
-    } else if (auto obj_system = std::dynamic_pointer_cast<System>(obj)) {
+    } else if (obj->ObjectType() == UniverseObjectType::OBJ_SYSTEM) {
+        auto obj_system = std::static_pointer_cast<System>(std::move(obj));
         // destroy all objects in system
         for (int system_id : obj_system->ObjectIDs()) {
             Destroy(system_id, empire_ids);
@@ -3065,7 +3069,8 @@ std::set<int> Universe::RecursiveDestroy(int object_id, const std::vector<int>& 
         // don't need to bother with removing things from system, fleets, or
         // ships, since everything in system is being destroyed
 
-    } else if (auto building = std::dynamic_pointer_cast<Building>(obj)) {
+    } else if (obj->ObjectType() == UniverseObjectType::OBJ_BUILDING) {
+        auto building = std::static_pointer_cast<Building>(std::move(obj));
         auto planet = m_objects->get<Planet>(building->PlanetID());
         if (planet)
             planet->RemoveBuilding(object_id);

commit fab0bee2215740570057c16939eaedf841458f62

fix type check typo introduced in 46341d578a9

universe/Universe.cpp
@@ -3017,7 +3017,7 @@ std::set<int> Universe::RecursiveDestroy(int object_id, const std::vector<int>& 
         Destroy(object_id, empire_ids);
         retval.insert(object_id);
 
-    } else if (obj->ObjectType() == UniverseObjectType::OBJ_SHIP) {
+    } else if (obj->ObjectType() == UniverseObjectType::OBJ_FLEET) {
         auto obj_fleet = std::static_pointer_cast<Fleet>(std::move(obj));
         for (int ship_id : obj_fleet->ShipIDs()) {
             if (system)