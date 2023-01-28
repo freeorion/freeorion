304556864a25b147da909f88350b6815efa1249e
Andrey <Cjkjvfnby@gmail.com>
2023-01-28 01:13:38 +0100

    Fix tests

---
+++ default/python/charting/log_parser/tests/conftest.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+
+current_dir = os.path.dirname(__file__)
+common = os.path.join(current_dir, "..", "..", "..")
+assert os.path.exists(common)
+sys.path.append(common)
+
+charting = os.path.join(current_dir, "..", "..")
+assert os.path.exists(charting)
+sys.path.append(charting)
