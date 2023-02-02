9dd3f9895024d50cf2343eac6c24a2da8ad10e91
geoffthemedio <geoffthemedio@gmail.com>
2023-02-02 23:45:49 +0100

    -pass strings by value with move -const

--- util/SitRepEntry.h
+++ util/SitRepEntry.h
@@ -49,16 +49,16 @@ private:
 //! events.
 //!
 //! @{
-[[nodiscard]] SitRepEntry CreateTechResearchedSitRep(const std::string& tech_name, int current_turn);
+[[nodiscard]] SitRepEntry CreateTechResearchedSitRep(std::string tech_name, int current_turn);
 [[nodiscard]] SitRepEntry CreateShipBuiltSitRep(int ship_id, int system_id, int shipdesign_id, int current_turn);
 [[nodiscard]] SitRepEntry CreateShipBlockBuiltSitRep(int system_id, int shipdesign_id, int number, int current_turn);
 [[nodiscard]] SitRepEntry CreateBuildingBuiltSitRep(int building_id, int planet_id, int current_turn);
 
-[[nodiscard]] SitRepEntry CreateTechUnlockedSitRep(const std::string& tech_name, int current_turn);
-[[nodiscard]] SitRepEntry CreatePolicyUnlockedSitRep(const std::string& policy_name, int current_turn);
-[[nodiscard]] SitRepEntry CreateBuildingTypeUnlockedSitRep(const std::string& building_type_name, int current_turn);
-[[nodiscard]] SitRepEntry CreateShipHullUnlockedSitRep(const std::string& ship_hull_name, int current_turn);
-[[nodiscard]] SitRepEntry CreateShipPartUnlockedSitRep(const std::string& ship_part_name, int current_turn);
+[[nodiscard]] SitRepEntry CreateTechUnlockedSitRep(std::string tech_name, int current_turn);
+[[nodiscard]] SitRepEntry CreatePolicyUnlockedSitRep(std::string policy_name, int current_turn);
+[[nodiscard]] SitRepEntry CreateBuildingTypeUnlockedSitRep(std::string building_type_name, int current_turn);
+[[nodiscard]] SitRepEntry CreateShipHullUnlockedSitRep(std::string ship_hull_name, int current_turn);
+[[nodiscard]] SitRepEntry CreateShipPartUnlockedSitRep(std::string ship_part_name, int current_turn);
 
 [[nodiscard]] FO_COMMON_API SitRepEntry CreateCombatSitRep(int system_id, int log_id, int empire_id, int current_turn);
 [[nodiscard]] FO_COMMON_API SitRepEntry CreateGroundCombatSitRep(int planet_id, int empire_id, int current_turn);
@@ -69,7 +69,7 @@ private:
 [[nodiscard]] FO_COMMON_API SitRepEntry CreateCombatDestroyedObjectSitRep(
     const UniverseObject* obj, int combat_system_id, int empire_id, int current_turn);
 [[nodiscard]] SitRepEntry               CreatePlanetDepopulatedSitRep(int planet_id);
-[[nodiscard]] FO_COMMON_API SitRepEntry CreatePlanetColonizedSitRep(int planet_id, const std::string& species);
+[[nodiscard]] FO_COMMON_API SitRepEntry CreatePlanetColonizedSitRep(int planet_id, std::string species);
 [[nodiscard]] FO_COMMON_API SitRepEntry CreatePlanetOutpostedSitRep(int planet_id);
 [[nodiscard]] FO_COMMON_API SitRepEntry CreatePlanetEstablishFailedSitRep(int planet_id, int ship_id);
 [[nodiscard]] FO_COMMON_API SitRepEntry CreatePlanetEstablishFailedVisibleOtherSitRep(int planet_id, int ship_id, int other_empire_id);
@@ -81,11 +81,11 @@ private:
 [[nodiscard]] FO_COMMON_API SitRepEntry CreateFleetArrivedAtDestinationSitRep(int system_id, int fleet_id, int recipient_empire_id,
                                                                               const ScriptingContext& context);
 [[nodiscard]] SitRepEntry               CreateEmpireEliminatedSitRep(int empire_id);
-[[nodiscard]] SitRepEntry               CreateVictorySitRep(const std::string& reason_string, int empire_id);
-[[nodiscard]] FO_COMMON_API SitRepEntry CreateSitRep(const std::string& template_string, int turn,
-                                                     const std::string& icon,
+[[nodiscard]] SitRepEntry               CreateVictorySitRep(std::string reason_string, int empire_id);
+[[nodiscard]] FO_COMMON_API SitRepEntry CreateSitRep(std::string template_string, int turn,
+                                                     std::string icon,
                                                      std::vector<std::pair<std::string, std::string>> parameters,
-                                                     const std::string& label = "", bool stringtable_lookup = true);
+                                                     std::string label = "", bool stringtable_lookup = true);
 //! @}
 
 template <typename Archive>
