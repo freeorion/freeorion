af31f7c280ed3ebd982f222a6bd85f0cfdef4f4a
O01eg <o01eg@yandex.ru>
2023-03-11 23:17:25 +0300

    Add yet another godot binary name

--- .github/godot-detect.cmake
+++ .github/godot-detect.cmake
@@ -1,4 +1,4 @@
-find_program(GODOT_EXECUTABLE NAMES godot3-server godot-headless godot3 godot godot3-runner REQUIRED)
+find_program(GODOT_EXECUTABLE NAMES godot3-server godot3-headless godot-headless godot3 godot godot3-runner REQUIRED)
 execute_process(COMMAND ${GODOT_EXECUTABLE} --version OUTPUT_VARIABLE GODOT_VERSION)
 string(STRIP "${GODOT_VERSION}" GODOT_VERSION)
 message("Found godot at ${GODOT_EXECUTABLE} version ${GODOT_VERSION}")
