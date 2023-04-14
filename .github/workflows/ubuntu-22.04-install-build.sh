2ae2d93d567792a9495d4c02cf6d8bde5b881aba
geoffthemedio <geoffthemedio@gmail.com>
2023-04-14 20:12:23 +0200

    bump Ubuntu to 22.04 in github workflow

---
+++ .github/workflows/ubuntu-22.04-install-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+sudo apt update
+sudo apt install -y libboost1.74-all-dev \
+	libglew-dev \
+	libogg-dev \
+	libopenal-dev \
+	libsdl2-dev \
+	libvorbis-dev \
+	cppcheck \
+	doxygen \
+	python3-pip \
+	ccache \
+	clang \
+	godot3-server
+
