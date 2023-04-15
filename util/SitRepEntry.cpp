5b81753add58e8a7255969250f85da19441dc9e7
geoffthemedio <geoffthemedio@gmail.com>
2023-04-15 16:28:46 +0200

    finish annexation sitreps

--- util/SitRepEntry.cpp
+++ util/SitRepEntry.cpp
@@ -375,8 +375,21 @@ SitRepEntry CreatePlanetDepopulatedSitRep(int planet_id, int current_turn) {
     return sitrep;
 }
 
-SitRepEntry CreatePlanetAnnexedSitRep(int planet_id, int original_owner_id, int annexer_empire_id, int current_turn)
-{ return SitRepEntry{UserStringNop("SITREP_PLANET_ANNEXED"), current_turn + 1, "", "", true}; }
+SitRepEntry CreatePlanetAnnexedSitRep(int planet_id, int original_owner_id, int annexer_empire_id, int current_turn) {
+    static constexpr std::string_view neutral_annex_txt = UserStringNop("SITREP_PLANET_ANNEXED");
+    static constexpr std::string_view other_empire_annex_txt = UserStringNop("SITREP_PLANET_ANNEXED_FROM_OTHER_EMPIRE");
+    static constexpr std::string_view neutral_annex_label = UserStringNop("SITREP_PLANET_ANNEXED_LABEL");
+    static constexpr std::string_view other_empire_annex_label = UserStringNop("SITREP_PLANET_ANNEXED_FROM_OTHER_EMPIRE_LABEL");
+
+    const auto msg = (original_owner_id == ALL_EMPIRES) ? neutral_annex_txt : other_empire_annex_txt;
+    const auto label = (original_owner_id == ALL_EMPIRES) ? neutral_annex_label : other_empire_annex_label;
+    SitRepEntry sitrep{msg.data(), current_turn + 1, "icons/sitrep/annexed.png", label.data(), true};
+    sitrep.AddVariable(VarText::PLANET_ID_TAG,     std::to_string(planet_id));
+    sitrep.AddVariable(VarText::EMPIRE_ID_TAG,     std::to_string(annexer_empire_id));
+    if (original_owner_id != ALL_EMPIRES)
+        sitrep.AddVariable(VarText::EMPIRE_ID_TAG, std::to_string(original_owner_id));
+    return sitrep;
+}
 
 SitRepEntry CreatePlanetColonizedSitRep(int planet_id, std::string species, int current_turn) {
     SitRepEntry sitrep(
