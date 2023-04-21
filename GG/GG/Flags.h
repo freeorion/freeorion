7af3138844408019ebf86478830013ff841ecac2
geoffthemedio <geoffthemedio@gmail.com>
2023-04-21 21:04:23 +0200

    disable not working code

--- GG/GG/Flags.h
+++ GG/GG/Flags.h
@@ -318,7 +318,7 @@ public:
     constexpr Flags(FlagType flag) :
         m_flags(flag.m_value)
     {
-#if defined(__cpp_lib_is_constant_evaluated)
+#if 0 && defined(__cpp_lib_is_constant_evaluated)
         if (!std::is_constant_evaluated()) {
             if (!FlagSpec<FlagType>::instance().contains(flag))
                 throw UnknownFlag("Invalid flag with value " + std::to_string(flag.m_value));
