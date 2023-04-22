47038c66dfe4157895a857b914c538c8b918d759
O01eg <o01eg@yandex.ru>
2023-04-22 19:24:46 +0300

    Update snap package to core22

--- .github/snap-xvfb-launch.sh
+++ .github/snap-xvfb-launch.sh
@@ -4,7 +4,19 @@ LIBGL_DEBUG=verbose glxinfo
 
 ps auxx | grep 'Xvfb' | grep -v grep
 
+export SDL_VIDEODRIVER=x11
+
 echo = Launching freeorion =
+
+LIBGL_DEBUG=verbose freeorion.freeorion &
+FOPID=$!
+sleep 10
+kill ${FOPID}
+wait ${FOPID}
+echo Freeorion return code $?
+cat ~/.local/share/freeorion/freeorion.log || echo No log file
+
+echo = Launching freeorion godot =
 
 export SDL_VIDEODRIVER=x11
 
@@ -13,6 +25,6 @@ FOPID=$!
 sleep 10
 kill ${FOPID}
 wait ${FOPID}
-echo Freeorion return code $?
-cat ~/.local/share/freeorion/freeorion.log || echo No log file
+echo Freeorion Godot return code $?
+cat ~/.local/share/freeorion/freeorion-godot.log || echo No log file
 
