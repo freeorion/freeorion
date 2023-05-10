0d4133c3e4e599ccc27b9345558205afbab0fee4
geoffthemedio <geoffthemedio@gmail.com>
2023-05-10 16:57:37 +0200

    remove #if checking for __cpp_impl_three_way_comparison

--- util/MultiplayerCommon.h
+++ util/MultiplayerCommon.h
@@ -272,14 +272,8 @@ struct PlayerInfo {
     Networking::ClientType  client_type = Networking::ClientType::INVALID_CLIENT_TYPE;
     bool                    host = false; //! true iff this is the host player
 
-#if defined(__cpp_impl_three_way_comparison)
+    static_assert(__cpp_impl_three_way_comparison);
     auto operator<=>(const PlayerInfo&) const = default;
-#else
-    bool operator==(const PlayerInfo& rhs) noexcept
-    { return name == rhs.name && empire_id == rhs.empire_id && client_type == rhs.client_type && host == rhs.host; }
-    bool operator!=(const PlayerInfo& rhs) noexcept
-    { return !operator==(rhs); }
-#endif
 };
 
 
