cb21c905533d2ca88d288c1540e176875823035a
Andrey <Cjkjvfnby@gmail.com>
2023-06-01 21:12:17 +0200

    Simplify code

--- default/python/common/fo_typing.py
+++ default/python/common/fo_typing.py
@@ -5,9 +5,10 @@ Since a lot of object are operated by their ID
 it is hard to distinguish one int from the another.
 """
 
-from typing import NewType
+from collections.abc import Iterable, Iterator
+from typing import Generic, NewType, TypeVar
 
-ObjectId = NewType("TargetId", int)
+ObjectId = NewType("ObjectId", int)
 PlanetId = NewType("PlanetId", ObjectId)
 FleetId = NewType("FleetId", ObjectId)
 SystemId = NewType("SystemId", ObjectId)
@@ -20,3 +21,71 @@ SpecialName = NewType("SpecialName", str)
 PartName = NewType("PartName", str)
 Turn = NewType("Turn", int)
 PlayerId = NewType("PlayerId", int)
+
+KT = TypeVar("KT")
+VT = TypeVar("VT")
+
+
+class Set(Generic[VT]):
+    def __contains__(self, number: VT) -> bool:
+        ...
+
+    def __iter__(self) -> Iterator[VT]:
+        ...
+
+    def __len__(self) -> int:
+        ...
+
+
+class Vec(Generic[VT]):
+    def __contains__(self, val: VT) -> bool:
+        ...
+
+    def __delitem__(self, val: VT) -> None:
+        ...
+
+    def __getitem__(self, ind: int) -> KT:
+        ...
+
+    def __iter__(self) -> Iterator[VT]:
+        ...
+
+    def __len__(self) -> int:
+        ...
+
+    def __setitem__(self, ind: int, val: VT) -> None:
+        ...
+
+    def append(self, val: VT) -> None:
+        ...
+
+    def extend(self, iterable: Iterable[VT]) -> None:
+        ...
+
+
+class Item(Generic[KT, VT]):
+    def key(self) -> KT:
+        ...
+
+    def data(self) -> VT:
+        ...
+
+
+class Map(Generic[KT, VT]):
+    def __contains__(self, key: KT) -> bool:
+        ...
+
+    def __delitem__(self, key: KT) -> None:
+        ...
+
+    def __getitem__(self, key: KT) -> VT:
+        ...
+
+    def __iter__(self) -> Iterator[Item[KT, VT]]:
+        ...
+
+    def __len__(self) -> int:
+        ...
+
+    def __setitem__(self, key: KT, value: VT) -> None:
+        ...
--- default/python/stub_generator/stub_generator/rtype/_property_mapping.py
+++ default/python/stub_generator/stub_generator/rtype/_property_mapping.py
@@ -1,43 +1,69 @@
+from common.fo_typing import (
+    BuildingId,
+    BuildingName,
+    EmpireId,
+    FleetId,
+    ObjectId,
+    PartName,
+    PlanetId,
+    PlayerId,
+    Set,
+    ShipId,
+    SpeciesName,
+    SystemId,
+    Turn,
+    Vec,
+)
 from stub_generator.stub_generator.collection_classes import make_type
 from stub_generator.stub_generator.rtype.mapper import Mapper
 
 _property_map = Mapper(
     {
-        ("shipIDs", "IntSet"): "Set[ShipId]",
-        ("shipIDs", "IntVec"): "Vec[ShipId]",
-        ("buildingIDs", "IntSet"): "Set[BuildingId]",
-        ("buildingIDs", "IntVec"): "Vec[BuildingId]",
-        ("planetIDs", "IntSet"): "Set[PlanetId]",
-        ("planetIDs", "IntVec"): "Vec[PlanetId]",
-        ("fleetIDs", "IntVec"): "Vec[FleetId]",
-        ("fleetIDs", "IntSet"): "Set[FleetId]",
-        ("systemIDs", "IntVec"): "Vec[SystemId]",
-        ("empireID", "int"): "EmpireId",
-        ("capitalID", "int"): "PlanetId",
-        ("locationID", "int"): "PlanetId",
-        ("owner", "int"): "EmpireId",
-        ("speciesName", "str"): "SpeciesName",
-        ("designedOnTurn", "int"): "Turn",
-        ("buildingTypeName", "str"): "BuildingName",
-        ("parts", "StringVec"): "Vec[PartName]",
-        ("recipient", "int"): "PlayerId",
-        ("sender", "int"): "PlayerId",
-        ("empire1", "int"): "EmpireId",
-        ("empire2", "int"): "EmpireId",
-        ("id", ""): "ObjectId",
-        ("systemID", ""): "SystemId",
-        ("systemIDs", ""): "SystemId",
-        ("name", ""): "str",
-        ("empireID", ""): "EmpireId",
-        ("description", ""): "str",
-        ("speciesName", ""): "SpeciesName",
-        ("capitalID", ""): "PlaneId",
-        ("owner", ""): "EmpireId",
-        ("designedOnTurn", ""): "Turn",
-        ("empire1", ""): "EmpireId",
-        ("empire2", ""): "EmpireId",
+        ("shipIDs", "IntSet"): Set[ShipId],
+        ("shipIDs", "IntVec"): Vec[ShipId],
+        ("buildingIDs", "IntSet"): Set[BuildingId],
+        ("buildingIDs", "IntVec"): Vec[BuildingId],
+        ("planetIDs", "IntSet"): Set[PlanetId],
+        ("planetIDs", "IntVec"): Vec[PlanetId],
+        ("fleetIDs", "IntVec"): Vec[FleetId],
+        ("fleetIDs", "IntSet"): Set[FleetId],
+        ("systemIDs", "IntVec"): Vec[SystemId],
+        ("empireID", "int"): EmpireId,
+        ("capitalID", "int"): PlanetId,
+        ("locationID", "int"): PlanetId,
+        ("owner", "int"): EmpireId,
+        ("speciesName", "str"): SpeciesName,
+        ("designedOnTurn", "int"): Turn,
+        ("buildingTypeName", "str"): BuildingName,
+        ("parts", "StringVec"): Vec[PartName],
+        ("recipient", "int"): PlayerId,
+        ("sender", "int"): PlayerId,
+        ("empire1", "int"): EmpireId,
+        ("empire2", "int"): EmpireId,
+        ("id", ""): ObjectId,
+        ("systemID", ""): SystemId,
+        ("systemIDs", ""): SystemId,
+        ("name", ""): str,
+        ("empireID", ""): EmpireId,
+        ("description", ""): str,
+        ("speciesName", ""): SpeciesName,
+        ("capitalID", ""): PlanetId,
+        ("owner", ""): EmpireId,
+        ("designedOnTurn", ""): Turn,
+        ("empire1", ""): EmpireId,
+        ("empire2", ""): EmpireId,
     }
 )
+
+
+_basic_types = {str, int, float, bool}
+
+
+def _is_new_type(val):
+    """
+    NewType is a function on Python3.9, in Python3.10 is changed to class.
+    """
+    return "NewType" in str(val)
 
 
 def update_property_rtype(attr_name: str, rtype: str):
@@ -48,6 +74,11 @@ def update_property_rtype(attr_name: str, rtype: str):
 
     key = (attr_name, rtype)
     if key in _property_map:
-        return _property_map[key]
+        property_class = _property_map[key]
+        if _is_new_type(property_class) or property_class in _basic_types:
+            return property_class.__name__
+        else:
+            _, _, rtype = str(property_class).rpartition(".")
+        return rtype
     else:
         return make_type(rtype)
