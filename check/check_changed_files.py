244ce75756f2c949e9c1d20e1165519270d27ca8
Andrey <Cjkjvfnby@gmail.com>
2023-07-15 19:49:52 +0200

    Add debug

--- check/check_changed_files.py
+++ check/check_changed_files.py
@@ -4,6 +4,7 @@ Check the files were changed and set GitHub output values
 import os
 import sys
 from argparse import ArgumentParser
+from contextlib import contextmanager
 from subprocess import getoutput
 from typing import TextIO
 
@@ -11,7 +12,22 @@ from check_file_changed._detectors import detect_file_groups
 from check_file_changed._workflow import Workflow, get_workflows
 
 
+@contextmanager
+def print_group(title: str):
+    if os.environ.get("GITHUB_ACTIONS") == "true":
+        print(f"::group::{title}")
+    yield
+    if os.environ.get("GITHUB_ACTIONS") == "true":
+        print("::endgroup::")
+
+
 def get_changed_files(change, base):
+    show_change = ["git" "show", change, "-q"]
+    print("Change:", getoutput(show_change))  # pyright: ignore[reportGeneralTypeIssues]
+
+    show_base = ["git" "show", base, "-q"]
+    print("Base:", getoutput(show_base))  # pyright: ignore[reportGeneralTypeIssues]
+
     cmd = ["git", "diff-tree", "--name-only", "-r", change, base]
     return getoutput(cmd).split("\n")  # pyright: ignore[reportGeneralTypeIssues]
 
@@ -22,8 +38,13 @@ def write_workflow_output(file: TextIO, workflows: set[Workflow]):
 
 
 def check_changed_files(*, change_ref, base_ref, dry_run):
-    changed_files = get_changed_files(change_ref, base_ref)
+    with print_group("Getting chendged files"):
+        changed_files = get_changed_files(change_ref, base_ref)
+        print("\n".join(changed_files))
     changed_file_groups = detect_file_groups(changed_files)
+    with print_group("Detected file Groups"):
+        print("\n".join(x.name for x in changed_file_groups))
+
     affected_workflows = get_workflows(changed_file_groups)
 
     print("Changed workflows:", ", ".join(x.name for x in sorted(affected_workflows, key=lambda x: x.name)))
