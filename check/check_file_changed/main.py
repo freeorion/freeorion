d07bd4d6d26fbbe114b58fba327558fa7ab8507d
Andrey <Cjkjvfnby@gmail.com>
2023-07-16 01:25:57 +0200

    Prettify output

--- check/check_file_changed/main.py
+++ check/check_file_changed/main.py
@@ -1,23 +1,28 @@
 """
-Check the files were changed and set GitHub output values
+Check the files were changed and set GitHub output values.
 """
 import os
-import sys
 from argparse import ArgumentParser
 from contextlib import contextmanager
 from subprocess import check_output
-from typing import TextIO
 
 from _detectors import detect_file_groups
 from _workflow import Workflow, get_workflows
 
 
+def is_github():
+    return os.environ.get("GITHUB_ACTIONS") == "true"
+
+
 @contextmanager
 def print_group(title: str):
-    if os.environ.get("GITHUB_ACTIONS") == "true":
+    if is_github():
         print(f"::group::{title}")
+    else:
+        print(title)
     yield
-    if os.environ.get("GITHUB_ACTIONS") == "true":
+    print()
+    if is_github():
         print("::endgroup::")
 
 
@@ -27,38 +32,40 @@ def get_changed_files(change, base):
     return output.decode().split("\n")
 
 
-def write_workflow_output(file: TextIO, workflows: set[Workflow]):
-    for workflow in workflows:
-        file.write(f"{workflow.name}=true\n")
+def format_output(workflows: set[Workflow]):
+    return [f"{workflow.name}=true" for workflow in workflows]
 
 
-def check_changed_files(*, change_ref, base_ref, dry_run):
+def check_changed_files(*, change_ref, base_ref):
     with print_group("Getting changed files"):
         changed_files = get_changed_files(change_ref, base_ref)
         print("\n".join(changed_files))
-    changed_file_groups = detect_file_groups(changed_files)
-    with print_group("Detected file Groups"):
+
+    with print_group("Detected file Groups:"):
+        changed_file_groups = detect_file_groups(changed_files)
         print("\n".join(x.name for x in changed_file_groups))
 
     affected_workflows = get_workflows(changed_file_groups)
 
-    print("Changed workflows:", ", ".join(x.name for x in sorted(affected_workflows, key=lambda x: x.name)))
+    print("Changed workflows:\n - %s" % "\n - ".join(x.name for x in sorted(affected_workflows, key=lambda x: x.name)))
+    print()
 
-    if dry_run:
-        write_workflow_output(sys.stdout, affected_workflows)
-    else:
+    output_text = "\n".join(format_output(affected_workflows))
+
+    if is_github():
         output_path = os.environ["GITHUB_OUTPUT"]
         with open(output_path, "a") as f:
-            write_workflow_output(f, affected_workflows)
-
+            f.write(output_text)
+    else:
+        print("Not on GitHub, expected output:\n")
+        print(output_text)
     print("Done")
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument("--change-ref", default="HEAD")
-    parser.add_argument("--base-ref", default="HEAD^1")
-    parser.add_argument("--dry-run", action="store_true")
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument("--change-ref", default="HEAD", help="The latest commit of PR")
+    parser.add_argument("--base-ref", default="HEAD^1", help="The latest commit of target branch (master)")
 
     args = parser.parse_args()
-    check_changed_files(change_ref=args.change_ref, base_ref=args.base_ref, dry_run=args.dry_run)
+    check_changed_files(change_ref=args.change_ref, base_ref=args.base_ref)
