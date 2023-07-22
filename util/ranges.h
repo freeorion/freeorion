dea75f3d3f4dfafcab2f1e170b88360435b849d9
geoffthemedio <geoffthemedio@gmail.com>
2023-07-23 00:13:36 +0200

    comment about not using std::views::join since there is apparently no fallback equivalent in Boost

--- util/ranges.h
+++ util/ranges.h
@@ -7,12 +7,13 @@ static constexpr auto& range_keys = std::views::keys;
 static constexpr auto& range_values = std::views::values;
 static constexpr auto& range_transform = std::views::transform;
 static constexpr auto& range_filter = std::views::filter;
+//static constexpr auto& range_join = std::views::join; // no equivalent in boost ranges
 static constexpr auto& range_find_if = std::ranges::find_if;
 #else
 # include <boost/range/adaptor/map.hpp>
 # include <boost/range/adaptor/filtered.hpp>
 # include <boost/range/adaptor/transformed.hpp>
-#include <boost/range/algorithm.hpp>
+# include <boost/range/algorithm.hpp>
 static const auto& range_keys = boost::adaptors::map_keys;
 static const auto& range_values = boost::adaptors::map_values;
 static const auto& range_filter = boost::adaptors::filtered;
