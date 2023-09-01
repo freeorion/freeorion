8fd2bb7faae84b517c6169b9ec5d309de09402f8
Andrey <Cjkjvfnby@gmail.com>
2023-09-01 22:48:48 +0200

    Add test-scripting as FOCS scripts

--- check/check_file_changed/check_coverage.py
+++ check/check_file_changed/check_coverage.py
@@ -12,20 +12,27 @@ from _detectors import detect_file, registered_detectors
 
 def list_all_files():
     cmd = ["git", "ls-tree", "--full-tree", "--name-only", "-r", "HEAD"]
-    return check_output(cmd).decode().split("\n")
+    return check_output(cmd).decode().strip().split("\n")
 
 
 def check_coverage():
     all_files = list_all_files()
 
     missed_count = 0
+    missed_files = []
 
     for path in all_files:
         result = detect_file(PurePath(path), registered_detectors)
         if result is None:
             missed_count += 1
-            print(path)
-    print(f"Missed statements: {missed_count} from {len(all_files)}")
+            missed_files.append(path)
+        else:
+            print(
+                f"  {path:<60}  {result}",
+            )
+    print(f"Missed files: {missed_count} from {len(all_files)}")
+    for f in missed_files:
+        print(" ", [f])
 
 
 if __name__ == "__main__":
