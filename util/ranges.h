4b08b78278f1de03996276a7763c28f1a0ef914d
Blue-Award <blue-award@users.noreply.github.com>
2023-09-22 02:48:27 +0200

    boost fallback for std::views::reverse

--- util/ranges.h
+++ util/ranges.h
@@ -9,6 +9,7 @@ static constexpr auto& range_values = std::views::values;
 static constexpr auto& range_transform = std::views::transform;
 static constexpr auto& range_filter = std::views::filter;
 //static constexpr auto& range_join = std::views::join; // no equivalent in boost ranges
+static constexpr auto& range_reverse = std::views::reverse;
 static constexpr auto& range_find_if = std::ranges::find_if;
 static constexpr auto& range_any_of = std::ranges::any_of;
 static constexpr auto& range_copy = std::ranges::copy;
@@ -20,6 +21,7 @@ static constexpr auto& range_end = std::ranges::end;
 # include <boost/range/adaptor/map.hpp>
 # include <boost/range/adaptor/filtered.hpp>
 # include <boost/range/adaptor/transformed.hpp>
+# include <boost/range/adaptor/reversed.hpp>
 # include <boost/range/algorithm.hpp>
 # include <boost/range/end.hpp>
 # include <boost/algorithm/cxx11/any_of.hpp>
@@ -28,6 +30,7 @@ static const auto& range_keys = boost::adaptors::map_keys;
 static const auto& range_values = boost::adaptors::map_values;
 static const auto& range_filter = boost::adaptors::filtered;
 static const auto& range_transform = boost::adaptors::transformed;
+static const auto& range_reverse = boost::adaptors::reversed;
 template <typename... Args>
 inline auto range_find_if(Args... args) { return boost::range::find_if(std::forward<Args>(args)...); }
 template <typename... Args>
