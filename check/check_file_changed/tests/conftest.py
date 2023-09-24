434455c073f3685f3e54b4fad7334da6d7dc1aac
Andrey <Cjkjvfnby@gmail.com>
2023-09-24 10:29:02 +0200

    [CI] Use external action to detect file changed (#4744)

--- check/check_file_changed/tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import sys
-from pathlib import Path
-
-this_dir = Path(__file__).parent
-checks_dir = this_dir.parent.parent.parent.as_posix()
-print(">>>>>", checks_dir)
-sys.path.append(checks_dir)
