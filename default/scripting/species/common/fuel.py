5808a275f03e128713e6647b1e43f2f73bdfc058
Andrey <Cjkjvfnby@gmail.com>
2023-10-23 22:24:20 +0200

    Convert fulver

--- default/scripting/species/common/fuel.py
+++ default/scripting/species/common/fuel.py
@@ -8,25 +8,25 @@ def FUEL_EFFECTSGROUP(label: str, fuel_change: float):
     return EffectsGroup(
         description=f"{label}_FUEL_DESC",
         scope=IsSource & Ship,
-        accountinglabel="@1@_FUEL_LABEL",
+        accountinglabel=f"{label}_FUEL_LABEL",
         priority=TARGET_AFTER_SCALING_PRIORITY,
-        effects=SetMaxFuel(value=Value + fuel_change),
+        effects=SetMaxFuel(value=fuel_change),
     )
 
 
 NO_FUEL = [
-    FUEL_EFFECTSGROUP("NO", 0),
+    FUEL_EFFECTSGROUP("NO", Value + 0),
 ]
 BAD_FUEL = [
-    FUEL_EFFECTSGROUP("BAD", -0.5),
+    FUEL_EFFECTSGROUP("BAD", Value - 0.5),
 ]
 GOOD_FUEL = [
-    FUEL_EFFECTSGROUP("GOOD", 0.5),
+    FUEL_EFFECTSGROUP("GOOD", Value + 0.5),
 ]
 AVERAGE_FUEL = []
 GREAT_FUEL = [
-    FUEL_EFFECTSGROUP("GREAT", 1),
+    FUEL_EFFECTSGROUP("GREAT", Value + 1),
 ]
 ULTIMATE_FUEL = [
-    FUEL_EFFECTSGROUP("ULTIMATE", 1.5),
+    FUEL_EFFECTSGROUP("ULTIMATE", Value + 1.5),
 ]
