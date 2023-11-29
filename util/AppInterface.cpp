b29bc0c74b7dc1dc9ef8964ad2609f70c7df5220
Andrey <Cjkjvfnby@gmail.com>
2023-11-29 23:55:00 +0100

    Add new dir for reading shared code

--- util/AppInterface.cpp
+++ util/AppInterface.cpp
@@ -60,11 +60,18 @@ void IApp::StartBackgroundParsing(const PythonParser& python, std::promise<void>
 
     DebugLogger() << "Start background parsing...";
 
+    // TODO: remove after all code is moved to macros
     // named value ref parsing can be done in parallel as the referencing happens after parsing
     if (IsExistingDir(rdir / "scripting/common"))
         GetNamedValueRefManager().SetNamedValueRefParse(Pending::ParseSynchronously(parse::named_value_refs, rdir / "scripting/common"));
     else
         ErrorLogger() << "Background parse path doesn't exist: " << (rdir / "scripting/common").string();
+
+    // named value ref parsing can be done in parallel as the referencing happens after parsing
+    if (IsExistingDir(rdir / "scripting/macros"))
+        GetNamedValueRefManager().SetNamedValueRefParse(Pending::ParseSynchronously(parse::named_value_refs, rdir / "scripting/macros"));
+    else
+        ErrorLogger() << "Background parse path doesn't exist: " << (rdir / "scripting/macros").string();
 
     if (IsExistingDir(rdir / "scripting/buildings"))
         GetBuildingTypeManager().SetBuildingTypes(Pending::StartAsyncParsing(parse::buildings, rdir / "scripting/buildings"));
