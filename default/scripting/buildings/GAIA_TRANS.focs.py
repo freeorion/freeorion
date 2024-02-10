49f6e467a2f8b7c6c40a6e3774003f7e79899e21
Andrey <Cjkjvfnby@gmail.com>
2024-02-10 10:54:40 +0100

    [FOCS] WIP convert buildings

---
+++ default/scripting/buildings/GAIA_TRANS.focs.py
@@ -0,0 +1,66 @@
+from focs._effects import (
+    AddSpecial,
+    AsteroidsType,
+    BuildBuilding,
+    Contains,
+    Destroy,
+    EffectsGroup,
+    Enqueued,
+    GasGiantType,
+    GenerateSitRepMessage,
+    Good,
+    HasSpecial,
+    IsBuilding,
+    IsSource,
+    Object,
+    OwnedBy,
+    Planet,
+    Source,
+    Target,
+)
+from macros.base_prod import BUILDING_COST_MULTIPLIER
+from macros.enqueue import DO_NOT_CONTAIN_FOR_ALL_TERRAFORM_PLANET_TYPES, ENQUEUE_BUILD_ONE_PER_PLANET
+
+try:
+    from focs._buildings import *
+except ModuleNotFoundError:
+    pass
+
+BuildingType(  # type: ignore[reportUnboundVariable]
+    name="BLD_GAIA_TRANS",
+    description="BLD_GAIA_TRANS_DESC",
+    buildcost=200 * Target.HabitableSize * BUILDING_COST_MULTIPLIER,
+    buildtime=12,
+    location=(
+        Planet()
+        & OwnedBy(empire=Source.Owner)
+        & Planet(environment=[Good])
+        & ~Planet(type=[AsteroidsType, GasGiantType])
+        & ~HasSpecial(name="GAIA_SPECIAL")
+        # No terraforming buildings contained or enqueued (neither popular nor any of the targeted ones)
+        & ~Contains(IsBuilding(name=["BLD_TERRAFORM_BEST"]))
+        & ~Enqueued(type=BuildBuilding, name="BLD_TERRAFORM_BEST")
+        & DO_NOT_CONTAIN_FOR_ALL_TERRAFORM_PLANET_TYPES()
+    ),
+    enqueuelocation=ENQUEUE_BUILD_ONE_PER_PLANET,
+    effectsgroups=[
+        EffectsGroup(
+            scope=(Object(id=Source.PlanetID) & Planet()),
+            effects=[
+                AddSpecial(name="GAIA_SPECIAL"),
+                GenerateSitRepMessage(
+                    message="EFFECT_GAIA",
+                    label="EFFECT_GAIA_LABEL",
+                    icon="icons/specials_huge/gaia.png",
+                    parameters={"planet": Target.ID},
+                    empire=Source.Owner,
+                ),
+            ],
+        ),
+        EffectsGroup(
+            scope=IsSource,
+            effects=[Destroy],
+        ),
+    ],
+    icon="icons/specials_huge/gaia.png",
+)
--- default/scripting/macros/enqueue.py
+++ default/scripting/macros/enqueue.py
@@ -4,6 +4,9 @@
 from focs._effects import BuildBuilding, Contains, CurrentContent, Enqueued, IsBuilding, OwnedBy, Source
 
 ENQUEUE_BUILD_ONE_PER_PLANET = (
+    # Only check for own buildings. The client may have seen a building once, which has been
+    # destroyed while outside vision range. In this case the building remains forever in the
+    # client's context, but it should not stop a player from rebuilding the same type.
     ~Contains(IsBuilding(name=[CurrentContent]) & OwnedBy(empire=Source.Owner))
     & ~Enqueued(type=BuildBuilding, name=CurrentContent)
     & OwnedBy(empire=Source.Owner)
@@ -17,3 +20,43 @@ ENQUEUE_ARTIFICIAL_PLANET_EXCLUSION = (
     & ~Enqueued(type=BuildBuilding, name="BLD_ART_FACTORY_PLANET")
     & ~Enqueued(type=BuildBuilding, name="BLD_ART_PARADISE_PLANET")
 )
+
+
+def DO_NOT_CONTAIN_FOR_ALL_TERRAFORM_PLANET_TYPES():
+    planet_types = [
+        "BARREN",
+        "TUNDRA",
+        "DESERT",
+        "TERRAN",
+        "OCEAN",
+        "SWAMP",
+        "TOXIC",
+        "INFERNO",
+        "RADIATED",
+        "BARREN",
+    ]
+
+    expressions = []
+
+    for planet_type in planet_types:
+        expressions.append(~Contains(IsBuilding(name=[f"BLD_TERRAFORM_{planet_type}"])))
+
+    # We add enqueue check in the second loop, because we want to preserve the same order of checks for better diff
+    # Once conversion of building is done, we should refactor this code.
+    for planet_type in planet_types:
+        expressions.append(~Enqueued(type=BuildBuilding, name=f"BLD_TERRAFORM_{planet_type}"))
+
+    return reduce(lambda x, y: x & y, expressions)
+
+
+def reduce(function, expressions):
+    if not expressions:
+        return
+
+    if len(expressions) == 1:
+        return expressions
+
+    result = expressions[0]
+    for expr in expressions[1:]:
+        result = function(result, expr)
+    return result
