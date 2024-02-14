d4c11c3839e531304bda3667dc40d81f98e40973
Michael Courtney <43386669+MichaelCourtney@users.noreply.github.com>
2024-02-14 21:27:03 +1100

    Swap research order of domesticated monsters and nest eradication (#4855)

--- default/scripting/techs/ship_hulls/organic/SHP_ORG_HULL.focs.py
+++ default/scripting/techs/ship_hulls/organic/SHP_ORG_HULL.focs.py
@@ -9,7 +9,7 @@ Tech(
     researchcost=16 * TECH_COST_MULTIPLIER,
     researchturns=5,
     tags=["PEDIA_ORGANIC_HULL_TECHS"],
-    prerequisites=["SHP_DOMESTIC_MONSTER"],
+    prerequisites=["GRO_MEGA_ECO"],
     unlock=[Item(type=UnlockShipHull, name="SH_ORGANIC"), Item(type=UnlockBuilding, name="BLD_SHIPYARD_ORG_ORB_INC")],
     graphic="hulls_design/organic_hull.png",
 )
