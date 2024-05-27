2645a75bfc2b2051024b091b26d7d1c67320d224
geoffthemedio <geoffthemedio@gmail.com>
2024-05-27 12:37:36 +0200

    make CheckSumCombine for floating point types constexpr

--- util/CheckSums.cpp
+++ util/CheckSums.cpp
@@ -5,136 +5,107 @@
 #if __has_include(<bit>)
 #  include <bit>
 #endif
-#include <cassert>
 #include <cmath>
 #include <float.h>
 #include <limits>
-#include <numeric>
-#include <stdexcept>
-
-namespace {
-    constexpr double log_base_e_of_10 = 2.30258509299404568401799145;
-    constexpr double log_base_10_of_e = 1/log_base_e_of_10;
-    constexpr double natural_log_base = 2.71828182845904523536028747;
-
-    constexpr auto CXAbs(auto in) noexcept { return in >= 0 ? in : -in; }
-
-    constexpr double CXPow(double base, unsigned int exp) {
-        if (exp == 0)
-            return 1.0;
-        if (exp == 1)
-            return base;
-        return CXPow(base, exp / 2) * CXPow(base, exp - exp/2);
-    }
-    static_assert(CXPow(12.253, 0) == 1.0);
-    static_assert(CXPow(12.253, 1) == 12.253);
-    static_assert(CXPow(2.0, 5) == 32.0);
-    static_assert(CXPow(4.0, 3) == 64.0);
-
-    constexpr uint64_t FacInt(uint8_t x) noexcept
-    { return (x <= 1) ? 1 :FacInt(x-1)*x; }
-    constexpr double Fac(uint8_t x) noexcept {
-        if (x <= 20)
-            return FacInt(x);
-        double accum = static_cast<double>(FacInt(20));
-        for (auto n = 21; n <= x; ++n)
-            accum *= n;
-        return accum;
-    }
-    static_assert(Fac(5) == 120.0);
-
-    constexpr void InPlaceSort(auto& arr) {
-#if defined(__cpp_lib_constexpr_algorithms)
-        std::sort(arr.begin(), arr.end());
-#else
-        if (!std::is_constant_evaluated()) {
-            std::sort(arr.begin(), arr.end());
-        } else {
-            for (auto it = arr.begin(); it != arr.end(); ++it)
-                std::swap(*it, *std::min_element(it, arr.end()));
-        }
-#endif
-    }
-
-    constexpr double CXPow(const double exp) {
-        std::array<double, 64> scratch{};
-        for (uint8_t n = 0; n < scratch.size(); ++n)
-            scratch[n] = CXPow(exp,n)/Fac(n);
-        InPlaceSort(scratch);
-#if defined(__cpp_lib_constexpr_numeric)
-        return std::accumulate(scratch.begin(), scratch.end(), 0.0);
-#else
-        double accum = 0.0;
-        for (const auto& s : scratch)
-            accum += s;
-        return accum;
-#endif
-    }
-    static_assert(CXAbs(CXPow(0) - 1) < 0.0000001);
-    static_assert(CXAbs(CXPow(1) - natural_log_base) < 0.0000001);
-    static_assert(CXAbs(CXPow(5.0) - 148.413159102576) < 0.0000001);
-    static_assert(CXAbs(CXPow(-3.14)*CXPow(3.14) - 1) < 0.0000001);
-
-    constexpr double CXPow10(const double x) noexcept
-    { return CXPow(x*log_base_e_of_10); }
-    static_assert(CXAbs(CXPow10(2.0)/100.0 - 1) < 0.0000001);
-
-    constexpr std::pair<double, std::size_t> CXLog10Impl(const double x, uint8_t max_iter = 20) {
-        if (x < 0)
-            throw std::invalid_argument("can't take log10 of negative number");
-        if (x == 0)
-            throw std::invalid_argument("can't take log10 of 0");
-        if (x == 1)
-            return {0.0, 0};
-
-        constexpr auto initial_guess = [](auto x_in) noexcept {
-            std::decay_t<decltype(x_in)> guess = 0;
-            while (x_in > 3.17) { x_in /= 10; ++guess; }  // 3.16 ~= 10^0.5
-            while (x_in < 0.315) { x_in *= 10; --guess; } // 0.316 ~= 10^-0.5
-            return guess;
-        };
-        static_assert(initial_guess(1) == 0);
-        static_assert(initial_guess(100) == 2);
-        static_assert(initial_guess(2.0) < 1.30 && initial_guess(2.0) > -0.7);
-        static_assert(initial_guess(3.0) < 1.47 && initial_guess(3.0) > -0.53);
-        static_assert(initial_guess(5.0) < 1.70 && initial_guess(5.0) > -0.3);
-        static_assert(initial_guess(8.0) < 1.90 && initial_guess(8.0) > -0.1);
-        static_assert(initial_guess(12.0) < 2.08 && initial_guess(12.0) > 0.08);
-
-        // determine initial search bounds
-        double guess = initial_guess(x);
-
-        // want to find y = log10(x), ie. find y such that x = 10^y
-        // equivalent to finding root f(y) = x - 10^y = 0
-        // f(y) = x - exp(ln(10)*y)
-        // f'(y) = -ln(10)*exp(ln(10)*y) = -ln(10)*10^y
-
-        // Newton: guess y_n+1 - y_n = -f(y_n)/f'(y_n)
-        //                           = -(x - 10^y_n) / [-ln(10)*10^y_n]
-        //                           = (1/ln(10))*(x - 10^y_n)/(10^y_n)
-        //                           = (1/ln(10))*(x/(10^y_n) - 1)
-
-        constexpr double one_over_ln10 = 0.434294481903251827651128918;
-
-        std::size_t iterations = 0;
-        for (; iterations < max_iter; ++iterations) {
-            auto adj_factor = one_over_ln10*(x/CXPow10(guess) - 1);
-            if (adj_factor == 0)
-                break;
-            guess += adj_factor;
-            ++iterations;
-        }
-        return {guess, iterations};
-    }
-    constexpr double CXLog10(const double x) { return CXLog10Impl(x).first; }
-
-    static_assert(CXAbs(CXLog10(1) - 0) < 0.0000001);
-    static_assert(CXAbs(CXLog10(natural_log_base) - log_base_10_of_e) < 0.0000001);
-    constexpr auto bignum = CXPow10(20);
-    static_assert(CXAbs(CXLog10(CXPow10(18.5)) - 18.5) < 0.00001);
-}
 
 namespace CheckSums {
+    static_assert(!abs(false));
+    static_assert(abs(true));
+    static_assert(abs(-1) == 1);
+    static_assert(abs(0) == 0);
+    static_assert(abs(1) == 1);
+    static_assert(cmp_less_equal(nlm<uint32_t>(), nlm<uint64_t>()));
+    static_assert(cmp_less_equal(nlm<int16_t>(), nlm<uint16_t>()));
+    static_assert(fits_in_uint32t<bool>());
+    static_assert(fits_in_uint32t<int16_t>());
+    static_assert(fits_in_uint32t<uint32_t>());
+    static_assert(!fits_in_uint32t<int64_t>());
+    static_assert(!fits_in_uint32t<uint64_t>());
+    enum class test_enum : int16_t { NEG1 = -1, ZERO, ONE, TWO };
+    static_assert(fits_in_uint32t<test_enum>());
+    static_assert(abs(test_enum::TWO) == test_enum::TWO);
+
+    static_assert(SeparateParts(-1.5) == std::pair<int64_t, double>{-2, 0.5});
+    static_assert(SeparateParts(21.5f) == std::pair<int64_t, float>{21, 0.5f});
+
+    constexpr double ok_frac_ac = 0.0000001;
+
+    static_assert(pow(0) == 1);
+    static_assert(pow(0.0) == 1);
+    static_assert(abs(pow(1) - natural_log_base) < ok_frac_ac);
+    static_assert(abs(pow(5.0) - 148.413159102576) < ok_frac_ac);
+    static_assert(abs(pow(-3.14)*pow(3.14) - 1) < ok_frac_ac);
+    static_assert(abs(1.0-pow(120.5)/21502307550479587137010156493499279428753405880629141.) < ok_frac_ac);
+    static_assert(abs(1.0-pow(200)/722597376812574925817747704218930569735687442852731928403269000000000000000000000000000.) < ok_frac_ac);
+
+    static_assert(pow(12.253, 0) == 1.0);    // special case, should be exact
+    static_assert(pow(12.253, 1) == 12.253); // special case, should be exact
+    static_assert(pow(2.0, 5) == 32.0);      // power of two, should be exactly represented
+    static_assert(pow(2, 5) == 32);
+    static_assert(pow(-2, 5) == -32);
+    static_assert(pow(4.0, -3) == 1/64.0);   // power of two, should be exactly represented
+    static_assert(pow(-4.0, -3) == -1/64.0); // power of two, should be exactly represented
+    static_assert(pow(-4, 3) == -64);    // power of two, should be exactly represented
+    static_assert(pow(4, 2) == 16);
+    static_assert(pow(-4, 2) == 16);
+
+    static_assert(abs(pow10(2.0)/100.0 - 1) < ok_frac_ac);
+    static_assert(abs(pow10(0.0) - 1) < ok_frac_ac);
+    static_assert(abs(pow10(-1) - 0.1) < ok_frac_ac);
+    static_assert(abs(pow10(-4)/0.0001 - 1) < ok_frac_ac);
+    static_assert(abs(pow10(0.00000005f)/1.0000001f - 1) < ok_frac_ac);
+    static_assert(abs(pow10(-0.00000005f)*1.0000001f - 1) < ok_frac_ac);
+    static_assert(abs(pow10(0.9999999f)/9.999997f - 1) < ok_frac_ac);
+    static_assert(abs(pow10(-0.9999999f)*9.999997f - 1) < ok_frac_ac);
+
+    static_assert(GetCheckSum(0.0f) == 0);
+    static_assert(GetCheckSum(0.0) == 0);
+    static_assert(GetCheckSum(1.0f) == 4000000);
+    static_assert(GetCheckSum(1.0) == 4000000);
+    static_assert(FLT_MIN < 2.0e-38f);
+    static_assert(GetCheckSum(2.0e-38f) == 3623010);
+    static_assert(GetCheckSum(2.0e-38) == 3623010);
+    static_assert(DBL_MIN < 4.0e-308);
+    static_assert(GetCheckSum(4.0e-308) == 926020);
+
+    static_assert(GetCheckSum(0.1) == 3990000);
+    static_assert(GetCheckSum(0.1f) == 3990000);
+
+    static_assert(GetCheckSum(pow10(-200)) == 2000000);
+    static_assert(GetCheckSum(pow10(-1)) == 3990000);
+    static_assert(GetCheckSum(pow10(0.0)) == 4000000);
+    static_assert(GetCheckSum(pow10(0.0f)) == 4000000);
+    static_assert(GetCheckSum(pow10(1.0)) == 4010000);
+    static_assert(GetCheckSum(pow10(1.0f)) == 4010000);
+    static_assert(GetCheckSum(pow10(0.0001)) == 4000000);
+    static_assert(GetCheckSum(pow10(0.01f)) == 4000100);
+    static_assert(GetCheckSum(pow10(1.00015)) == 4010001);
+    static_assert(GetCheckSum(pow10(1.00015f)) == 4010001);
+    static_assert(GetCheckSum(pow10(2)) == 4020000);
+    static_assert(GetCheckSum(pow10(200)) == 6000000);
+
+#if !defined(__cpp_lib_constexpr_cmath)
+    static_assert(LogNearestInteger(1.0).first == 0);
+#endif
+
+    constexpr double log_base_10_of_e = 1/log_base_e_of_10;
+
+    static_assert(abs(log10(1.0) - 0) < ok_frac_ac);
+    static_assert(abs(log10(natural_log_base) - log_base_10_of_e) < ok_frac_ac);
+    static_assert(abs(log10(pow10(18.5)) - 18.5) < ok_frac_ac);
+    static_assert(abs(log10(pow10(300.5)) - 300.5) < ok_frac_ac);
+
+    static_assert(noexcept(99837 + static_cast<unsigned int>(43.0f)));
+    static_assert(DBL_MAX_10_EXP < 400);
+    static_assert(FLT_MAX_10_EXP < 40);
+
+#if !defined(__cpp_lib_constexpr_cmath)
+    static_assert([](){std::array arr{1, 5, -1, 2, 0, 0}; InPlaceSort(arr); return arr; }() == std::array{-1, 0, 0, 1, 2, 5});
+    static_assert([](){std::array arr{1.0, 5.0, -1.0, 2.0, 0.0, 0.0}; InPlaceSort(arr); return arr; }() == std::array{-1.0, 0.0, 0.0, 1.0, 2.0, 5.0});
+    static_assert([](){std::array arr{1.0f, 5.0f, -1.0f, 2.0f, 0.0f, 0.0f}; InPlaceSort(arr); return arr; }() == std::array{-1.0f, 0.0f, 0.0f, 1.0f, 2.0f, 5.0f});
+#endif
+
     constexpr auto csc = [](const auto in) noexcept {
         constexpr auto csc_ = [](const auto in, const auto csc_) noexcept {
             if constexpr (std::is_arithmetic_v<decltype(in)>) {
@@ -223,40 +194,9 @@ namespace CheckSums {
     }();
     static_assert(long_chars_csc == 5816);
 
-
     static_assert(CHECKSUM_MODULUS < std::numeric_limits<uint32_t>::max());
     static_assert(CHECKSUM_MODULUS < std::numeric_limits<int32_t>::max());
 
     static_assert(noexcept(99253 + static_cast<unsigned int>(43.0)));
     static_assert(noexcept(73423 % CHECKSUM_MODULUS));
-    void CheckSumCombine(uint32_t& sum, double t) noexcept(csc_double) {
-        static_assert(DBL_MAX_10_EXP < 400);
-        if (t == 0.0)
-            return;
-        // biggest and smallest possible double should be ~10^(+/-308)
-        // taking log gives a number in the range +/- 309
-        // adding 400 gives numbers in the range ~0 to 800
-        // multiplying by 10'000 gives numbers in the range ~0 to 8'000'000
-        if (std::is_constant_evaluated())
-            sum += static_cast<unsigned int>((CXLog10(CXAbs(t)) + 400.0) * 10000.0);
-        else
-            sum += static_cast<unsigned int>((std::log10(std::abs(t)) + 400.0) * 10000.0);
-        sum %= CHECKSUM_MODULUS;
-    }
-
-    static_assert(noexcept(99837 + static_cast<unsigned int>(43.0f)));
-    void CheckSumCombine(uint32_t& sum, float t) noexcept {
-        static_assert(FLT_MAX_10_EXP < 40);
-        if (t == 0.0f)
-            return;
-        // biggest and smallest possible float should be ~10^(+/-38)
-        // taking log gives a number in the range +/- 39
-        // adding 40 gives numbers in the range ~0 to 80
-        // multiplying by 100'000 gives numbers in the range ~0 to 8'000'000
-        if (std::is_constant_evaluated())
-            sum += static_cast<unsigned int>((CXLog10(CXAbs(t)) + 40.0f) * 100000.0f);
-        else
-            sum += static_cast<unsigned int>((std::log10(std::abs(t)) + 40.0f) * 100000.0f);
-        sum %= CHECKSUM_MODULUS;
-    }
 }
