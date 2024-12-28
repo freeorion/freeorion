438a14cb2764eee810b5652f5c484cb5e1a16e07
geoffthemedio <geoffthemedio@gmail.com>
2024-12-28 11:11:31 +0100

    copy string_view into a (null terminated) string before passing to sscanf

--- universe/Meter.cpp
+++ universe/Meter.cpp
@@ -136,8 +136,9 @@ int Meter::SetFromChars(std::string_view chars) noexcept(have_noexcept_to_chars)
 
     static_assert(noexcept(result.ptr - chars.data()));
 #else
+    const std::string null_terminated_chars{chars};
     int chars_consumed = 0;
-    std::sscanf(chars.data(), "%d %d%n", &cur, &init, &chars_consumed);
+    std::sscanf(null_terminated_chars.data(), "%d %d%n", &cur, &init, &chars_consumed);
     return chars_consumed;
 #endif
 
