48d0f85f9b05497ced4d682a0b652b19be100296
agrrr3 <agrrr3@users.noreply.github.com>
2025-01-29 22:27:05 +0100

    Make Transpatial Hull available early
    * split the hull tech from the drive tech (480 PP -> 80PP + 400PP).
      the hull needs NANOTECH_PROD which is a useful tech on the way to super robo hulls
      the drive tech has exactly the same in-/direct prereqs like before
      cheapest hull in PP and RP which offers a core slot
      as closest alternative the composite line needs about 200PP but is more robust
    * advanced engineering bay only necessary to build the drive
      ship yard + orbital drydock is enough for the hull
    * make the hull a low fuel good scout (GOOD_HULL_DETECTION),
      base detection slightly better than protoplasmic hull
      so it will be nice hidden scout with the drive

--- default/scripting/techs/ship_hulls/robotic/SHP_TRANSSPACE.focs.py
+++ default/scripting/techs/ship_hulls/robotic/SHP_TRANSSPACE.focs.py
@@ -2,18 +2,32 @@ from focs._tech import *
 from macros.base_prod import TECH_COST_MULTIPLIER
 
 Tech(
+    name="SHP_TRANSSPACE_HULL",
+    description="SHP_TRANSSPACE_HULL_DESC",
+    short_description="SHIP_HULL_UNLOCK_SHORT_DESC",
+    category="SHIP_HULLS_CATEGORY",
+    researchcost=80 * TECH_COST_MULTIPLIER,
+    researchturns=8,
+    tags=["PEDIA_ROBOTIC_HULL_TECHS"],
+    prerequisites=["PRO_NANOTECH_PROD"],
+    unlock=[
+        Item(type=UnlockShipHull, name="SH_TRANSSPATIAL"),
+    ],
+    graphic="icons/ship_hulls/trans_spatial_hull_small.png",
+)
+
+Tech(
     name="SHP_TRANSSPACE_DRIVE",
     description="SHP_TRANSSPACE_DRIVE_DESC",
     short_description="SHIP_HULL_UNLOCK_SHORT_DESC",
-    category="SHIP_HULLS_CATEGORY",
-    researchcost=480 * TECH_COST_MULTIPLIER,
+    category="SHIP_PARTS_CATEGORY",
+    researchcost=400 * TECH_COST_MULTIPLIER,
     researchturns=8,
     tags=["PEDIA_ROBOTIC_HULL_TECHS"],
-    prerequisites=["SHP_NANOROBO_MAINT"],
+    prerequisites=["SHP_TRANSSPACE_HULL", "SHP_NANOROBO_MAINT"],
     unlock=[
-        Item(type=UnlockShipHull, name="SH_TRANSSPATIAL"),
         Item(type=UnlockShipPart, name="FU_TRANSPATIAL_DRIVE"),
         Item(type=UnlockBuilding, name="BLD_SHIPYARD_CON_ADV_ENGINE"),
     ],
-    graphic="icons/ship_hulls/trans_spatial_hull_small.png",
+    graphic="icons/ship_parts/engine-4.png",
 )
