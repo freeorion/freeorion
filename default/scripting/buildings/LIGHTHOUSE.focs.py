3832905ad69a8e6f7255ebdb43ff44b8bf049eba
Andrey <Cjkjvfnby@gmail.com>
2025-03-22 11:24:25 +0100

    [FOCS] Convert buildings

---
+++ default/scripting/buildings/LIGHTHOUSE.focs.py
@@ -0,0 +1,91 @@
+from buildings.buildings import SPECIES_LIKES_OR_DISLIKES_BUILDING_STABILITY_EFFECTS
+from focs._effects import (
+    Contains,
+    EffectsGroup,
+    EnemyOf,
+    InSystem,
+    IsBuilding,
+    IsSource,
+    LocalCandidate,
+    Max,
+    MinOf,
+    NamedReal,
+    OwnedBy,
+    Planet,
+    SetSpeed,
+    SetStealth,
+    Ship,
+    Source,
+    Speed,
+    Statistic,
+    Unowned,
+    Value,
+    WithinDistance,
+)
+from macros.base_prod import BUILDING_COST_MULTIPLIER
+from macros.enqueue import ENQUEUE_BUILD_ONE_PER_PLANET
+
+try:
+    from focs._buildings import *
+except ModuleNotFoundError:
+    pass
+
+BuildingType(  # type: ignore[reportUnboundVariable]
+    name="BLD_LIGHTHOUSE",
+    description="BLD_LIGHTHOUSE_DESC",
+    buildcost=25 * BUILDING_COST_MULTIPLIER,
+    buildtime=10,
+    location=(Planet() & ~Contains(IsBuilding(name=["BLD_LIGHTHOUSE"])) & OwnedBy(empire=Source.Owner)),
+    enqueuelocation=ENQUEUE_BUILD_ONE_PER_PLANET,
+    effectsgroups=[
+        *SPECIES_LIKES_OR_DISLIKES_BUILDING_STABILITY_EFFECTS,
+        EffectsGroup(
+            scope=(Ship & WithinDistance(distance=0.00001, condition=IsSource)),
+            stackinggroup="LIGHTHOUSE_IMMEDIATE_STEALTH_STACK",
+            effects=[SetStealth(value=Value - NamedReal(name="BLD_LIGHTHOUSE_IMMEDIATE_STEALTH_MALUS", value=20))],
+        ),
+        # Highly Self Illuminated, automatically "registered in catalog"
+        EffectsGroup(
+            scope=IsSource,
+            stackinggroup="LIGHTHOUSE_CATALOG_STEALTH_STACK",
+            accountinglabel="BLD_LIGHTHOUSE_CATALOG_STEALTH_LABEL",
+            effects=[SetStealth(value=Value - NamedReal(name="BLD_LIGHTHOUSE_SELF_STEALTH_MALUS", value=20))],
+        ),
+        # takes the maximum age of all lighthouse buildings in the system regardless of owner (by just triggering for the oldest ones)
+        EffectsGroup(
+            scope=WithinDistance(distance=0.00001, condition=IsSource),
+            activation=(
+                Source.Age
+                >= (
+                    Statistic(
+                        int,
+                        Max,
+                        value=LocalCandidate.Age,
+                        condition=InSystem(id=Source.SystemID) & IsBuilding(name=["BLD_LIGHTHOUSE"]),
+                    )
+                )
+            ),
+            stackinggroup="LIGHTHOUSE_CATALOG_STEALTH_STACK",
+            accountinglabel="BLD_LIGHTHOUSE_CATALOG_STEALTH_LABEL",
+            effects=[
+                SetStealth(
+                    value=Value
+                    - MinOf(float, Source.Age, NamedReal(name="BLD_LIGHTHOUSE_CATALOG_STEALTH_MALUS", value=10))
+                )
+            ],
+        ),
+        EffectsGroup(
+            scope=(
+                Ship
+                & ~(OwnedBy(affiliation=EnemyOf, empire=Source.Owner) | Unowned)
+                & WithinDistance(
+                    distance=NamedReal(name="BLD_LIGHTHOUSE_SPEEDUP_DISTANCE", value=50), condition=IsSource
+                )
+                & Speed(low=1)  # Immobile objects do not get the starlane speed boost.
+            ),
+            stackinggroup="LIGHTHOUSE_SPEED_STACK",
+            effects=[SetSpeed(value=Value + NamedReal(name="BLD_LIGHTHOUSE_SPEEDUP", value=20))],
+        ),
+    ],
+    icon="icons/building/lighthouse.png",
+)
---
+++ default/scripting/buildings/NEST_ERADICATOR.focs.py
@@ -0,0 +1,81 @@
+from focs._effects import (
+    BuildBuilding,
+    Contains,
+    Destroy,
+    EffectsGroup,
+    Enqueued,
+    GenerateSitRepMessage,
+    HasSpecial,
+    IsBuilding,
+    IsSource,
+    Object,
+    OwnedBy,
+    Planet,
+    RemoveSpecial,
+    Source,
+    Target,
+)
+from macros.priorities import (
+    SPECIAL_REMOVAL_PRIORITY,
+)
+
+try:
+    from focs._buildings import *
+except ModuleNotFoundError:
+    pass
+
+
+def EG_NEST_REMOVAL(monster_name):
+    special_name = f"{monster_name}_NEST_SPECIAL"
+
+    return EffectsGroup(
+        scope=(Object(id=Source.PlanetID) & Planet() & HasSpecial(name=special_name)),
+        # Remove the special at very late priority so meter changes based on it
+        # (e.g. stability likes/dislikes) are allowed to happen beforehand.
+        # This is to consistent with meter prediction in UI (which does not take special removal into account)
+        # NB: only works as intended if we do not do a second full effect application round for setting meters of newly created objects)
+        #     else the special will be already removed when the second application happens and the meter effects wont happen
+        priority=SPECIAL_REMOVAL_PRIORITY,
+        effects=[
+            RemoveSpecial(name=special_name),
+            GenerateSitRepMessage(
+                message="EFFECT_NEST_REMOVAL",
+                label="EFFECT_NEST_REMOVAL_LABEL",
+                icon="icons/building/nest_eradicator.png",
+                parameters={"planet": Target.ID},
+                empire=Source.Owner,
+            ),
+        ],
+    )
+
+
+BuildingType(  # type: ignore[reportUnboundVariable]
+    name="BLD_NEST_ERADICATOR",
+    description="BLD_NEST_ERADICATOR_DESC",
+    buildcost=80,
+    buildtime=5,
+    location=(
+        Planet()
+        & OwnedBy(empire=Source.Owner)
+        & ~Contains(IsBuilding(name=["BLD_NEST_ERADICATOR"]))
+        & (
+            HasSpecial(name="JUGGERNAUT_NEST_SPECIAL")
+            | HasSpecial(name="KRAKEN_NEST_SPECIAL")
+            | HasSpecial(name="SNOWFLAKE_NEST_SPECIAL")
+        )
+    ),
+    enqueuelocation=~Enqueued(
+        type=BuildBuilding,
+        name="BLD_NEST_ERADICATOR",
+    ),
+    effectsgroups=[
+        EG_NEST_REMOVAL("JUGGERNAUT"),
+        EG_NEST_REMOVAL("KRAKEN"),
+        EG_NEST_REMOVAL("SNOWFLAKE"),
+        EffectsGroup(
+            scope=IsSource,
+            effects=[Destroy],
+        ),
+    ],
+    icon="icons/building/nest_eradicator.png",
+)
