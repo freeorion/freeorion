6130db69c8f2392dc481f8ce40f41ab369393940
geoffthemedio <geoffthemedio@gmail.com>
2025-03-30 15:49:36 +0200

    remove extra "error:" from unit test logs

--- test/parse/main.cpp
+++ test/parse/main.cpp
@@ -11,19 +11,13 @@ struct IUnknown; // Workaround for "combaseapi.h(229,21): error C2760: syntax er
 #include "CommonTest.h"
 
 // Produces an GCC-ish message prefix for various frontends that understand gcc output.
-struct gcc_log_formatter : 
-    public boost::unit_test::output::compiler_log_formatter
-{
+struct gcc_log_formatter : public boost::unit_test::output::compiler_log_formatter {
     void print_prefix(std::ostream& output, boost::unit_test::const_string file_name, std::size_t line) override
-    {
-        output << file_name << ':' << line << ": error: ";
-    }
+    { output << file_name << ':' << line << " : "; }
 };
 
-struct boost_test_config
-{
-    boost_test_config()
-    {
+struct boost_test_config {
+    boost_test_config() {
         boost::unit_test::unit_test_log.set_formatter(new gcc_log_formatter);
         boost::unit_test::unit_test_monitor.register_exception_translator<boost::spirit::qi::expectation_failure<parse::token_iterator> >(&print_expectation_failure);
     }
