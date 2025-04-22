6dd584d8a78c9666333b79f8a795e5f2799d6d16
O01eg <o01eg@yandex.ru>
2025-04-22 21:42:06 +0400

    CI: Update Ubuntu used for address sanitizer to 24.04

--- .github/workflows/ubuntu-24.04-install-build.sh
+++ .github/workflows/ubuntu-24.04-install-build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -e
 
 sudo apt update
-sudo apt install -y libboost1.71-all-dev \
+sudo apt install -y libboost-all-dev \
 	libglew-dev \
 	libogg-dev \
 	libopenal-dev \
 	libsdl2-dev \
 	libvorbis-dev \
+	libfreetype-dev \
 	cppcheck \
 	doxygen \
 	python3-pip \
 	ccache \
-	clang \
 	godot3-server
 
