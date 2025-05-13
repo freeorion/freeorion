ad83e5290c043e142dbc143e6139ea396c32b8cf
geoffthemedio <geoffthemedio@gmail.com>
2025-05-13 23:09:14 +0200

    internal compiler error workarounds

--- universe/Conditions.cpp
+++ universe/Conditions.cpp
@@ -2948,17 +2948,16 @@ std::unique_ptr<Condition> CreatedOnTurn::Clone() const {
 // Contains                                              //
 ///////////////////////////////////////////////////////////
 namespace StaticTests {
-#if defined(__cpp_lib_constexpr_vector) && (!defined(_MSC_VER) || _MSC_VER >= 1942) && (!defined(__clang_major__) || (__clang_major__ >= 15)) && (!defined(__GNUC__) || (__GNUC__ > 12))
+#if defined(__cpp_lib_constexpr_vector) && (!defined(_MSC_VER) || (_MSC_VER >= 1942 && _MSC_VER != 1944)) && (!defined(__clang_major__) || (__clang_major__ >= 15)) && (!defined(__GNUC__) || (__GNUC__ > 12))
     struct ContainerTestObj : public UniverseObjectCXBase {
+        std::vector<int> contained_ids;
         constexpr explicit ContainerTestObj(std::vector<int> contained_ids_ = {}, int this_id = 2) :
             UniverseObjectCXBase(UniverseObjectType::INVALID_UNIVERSE_OBJECT_TYPE),
             contained_ids(contained_ids_)
         { this->SetID(this_id); }
 
-        constexpr std::span<const int> ContainedObjectIDs() const override;
-        std::vector<int> contained_ids;
+        constexpr std::span<const int> ContainedObjectIDs() const override { return contained_ids; }
     };
-    constexpr std::span<const int> ContainerTestObj::ContainedObjectIDs() const { return contained_ids; }
 
     static_assert(ContainerTestObj{}.ID() == 2);
     static_assert(ContainerTestObj{std::vector{1, 2, 3}, 5}.Contains(3));
@@ -10837,8 +10836,10 @@ namespace StaticTests {
     constexpr auto contains_aggressive_cx1 = Contains<Aggressive>{};
     constexpr auto contains_aggressive_cx2 = Contains{Aggressive{true}};
     constexpr auto contains_aggressive_cx3 = Contains<Aggressive>{true};
+#if (!defined(_MSC_VER) || (_MSC_VER != 1944))
     static_assert(static_cast<const Condition&>(contains_target_cx) != contains_capital_cx);
     static_assert(static_cast<const Condition&>(contains_capital_cx) != contains_aggressive_cx1);
+#endif
     static_assert(contains_aggressive_cx1 == contains_aggressive_cx2);
     static_assert(contains_aggressive_cx2 == contains_aggressive_cx3);
 
