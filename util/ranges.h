916cf4fd5f9a2511d2649a9e44c6a25819aa15ca
geoffthemedio <geoffthemedio@gmail.com>
2025-05-18 18:38:41 +0200

    -rework boost-based range_keys and range_values to work better for non map-like containers -pass stuff to range adaptors with perfect forwarding -fix forwarding

--- util/ranges.h
+++ util/ranges.h
@@ -31,10 +31,7 @@ inline constexpr auto& range_count_if = std::ranges::count_if;
 inline constexpr auto& range_distance = std::ranges::distance;
 #else
 # include <span>
-# include <boost/range/adaptor/map.hpp>
-# include <boost/range/adaptor/filtered.hpp>
-# include <boost/range/adaptor/transformed.hpp>
-# include <boost/range/adaptor/reversed.hpp>
+# include <boost/range/adaptors.hpp>
 # include <boost/range/algorithm.hpp>
 # include <boost/range/begin.hpp>
 # include <boost/range/end.hpp>
@@ -42,54 +39,66 @@ inline constexpr auto& range_distance = std::ranges::distance;
 # include <boost/algorithm/cxx11/all_of.hpp>
 # include <boost/algorithm/cxx11/none_of.hpp>
 # include <boost/algorithm/cxx11/copy_if.hpp>
-inline const auto& range_keys = boost::adaptors::map_keys;
-inline const auto& range_values = boost::adaptors::map_values;
+
 inline const auto& range_filter = boost::adaptors::filtered;
 inline const auto& range_transform = boost::adaptors::transformed;
 inline const auto& range_reverse = boost::adaptors::reversed;
 
-template <typename... Args>
-inline auto range_find_if(Args... args) { return boost::range::find_if(std::forward<Args>(args)...); }
-template <typename... Args>
-inline auto range_find(Args... args) { return boost::range::find(std::forward<Args>(args)...); }
+static constexpr struct range_keys_t {} range_keys{};
+auto operator|(auto&& r, const range_keys_t&)
+{ return boost::adaptors::keys(std::forward<decltype(r)>(r)); }
+
+static constexpr struct range_values_t {} range_values{};
+auto operator|(auto&& r, const range_values_t&)
+{ return boost::adaptors::values(std::forward<decltype(r)>(r)); }
 
 template <typename... Args>
-inline const auto range_any_of(Args... args) { return boost::algorithm::any_of(std::forward<Args>(args)...); }
+inline auto range_find_if(Args&&... args) { return boost::range::find_if(std::forward<Args>(args)...); }
+template <typename... Args>
+inline auto range_find(Args&&... args) { return boost::range::find(std::forward<Args>(args)...); }
+
+template <typename... Args>
+inline const auto range_any_of(Args&&... args) { return boost::algorithm::any_of(std::forward<Args>(args)...); }
 
 #if defined(__GNUC__)
 template <typename T, std::size_t E, typename... Args>
-inline const auto range_any_of(std::span<T, E> s, Args... args)
+inline const auto range_any_of(std::span<T, E> s, Args&&... args)
 { return boost::algorithm::any_of(s.begin(), s.end(), std::forward<Args>(args)...); }
 #endif
 
 template <typename... Args>
-inline const auto range_all_of(Args... args) { return boost::algorithm::all_of(std::forward<Args>(args)...); }
+inline const auto range_all_of(Args&&... args) { return boost::algorithm::all_of(std::forward<Args>(args)...); }
 template <typename... Args>
-inline const auto range_none_of(Args... args) { return boost::algorithm::none_of(std::forward<Args>(args)...); }
+inline const auto range_none_of(Args&&... args) { return boost::algorithm::none_of(std::forward<Args>(args)...); }
 template <typename... Args>
-inline auto range_copy(Args... args) { return boost::range::copy(std::forward<Args>(args)...); }
+inline auto range_copy(Args&&... args) { return boost::range::copy(std::forward<Args>(args)...); }
 template <typename... Args>
-inline auto range_copy_if(Args... args) { return boost::algorithm::copy_if(std::forward<Args>(args)...); }
+inline auto range_copy_if(Args&&... args) { return boost::algorithm::copy_if(std::forward<Args>(args)...); }
 template <typename... Args>
-inline auto range_max_element(Args... args) { return boost::range::max_element(std::forward<Args>(args)...); }
+inline auto range_max_element(Args&&... args) { return boost::range::max_element(std::forward<Args>(args)...); }
 template <typename... Args>
-inline auto range_min_element(Args... args) { return boost::range::min_element(std::forward<Args>(args)...); }
+inline auto range_min_element(Args&&... args) { return boost::range::min_element(std::forward<Args>(args)...); }
 template <typename... Args>
-inline auto range_equal(Args... args) { return boost::range::equal_range(std::forward<Args>(args)...); }
+inline auto range_equal(Args&&... args) { return boost::range::equal_range(std::forward<Args>(args)...); }
 template <typename... Args>
-inline auto range_end(Args... args) { return boost::end(std::forward<Args>(args)...); }
+inline auto range_end(Args&&... args) { return boost::end(std::forward<Args>(args)...); }
 template <typename... Args>
-inline auto range_count_if(Args... args) { return boost::count_if(std::forward<Args>(args)...); }
+inline auto range_count_if(Args&&... args) { return boost::count_if(std::forward<Args>(args)...); }
 template <typename... Args>
-inline auto range_distance(Args... args) { return boost::distance(std::forward<Args>(args)...); }
+inline auto range_distance(Args&&... args) { return boost::distance(std::forward<Args>(args)...); }
 #endif
 
 #if defined(__cpp_lib_ranges_contains)
 inline constexpr auto& range_contains = std::ranges::contains;
 #else
 template <typename Rng, typename... Args>
-inline auto range_contains(Rng&& rng, Args... args)
-{ return range_find(rng, std::forward<Args>(args)...) != range_end(rng); }
+inline auto range_contains(Rng&& rng, Args&&... args)
+{
+    if constexpr (requires { rng.contains(std::forward<Args>(args)...); })
+        return rng.contains(std::forward<Args>(args)...);
+    else
+        return std::find(std::begin(rng), std::end(rng), std::forward<Args>(args)...) != std::end(rng);
+}
 #endif
 
 #endif
