388d486b94930584f3055a90aca48f5ac63ca69e
geoffthemedio <geoffthemedio@gmail.com>
2025-07-25 16:40:15 +0200

    expose Condition::NoOp as NoOpLog

--- parse/ConditionPythonParser.cpp
+++ parse/ConditionPythonParser.cpp
@@ -883,6 +883,8 @@ void RegisterGlobalsConditions(boost::python::dict& globals) {
     globals["Capital"] = make_wrapped<Condition::Capital>();
     globals["Stationary"] = make_wrapped<Condition::Stationary>();
 
+    globals["NoOpLog"] = make_wrapped<Condition::NoOp>();
+
     globals["Unowned"] = make_wrapped<Condition::EmpireAffiliation>(EmpireAffiliationType::AFFIL_NONE);
     globals["IsHuman"] = make_wrapped<Condition::EmpireAffiliation>(EmpireAffiliationType::AFFIL_HUMAN);
 
