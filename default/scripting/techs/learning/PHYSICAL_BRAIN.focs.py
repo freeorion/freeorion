ea47574e6f3c10bda10d7206c7cc36adde64318a
agrrr3 <agrrr3@users.noreply.github.com>
2025-08-12 22:29:58 +0200

    Postpone Automatic History Analyzer and make it more expensive
    https://freeorion.org/forum/viewtopic.php?p=121862&
    
    Behold the brainlessness of building the AHA!
    The automatic history analyzer is very powerful we want to delay it
    hoping some empires will choose to invest into something else before
    
    - Adding LRN_ALGO_ELEGANCE (18RP) as prerequisite to LRN_PHYS_BRAIN.
    - Increase RP cost of LRN_PHYS_BRAIN (maybe 20 RP, +10PP, +100%).
    
    LRN_PHYS_BRAIN also unlocks translinguistics and liberty
    in order not to delay liberty so much:
    - move LIBERTY policy to LRN_ALGO_ELEGANCE
    
    AI: research and policy choices should be looked at
    en: the liberty and algorithmic elegance fluff might need enhancement
    
    This delays building the AHA about 3-4 turns if fully committed to it.
    QA: playtested researching & building the AHA
    Probably needs more delay, but no harm in playtesting it in mainline,

--- default/scripting/techs/learning/PHYSICAL_BRAIN.focs.py
+++ default/scripting/techs/learning/PHYSICAL_BRAIN.focs.py
@@ -6,9 +6,10 @@ Tech(
     description="LRN_PHYS_BRAIN_DESC",
     short_description="BUILDING_UNLOCK_SHORT_DESC",
     category="LEARNING_CATEGORY",
-    researchcost=10 * TECH_COST_MULTIPLIER,
+    researchcost=20 * TECH_COST_MULTIPLIER,
     researchturns=2,
     tags=["PEDIA_LEARNING_CATEGORY"],
-    unlock=[Item(type=UnlockPolicy, name="PLC_LIBERTY"), Item(type=UnlockBuilding, name="BLD_AUTO_HISTORY_ANALYSER")],
+    prerequisites=["LRN_ALGO_ELEGANCE"],
+    unlock=Item(type=UnlockBuilding, name="BLD_AUTO_HISTORY_ANALYSER"),
     graphic="icons/tech/the_physical_brain.png",
 )
