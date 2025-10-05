fdb9c4339f661d9fd1ddf987595fc372ce00ed26
SomeTroglodyte <63000004+SomeTroglodyte@users.noreply.github.com>
2025-10-05 18:45:41 +0200

    Test only for own empire having a field repeller queued

--- default/scripting/buildings/FIELD_REPELLOR.focs.py
+++ default/scripting/buildings/FIELD_REPELLOR.focs.py
@@ -24,7 +24,8 @@ BuildingType(  # type: ignore[reportUnboundVariable]
     buildtime=5,
     location=(Number(high=0, condition=IsBuilding(name=[ThisBuilding]) & OwnedBy(empire=Source.Owner))),
     enqueuelocation=(
-        ENQUEUE_BUILD_ONE_PER_PLANET & Number(high=0, condition=Enqueued(type=BuildBuilding, name=ThisBuilding))
+        ENQUEUE_BUILD_ONE_PER_PLANET
+        & Number(high=0, condition=Enqueued(type=BuildBuilding, name=ThisBuilding, empire=Source.Owner))
     ),
     effectsgroups=[
         EffectsGroup(
