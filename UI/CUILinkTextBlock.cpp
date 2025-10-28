f942d060970e07898888da81f84614691a1a9590
geoffthemedio <geoffthemedio@gmail.com>
2025-10-28 10:09:46 +0100

    -add a custom RichText block containing a CUIMultiEdit with tags ignored -override block factory for unformatted text in pedia -use that for showing stringtable contents instead of the default TextBlock, so as to allow text selection

--- UI/CUILinkTextBlock.cpp
+++ UI/CUILinkTextBlock.cpp
@@ -3,6 +3,9 @@
 #include "../util/VarText.h"
 #include "CUIControls.h"
 
+//////////////////////
+// CUILinkTextBlock //
+//////////////////////
 CUILinkTextBlock::CUILinkTextBlock(std::string str, GG::Clr color, GG::Flags<GG::WndFlag> flags) :
     GG::BlockControl(GG::X0, GG::Y0, GG::X1, flags | GG::INTERACTIVE),
     m_link_text(GG::Wnd::Create<CUILinkTextMultiEdit>(
@@ -55,4 +58,49 @@ std::shared_ptr<GG::BlockControl> CUILinkTextBlock::Factory::CreateFromTag(
     block->m_link_text->SetDecorator(VarText::FOCS_VALUE_TAG, TextLinker::DecoratorType::ValueRef);
 
     return block;
+}
+
+
+//////////////////
+// CUITextBlock //
+//////////////////
+CUITextBlock::CUITextBlock(std::string str, GG::Clr color, GG::Flags<GG::WndFlag> flags) :
+    GG::BlockControl(GG::X0, GG::Y0, GG::X1, flags | GG::INTERACTIVE),
+    m_text(GG::Wnd::Create<CUIMultiEdit>(
+        std::move(str),
+        GG::MULTI_WORDBREAK | GG::MULTI_READ_ONLY | GG::MULTI_LEFT | GG::MULTI_LINEWRAP |
+        GG::MULTI_TOP | GG::MULTI_NO_HSCROLL | GG::MULTI_NO_VSCROLL))
+{}
+
+void CUITextBlock::CompleteConstruction() {
+    GG::BlockControl::CompleteConstruction();
+
+    AttachChild(m_text);
+
+    m_text->SetColor(GG::CLR_ZERO);
+    m_text->SetInteriorColor(GG::CLR_ZERO);
+
+    SetName("CUITextBlock");
+}
+
+GG::Pt CUITextBlock::SetMaxWidth(GG::X width) {
+    m_text->Resize(GG::Pt(width, GG::Y1));
+    // Resize to have enough place to show the whole text.
+    GG::Pt size = m_text->FullSize();
+
+    // Only resize when changed.
+    if (size != m_text->Size())
+        m_text->Resize(size);
+
+    if (size != Size())
+        Resize(size);
+
+    return size;
+}
+
+std::shared_ptr<GG::BlockControl> CUITextBlock::Factory::CreateFromTag(
+    const GG::RichText::TAG_PARAMS&, std::string content,
+    std::shared_ptr<GG::Font>, GG::Clr color, GG::Flags<GG::TextFormat>) const
+{
+    return GG::Wnd::Create<CUITextBlock>(std::move(content), color, GG::NO_WND_FLAGS);
 }
