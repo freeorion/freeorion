0e6d40c3dbbe7f9a894c2638f290d785dd27180e
O01eg <o01eg@yandex.ru>
2025-12-24 12:56:43 +0400

    Implement Python FOCS for encyclopedia parser

---
+++ test-scripting/encyclopedia/ENC_SPECIES.focs.py
@@ -0,0 +1,9 @@
+from focs._encyclopedia_articles import Article
+
+Article(
+    name="ENC_SPECIES",
+    category="ENC_SPECIES",
+    short_description="",
+    description="ENC_SPECIES_DESC",
+    icon="icons/species/humanoid-01.png",
+)
