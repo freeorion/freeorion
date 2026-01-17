5ae41bce42eea51c1baed27a1b0cca3e51a6bf4d
geoffthemedio <geoffthemedio@gmail.com>
2026-01-17 14:43:30 +0100

    -rework FO_DEF_ENUM_IMPL to pre-process the input sequence to normalize for with or without an underling value specified per enum-value-name, with the transfomred sequence always having format (enumValueName, "enumValueName", flagIfValuePresent, maybeValue) -add tests of implementation macros -grooming

--- universe/Enums.cpp
+++ universe/Enums.cpp
@@ -1,3 +1,71 @@
-#include "Enums.h"
+#include "../util/Enum.h"
+
+#include <boost/preprocessor/seq/elem.hpp>
+#include <boost/preprocessor/seq/size.hpp>
+#include <boost/preprocessor/tuple/elem.hpp>
+#include <boost/preprocessor/tuple/size.hpp>
+
+#include <tuple>
+
+namespace {
+using namespace std::literals;
+
+FO_DEF_ENUM(OneT, int8_t, ((a, "a", 0, 0))((v, "v", 1, 5)))
+static_assert(OneT::v > OneT::a);
+
+FO_DEF_ENUM_TOSTRING(OneT, 1, ((a, "a", 0, 0))((v, "v", 1, 5)))
+static_assert(to_string(OneT::v) == "v");
+
+static_assert([]() {
+    using values_array_t = std::array<std::pair<OneT, std::string_view>, 2>;
+    constexpr values_array_t vals{{
+        FO_DEF_ENUM_ITERATE_VALUE(_, OneT, (a, "a", 0, 0))
+        FO_DEF_ENUM_ITERATE_VALUE(_, OneT, (v, "v"))
+    }};
+    return vals[0].first == OneT::a;
+}());
+
+FO_DEF_ENUM_ITERATE(OneT, 1, ((a, "a", 0, 0))((v, "v", 1, 5)))
+static_assert(OneTValues().size() == 2);
+
+FO_DEF_ENUM_FROM_STRING(OneT, 1)
+static_assert(OneTFromString("v") == OneT::v);
+
+FO_DEF_ENUM_IMPL_IMPL(TwoT, int8_t, 1, ((b, "b", 1, 1))((c, "c", 1, 0)))
+static_assert(TwoTFromString(to_string(TwoT::b)) == TwoT::b);
+
+using TestTuple = std::tuple<TwoT, std::string_view, int, int>;
+static_assert((TestTuple FO_DEF_ENUM_TX_VALUE_TUPLE(TwoT::b, 1, 6))  ==  TestTuple{TwoT::b, "TwoT::b"sv, 1, 6});
+static_assert((TestTuple FO_DEF_ENUM_TX_VALUE_TUPLE(TwoT::b, 0, 1))  ==  TestTuple{TwoT::b, "TwoT::b"sv, 0, 1});
+
+static_assert( BOOST_PP_TUPLE_SIZE((TwoT::b, 1, 6)) == 3u );
+
+static_assert((TestTuple FO_DEF_ENUM_TX_VALUE((TwoT::b, 6)))  ==  TestTuple{TwoT::b, "TwoT::b"sv, 1, 6}); // identified two elements in input, indicated with flag 1 and passed along value 6
+static_assert((TestTuple FO_DEF_ENUM_TX_VALUE((TwoT::b)))     ==  TestTuple{TwoT::b, "TwoT::b"sv, 0, 0}); // identified on element in input, indicated with flag 0 and default value 0
+
+#define input_seq_0 ((TwoT::c))((TwoT::b, 5))((TwoT::b, 0))
+#define tx_seq_0 FO_DEF_ENUM_TX_VALUES(input_seq_0)
+static_assert( BOOST_PP_SEQ_SIZE(tx_seq_0) == 3u );
+#define tx_seq_0_elem0 BOOST_PP_SEQ_ELEM(0, tx_seq_0)
+static_assert( BOOST_PP_TUPLE_SIZE(tx_seq_0_elem0) == 4u );
+static_assert( BOOST_PP_TUPLE_ELEM(0, tx_seq_0_elem0) == TwoT::c);
+#define tx_seq_0_elem1 BOOST_PP_SEQ_ELEM(1, tx_seq_0)
+static_assert( BOOST_PP_TUPLE_SIZE(tx_seq_0_elem1) == 4u );
+static_assert( BOOST_PP_TUPLE_ELEM(0, tx_seq_0_elem1) == TwoT::b);
 
 
+#define TEST_SEQ2_RAW ((NEG_ONE, -1))((ZERO))((ONE))((TWO))((THREE))
+static_assert( BOOST_PP_SEQ_SIZE(TEST_SEQ2_RAW) == 5u );
+static_assert( BOOST_PP_TUPLE_SIZE(BOOST_PP_SEQ_ELEM(0, TEST_SEQ2_RAW)) == 2u );
+static_assert( BOOST_PP_TUPLE_SIZE(BOOST_PP_SEQ_ELEM(1, TEST_SEQ2_RAW)) == 1u );
+
+#define TEST_SEQ2_RAW_SEQ_ELEM0 BOOST_PP_SEQ_ELEM(0, TEST_SEQ2_RAW)
+
+inline constexpr std::pair<std::string_view, int> neg_one {
+    BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(0, TEST_SEQ2_RAW_SEQ_ELEM0)) ,
+    BOOST_PP_TUPLE_ELEM(1, TEST_SEQ2_RAW_SEQ_ELEM0)
+};
+static_assert( neg_one == std::pair{"NEG_ONE"sv, -1});
+
+
+}
--- util/Enum.h
+++ util/Enum.h
@@ -8,6 +8,7 @@
 #include <string>
 
 #include <boost/preprocessor/comparison/equal.hpp>
+#include <boost/preprocessor/comparison/greater.hpp>
 #include <boost/preprocessor/control/if.hpp>
 #include <boost/preprocessor/seq/for_each.hpp>
 #include <boost/preprocessor/seq/transform.hpp>
@@ -18,32 +19,41 @@
 
 
 /** @brief Implementation detail for FO_ENUM */
-#define FO_DEF_ENUM_VALUE(r, _, elem) \
-    BOOST_PP_IF(BOOST_PP_EQUAL(BOOST_PP_TUPLE_SIZE(elem), 3), \
-        BOOST_PP_TUPLE_ELEM(0, elem) = BOOST_PP_TUPLE_ELEM(1, elem), \
-        BOOST_PP_TUPLE_ELEM(0, elem)),
+#define FO_DEF_ENUM_VALUE_IMPL(elemName, haveValue, maybeValue) \
+    BOOST_PP_IF(haveValue, elemName = maybeValue, elemName) ,
+
+#define FO_DEF_ENUM_VALUE(s, _, nameStringValue) \
+    FO_DEF_ENUM_VALUE_IMPL( \
+        BOOST_PP_TUPLE_ELEM(0, nameStringValue), \
+        BOOST_PP_TUPLE_ELEM(2, nameStringValue), \
+        BOOST_PP_TUPLE_ELEM(3, nameStringValue) \
+    )
 
 /** @brief Implementation detail for FO_ENUM */
-#define FO_DEF_ENUM(enumName, underlyingType, values) \
+#define FO_DEF_ENUM(enumName, underlyingType, namesStringsValues) \
 enum class enumName : underlyingType { \
-    BOOST_PP_SEQ_FOR_EACH(FO_DEF_ENUM_VALUE, _, values) \
+    BOOST_PP_SEQ_FOR_EACH(FO_DEF_ENUM_VALUE, _, namesStringsValues) \
 };
 
 
 /** @brief Implementation detail for FO_ENUM */
-#define FO_DEF_ENUM_TOSTRING_CASE_ELEM(r, enumName, elemName) \
-    case enumName :: elemName: return BOOST_PP_STRINGIZE(elemName); break;
+#define FO_DEF_ENUM_TOSTRING_CASE_IMPL(enumName, elemName, elemNameString) \
+    case enumName :: elemName : return elemNameString; break;
 
-#define FO_DEF_ENUM_TOSTRING_CASE(r, enumName, elem) \
-    FO_DEF_ENUM_TOSTRING_CASE_ELEM(r, enumName, BOOST_PP_TUPLE_ELEM(0, elem))
+#define FO_DEF_ENUM_TOSTRING_CASE(s, enumName, nameStringValue) \
+    FO_DEF_ENUM_TOSTRING_CASE_IMPL( \
+        enumName, \
+        BOOST_PP_TUPLE_ELEM(0, nameStringValue), \
+        BOOST_PP_TUPLE_ELEM(1, nameStringValue) \
+    )
 
 /** Defines to_string(enumName) and ostream& operator<<(ostream&, enumName) */
-#define FO_DEF_ENUM_TOSTRING(enumName, tupleSize, values) \
+#define FO_DEF_ENUM_TOSTRING(enumName, tupleSize, namesStringsValues) \
 constexpr BOOST_PP_IF(BOOST_PP_EQUAL(tupleSize, 2), friend, BOOST_PP_EMPTY()) \
 std::string_view to_string(enumName value) noexcept { \
     switch(value) \
     { \
-        BOOST_PP_SEQ_FOR_EACH(FO_DEF_ENUM_TOSTRING_CASE, enumName, values) \
+        BOOST_PP_SEQ_FOR_EACH(FO_DEF_ENUM_TOSTRING_CASE, enumName, namesStringsValues) \
         default: return ""; break; \
     } \
 } \
@@ -54,31 +64,35 @@ std::ostream& operator <<(std::ostream& stream, enumName value) \
 
 
 /** Assembles one initializer list pair of {enumName::elem, "elem"} */
-#define FO_DEF_ENUM_ITERATE_ELEM(r, enumName, elem) \
-        {enumName :: elem, BOOST_PP_STRINGIZE(elem) },
+#define FO_DEF_ENUM_ITERATE_ELEM(enumName, elemName, elemNameString) \
+        { enumName :: elemName, elemNameString },
 
 /** @brief Implementation detail for FO_ENUM.
   * elem may be a single enumeration value (enum_value) or may be
   * an enumeration value and underly type representation (enum_value = N).
   * This extracts just the enumeration value and passes that along. */
-#define FO_DEF_ENUM_ITERATE_VALUE(r, enumName, elem) \
-    FO_DEF_ENUM_ITERATE_ELEM(r, enumName, BOOST_PP_TUPLE_ELEM(0, elem))
+#define FO_DEF_ENUM_ITERATE_VALUE(_, enumName, nameStringValue) \
+    FO_DEF_ENUM_ITERATE_ELEM( \
+        enumName, \
+        BOOST_PP_TUPLE_ELEM(0, nameStringValue), \
+        BOOST_PP_TUPLE_ELEM(1, nameStringValue) \
+    )
 
 /** Assemables an array of pair<enumName, std::string_vew> where the
   * string_view are the text representations of the enumName.
   * Also defines a getter for that array as enumNameValues(). */
-#define FO_DEF_ENUM_ITERATE(enumName, tupleSize, values) \
+#define FO_DEF_ENUM_ITERATE(enumName, tupleSize, namesStringsValues) \
 struct BOOST_PP_CAT(enumName, Impl) { \
-    static constexpr std::size_t values_count = BOOST_PP_SEQ_SIZE(values); \
+    static constexpr std::size_t values_count = BOOST_PP_SEQ_SIZE(namesStringsValues); \
     using values_array_t = std::array<std::pair<enumName, std::string_view>, values_count>; \
     static constexpr values_array_t vals {{ \
-    BOOST_PP_SEQ_FOR_EACH(FO_DEF_ENUM_ITERATE_VALUE, enumName, values) \
+    BOOST_PP_SEQ_FOR_EACH(FO_DEF_ENUM_ITERATE_VALUE, enumName, namesStringsValues) \
     }}; \
 }; \
 inline \
 BOOST_PP_IF(BOOST_PP_EQUAL(tupleSize, 2), static, BOOST_PP_EMPTY()) \
 constexpr auto& BOOST_PP_CAT(enumName, Values)() noexcept \
-{ return BOOST_PP_CAT(enumName, Impl) ::vals; }
+{ return BOOST_PP_CAT(enumName, Impl) :: vals; }
 
 
 /** Defines enumName enumNameFromString(string_view sv, enumName not_found_result)
@@ -105,25 +119,48 @@ std::istream& operator >>(std::istream& stream, enumName & value) { \
 
 /** @brief Implementation detail for FO_ENUM.
   * tupleSize is expected to be 2 for a non-class enum and 3 for an enum defined in a class. */
-#define FO_DEF_ENUM_IMPL_IMPL(enumName, underlyingType, tupleSize, valuesStrings) \
-    FO_DEF_ENUM(enumName, underlyingType, valuesStrings) \
-    FO_DEF_ENUM_TOSTRING(enumName, tupleSize, valuesStrings) \
-    FO_DEF_ENUM_ITERATE(enumName, tupleSize, valuesStrings) \
+#define FO_DEF_ENUM_IMPL_IMPL(enumName, underlyingType, tupleSize, namesStringsValues) \
+    FO_DEF_ENUM(enumName, underlyingType, namesStringsValues) \
+    FO_DEF_ENUM_TOSTRING(enumName, tupleSize, namesStringsValues) \
+    FO_DEF_ENUM_ITERATE(enumName, tupleSize, namesStringsValues) \
     FO_DEF_ENUM_FROM_STRING(enumName, tupleSize)
 
 
-/** @brief Implementation detail for FO_ENUM */
-#define FO_DEF_ENUM_ADD_STRING_REPR(s, _, elem) \
-    BOOST_PP_TUPLE_PUSH_BACK(elem, BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(0, elem)))
+/** Combines into a 4-tuple: 
+  * ( element name, string representation of element name, is there a value?, the value or a default value )*/
+#define FO_DEF_ENUM_TX_VALUE_TUPLE(elemName, haveValue, maybeValue) \
+    ( elemName, BOOST_PP_STRINGIZE(elemName), haveValue, maybeValue ) 
 
-#define FO_ENUM_VALUES_WITH_STRINGS(values) \
-    BOOST_PP_SEQ_TRANSFORM(FO_DEF_ENUM_ADD_STRING_REPR, _, values)
+/** passes along element name and whether there is a value, and
+  * passes either the value or () */
+#define FO_DEF_ENUM_TX_VALUE_IMPL(elemName, haveValue, elem) \
+    FO_DEF_ENUM_TX_VALUE_TUPLE( \
+        elemName, \
+        haveValue, \
+        BOOST_PP_IF(haveValue, BOOST_PP_TUPLE_ELEM(1, elem), 0) \
+    )
+
+/** Extracts first sub-element (enum value name).
+  * Checks whether there is more than one sub-element. If so, passes 1, and if not, passes 0, as a flag.
+  * Also passes the input element. */
+#define FO_DEF_ENUM_TX_VALUE(elem) \
+    FO_DEF_ENUM_TX_VALUE_IMPL( \
+        BOOST_PP_TUPLE_ELEM(0, elem), \
+        BOOST_PP_IF(BOOST_PP_GREATER(BOOST_PP_TUPLE_SIZE(elem), 1), 1, 0), \
+        elem \
+    )
+
+#define FO_DEF_ENUM_TX_VALUE_WRAP(s, _, elem) \
+    FO_DEF_ENUM_TX_VALUE(elem)
+
+#define FO_DEF_ENUM_TX_VALUES(values) \
+    BOOST_PP_SEQ_TRANSFORM(FO_DEF_ENUM_TX_VALUE_WRAP, _, values)
 
 /** @brief Extracts EnumName from (EnumName) or (ClassName, EnumName) */
 #define FO_ENUM_NAME_FROM_TYPENAME(typeName) \
-BOOST_PP_IF(BOOST_PP_EQUAL(BOOST_PP_TUPLE_SIZE(typeName), 2), \
-            BOOST_PP_TUPLE_ELEM(1, typeName), \
-            BOOST_PP_TUPLE_ELEM(0, typeName))
+    BOOST_PP_IF(BOOST_PP_EQUAL(BOOST_PP_TUPLE_SIZE(typeName), 2), \
+                BOOST_PP_TUPLE_ELEM(1, typeName), \
+                BOOST_PP_TUPLE_ELEM(0, typeName))
 
 /** @brief Implementation detail for FO_ENUM. */
 #define FO_DEF_ENUM_IMPL(typeName, underlyingType, values) \
@@ -131,7 +168,7 @@ BOOST_PP_IF(BOOST_PP_EQUAL(BOOST_PP_TUPLE_SIZE(typeName), 2), \
         FO_ENUM_NAME_FROM_TYPENAME(typeName), \
         underlyingType, \
         BOOST_PP_TUPLE_SIZE(typeName), \
-        FO_ENUM_VALUES_WITH_STRINGS(values) \
+        FO_DEF_ENUM_TX_VALUES(values) \
     )
 
 /** @brief Define an enumeration
