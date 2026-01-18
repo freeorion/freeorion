6f26f8e18bd7df1e6f92d5651d67b78944d538ed
geoffthemedio <geoffthemedio@gmail.com>
2026-01-18 13:41:13 +0100

    add safety checks for irregular values in DoubleToString

--- util/i18n.cpp
+++ util/i18n.cpp
@@ -532,8 +532,15 @@ std::string DoubleToString(double val, int digits, bool always_show_sign) {
     digits = std::max(digits, 2);
 
     // default result for sentinel value
-    if (val == UNKNOWN_UI_DISPLAY_VALUE)
+    if (val == UNKNOWN_UI_DISPLAY_VALUE) {
         return UserString("UNKNOWN_VALUE_SYMBOL");
+    } if (std::isnan(val)) {
+        return "";
+    } else if (std::isinf(val)) {
+        return val > 0 ? "∞" : "-∞";
+    } else if (!std::isfinite(val)) {
+        return "";
+    }
 
     double mag = std::abs(val);
 
