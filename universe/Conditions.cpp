freeorion/freeorion

commit 44f1d65e0ead053dc8989631bb9701562a929a9b

use !EvalAny if checking for 0 subcondition candidate matches in Number::Match, with no lower limit or a lower limit of 0

universe/Conditions.cpp
@@ -367,19 +367,18 @@ void Number::Eval(const ScriptingContext& parent_context,
 bool Number::Match(const ScriptingContext& local_context) const {
     // get acceptable range of subcondition matches for candidate
     const int low = (m_low ? std::max(0, m_low->Eval(local_context)) : 0);
-    if (low == 1 && !m_high)
+    if (low == 1 && !m_high) // require at least one, don't care if more
         return m_condition->EvalAny(local_context);
+    const int high = (m_high ? std::min(m_high->Eval(local_context), INT_MAX) : INT_MAX);
+    if (high == 0 && (!m_low || low == 0)) // require none, don't care how many if not none
+        return !m_condition->EvalAny(local_context);
 
     // get set of all UniverseObjects that satisfy m_condition
     const ObjectSet condition_matches = m_condition->Eval(local_context);
 
     // compare number of objects that satisfy m_condition to the acceptable range of such objects
     const int matched = condition_matches.size();
-    if (low > matched)
-        return false;
-
-    const int high = (m_high ? std::min(m_high->Eval(local_context), INT_MAX) : INT_MAX);
-    return matched <= high;
+    return (low >= matched && matched <= high);
 }
 
 void Number::SetTopLevelContent(const std::string& content_name) {

commit 0f2329b909fde3c9bb155d42905a666c2c93c0fb

Fix logical error in Number condition (regression)
* regression introduced in 44f1d65e0ead053dc8989631bb9701562a929a9b

universe/Conditions.cpp
@@ -381,7 +381,7 @@ bool Number::Match(const ScriptingContext& local_context) const {
 
     // compare number of objects that satisfy m_condition to the acceptable range of such objects
     const int matched = condition_matches.size();
-    return (low >= matched && matched <= high);
+    return (low <= matched && matched <= high);
 }
 
 void Number::SetTopLevelContent(const std::string& content_name) {