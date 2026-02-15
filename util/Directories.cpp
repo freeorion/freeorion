52d2c2540b3edc1d60769d73e7e88e4d1e81b475
O01eg <o01eg@yandex.ru>
2026-02-15 19:18:47 +0400

    Fix Android Python library after SDK update

--- util/Directories.cpp
+++ util/Directories.cpp
@@ -509,7 +509,7 @@ void InitDirs(std::string const& argv0, bool test)
 
     s_python_home = s_cache_dir / "python";
     fs::create_directories(s_python_home / "lib");
-    CopyInitialResourceAndroid("lib/python39.zip");
+    CopyInitialResourceAndroid("lib/python310.zip");
 #endif
 
     g_initialized = true;
