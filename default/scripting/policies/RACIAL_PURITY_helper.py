e3f35e3a9a9df50ad511402f026b74dc812e3b8c
agrrr3 <agrrr3@users.noreply.github.com>
2026-02-19 17:49:41 +0100

    [debug][focs] more RACIAL_PURITY_helper debug

--- default/scripting/policies/RACIAL_PURITY_helper.py
+++ default/scripting/policies/RACIAL_PURITY_helper.py
@@ -18,12 +18,25 @@ racial_purity_once_per_turn_effectgroups = [
             condition=(EmpireHasAdoptedPolicy(empire=Source.Owner, name="PLC_RACIAL_PURITY")),
             effects=[
                 #SetEmpireMeter(empire=Source.Owner, meter="PLC_RACIAL_PURITY_TURN_ACTIVE", value=CurrentTurn),
-                SetEmpireMeter(empire=Source.Owner, meter="PLC_RACIAL_PURITY_TURN_ACTIVE", value=0.0+CurrentTurn),
+                SetEmpireMeter(empire=Source.Owner, meter="PLC_RACIAL_PURITY_TURN_ACTIVE", value=Value+CurrentTurn),
+                GenerateSitRepMessage(
+                    message="Test Set ; last rac pur turn: %rawtext:current% (was %rawtext:lastturn%) %rawtext:slcurrent% (w %rawtext:sllastturn%)",
+                    label="SITREP_WELCOME_LABEL",
+                    NoStringtableLookup=True,
+                    icon="icons/tech/categories/spy.png",
+                    parameters={
+                        "current": EmpireMeterValue(empire=Source.Owner, meter="PLC_RACIAL_PURITY_TURN_ACTIVE"),
+                        "lastturn": Value(EmpireMeterValue(empire=Source.Owner, meter="PLC_RACIAL_PURITY_TURN_ACTIVE")),
+                        "slcurrent": EmpireMeterValue(empire=Source.Owner, meter="SOCIAL_CATEGORY_NUM_POLICY_SLOTS"),
+                        "sllastturn": Value(EmpireMeterValue(empire=Source.Owner, meter="SOCIAL_CATEGORY_NUM_POLICY_SLOTS")),
+                    },
+                    empire=Source.Owner,
+                ),
             ],
             else_=[
                 SetEmpireMeter(empire=Source.Owner, meter="SOCIAL_CATEGORY_NUM_POLICY_SLOTS", value=Value+1),
                 GenerateSitRepMessage(
-                    message="Test ; last rac pur turn: %lastturn%",
+                    message="Test NoSet ; last rac pur turn: %rawtext:lastturn%",
                     label="SITREP_WELCOME_LABEL",
                     NoStringtableLookup=True,
                     icon="icons/tech/categories/spy.png",
