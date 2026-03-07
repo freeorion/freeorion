3b80d4ec4f1386e86aaf5066eafde6fcebede914
O01eg <397177+o01eg@users.noreply.github.com>
2026-03-07 19:00:06 +0400

    Add CreatePlanet effect

--- parse/EffectsPythonModuleParser.cpp
+++ parse/EffectsPythonModuleParser.cpp
@@ -4,7 +4,9 @@
 #include <boost/python/docstring_options.hpp>
 #include <boost/python/module.hpp>
 #include <boost/python/raw_function.hpp>
+#include <boost/python/stl_iterator.hpp>
 
+#include "EnumPythonParser.h"
 #include "EffectPythonParser.h"
 #include "ValueRefPythonParser.h"
 
@@ -31,12 +33,35 @@ namespace {
         auto error_str = std::string{"Either focus or x, y should be set for MoveInOrbit ("} + __func__ + ")";
         throw std::runtime_error(error_str);
     }
+
+    effect_wrapper insert_create_planet(const boost::python::tuple& args, const boost::python::dict& kw) {
+        std::unique_ptr<ValueRef::ValueRef<PlanetType>> type = pyobject_to_vref_enum<PlanetType>(kw["type"]);
+        std::unique_ptr<ValueRef::ValueRef<PlanetSize>> size = pyobject_to_vref_enum<PlanetSize>(kw["planetsize"]);
+
+        std::unique_ptr<ValueRef::ValueRef<std::string>> name;
+        if (kw.has_key("name"))
+            name = pyobject_to_vref<std::string>(kw["name"]);
+
+        std::vector<std::unique_ptr<Effect::Effect>> effects;
+        if (kw.has_key("initial_effects")) {
+            boost::python::stl_input_iterator<effect_wrapper> effects_begin(kw["initial_effects"]), effects_end;
+            for (auto it = effects_begin; it != effects_end; ++ it)
+                effects.push_back(ValueRef::CloneUnique(it->effect));
+        }
+
+        return effect_wrapper(std::make_shared<Effect::CreatePlanet>(
+            std::move(type),
+            std::move(size),
+            std::move(name),
+            std::move(effects)));
+    }
 }
 
 BOOST_PYTHON_MODULE(_effects_new) {
     boost::python::docstring_options doc_options(true, true, false);
 
     boost::python::def("MoveInOrbit", boost::python::raw_function(insert_move_in_orbit));
+    boost::python::def("CreatePlanet", boost::python::raw_function(insert_create_planet));
 }
 
 
