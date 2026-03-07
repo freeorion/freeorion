16158f36c70aae4ec8e1d61f88dcce2ee7180dba
O01eg <397177+o01eg@users.noreply.github.com>
2026-03-07 19:00:06 +0400

    Add CreatePlanet effect

--- default/scripting/focs/_effects_new.pyi
+++ default/scripting/focs/_effects_new.pyi
@@ -1,8 +1,14 @@
-from focs._types import _Condition, _Effect, _FloatParam
+from focs._types import _Condition, _Effect, _FloatParam, _PlanetSize, _PlanetType, _StringParam
 
 def MoveInOrbit(
     speed: _FloatParam,
     focus: _Condition = ...,
     x: _FloatParam = ...,
     y: _FloatParam = ...,
+) -> _Effect: ...
+def CreatePlanet(
+    type: _PlanetType,
+    planetsize: _PlanetSize,
+    name: _StringParam = ...,
+    initial_effects: list[_Effect] = ...,
 ) -> _Effect: ...
