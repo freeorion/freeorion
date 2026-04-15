3275c0fb055134e93baafb0eb5de4e16bff120ec
agrrr3 <agrrr3@users.noreply.github.com>
2026-04-15 19:19:05 +0200

    Growth bonus per habitable planet size as named value

--- default/scripting/species/species_macros/population.py
+++ default/scripting/species/species_macros/population.py
@@ -38,6 +38,8 @@ from focs._value_refs import (
     CurrentTurn,
     MaxOf,
     MinOf,
+    NamedIntegerLookup,
+    NamedReal,
     Value,
 )
 from macros.misc import GROWTH_RATE_FACTOR
@@ -116,7 +118,14 @@ _SELF_SUSTAINING_BONUS = EffectsGroup(
     activation=Planet(environment=[Good]) & HasTag(name="SELF_SUSTAINING"),
     accountinglabel="SELF_SUSTAINING_LABEL",
     priority=TARGET_POPULATION_AFTER_SCALING_PRIORITY,
-    effects=SetTargetPopulation(value=Value + 3 * Target.HabitableSize),  # Gets the same bonus as three growth specials
+    effects=SetTargetPopulation(
+        value=Value
+        + NamedReal(
+            name="SELF_SUSTAINING_TARGET_POPULATION_PER_SIZE_BONUS",
+            value=3 * NamedIntegerLookup(name="GROWTH_SPECIAL_TARGET_POPULATION_PER_SIZE_BONUS"),
+        )
+        * Target.HabitableSize
+    ),  # Gets the same bonus as three growth specials
 )
 
 _PHOTOTROPHIC_BONUS = [
