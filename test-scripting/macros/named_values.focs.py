dbce1f2894d5922eb2f8f58bfaec1e34c8dff98a
O01eg <397177+o01eg@users.noreply.github.com>
2026-04-24 15:32:38 +0400

    Convert named values parser test to Python FOCS

---
+++ test-scripting/macros/named_values.focs.py
@@ -0,0 +1,9 @@
+from focs._effects import (
+    GameRule,
+    NamedInteger,
+    NamedReal,
+)
+
+NamedInteger(name="MIN_MONSTER_DISTANCE", value=GameRule(type=int, name="RULE_MINIMUM_MONSTER_DISTANCE_CAPITAL") - 1)
+
+NamedReal(name="BLD_BLACK_HOLE_POW_GEN_MIN_STABILITY", value=20)
