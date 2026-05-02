759018663d26939c8fa969f6ff5e01769f0fe1bd
O01eg <397177+o01eg@users.noreply.github.com>
2026-05-02 19:37:13 +0400

    Screenshot from snap

--- .github/snap-xvfb-launch.sh
+++ .github/snap-xvfb-launch.sh
@@ -1,30 +1,42 @@
 #!/bin/bash
 
-LIBGL_DEBUG=verbose glxinfo
-
 ps auxx | grep 'Xvfb' | grep -v grep
 
 export SDL_VIDEODRIVER=x11
 
-echo = Launching freeorion =
+echo "::group::Launching freeorion at $(pwd)"
 
-LIBGL_DEBUG=verbose freeorion.freeorion &
+LIBGL_DEBUG=verbose /snap/bin/freeorion \
+  -q \
+  --log-file $(pwd)/freeorion.log \
+  --audio.effects.enabled 0 \
+  --audio.music.enabled 0 \
+  --auto-advance-n-turns 100 \
+  --video.fullscreen.width 1280 \
+  --video.windowed.width 1280 \
+  --video.fullscreen.height 720 \
+  --video.windowed.height 720 \
+  --video.windowed.top 0 \
+  --setup.ai.player.count 3 &
 FOPID=$!
-sleep 10
-kill ${FOPID}
+echo "FreeOrion started pid ${FOPID}"
+sleep 25
+import -display :99 -window root $(pwd)/screenshot.png
+kill -9 ${FOPID}
 wait ${FOPID}
 echo Freeorion return code $?
-cat ~/.local/share/freeorion/freeorion.log || echo No log file
 
-echo = Launching freeorion godot =
+echo "::endgroup::"
+echo "::group::Launching freeorion godot"
 
 export SDL_VIDEODRIVER=x11
 
-LIBGL_DEBUG=verbose freeorion.freeorion-godot &
+LIBGL_DEBUG=verbose /snap/bin/freeorion.freeorion-godot --log-file $(pwd)/freeorion-godot.log &
 FOPID=$!
+echo "FreeOrion Godot started pid ${FOPID}"
 sleep 10
-kill ${FOPID}
+kill -9 ${FOPID}
 wait ${FOPID}
 echo Freeorion Godot return code $?
-cat ~/.local/share/freeorion/freeorion-godot.log || echo No log file
+echo "::endgroup::"
 
