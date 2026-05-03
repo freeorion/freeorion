d4ee1673fc0cc47653123e25cc1c419f7c2d3a73
geoffthemedio <geoffthemedio@gmail.com>
2026-05-03 14:31:03 +0200

    increase buffer size to account for count

--- util/SerializeEmpire.cpp
+++ util/SerializeEmpire.cpp
@@ -55,10 +55,16 @@ namespace {
     constexpr uint8_t uint16_t_digits = 5; // digits in base 10 of 65536 = 2^16
     static_assert(Pow(10, uint16_t_digits + 1) > uint16_t_max);
 
+    constexpr uint8_t uint8_t_max = std::numeric_limits<uint8_t>::max();
+    constexpr uint8_t uint8_t_digits = 3; // digits in base 10 of 256 = 2^8
+    static_assert(Pow(10, uint8_t_digits + 1) > uint8_t_max);
+
+
     constexpr uint8_t data_arr_sz = 8;
     using DataArrT = std::array<std::pair<uint16_t, uint16_t>, data_arr_sz>;
 
-    constexpr std::size_t buffer_size = data_arr_sz*2*(uint16_t_digits + 1); // space for "65535 12345 " repeated data_arr_sz times
+    // space for "65535 12345 " repeated data_arr_sz times, and "255" for the count
+    constexpr std::size_t buffer_size = data_arr_sz*2*(uint16_t_digits + 1) + uint8_t_digits;
 
     // returns { next unconsumed char*, true/false did the parse succeed }
     // parsed value returned in \a val_out
@@ -121,8 +127,12 @@ namespace {
             if (tally >= count)
                 break;
             ++tally;
+            if (buffer_next == buffer_end)
+                break;
             *buffer_next++ = ' ';
             buffer_next += ToChars(offset, buffer_next, buffer_end);
+            if (buffer_next == buffer_end)
+                break;
             *buffer_next++ = ' ';
             buffer_next += ToChars(sz, buffer_next, buffer_end);
         }
