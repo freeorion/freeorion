1d0fb2ca12961ff443990c0ee34859dfd6af1ea7
O01eg <397177+o01eg@users.noreply.github.com>
2026-05-05 07:47:16 +0400

    Ignore loading of _typing module

--- parse/PythonParser.cpp
+++ parse/PythonParser.cpp
@@ -511,7 +511,9 @@ py::object PythonParser::find_spec(const std::string& fullname, const py::object
         module_path.replace_extension("py");
         if (IsExistingFile(module_path))
             return py::object(module_spec(fullname, parent, *this));
-        else {
+        else if (fullname == "_typing") {
+            return py::object();
+        } else {
             ErrorLogger() << "Couldn't find file for module spec " << fullname;
             throw import_error("Couldn't find file for module spec " + fullname);
         }
