1b63a0be54600f0e49fa7f2c5743db2ab4dfdc09
agrrr3 <agrrr3@users.noreply.github.com>
2026-06-25 00:10:45 +0200

    Simply dont apply unstealthiness for freshly built ships. Fixes weirdness

--- default/scripting/techs/spy/ROOT_DECEPTION.focs.py
+++ default/scripting/techs/spy/ROOT_DECEPTION.focs.py
@@ -190,8 +190,9 @@ Tech(
                 AddSpecial(name=base_stealth_special, capacity=Value(Target.Stealth)), # record server value
                 Conditional( # if there is a discrepancy between server and client value, sync to server value
                     #condition=0.01 > Abs(SpecialCapacity(name=base_stealth_special, object=Target.ID) - Value(Target.Stealth)),
-                    condition=0.001 < ((SpecialCapacity(name=base_stealth_special, object=LocalCandidate.ID) - Value(LocalCandidate.Stealth))
-                                     *(SpecialCapacity(name=base_stealth_special, object=LocalCandidate.ID) - Value(LocalCandidate.Stealth))),
+                    condition=HasSpecial(name=base_stealth_special) # should be false iff the ship was built this turn
+                        & (0.001 < ((SpecialCapacity(name=base_stealth_special, object=LocalCandidate.ID) - Value(LocalCandidate.Stealth))
+                                     *(SpecialCapacity(name=base_stealth_special, object=LocalCandidate.ID) - Value(LocalCandidate.Stealth)))),
                     effects=[
                         SetStealth(value=SpecialCapacity(name=base_stealth_special, object=Target.ID)),
                     ],
