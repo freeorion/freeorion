bd1a35cba999273b1896d440754a4e8fa9f76831
O01eg <397177+o01eg@users.noreply.github.com>
2026-07-24 18:18:44 +0400

    Init dirs in Godot 4.4 FreeOrionNode

--- client/godot4/FreeOrionNode.cpp
+++ client/godot4/FreeOrionNode.cpp
@@ -1,5 +1,18 @@
 #include "FreeOrionNode.h"
 
+#include <godot_cpp/classes/os.hpp>
+
+#include "../../util/Directories.h"
+
 void FreeOrionNode::_bind_methods() {
 
 }
+
+FreeOrionNode::FreeOrionNode() {
+    std::string executable_path = godot::OS::get_singleton()->get_executable_path().utf8().get_data();
+
+    InitDirs(executable_path);
+}
+
+FreeOrionNode::~FreeOrionNode()
+{ }
