802146abed8d58190c99b8c1d6dc03f96f40c27e
O01eg <397177+o01eg@users.noreply.github.com>
2026-07-25 23:15:57 +0400

    Test FreeOrionNode existence in Godot 4.4 project

--- godot4/test/integration/test_quickstart.gd
+++ godot4/test/integration/test_quickstart.gd
@@ -1,5 +1,12 @@
 extends GutTest
 
 
-func test_empty():
-	pass_test("Empty")
+func test_quickstart():
+	assert_not_null(GlobalFreeOrionNode)
+	assert_true(is_instance_valid(GlobalFreeOrionNode))
+
+	assert_not_null(GlobalFreeOrionNode.get_version())
+	assert_typeof(GlobalFreeOrionNode.get_version(), TYPE_STRING)
+
+	assert_not_null(GlobalFreeOrionNode.get_user_data_dir())
+	assert_typeof(GlobalFreeOrionNode.get_user_data_dir(), TYPE_STRING)
