1a1e4686ce2bd694516affa27b9c26d1cd46c276
O01eg <397177+o01eg@users.noreply.github.com>
2026-07-26 18:26:54 +0400

    Fix OptionsDB for multiple initializations

--- client/godot4/FreeOrionNode.cpp
+++ client/godot4/FreeOrionNode.cpp
@@ -3,6 +3,9 @@
 #include <godot_cpp/classes/os.hpp>
 
 #include "../../util/Directories.h"
+#include "../../util/i18n.h"
+#include "../../util/OptionsDB.h"
+#include "../../util/Version.h"
 
 void FreeOrionNode::_bind_methods() {
 
@@ -12,6 +15,14 @@ FreeOrionNode::FreeOrionNode() {
     std::string executable_path = godot::OS::get_singleton()->get_executable_path().utf8().get_data();
 
     InitDirs(executable_path);
+
+    if (!GetOptionsDB().OptionExists("misc.server-local-binary.path")) {
+#ifdef FREEORION_WIN32
+        GetOptionsDB().Add<std::string>("misc.server-local-binary.path", UserStringNop("OPTIONS_DB_FREEORIOND_PATH"),   PathToString(GetBinDir() / "freeoriond.exe"));
+#else
+        GetOptionsDB().Add<std::string>("misc.server-local-binary.path", UserStringNop("OPTIONS_DB_FREEORIOND_PATH"),   PathToString(GetBinDir() / "freeoriond"));
+#endif
+    }
 }
 
 FreeOrionNode::~FreeOrionNode()
