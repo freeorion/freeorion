667aa72928ef7a39d643108ad9b4dabe39c6a25d
O01eg <397177+o01eg@users.noreply.github.com>
2026-07-29 08:16:02 +0400

    Fix GUT error

--- godot4/addons/gut/gut_loader.gd
+++ godot4/addons/gut/gut_loader.gd
@@ -32,7 +32,7 @@ static var were_addons_disabled : bool = true
 @warning_ignore("unsafe_property_access")
 @warning_ignore("untyped_declaration")
 static func _static_init() -> void:
-	were_addons_disabled = ProjectSettings.get(str(WARNING_PATH, 'exclude_addons'))
+	were_addons_disabled = ProjectSettings.get_setting(str(WARNING_PATH, 'exclude_addons'), true)
 	ProjectSettings.set(str(WARNING_PATH, 'exclude_addons'), true)
 
 	var WarningsManager = load('res://addons/gut/warnings_manager.gd')
