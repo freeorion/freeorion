6fbad10c6917650694a58acda1671d4b5080677f
geoffthemedio <geoffthemedio@gmail.com>
2026-08-23 10:25:11 +0200

    .string() to PathToString

--- test/system/ClientAppFixture.cpp
+++ test/system/ClientAppFixture.cpp
@@ -37,7 +37,7 @@ ClientAppFixture::ClientAppFixture() :
     // Dirty hack to output log to console.
     InitLoggingSystem("/proc/self/fd/1", "Test");
 #else
-    InitLoggingSystem((GetUserDataDir() / "test.log").string(), "Test");
+    InitLoggingSystem(PathToString(GetUserDataDir() / "test.log"), "Test");
 #endif
     //InitLoggingOptionsDBSystem();
 
