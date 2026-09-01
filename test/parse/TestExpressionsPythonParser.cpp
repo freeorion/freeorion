c2c3d5f8492c97850b43695a386f6d61d8175902
O01eg <397177+o01eg@users.noreply.github.com>
2026-09-01 23:12:27 +0400

    Test Python FOCS effect expression

--- test/parse/TestExpressionsPythonParser.cpp
+++ test/parse/TestExpressionsPythonParser.cpp
@@ -1,6 +1,7 @@
 #include <boost/test/unit_test.hpp>
 
 #include "parse/PythonParser.h"
+#include "parse/EffectPythonParser.h"
 
 #include "ParserAppFixture.h"
 
@@ -15,13 +16,22 @@ public:
 
 BOOST_FIXTURE_TEST_SUITE(TestExpressionsPythonParser, ExpressionParserAppFixture)
 
-BOOST_AUTO_TEST_CASE(test_1) {
+BOOST_AUTO_TEST_CASE(test_effect) {
     PythonParser parser(m_python);
 
+    parser.LoadEffectsModule();
+    parser.LoadConditionsModule();
+
     py::dict globals;
+    const char* imports_code = R"(
+from focs._effects import MoveTowards
+from focs._conditions import IsSource
+    )";
     try {
-        py::object result = PythonCommon::CompileEvalExpression("2 + 2", globals);
-        BOOST_CHECK_EQUAL(py::extract<int>(result)(), 4);
+        PythonCommon::CompileEval(imports_code, "<imports>", globals);
+        py::object obj_result = PythonCommon::CompileEvalExpression("MoveTowards(speed=5, target=IsSource)", globals);
+        auto result = py::extract<effect_wrapper>(obj_result)();
+        BOOST_CHECK_EQUAL("MoveTowards destination = Source\n\n", result.effect->Dump(0));
     } catch (const boost::python::error_already_set&) {
         m_python.HandleErrorAlreadySet();
         BOOST_FAIL("Expression evaluation failed");
