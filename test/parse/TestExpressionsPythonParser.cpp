f9911291cb63fa4bff7551f555b171d9f07be4f6
geoffthemedio <geoffthemedio@gmail.com>
2026-09-12 13:21:16 +0200

    wrap string literal in FilenameToPath

--- test/parse/TestExpressionsPythonParser.cpp
+++ test/parse/TestExpressionsPythonParser.cpp
@@ -2,6 +2,7 @@
 
 #include "parse/PythonParser.h"
 #include "parse/EffectPythonParser.h"
+#include "../../util/Directories.h"
 
 #include "ParserAppFixture.h"
 
@@ -28,7 +29,7 @@ from focs._effects import MoveTowards
 from focs._conditions import IsSource
     )";
     try {
-        PythonCommon::CompileEval(imports_code, "<imports>", globals);
+        PythonCommon::CompileEval(imports_code, FilenameToPath("<imports>"), globals);
         py::object obj_result = PythonCommon::CompileEvalExpression("MoveTowards(speed=5, target=IsSource)", globals);
         auto result = py::extract<effect_wrapper>(obj_result)();
         BOOST_CHECK_EQUAL("MoveTowards destination = Source\n\n", result.effect->Dump(0));
